d13c52a4aa0f93ac41558e6fd3c6288936c6599b
Brian Folts <brian.folts@gmail.com>
2019-03-04 18:52:16 -0800

    More unit tests for E cards

---
+++ tests/cards/EnergySaving.spec.ts
@@ -0,0 +1,21 @@
+
+import { expect } from "chai";
+import { EnergySaving } from "../../src/cards/EnergySaving";
+import { Color } from "../../src/Color";
+import { Player } from "../../src/Player";
+import { Game } from "../../src/Game";
+
+describe("EnergySaving", function () {
+    it("Should play", function () {
+        const card = new EnergySaving();
+        const player = new Player("test", Color.BLUE, false);
+        const game = new Game("foobar", [player], player);
+        const action = card.play(player, game);
+        expect(player.energyProduction).to.eq(0);
+        expect(action).to.eq(undefined);
+        const landSpace = game.getAvailableSpacesOnLand(player)[0];
+        game.addCityTile(player, landSpace.id);
+        card.play(player, game);
+        expect(player.energyProduction).to.eq(1); 
+    });
+});
---
+++ tests/cards/EnergyTapping.spec.ts
@@ -0,0 +1,31 @@
+
+import { expect } from "chai";
+import { EnergyTapping } from "../../src/cards/EnergyTapping";
+import { Color } from "../../src/Color";
+import { Player } from "../../src/Player";
+import { Game } from "../../src/Game";
+import { SelectPlayer } from "../../src/inputs/SelectPlayer";
+
+describe("EnergyTapping", function () {
+    it("Should throw", function () {
+        const card = new EnergyTapping();
+        const player = new Player("test", Color.BLUE, false);
+        const game = new Game("foobar", [player], player);
+        const action = card.play(player, game);
+        expect(action).not.to.eq(undefined);
+        expect(action instanceof SelectPlayer).to.eq(true);
+        expect(function () { action.cb(player); }).to.throw("Selected player has no energy production");
+    });
+    it("Should play", function () {
+        const card = new EnergyTapping();
+        const player = new Player("test", Color.BLUE, false);
+        const game = new Game("foobar", [player], player);
+        const action = card.play(player, game);
+        expect(action).not.to.eq(undefined);
+        expect(action instanceof SelectPlayer).to.eq(true);
+        player.energyProduction = 1;
+        action.cb(player);
+        expect(player.energyProduction).to.eq(1);
+        expect(player.victoryPoints).to.eq(-1);
+    });
+});

c8d6a57bda72d98c86ef6452d3210c1009b3023e
Brian Folts <brian.folts@gmail.com>
2019-03-04 20:23:44 -0800

    Another unit test for an E card

---
+++ tests/cards/EquatorialMagnetizer.spec.ts
@@ -0,0 +1,32 @@
+
+import { expect } from "chai";
+import { EquatorialMagnetizer } from "../../src/cards/EquatorialMagnetizer";
+import { Color } from "../../src/Color";
+import { Player } from "../../src/Player";
+import { Game } from "../../src/Game";
+
+describe("EquatorialMagnetizer", function () {
+    it("Should throw", function () {
+        const card = new EquatorialMagnetizer();
+        const player = new Player("test", Color.BLUE, false);
+        const game = new Game("foobar", [player], player);
+        expect(function () { card.action(player, game); }).to.throw("Need energy production");
+    });
+    it("Should play", function () {
+        const card = new EquatorialMagnetizer();
+        const player = new Player("test", Color.BLUE, false);
+        const game = new Game("foobar", [player], player);
+        const action = card.play(player, game);
+        expect(action).to.eq(undefined);
+    });
+    it("Should act", function () {
+        const card = new EquatorialMagnetizer();
+        const player = new Player("test", Color.BLUE, false);
+        const game = new Game("foobar", [player], player);
+        player.energyProduction = 1;
+        const action = card.action(player, game);
+        expect(action).to.eq(undefined);
+        expect(player.energyProduction).to.eq(0);
+        expect(player.terraformRating).to.eq(21);
+    });
+});

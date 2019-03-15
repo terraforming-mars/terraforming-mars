f7ccbd4d333b723ab264f76a189db33bbbb9b89f
Brian Folts <brian.folts@gmail.com>
2019-03-14 20:47:28 -0700

    Fix to add city to colony for PhobosSpaceHaven

--- src/cards/PhobosSpaceHaven.ts
+++ src/cards/PhobosSpaceHaven.ts
@@ -5,6 +5,7 @@ import { CardType } from "./CardType";
 import { Player } from "../Player";
 import { Game } from "../Game";
 import { SpaceName } from "../SpaceName";
+import { SpaceType } from "../SpaceType";
 
 export class PhobosSpaceHaven implements IProjectCard {
     public cost: number = 25;
@@ -14,7 +15,7 @@ export class PhobosSpaceHaven implements IProjectCard {
     public text: string = "Increase your titanium production 1 step and place a city tile on the reserved area. Gain 3 victory points.";
     public description: string = "The doorway to mars.";
     public play(player: Player, game: Game) {
-        game.addCityTile(player, SpaceName.PHOBOS_SPACE_HAVEN);
+        game.addCityTile(player, SpaceName.PHOBOS_SPACE_HAVEN, SpaceType.COLONY);
         player.titaniumProduction++;
         player.victoryPoints += 3;
         return undefined;

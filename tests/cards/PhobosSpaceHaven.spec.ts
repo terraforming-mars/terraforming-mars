2b3a0b9e1db6f3a56c0a743cc7f4e80ac3bf856f
Brian Folts <brian.folts@gmail.com>
2019-03-15 06:29:56 -0700

    More P card unit tests

---
+++ tests/cards/PhobosSpaceHaven.spec.ts
@@ -0,0 +1,19 @@
+
+import { expect } from "chai";
+import { PhobosSpaceHaven } from "../../src/cards/PhobosSpaceHaven";
+import { Color } from "../../src/Color";
+import { Player } from "../../src/Player";
+import { Game } from "../../src/Game";
+
+describe("PhobosSpaceHaven", function () {
+    it("Should play", function () {
+        const card = new PhobosSpaceHaven();
+        const player = new Player("test", Color.BLUE, false);
+        const game = new Game("foobar", [player], player);
+        const action = card.play(player, game);
+        expect(action).to.eq(undefined);
+        expect(player.titaniumProduction).to.eq(1);
+        expect(player.victoryPoints).to.eq(3);
+        expect(game.getCitiesInPlay()).to.eq(1);
+    });
+});
---
+++ tests/cards/PhysicsComplex.spec.ts
@@ -0,0 +1,36 @@
+
+import { expect } from "chai";
+import { PhysicsComplex } from "../../src/cards/PhysicsComplex";
+import { Color } from "../../src/Color";
+import { Player } from "../../src/Player";
+import { Game } from "../../src/Game";
+
+describe("PhysicsComplex", function () {
+    it("Should throw", function () {
+        const card = new PhysicsComplex();
+        const player = new Player("test", Color.BLUE, false);
+        const game = new Game("foobar", [player], player);
+        expect(function () { card.action(player, game); }).to.throw("Requires 6 energy");
+    });
+    it("Should play", function () {
+        const card = new PhysicsComplex();
+        const player = new Player("test", Color.BLUE, false);
+        const game = new Game("foobar", [player], player);
+        const action = card.play(player, game);
+        expect(action).to.eq(undefined);
+        expect(game.onGameEnd.length).to.eq(1);
+        card.scienceResources = 4;
+        game.onGameEnd[0]();
+        expect(player.victoryPoints).to.eq(8);
+    });
+    it("Should act", function () {
+        const card = new PhysicsComplex();
+        const player = new Player("test", Color.BLUE, false);
+        const game = new Game("foobar", [player], player);
+        player.energy = 6;
+        const action = card.action(player, game);
+        expect(player.energy).to.eq(0);
+        expect(card.scienceResources).to.eq(1);
+        expect(action).to.eq(undefined);
+    });
+});

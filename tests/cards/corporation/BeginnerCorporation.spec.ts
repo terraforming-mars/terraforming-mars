04b58ac19c44eab9363c58dc76b99f17093f7712
Brian Folts <brian.folts@gmail.com>
2019-03-21 16:48:47 -0700

    Unit test for BeginnerCorporation card

---
+++ tests/cards/corporation/BeginnerCorporation.spec.ts
@@ -0,0 +1,16 @@
+
+import { expect } from "chai";
+import { BeginnerCorporation } from "../../../src/cards/corporation/BeginnerCorporation";
+import { Color } from "../../../src/Color";
+import { Player } from "../../../src/Player";
+import { Game } from "../../../src/Game";
+
+describe("BeginnerCorporation", function () {
+    it("Should play", function () {
+        const card = new BeginnerCorporation();
+        const player = new Player("test", Color.BLUE, false);
+        const game = new Game("foobar", [player], player);
+        const action = card.play(player, game);
+        expect(action).to.eq(undefined);
+    });
+});

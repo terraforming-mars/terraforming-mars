6cfb836a05b6baa89a0fd305c80631449aacb9aa
Brian Folts <brian.folts@gmail.com>
2019-10-22 19:12:48 -0700

    Fix build fir ImportedNitrogen test failure with occupied space due to solo play enhancements

--- tests/cards/ImportedNitrogen.spec.ts
+++ tests/cards/ImportedNitrogen.spec.ts
@@ -12,7 +12,8 @@ describe("ImportedNitrogen", function () {
     it("Should play", function () {
         const card = new ImportedNitrogen();
         const player = new Player("test", Color.BLUE, false);
-        const game = new Game("foobar", [player], player);
+        const otherPlayer = new Player("test2", Color.RED, false);
+        const game = new Game("foobar", [player, otherPlayer], player);
         const pets = new Pets();
         const tardigrades = new Tardigrades();
         player.playedCards.push(pets, tardigrades);
@@ -21,7 +22,7 @@ describe("ImportedNitrogen", function () {
         expect(action instanceof AndOptions).to.eq(true);
         const andAction = action as AndOptions;
         andAction.cb();
-        expect(player.terraformRating).to.eq(15);
+        expect(player.terraformRating).to.eq(21);
         expect(player.plants).to.eq(4);
         andAction.options[0].cb([tardigrades]);
         expect(player.getResourcesOnCard(tardigrades)).to.eq(3);

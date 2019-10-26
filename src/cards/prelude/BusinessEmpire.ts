8d20325a97074d689c1abd0f4095ed57bdf26c9f
vincentneko <vincentneko@gmail.com>
2019-10-26 05:33:00 +0200

    Add cost variable

--- src/cards/prelude/BusinessEmpire.ts
+++ src/cards/prelude/BusinessEmpire.ts
@@ -5,9 +5,10 @@ import { PreludeCard } from "./PreludeCard";
 import { IProjectCard } from "../IProjectCard";
 
 export class BusinessEmpire extends PreludeCard implements IProjectCard {
+    public cost: number = 6;    
     public tags: Array<Tags> = [Tags.EARTH];
     public name: string = "Business Empire";
-    public text: string = "Increase MC production 6 steps. Remove 6 MC.";
+    public text: string = "Increase MC production 6 steps. Pay 6 MC.";
     public description: string = "Numerous asset on Earth to support your Mars efforts";
     public play(player: Player, _game: Game) {     
 			player.megaCredits -= 6;

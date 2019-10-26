16c1535139bc729bf2f0210f57c3c77629d60c12
vincentneko <vincentneko@gmail.com>
2019-10-26 05:33:36 +0200

    Fix spacing

--- src/cards/prelude/GalileanMining.ts
+++ src/cards/prelude/GalileanMining.ts
@@ -6,7 +6,7 @@ import { IProjectCard } from "../IProjectCard";
 
 export class GalileanMining extends PreludeCard implements IProjectCard {
     public tags: Array<Tags> = [Tags.JOVIAN];
-	public cost: number = 5;
+    public cost: number = 5;
     public name: string = "Galilean Mining";
     public text: string = "Increase your titanium production 2 steps. Pay 5 MC.";
     public description: string = "The big moons of Jupiter are great for mining";

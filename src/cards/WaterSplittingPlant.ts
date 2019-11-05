59af469fac81f18116af5e160927d5db4736d588
vincentneko <vincentneko@gmail.com>
2019-11-05 12:01:13 +0100

    Remove unecessary check

--- src/cards/WaterSplittingPlant.ts
+++ src/cards/WaterSplittingPlant.ts
@@ -23,9 +23,6 @@ export class WaterSplittingPlant implements IProjectCard {
         return player.energy >= 3;
     }
     public action(player: Player, game: Game) {
-        if (player.energy < 3) {
-            throw "Need 3 energy";
-        }
         player.energy -= 3;
         return game.increaseOxygenLevel(player, 1);
     }

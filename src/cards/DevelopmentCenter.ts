779035c98b7a199ce45a8eeded59adf6cbc29a01
Brian Folts <brian.folts@gmail.com>
2019-11-18 16:45:25 -0800

    Add eslint rules for D cards

--- src/cards/DevelopmentCenter.ts
+++ src/cards/DevelopmentCenter.ts
@@ -1,32 +1,32 @@
 
-import { IActionCard } from "./ICard";
-import { IProjectCard } from "./IProjectCard";
-import { Tags } from "./Tags";
-import { CardType } from "./CardType";
-import { Player } from "../Player";
-import { Game } from "../Game";
+import {IActionCard} from './ICard';
+import {IProjectCard} from './IProjectCard';
+import {Tags} from './Tags';
+import {CardType} from './CardType';
+import {Player} from '../Player';
+import {Game} from '../Game';
 
 export class DevelopmentCenter implements IActionCard, IProjectCard {
     public cost: number = 11;
     public tags: Array<Tags> = [Tags.SCIENCE, Tags.STEEL];
-    public name: string = "Development Center";
+    public name: string = 'Development Center';
     public cardType: CardType = CardType.ACTIVE;
-    public actionText: string = "Spend 1 energy to draw a card.";
-    public text: string = "";
+    public actionText: string = 'Spend 1 energy to draw a card.';
+    public text: string = '';
     public requirements: undefined;
-    public description: string = "Ensuring a constant influx of ideas.";
+    public description: string = 'Ensuring a constant influx of ideas.';
     public canPlay(): boolean {
-        return true;
+      return true;
     }
     public play() {
-        return undefined;
+      return undefined;
     }
     public canAct(player: Player): boolean {
-        return player.energy > 0;
+      return player.energy > 0;
     }
     public action(player: Player, game: Game) {
-        player.energy--;
-        player.cardsInHand.push(game.dealer.dealCard());
-        return undefined;
+      player.energy--;
+      player.cardsInHand.push(game.dealer.dealCard());
+      return undefined;
     }
 }

acd4faf84bcc44ab70a5c3b19161e554e9d6a038
vincentneko <vincentneko@gmail.com>
2019-11-26 20:26:35 +0100

    Change generation limit for solo prelude

--- src/Game.ts
+++ src/Game.ts
@@ -323,8 +323,9 @@ export class Game {
     }
 
     private gameIsOver(): boolean {
-      // Single player game is done after generation 14
-      if (this.players.length === 1 && this.generation === 14) {
+      // Single player game is done after generation 14 or 12 with prelude
+      if (this.players.length === 1 &&
+         (this.generation === 14 || (this.generation === 12 && this.preludeExtension)) {
         return true;
       }
       return this.marsIsTerraformed();

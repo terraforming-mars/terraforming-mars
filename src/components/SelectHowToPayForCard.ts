36d2a5203feba40e558186866dcc0fd470468ca9
bafolts <brian.folts@hbo.com>
2019-12-11 18:44:14 -0800

    Client side validation for SelectHowToPayForCard

--- src/components/SelectHowToPayForCard.ts
+++ src/components/SelectHowToPayForCard.ts
@@ -7,7 +7,8 @@ interface SelectHowToPayForCardModel {
     megaCredits: number;
     steel: number;
     titanium: number;
-	microbes: number;
+    microbes: number;
+    warning: string | undefined;
 }
 
 import { HowToPay } from "../inputs/HowToPay";
@@ -23,7 +24,8 @@ export const SelectHowToPayForCard = Vue.component("select-how-to-pay-for-card",
             megaCredits: 0,
             steel: 0,
             titanium: 0,
-            microbes: 0
+            microbes: 0,
+            warning: undefined
         } as SelectHowToPayForCardModel;
     },
     components: {
@@ -37,7 +39,10 @@ export const SelectHowToPayForCard = Vue.component("select-how-to-pay-for-card",
                     return card.cost;
                 }
             }
-            return 36; // max card cost;
+            // We should always find a card
+            // It may make more sense to throw
+            // an error here that the card wasn't found
+            return 36; // max card cost
         },
         canUseHeat: function () {
             return this.playerinput.canUseHeatAsMegaCredits;
@@ -75,14 +80,41 @@ export const SelectHowToPayForCard = Vue.component("select-how-to-pay-for-card",
             }
             return false;			
         },
+        hasWarning: function () {
+            return this.$data.warning !== undefined;
+        },
         save: function () {
             const htp: HowToPay = {
-                heat: parseInt(this.$data.heat),
-                megaCredits: parseInt(this.$data.megaCredits),
-                steel: parseInt(this.$data.steel),
-                titanium: parseInt(this.$data.titanium),
-                microbes: parseInt(this.$data.microbes)
+                heat: this.$data.heat,
+                megaCredits: this.$data.megaCredits,
+                steel: this.$data.steel,
+                titanium: this.$data.titanium,
+                microbes: this.$data.microbes
             };
+            if (htp.megaCredits > this.player.megaCredits) {
+                this.$data.warning = "You don't have that many mega credits";
+                return;
+            }
+            if (htp.microbes > this.player.microbes) {
+                this.$data.warning = "You don't have enough microbes";
+                return;
+            }
+            if (htp.heat > this.player.heat) {
+                this.$data.warning = "You don't have enough heat";
+                return;
+            }
+            if (htp.titanium > this.player.titanium) {
+                this.$data.warning = "You don't have enough titanium";
+                return;
+            }
+            if (htp.steel > this.player.steel) {
+                this.$data.warning = "You don't have enough steel";
+                return;
+            }
+            if (htp.heat + htp.megaCredits + (htp.steel * this.player.steelValue) + (htp.titanium * this.player.titaniumValue) < this.getCardCost()) {
+                this.$data.warning = "Haven't spent enough";
+                return;
+            }
             this.onsave([[
                 this.$data.card,
                 JSON.stringify(htp)
@@ -90,34 +122,37 @@ export const SelectHowToPayForCard = Vue.component("select-how-to-pay-for-card",
         }
     },
     template: `
-        <div>
-            <div v-if="showtitle === true">{{playerinput.title}}</div>
-            <label v-for="availableCard in playerinput.cards" :key="availableCard" style="display:block;font-size:12px">
-                <input class="nes-radio" type="radio" v-model="card" :value="availableCard" />  
-                <card class="cardbox" :card="availableCard"></card>
-            </label>
-            <div class="nofloat nes-field" v-if="canUseSteel()">
-                <label class="nofloat">Steel:</label>
-                <input class="nes-input" type="number" value="0" min="0" max="100" v-model="steel" />
-            </div>
-            <div v-if="canUseTitanium()" class="nes-field">
-                <label class="nofloat">Titanium:</label>
-                <input class="nes-input" type="number" value="0" min="0" max="100" v-model="titanium" />
-            </div>
-            <div v-if="canUseHeat()" class="nes-field">
-                <label class="nofloat">Heat:</label>
-                <input class="nes-input" type="number" value="0" min="0" max="100" v-model="heat" />
-            </div>
-            <div v-if="canUseMicrobes()" class="nes-field">
-                <label class="nofloat">Microbes:</label>
-                <input class="nes-input" type="number" value="0" min="0" max="100" v-model="microbes" />
-            </div>			
-            <div class="nofloat nes-field">
-                <label class="nofloat">Mega Credit:</label>
-                <input class="nes-input" type="number" value="0" min="0" :max="getCardCost()" v-model="megaCredits" />
-            </div>
-            <button class="nes-btn" v-on:click="save">Save</button>
-        </div>
-    `
+<div>
+  <div v-if="showtitle === true">{{playerinput.title}}</div>
+  <label v-for="availableCard in playerinput.cards" :key="availableCard" style="display:block;font-size:12px">
+    <input class="nes-radio" type="radio" v-model="card" :value="availableCard" />
+    <card class="cardbox" :card="availableCard"></card>
+  </label>
+  <div class="nofloat nes-field" v-if="canUseSteel()">
+    <label class="nofloat">Steel:</label>
+    <input class="nes-input" type="number" value="0" min="0" :max="player.steel" v-model.number="steel" />
+  </div>
+  <div v-if="canUseTitanium()" class="nes-field">
+    <label class="nofloat">Titanium:</label>
+    <input class="nes-input" type="number" value="0" min="0" :max="player.titanium" v-model.number="titanium" />
+  </div>
+  <div v-if="canUseHeat()" class="nes-field">
+    <label class="nofloat">Heat:</label>
+    <input class="nes-input" type="number" value="0" min="0" :max="player.heat" v-model.number="heat" />
+  </div>
+  <div v-if="canUseMicrobes()" class="nes-field">
+    <label class="nofloat">Microbes:</label>
+    <input class="nes-input" type="number" value="0" min="0" :max="player.microbes" v-model.number="microbes" />
+  </div>
+  <div class="nofloat nes-field">
+    <label class="nofloat">Mega Credit:</label>
+    <input class="nes-input" type="number" value="0" min="0" :max="getCardCost()" v-model.number="megaCredits" />
+  </div>
+  <div v-if="hasWarning()" class="nes-container is-rounded">
+    <span class="nes-text is-warning">{{ warning }}</span>
+  </div>
+  <button class="nes-btn" v-on:click="save">Save</button>
+</div>
+`
 });
 

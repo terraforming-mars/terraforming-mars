22a210486e09e606c13eaccae1978b5504027b9d
alrusdi <alrusdi@gmail.com>
2019-12-16 13:43:45 +0500

    Rework some cards requirements check code; Add new tests; #81

--- src/cards/Ants.ts
+++ src/cards/Ants.ts
@@ -24,23 +24,32 @@ export class Ants implements IActionCard, IProjectCard {
       return undefined;
     }
     public canAct(_player: Player, game: Game): boolean {
-      return this.getAvailableCards(game).length > 0;
+      return this.getAvailableCards(game, _player).length > 0;
     }
-    private getAvailableCards(game: Game): Array<IProjectCard> {
+    private getAvailableCards(game: Game, currentPlayer: Player): Array<IProjectCard> {
       const availableCards: Array<IProjectCard> = [];
-      game.getPlayers().forEach((gamePlayer) => {
-        gamePlayer.playedCards.forEach((playedCard) => {
-          if (gamePlayer.getResourcesOnCard(playedCard) > 0) {
-            availableCards.push(playedCard);
-          }
-        });
-      });
+      for (const gamePlayer of game.getPlayers()) {
+        // Microbes of this player are protected
+        if (gamePlayer.hasProtectedHabitats() && gamePlayer.id !== currentPlayer.id) continue;
+
+        for (const playedCard of gamePlayer.playedCards) {
+          // Do not remove Microbes from this card itself
+          if (this.name === playedCard.name) continue;
+          // No resources, sorry
+          if (gamePlayer.getResourcesOnCard(playedCard) < 1) continue;
+          // Resources are not the Microbes
+          if (playedCard.resourceType !== ResourceType.MICROBE) continue;
+
+          availableCards.push(playedCard);
+        }
+      }
       return availableCards;
     }
     public action(player: Player, game: Game) {
-      const availableCards: Array<IProjectCard> = this.getAvailableCards(game);
+      const availableCards: Array<IProjectCard> = this.getAvailableCards(game, player);
       return new SelectCard('Select card to remove microbe', availableCards,
           (foundCards: Array<IProjectCard>) => {
+            // TODO Log here
             game.getCardPlayer(foundCards[0].name).
                 removeMicrobes(player, foundCards[0], 1);
             player.addResourceTo(this);
--- src/cards/GiantIceAsteroid.ts
+++ src/cards/GiantIceAsteroid.ts
@@ -8,6 +8,7 @@ import { CardType } from "./CardType";
 import { Player } from "../Player";
 import { Game } from "../Game";
 import { ISpace } from "../ISpace";
+import * as constants from "../constants";
 
 export class GiantIceAsteroid implements IProjectCard {
     public cost: number = 36;
@@ -17,39 +18,44 @@ export class GiantIceAsteroid implements IProjectCard {
     public canPlay(): boolean {
         return true;
     }
+
     public play(player: Player, game: Game) {
-        if (game.getPlayers().length == 1) {
-            return new AndOptions(
-                () => {
-                    return game.increaseTemperature(player, 2);
-                },
-                new SelectSpace("Select first space for ocean tile", game.getAvailableSpacesForOcean(player), (space: ISpace) => {
-                    game.addOceanTile(player, space.id);
+        var opts: Array<SelectPlayer | SelectSpace> = [];
+
+        if (player.isAnyOtherPlayerHasPlants(game)) {
+            opts.push(
+                new SelectPlayer(game.getPlayers(), "Select player to remove up to 6 plants from", (foundPlayer: Player) => {
+                    foundPlayer.removePlants(player, 6);
                     return undefined;
-                }),
-                new SelectSpace("Select second space for ocean tile", game.getAvailableSpacesForOcean(player), (space: ISpace) => {
+                })
+            )
+        }
+
+        let oceansCount = game.getOceansOnBoard();
+
+        if (oceansCount + 1 <= constants.MAX_OCEAN_TILES) {
+            opts.push(
+                new SelectSpace("Select space for ocean tile", game.getAvailableSpacesForOcean(player), (space: ISpace) => {
                     game.addOceanTile(player, space.id);
                     return undefined;
                 })
-            );
+            )
+        }
+
+        if (oceansCount + 2 <= constants.MAX_OCEAN_TILES) {
+            opts.push(
+                new SelectSpace("Select space for second ocean tile", game.getAvailableSpacesForOcean(player), (space: ISpace) => {
+                    game.addOceanTile(player, space.id);
+                    return undefined;
+                })
+            )
         }
 
         return new AndOptions(
             () => {
                 return game.increaseTemperature(player, 2);
             },
-            new SelectPlayer(game.getPlayers(), "Select player to remove up to 6 plants from", (foundPlayer: Player) => {
-                foundPlayer.removePlants(player, 6);
-                return undefined;
-            }),
-            new SelectSpace("Select first space for ocean tile", game.getAvailableSpacesForOcean(player), (space: ISpace) => {
-                game.addOceanTile(player, space.id);
-                return undefined;
-            }),
-            new SelectSpace("Select second space for ocean tile", game.getAvailableSpacesForOcean(player), (space: ISpace) => {
-                game.addOceanTile(player, space.id);
-                return undefined;
-            })
+            ...opts
         );
     }
 }

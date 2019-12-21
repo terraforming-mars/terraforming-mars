0b8d907495086d9ea19c23ea8ae50877eea353ed
vincentneko <vincentneko@gmail.com>
2019-12-21 10:34:52 +0100

    Issue #177

--- tests/cards/InventorsGuild.spec.ts
+++ tests/cards/InventorsGuild.spec.ts
@@ -4,6 +4,8 @@ import { InventorsGuild } from "../../src/cards/InventorsGuild";
 import { Color } from "../../src/Color";
 import { Player } from "../../src/Player";
 import { Game } from "../../src/Game";
+import { SelectCard } from "../../src/inputs/SelectCard";
+import { IProjectCard } from "../../src/cards/IProjectCard";
 
 describe("InventorsGuild", function () {
     it("Should play", function () {
@@ -28,11 +30,13 @@ describe("InventorsGuild", function () {
         player.megaCredits = 3;
         const action = card.action(player, game);
         expect(action).not.to.eq(undefined);
-        action!.options[0].cb([card]);
+        expect(action instanceof SelectCard).to.eq(true);
+        (action! as SelectCard<IProjectCard>).cb([]);
+        expect(game.dealer.discarded.length).to.eq(1);
+        expect(player.megaCredits).to.eq(3);
+        player.megaCredits = 3;
+        (action as SelectCard<IProjectCard>).cb([(action as SelectCard<IProjectCard>).cards[0]]);
         expect(player.megaCredits).to.eq(0);
         expect(player.cardsInHand.length).to.eq(1);
-        action!.options[1].cb();
-        expect(game.dealer.discarded.length).to.eq(1);
-        expect(player.cardsInHand[0]).to.eq(game.dealer.discarded[0]);
     });
 });

996428e5fe2c24f7f48756cc4c52dccd05096e7f
Brian Folts <brian.folts@gmail.com>
2020-01-01 12:35:07 -0800

    Change ResearchCoordination back from extending PreludeCard

--- src/cards/prelude/ResearchCoordination.ts
+++ src/cards/prelude/ResearchCoordination.ts
@@ -1,12 +1,16 @@
 
+import { CardType } from "../CardType";
 import { IProjectCard } from "../IProjectCard";
 import { Tags } from "../Tags";
-import { PreludeCard } from "./PreludeCard";
 
-export class ResearchCoordination extends PreludeCard implements IProjectCard {
+export class ResearchCoordination implements IProjectCard {
     public cost: number = 4;
+    public cardType: CardType = CardType.AUTOMATED;
     public tags: Array<Tags> = [Tags.WILDCARD];
     public name: string = "Research Coordination";
+    public canPlay(): boolean {
+        return true;
+    }
     public play() {
         return undefined;
     }
--- tests/cards/prelude/ResearchCoordination.spec.ts
+++ tests/cards/prelude/ResearchCoordination.spec.ts
@@ -3,6 +3,10 @@ import { expect } from "chai";
 import { ResearchCoordination } from "../../../src/cards/prelude/ResearchCoordination";
 
 describe("ResearchCoordination", function () {
+    it("Can play", function () {
+        const card = new ResearchCoordination();
+        expect(card.canPlay()).to.eq(true);
+    });
     it("Should play", function () {
         const card = new ResearchCoordination();
         const action = card.play();

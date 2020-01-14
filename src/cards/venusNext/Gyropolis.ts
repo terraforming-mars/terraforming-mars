c8525f71b9d9cdeb28544dc59f4fe4612cc7f396
alrusdi <alrusdi@gmail.com>
2020-01-14 12:08:37 +0500

    Disallow to play Giropolis if no spaces for city available; #197

--- src/cards/venusNext/Gyropolis.ts
+++ src/cards/venusNext/Gyropolis.ts
@@ -13,12 +13,13 @@ export class Gyropolis implements IProjectCard {
     public tags: Array<Tags> = [Tags.CITY, Tags.STEEL];
     public name: string = "Gyropolis";
     public cardType: CardType = CardType.AUTOMATED;
-    public canPlay(player: Player): boolean {
+    public canPlay(player: Player, game: Game): boolean {
+        if (game.board.getAvailableSpacesForCity(player).length === 0) return false;
         return player.getProduction(Resources.ENERGY) >= 2;
       }
     public play(player: Player, game: Game) {
         player.setProduction(Resources.ENERGY,-2);
-        player.setProduction(Resources.MEGACREDITS, player.getTagCount(Tags.EARTH) + player.getTagCount(Tags.VENUS)); 
+        player.setProduction(Resources.MEGACREDITS, player.getTagCount(Tags.EARTH) + player.getTagCount(Tags.VENUS));
         return new SelectSpace("Select space for city tile", game.board.getAvailableSpacesForCity(player), (space: ISpace) => {
             game.addCityTile(player, space.id);
             return undefined;

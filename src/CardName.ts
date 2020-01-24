35d6975481f7c853cc7d1b1e29403d6eacdb5330
vincentneko <vincentneko@gmail.com>
2020-01-24 12:07:06 +0100

    Various Fixes

--- src/CardName.ts
+++ src/CardName.ts
@@ -3,7 +3,5 @@ export enum CardName {
     PROTECTED_HABITATS = "Protected Habitats",
     DIRIGIBLES = "Dirigibles",
     PSYCHROPHILES = "Psychrophiles",
-    ARCTIC_ALGAE = "Arctic Algae",
-    STORMCRAFT_INCORPORATED = "StormCraft Incorporated",
-    CELESTIC = "Celestic"
+    ARCTIC_ALGAE = "Arctic Algae"
 }

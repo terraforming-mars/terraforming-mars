9ef9b4d37b6b106aa4674dd371df9a8924bf1302
vincentneko <vincentneko@gmail.com>
2020-02-06 12:37:42 +0100

    Small Fixes

--- src/milestones/Energizer.ts
+++ src/milestones/Energizer.ts
@@ -5,7 +5,7 @@ import { Resources } from "../Resources";
 
 export class Energizer implements IMilestone {
     public name: string = "Energizer";
-    public description: string = "Eequires that you have 6 energy production"
+    public description: string = "Requires that you have 6 energy production"
     public canClaim(player: Player, _game: Game): boolean {
         return player.getProduction(Resources.ENERGY) >= 6;
     }   

3281d7ffe4cd10e450f95c8b4e1912108de7bf6b
vincentneko <vincentneko@gmail.com>
2020-02-09 22:15:00 +0100

    Add cube marker

--- src/colonies/Titan.ts
+++ src/colonies/Titan.ts
@@ -13,7 +13,7 @@ export class Titan extends Colony implements IColony {
         this.afterTrade(this);
     }
     public onColonyPlaced(player: Player): undefined {
-        this.colonies.push(player);
+        super.addColony(this, player);
         player.setProduction(Resources.PLANTS);
         return undefined;
     }

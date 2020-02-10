1ee259271023990e56caf0006e045c382f2e4fd2
vincentneko <vincentneko@gmail.com>
2020-02-10 19:01:32 +0100

    Add Callisto and Colony dealer

--- src/components/Colony.ts
+++ src/components/Colony.ts
@@ -42,11 +42,13 @@ export const Colony = Vue.component("colony", {
       <div v-if="colony.name === 'Ganymede'" class="resource plant"></div>
       <div v-if="colony.name === 'Europa'" class="resource money">1</div>
       <div v-if="colony.name === 'Titan'" class="resource floater"></div>
+      <div v-if="colony.name === 'Callisto'" class="resource energy"></div>
       <span class="colony-background-color">
         Colony Bonus
         </span><br>
       <div v-if="colony.name === 'Ganymede'" class="resource plant" style="margin-left:20px;"></div>
       <div v-if="colony.name === 'Titan'" class="resource floater" style="margin-left:20px;"></div>
+      <div v-if="colony.name === 'Callisto'" class="resource energy" style="margin-left:20px;"></div>
       <div v-if="colony.name !== 'Europa'" class="white-x"></div>
       <span v-if="colony.name !== 'Europa'" class="colony-background-color">
         Trade Income
@@ -74,6 +76,25 @@ export const Colony = Vue.component("colony", {
       <div>6</div>
     </div>
 
+    <div v-if="colony.name === 'Callisto'" class="colony-grid-container">
+    <div><div class="production-box"><div class="production energy"></div></div></div>
+    <div><div class="production-box"><div class="production energy"></div></div></div>
+    <div><div class="production-box"><div class="production energy"></div></div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
+  <div v-if="colony.name === 'Callisto'" class="colony-grid-container2">
+    <div>0</div>
+    <div>2</div>
+    <div>3</div>
+    <div>5</div>
+    <div>7</div>
+    <div>10</div>
+    <div>13</div>
+  </div>
+
     <div v-if="colony.name === 'Europa'" class="colony-grid-container">
     <div><div class="tile ocean-tile ocean-tile-colony"></div></div>
     <div><div class="tile ocean-tile ocean-tile-colony"></div></div>
@@ -97,24 +118,21 @@ export const Colony = Vue.component("colony", {
   <div><div class="resource floater resource-stacked " style="positon:absolute;margin-bottom:0px;margin-top:6px;z-index:1;"></div>
       <div class="resource floater resource-stacked " style="positon:absolute;margin-top:-20px;"></div>
       <div class="resource floater resource-stacked " style="positon:absolute;margin-top:-20px;margin-left:-5px;"></div>
-      <div v-if="colony.trackPosition === 0" class="colony_cube"></div>
   </div>
   <div><div class="resource floater resource-stacked " style="positon:absolute;margin-bottom:0px;margin-top:6px;z-index:1;"></div>
       <div class="resource floater resource-stacked " style="positon:absolute;margin-top:-20px;"></div>
       <div class="resource floater resource-stacked " style="positon:absolute;margin-top:-20px;margin-left:-5px;"></div>
-      <div v-if="colony.trackPosition === 1" class="colony_cube"></div>
   </div>
   <div><div class="resource floater resource-stacked " style="positon:absolute;margin-bottom:0px;margin-top:6px;z-index:1;"></div>
       <div class="resource floater resource-stacked " style="positon:absolute;margin-top:-20px;"></div>
       <div class="resource floater resource-stacked " style="positon:absolute;margin-top:-20px;margin-left:-5px;"></div>
-      <div v-if="colony.trackPosition === 2" class="colony_cube"></div>
   </div>
   <div></div>
   <div></div>
   <div></div>
   <div></div>
-</div>
-<div v-if="colony.name === 'Titan'" class="colony-grid-container2">
+  </div>
+  <div v-if="colony.name === 'Titan'" class="colony-grid-container2">
   <div>0</div>
   <div>1</div>
   <div>1</div>
@@ -122,7 +140,7 @@ export const Colony = Vue.component("colony", {
   <div>3</div>
   <div>3</div>
   <div>4</div>
-</div>
+  </div>
 
     </div>
     `

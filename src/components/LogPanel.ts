77ac32208ce006f78ebcd6ad7e7cd248ebeba15c
alrusdi <alrusdi@gmail.com>
2020-03-07 12:25:31 +0500

    Fix log panel scrolling

--- src/components/LogPanel.ts
+++ src/components/LogPanel.ts
@@ -7,21 +7,23 @@ export const LogPanel = Vue.component("log-panel", {
     },
     methods: {
         scrollToEnd: function() { 
-            const scrollablePanel = document.getElementById("logpanel");
+            const scrollablePanel = document.getElementById("logpanel-scrollable");
             if (scrollablePanel !== null) {
                 scrollablePanel.scrollTop = scrollablePanel.scrollHeight;
             }
         }    
     },
     mounted: function () {
-        this.scrollToEnd();
+        this.$nextTick(this.scrollToEnd);
     },
     template: `
-    <div id="logpanel" class="panel custom-panel">
-        <div class="panel-body">
-            <div v-for="message in messages">
-                {{message}}
-            </div>
+    <div class="panel log-panel">
+        <div id="logpanel-scrollable" class="panel-body">
+            <ul v-if="messages">
+                <li v-for="message in messages">
+                    {{message}}
+                </li>
+            </ul>
         </div>
     </div>
     `

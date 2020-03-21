741d078cb29fd4465d2eaaf63b68d0b2f00a1502
alrusdi <alrusdi@gmail.com>
2020-03-21 18:38:15 +0500

    Fixes by linter warnings

--- src/PreferencesManger.ts
+++ src/PreferencesManger.ts
@@ -1,7 +1,7 @@
 export class PreferencesManager {
     static keys: Array<string> = ["hide_corporation", "hide_hand", "hide_cards", "hide_awards_and_milestones", "hide_turnorder", "small_cards", "lang"];
     static preferencesValues: Map<string, boolean> = new Map<string, boolean>();
-    static localStorageSupported: boolean = typeof window['localStorage'] != "undefined" && window['localStorage'] != null;
+    static localStorageSupported: boolean = typeof window["localStorage"] !== undefined && window["localStorage"] !== null;
 
     static saveValue(name: string, val: string): void {
         if ( ! PreferencesManager.localStorageSupported) return;

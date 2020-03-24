a33962adb5702bd09f43d7e659d391ae4d253f65
alrusdi <alrusdi@gmail.com>
2020-03-25 00:21:28 +0500

    Fix counting of animal tags; #447

--- src/Game.ts
+++ src/Game.ts
@@ -42,6 +42,7 @@ import {SelectResourceProductionDecrease} from "./interrupts/SelectResourceProdu
 import {ICard} from "./cards/ICard";
 import {SelectResourceDecrease} from "./interrupts/SelectResourceDecrease";
 import {SelectHowToPayInterrupt} from "./interrupts/SelectHowToPayInterrupt";
+import { Pets } from "./cards/Pets";
 
 export class Game {
     public activePlayer: Player;
@@ -112,6 +113,7 @@ export class Game {
         this.draftVariant = false;
         this.setupSolo();
       }
+      first.cardsInHand.push(new Pets())
 
       let corporationCards = ALL_CORPORATION_CARDS.slice();
       // Add prelude corporations cards

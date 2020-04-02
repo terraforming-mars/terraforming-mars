31d85c4083ea0e6ff10fc23b33d203b45a1a45dc
Brian Folts <brian.folts@gmail.com>
2020-04-02 06:54:54 -0700

    Update Diversifier milestone to support wildcards

--- src/milestones/Diversifier.ts
+++ src/milestones/Diversifier.ts
@@ -1,25 +1,31 @@
 import { IMilestone } from "./IMilestone";
 import { Player } from "../Player";
-import { Game } from "../Game";
 import { CardType } from "../cards/CardType";
+import { Tags } from "../cards/Tags";
 
 export class Diversifier implements IMilestone {
     public name: string = "Diversifier";
     public description: string = "Requires that you have 8 different tags in play"
-    public canClaim(player: Player, _game: Game): boolean {
-        let allTags = new Set();
+    public canClaim(player: Player): boolean {
+        const allTags: Tags[] = [];
+        let wildcardCount: number = 0;
+        const uniqueTags: Set<Tags> = new Set<Tags>();
         if (player.corporationCard !== undefined && player.corporationCard.tags.length > 0) {
-          player.corporationCard.tags.forEach(tag => {
-            allTags.add(tag);
-          });
+          player.corporationCard.tags.forEach((tag) => allTags.push(tag));
         }
         player.playedCards.filter((card) => card.cardType !== CardType.EVENT)
-          .forEach(card => {
-            card.tags.forEach(tag => {
-              allTags.add(tag);
-            });  
+          .forEach((card) => {
+            card.tags.forEach((tag) => {
+              allTags.push(tag);
+            });
+          });
+        for (const tags of allTags) {
+          if (tags === Tags.WILDCARD) {
+            wildcardCount++;
+          } else {
+            uniqueTags.add(tags);
           }
-        );        
-        return allTags.size >= 8;
+        }
+        return uniqueTags.size + wildcardCount >= 8;
     }   
 }

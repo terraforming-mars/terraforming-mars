1c70b3c18418fe4531124020058d86cf60d0a0d4
Riccardo Zulian <riccardo.zulian@gmail.com>
2020-04-03 15:39:52 +0200

    fixed single player and added test

--- src/cards/AsteroidMiningConsortium.ts
+++ src/cards/AsteroidMiningConsortium.ts
@@ -12,12 +12,9 @@ export class AsteroidMiningConsortium implements IProjectCard {
     public cardType: CardType = CardType.AUTOMATED;
     public name: CardName = CardName.ASTEROID_MINING_CONSORTIUM;
     public canPlay(player: Player, game: Game): boolean {
-      if (game.getPlayers().length > 1 
-        && ( player.getProduction(Resources.TITANIUM)==0 ||
-        game.getPlayers().filter(p => p.id !== player.id && p.getProduction(Resources.TITANIUM) > 0).length === 0 )) {
-            return false; //PLayer hasn't production or No other player to reduce resource from
-      }
-      return player.getProduction(Resources.TITANIUM) >= 1;
+      const othersHaveTitaniumProduction = game.getPlayers().length === 0 || 
+        game.getPlayers().filter(p => p.id !== player.id && p.getProduction(Resources.TITANIUM) > 0).length === 0  ?  false : true; 
+      return player.getProduction(Resources.TITANIUM) >= 1 || othersHaveTitaniumProduction;
     }
     public play(player: Player, game: Game) {
       game.addResourceProductionDecreaseInterrupt(player, Resources.TITANIUM, 1);

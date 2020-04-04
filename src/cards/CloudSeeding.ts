0ea48fbc67b2e46c6f93a3b095ea3a1b7830a887
Riccardo Zulian <riccardo.zulian@gmail.com>
2020-04-04 10:39:05 +0200

    CloudSeeding refactory

--- src/cards/CloudSeeding.ts
+++ src/cards/CloudSeeding.ts
@@ -4,7 +4,6 @@ import {Tags} from './Tags';
 import {CardType} from './CardType';
 import {Player} from '../Player';
 import {Game} from '../Game';
-import {SelectPlayer} from '../inputs/SelectPlayer';
 import { Resources } from "../Resources";
 import { CardName } from '../CardName';
 
@@ -14,29 +13,16 @@ export class CloudSeeding implements IProjectCard {
     public name: CardName = CardName.CLOUD_SEEDING;
     public cardType: CardType = CardType.AUTOMATED;
     
-    private playersWithHeatProduction(game: Game): Array<Player> {
-      return game.getPlayers().filter((player) => player.getProduction(Resources.HEAT) >= 1);
+    public canPlay(player: Player, game: Game): boolean {
+      return player.getProduction(Resources.MEGACREDITS) > -5 &&
+        game.board.getOceansOnBoard() >= 3 - player.getRequirementsBonus(game) &&
+        game.someoneHasResourceProduction(Resources.HEAT,1);
     }
 
-    public canPlay(player: Player, game: Game): boolean {
-      if (this.playersWithHeatProduction(game).length === 0) return false;
-      return player.getProduction(Resources.MEGACREDITS) > -5 &&
-        game.board.getOceansOnBoard() >= 3 - player.getRequirementsBonus(game);
-    }
-    private doPlay(player: Player): undefined {
+    public play(player: Player, game: Game) {
+      game.addResourceProductionDecreaseInterrupt(player, Resources.HEAT, 1);
       player.setProduction(Resources.MEGACREDITS,-1);
       player.setProduction(Resources.PLANTS,2);
       return undefined;
-    }
-    public play(player: Player, game: Game) {
-      if (game.getPlayers().length === 1) return this.doPlay(player);
-      return new SelectPlayer(
-          this.playersWithHeatProduction(game),
-          'Select player to decrease heat production 1 step',
-          (foundPlayer: Player) => {
-            foundPlayer.setProduction(Resources.HEAT,-1,game,player);
-            return this.doPlay(player);
-          }
-      );
     }
 }

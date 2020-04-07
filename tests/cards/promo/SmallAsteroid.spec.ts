f01b82f57a3f8f870d1888063f76a3ef82a938ce
Daniel Leinich <leinich@gmx.net>
2020-04-07 13:14:13 +0200

    Added test for solo mode

--- tests/cards/promo/SmallAsteroid.spec.ts
+++ tests/cards/promo/SmallAsteroid.spec.ts
@@ -17,4 +17,13 @@ describe("SmallAsteroid", function () {
         expect(player2.getResource(Resources.PLANTS)).to.eq(1);
         expect(game.getTemperature()).to.eq(-28);
     });
+    it("Doesn't remove plants in solo mode", function() {
+        const card = new SmallAsteroid();
+        const player = new Player("test", Color.BLUE, false);
+        player.setResource(Resources.PLANTS, 3);
+        const game = new Game("solo", [player], player);
+        const play = card.play(player, game);
+        expect(play).to.eq(undefined);
+        expect(player.getResource(Resources.PLANTS)).to.eq(3);
+    });
 });

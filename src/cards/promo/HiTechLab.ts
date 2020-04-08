28a65fd87af4176343a4bc85902bddbf0c2aa330
Daniel Leinich <leinich@gmx.net>
2020-04-08 22:32:04 +0200

    Added promo card Hi-Tech Lab (#539)

---
+++ src/cards/promo/HiTechLab.ts
@@ -0,0 +1,42 @@
+import { IProjectCard } from "../IProjectCard";
+import { CardName } from "../../CardName";
+import { CardType } from "../CardType";
+import { Tags } from "../Tags";
+import { Player } from "../../Player";
+import { Resources } from "../../Resources";
+import { Game } from "../../Game";
+import { SelectAmount } from "../../inputs/SelectAmount";
+import { SelectFromCards } from "../../interrupts/SelectFromCards";
+
+export class HiTechLab implements IProjectCard {
+
+    public name: CardName = CardName.HI_TECH_LAB;
+    public cost: number = 17;
+    public tags: Array<Tags> = [Tags.SCIENCE, Tags.STEEL];
+    public cardType: CardType = CardType.ACTIVE;
+
+    public play() {
+        return undefined;
+    }
+
+    public canAct(player: Player): boolean {
+        return player.getResource(Resources.ENERGY) > 0;
+    }
+
+    public action(player: Player, game: Game) {
+        return new SelectAmount("Select amount of energy to spend", (amount: number) => {
+            player.setResource(Resources.ENERGY, -amount);
+            let cardsDrawn: Array<IProjectCard> = [];
+            for (let counter = 0; counter < amount; counter++) {
+                cardsDrawn.push(game.dealer.dealCard());
+            };
+            game.addInterrupt(new SelectFromCards(player, game, "Select card to take into hand", cardsDrawn));
+            return undefined;
+        }, player.getResource(Resources.ENERGY));
+    }
+
+    public getVictoryPoints() {
+        return 1;
+    }
+
+}

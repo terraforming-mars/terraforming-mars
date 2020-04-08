867610fffa02b8d5547e990d668b08779b9856ac
Daniel Leinich <leinich@gmx.net>
2020-04-08 11:47:52 +0200

    Added card with wildcad tag to test

--- tests/cards/promo/OrbitalCleanup.spec.ts
+++ tests/cards/promo/OrbitalCleanup.spec.ts
@@ -5,6 +5,7 @@ import { Research } from "../../../src/cards/Research"
 import { Color } from "../../../src/Color";
 import { Player } from "../../../src/Player";
 import { Resources } from "../../../src/Resources";
+import { ResearchCoordination } from "../../../src/cards/prelude/ResearchCoordination";
 
 describe("OrbitalCleanup", function () {
     it("Can't play if not enough MC available", function () {
@@ -25,9 +26,10 @@ describe("OrbitalCleanup", function () {
         const player = new Player("test", Color.BLUE, false);
         player.playedCards.push(new Research());
         player.playedCards.push(new AdvancedAlloys());
+        player.playedCards.push(new ResearchCoordination());
         expect(card.play(player)).to.eq(undefined);
         expect(card.action(player)).to.eq(undefined);
-        expect(player.getResource(Resources.MEGACREDITS)).to.eq(3);
+        expect(player.getResource(Resources.MEGACREDITS)).to.eq(4);
     });
     it("Should give victory points", function () {
         const card = new OrbitalCleanup();

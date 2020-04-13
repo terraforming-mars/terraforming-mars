149edb2d49628a620d4776cf0b1fc0a35bfd2658
Pierre HILBERT <pierre.hilbert@decathlon.com>
2020-04-13 12:31:28 +0200

    Turmoil: Wildlife Dome

---
+++ tests/cards/turmoil/WildlifeDome.spec.ts
@@ -0,0 +1,38 @@
+import { expect } from "chai";
+import { WildlifeDome } from "../../../src/cards/turmoil/WildlifeDome";
+import { Player } from "../../../src/Player";
+import { Color } from "../../../src/Color";
+import { BoardName } from '../../../src/BoardName';
+import { GameOptions, Game } from '../../../src/Game';
+import { PartyName } from "../../../src/turmoil/parties/PartyName";
+
+describe("WildlifeDome", function () {
+    it("Should play", function () {
+        const card = new WildlifeDome();
+        const player = new Player("test", Color.BLUE, false);
+        const gameOptions = {
+            draftVariant: false,
+            preludeExtension: false,
+            venusNextExtension: true,
+            coloniesExtension: false,
+            turmoilExtension: true,
+            boardName: BoardName.ORIGINAL,
+            showOtherPlayersVP: false,
+            customCorporationsList: [],
+            solarPhaseOption: false
+          } as GameOptions;
+        const game = new Game("foobar", [player,player], player, gameOptions);  
+        expect(card.canPlay(player, game)).to.eq(false);
+        if (game.turmoil !== undefined) {
+            let greens = game.turmoil.getPartyByName(PartyName.GREENS);
+            if (greens !== undefined) {
+                greens.delegates.push(player, player);
+                expect(card.canPlay(player, game)).to.eq(true); 
+            }
+        } 
+        const action = card.play(player, game);
+        expect(action).not.to.eq(undefined);
+        action.cb(action.availableSpaces[0]);
+        expect(game.getOxygenLevel()).to.eq(1);
+    });
+});

09912488d174fa7f21a4dd1b16e7d2d227628cb6
Pierre HILBERT <pierre.hilbert@decathlon.com>
2020-04-14 15:13:09 +0200

    Turmoil: Correct tests

--- tests/globalEvents/SnowCover.spec.ts
+++ tests/globalEvents/SnowCover.spec.ts
@@ -12,7 +12,7 @@ describe("SnowCover", function () {
         const player = new Player("test", Color.BLUE, false);
         const player2 = new Player("test2", Color.RED, false);
         const game = new Game("foobar", [player,player2], player);
-        const turmoil = new Turmoil();
+        const turmoil = new Turmoil(game);
         turmoil.chairman = player2;
         turmoil.dominantParty = new Kelvinists();
         turmoil.dominantParty.partyLeader = player2;

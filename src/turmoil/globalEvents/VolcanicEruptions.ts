839c973460cc66ab76fc8a7806abf7d788798cf2
Pierre HILBERT <pierre.hilbert@decathlon.com>
2020-04-14 19:52:32 +0200

    Turmoil: Volcanic Eruptions

---
+++ src/turmoil/globalEvents/VolcanicEruptions.ts
@@ -0,0 +1,21 @@
+import { IGlobalEvent } from './IGlobalEvent';
+import { GlobalEventName } from './GlobalEventName';
+import { PartyName } from '../parties/PartyName';
+import { Game } from '../../Game';
+import { Resources } from '../../Resources';
+import { Turmoil } from '../Turmoil';
+
+export class VolcanicEruptions implements IGlobalEvent {
+    public name = GlobalEventName.VOLCANIC_ERUPTIONS;
+    public revealedDelegate = PartyName.SCIENTISTS;
+    public currentDelegate = PartyName.KELVINISTS;
+    public resolve(game: Game, turmoil: Turmoil) {
+        game.increaseTemperature(game.getPlayers()[0], 2, true);
+        game.getPlayers().forEach(player => {
+            const amount = turmoil.getPlayerInfluence(player);
+            if (amount > 0) {
+                player.setProduction(Resources.HEAT, amount);
+            }
+        });    
+    }
+}    

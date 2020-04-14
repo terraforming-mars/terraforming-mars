71b4f7bdce7c08053aaf64761910825af8a0721b
Pierre HILBERT <pierre.hilbert@decathlon.com>
2020-04-14 10:43:23 +0200

    Turmoil: Terraformer Milestone is 26 not 35 with Turmoil

--- src/milestones/Terraformer.ts
+++ src/milestones/Terraformer.ts
@@ -4,8 +4,9 @@ import { Game } from "../Game";
 
 export class Terraformer implements IMilestone {
     public name: string = "Terraformer";
-    public description: string = "Having a terraform rating of at least 35"
+    public terraformRating: number = 35;
+    public description: string = "Having a terraform rating of at least "+this.terraformRating;
     public canClaim(player: Player, _game: Game): boolean {
-        return player.terraformRating >= 35;
+        return player.terraformRating >= this.terraformRating;
     }   
 }

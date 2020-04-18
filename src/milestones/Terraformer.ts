00ad8d1ae6c414de6c79c483e48a640ad0c01e8a
vincentneko <vincentneko@gmail.com>
2020-04-18 16:40:31 +0200

    Fix Terraformer description

--- src/milestones/Terraformer.ts
+++ src/milestones/Terraformer.ts
@@ -6,7 +6,7 @@ export class Terraformer implements IMilestone {
     public name: string = "Terraformer";
     private terraformRating: number = 35;
     private terraformRatingTurmoil: number = 26;
-    public description: string = "Having a terraform rating of at least "+this.terraformRating;
+    public description: string = "Having a terraform rating of at least "+this.terraformRating+ " or " + this.terraformRatingTurmoil + " with Turmoil.";
     public canClaim(player: Player, game: Game): boolean {
         if (game.turmoilExtension) {
             return player.getTerraformRating() >= this.terraformRatingTurmoil;

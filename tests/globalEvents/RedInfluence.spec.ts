4d58a9d04e9304bc820a739aa68bbc44b2b31f00
Pierre HILBERT <pierre.hilbert@decathlon.com>
2020-04-20 19:02:05 +0200

    Turmoil: Red Influence

---
+++ tests/globalEvents/RedInfluence.spec.ts
@@ -0,0 +1,30 @@
+import { expect } from "chai";
+import { RedInfluence } from "../../src/turmoil/globalEvents/RedInfluence";
+import { Player } from "../../src/Player";
+import { Color } from "../../src/Color";
+import { Game } from '../../src/Game';
+import { Turmoil } from '../../src/turmoil/Turmoil';
+import { Kelvinists } from '../../src/turmoil/parties/Kelvinists';
+import { Resources } from "../../src/Resources";
+
+describe("RedInfluence", function () {
+    it("resolve play", function () {
+        const card = new RedInfluence();
+        const player = new Player("test", Color.BLUE, false);
+        const player2 = new Player("test2", Color.RED, false);
+        const game = new Game("foobar", [player,player2], player);
+        const turmoil = new Turmoil(game);
+        player.setTerraformRating(23);
+        player.megaCredits = 10;
+        player2.megaCredits = 10;
+        turmoil.chairman = player2;
+        turmoil.dominantParty = new Kelvinists();
+        turmoil.dominantParty.partyLeader = player2;
+        turmoil.dominantParty.delegates.push(player2);
+        card.resolve(game, turmoil);
+        expect(player.getResource(Resources.MEGACREDITS)).to.eq(4);
+        expect(player2.getResource(Resources.MEGACREDITS)).to.eq(4);
+        expect(player.getProduction(Resources.MEGACREDITS)).to.eq(0);
+        expect(player2.getProduction(Resources.MEGACREDITS)).to.eq(3);
+    });
+});

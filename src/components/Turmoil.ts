5e426261ff439562d077c61c437a3604d293740b
alrusdi <alrusdi@gmail.com>
2020-04-23 00:40:30 +0500

    New Turmoil design

--- src/components/Turmoil.ts
+++ src/components/Turmoil.ts
@@ -6,6 +6,9 @@ export const Turmoil = Vue.component("turmoil", {
       "turmoil"
     ],
     methods: {
+      partyNameToCss: function (party: PartyName): string {
+        return party.toLowerCase().replace(" ", "_");
+      },
       getBonus: function (party: PartyName) {
         if (party === PartyName.MARS) {
           return `<div class="resource money party-resource">1</div> / 
@@ -44,70 +47,63 @@ export const Turmoil = Vue.component("turmoil", {
       }
     },
     template: `
-    <div class="turmoil">
+    <div class="turmoil" v-trim-whitespace>
       <div class="events-board">
           <div v-if="turmoil.distant" class="global-event">
-            <div class="event-title">{{ turmoil.distant.name.toUpperCase() }}</div>
-            <div class="event-parties">
-              <div class="event-party">{{ turmoil.distant.revealed.toUpperCase() }}</div>
-              <div class="event-party">{{ turmoil.distant.current.toUpperCase() }}</div>
-            </div>
-            <div class="event-content">{{ turmoil.distant.description }}</div>
+            <div class="event-party event-party--top" :class="'event-party--'+partyNameToCss(turmoil.distant.revealed)">{{ turmoil.distant.revealed }}</div>
+            <div class="event-party event-party--bottom" :class="'event-party--'+partyNameToCss(turmoil.distant.current)">{{ turmoil.distant.current }}</div>
+            <div class="event-content"><div class="event-text">{{ turmoil.distant.description }}</div></div>
           </div>
-          <div v-if="turmoil.comming" class="global-event">
-            <div class="event-title">{{ turmoil.comming.name.toUpperCase() }}</div>
-            <div class="event-parties">
-              <div class="event-party">{{ turmoil.comming.revealed.toUpperCase() }}</div>
-              <div class="event-party">{{ turmoil.comming.current.toUpperCase() }}</div>
-            </div>
+          <div v-if="turmoil.comming" class="global-event global-event--comming">
+            <div class="event-party event-party--top" :class="'event-party--'+partyNameToCss(turmoil.comming.revealed)">{{ turmoil.comming.revealed }}</div>
+            <div class="event-party event-party--bottom" :class="'event-party--'+partyNameToCss(turmoil.comming.current)">{{ turmoil.comming.current }}</div>
             <div class="event-content">{{ turmoil.comming.description }}</div>
           </div>
-          <div v-if="turmoil.current" class="global-event global-event-current">
-            <div class="event-title">{{ turmoil.current.name.toUpperCase() }}</div>
-            <div class="event-parties">
-              <div class="event-party">{{ turmoil.current.revealed.toUpperCase() }}</div>
-              <div class="event-party">{{ turmoil.current.current.toUpperCase() }}</div>
-            </div>
+          <div v-if="turmoil.current" class="global-event global-event--current">
+            <div class="event-party event-party--top" :class="'event-party--'+partyNameToCss(turmoil.current.revealed)">{{ turmoil.current.revealed }}</div>
+            <div class="event-party event-party--bottom" :class="'event-party--'+partyNameToCss(turmoil.current.current)">{{ turmoil.current.current }}</div>
             <div class="event-content">{{ turmoil.current.description }}</div>
           </div>
+      </div>
+      
+      <div class="turmoil-board">
+        <div class="turmoil-header">
+          <div class="turmoil-lobby">
+            <div class="lobby-spot" v-for="n in 5" :key="n">
+                <div v-if="turmoil.lobby.length >= n" :class="'player-token '+turmoil.lobby[n-1]"></div>
+            </div>
+          </div>
+          <div class="dominant-party-bonus" v-html="getBonus(turmoil.dominant)"></div>
+          <div class="chairman-spot"><div v-if="turmoil.chairman" :class="'player-token '+turmoil.chairman"></div></div>
+          <div class="turmoil-reserve">
+              <div class="lobby-spot" v-for="n in 6" :key="n">
+                <div v-if="turmoil.reserve.length >= n" :class="'player-token '+turmoil.reserve[n-1].color">{{ turmoil.reserve[n-1].number }}</div>
+              </div>
+          </div>
         </div>
-        <div class="turmoil-board">
-          <div class="turmoil-header">
-            <div class="grid-lobby">
-              <div class="lobby-spot" v-for="n in 5" :key="n">
-                  <div v-if="turmoil.lobby.length >= n" :class="'player-token '+turmoil.lobby[n-1]"></div>
-              </div>
-            </div>
-            <div class="label-lobby">LOBBY</div>
-            <div class="chairman-spot"><div v-if="turmoil.chairman" :class="'player-token '+turmoil.chairman"></div></div>
-            <div class="label-5mc">RESERVE</div>
-            <div class="grid-lobby">
-                <div class="lobby-spot" v-for="n in 6" :key="n">
-                  <div v-if="turmoil.reserve.length >= n" :class="'player-token '+turmoil.reserve[n-1].color">{{ turmoil.reserve[n-1].number }}</div>
-                </div>
-            </div>
+
+        <div class="grid-leaders">
+          <div v-for="party in turmoil.parties" :class="['leader-spot', 'leader-spot--'+partyNameToCss(party.name), {'player-token-new-leader': (party.name === turmoil.dominant)}]">
+            <div class="delegate-spot">
+              <div v-if="party.partyLeader" :class="['player-token', party.partyLeader]"></div>
+            </div>    
           </div>
-          <div class="grid-leaders">
-            <div v-for="party in turmoil.parties" :class="'leader-spot leader-spot--'+party.name.toLowerCase().replace(/ /g,'')">
-              <div class="delegate-spot">
-                <div v-if="party.partyLeader" :class="['player-token', party.partyLeader, {'player-token-new-leader': (party.name === turmoil.dominant)}]"></div>
-              </div>    
+        </div>
+
+        <div class="grid-parties">
+          <div v-for="party in turmoil.parties" :class="'board-party board-party--'+partyNameToCss(party.name)">
+            <div class="grid-delegates">
+              <div class="delegate-spot" v-for="n in 6" :key="n">
+                <div v-if="party.delegates.length >= n" :class="'player-token '+party.delegates[n-1].color">{{ party.delegates[n-1].number }}</div>
+              </div>
             </div>
-          </div>
-          <div class="grid-parties">
-            <div v-for="party in turmoil.parties" :class="'board-party board-party--'+party.name.toLowerCase().replace(/ /g,'')">
-              <div class="grid-delegates">
-                <div class="delegate-spot" v-for="n in 6" :key="n">
-                  <div v-if="party.delegates.length >= n" :class="'player-token '+party.delegates[n-1].color">{{ party.delegates[n-1].number }}</div>
-                </div>
-              </div>
-              <div :class="'party-name party-name--'+party.name.toLowerCase().replace(/ /g,'')">{{party.name.toUpperCase()}}</div>
-              <div class="party-bonus">
-                <span v-html="getBonus(party.name)"></span>
-              </div>
+            <div :class="'party-name party-name--'+partyNameToCss(party.name)">{{party.name}}</div>
+            <div class="party-bonus">
+              <span v-html="getBonus(party.name)"></span>
             </div>
           </div>
         </div>
       </div>
+    </div>
     `
 });

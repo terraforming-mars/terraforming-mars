2f9ff41f8eda7032a5349b6af7a1d66151e81a7e
vincentneko <vincentneko@gmail.com>
2020-05-02 19:37:18 +0200

    Use filter instead of splice

--- src/Player.ts
+++ src/Player.ts
@@ -1943,12 +1943,7 @@ export class Player implements ILoadable<SerializedPlayer, Player>{
       // Prelude cards have to be played first
       if (this.preludeCardsInHand.length > 0) {
         // Remove unplayable prelude cards
-        this.preludeCardsInHand.forEach(card => {
-          if (card.canPlay !== undefined && !card.canPlay(this, game)) {
-            const preludeCardIndex = this.preludeCardsInHand.findIndex((preludecard) => preludecard.name === card.name);
-            this.preludeCardsInHand.splice(preludeCardIndex, 1);
-          }
-        });
+        this.preludeCardsInHand = this.preludeCardsInHand.filter(card => card.canPlay !== undefined && card.canPlay(this, game));
         if (this.preludeCardsInHand.length === 0) {
           game.playerIsFinishedTakingActions();
           return;

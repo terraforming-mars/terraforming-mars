bd2964276409d1756806f179b4ad5fe572ba2762
glass4pk <glass4pk@gmail.com>
2020-05-27 17:18:02 +0800

    调整公司选牌限制

--- src/Game.ts
+++ src/Game.ts
@@ -212,8 +212,9 @@ export class Game implements ILoadable<SerializedGame, Game> {
       for (const player of players) {
         if (!player.beginner) {
           // Failsafe for exceding corporation pool - Minimum is 12
-          if (this.startingCorporations * this.players.length > 12) {
-            this.startingCorporations = 2;
+          // By ChainLee : Minimum increase to 25
+          if (this.startingCorporations * this.players.length > 25) {
+            this.startingCorporations = 5;
           }
           for (let i = 0; i < this.startingCorporations; i++) {
             const corpCard : CorporationCard | undefined = corporationCards.pop();
@@ -467,6 +468,7 @@ export class Game implements ILoadable<SerializedGame, Game> {
       let dealtCards: Array<IProjectCard> = [];
       let corporation: CorporationCard;
       const result: AndOptions = new AndOptions(() => { this.playCorporationCard(player, corporation); return undefined; });
+        // draw all card
       for (let i = 0; i < 10; i++) {
         dealtCards.push(this.dealer.dealCard());
       }

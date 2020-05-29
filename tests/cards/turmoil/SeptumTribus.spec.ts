0fa5bd8ea371bc4137965ea0e795c9ed53c637cf
waichuan <nwai1990@hotmail.com>
2020-05-30 02:56:38 +0800

    Add test for Septum Tribus

---
+++ tests/cards/turmoil/SeptumTribus.spec.ts
@@ -0,0 +1,79 @@
+import { expect } from "chai";
+import { SeptumTribus } from "../../../src/cards/turmoil/SeptumTribus";
+import { Color } from "../../../src/Color";
+import { Player } from "../../../src/Player";
+import { Game, GameOptions } from '../../../src/Game';
+import { BoardName } from "../../../src/BoardName";
+import { PartyName } from "../../../src/turmoil/parties/PartyName";
+
+describe("SeptumTribus", function () {
+    it("Should play", function () {
+        const card = new SeptumTribus();        
+        const player = new Player("test", Color.BLUE, false);
+        const player2 = new Player("test", Color.RED, false);
+        const gameOptions = {
+            draftVariant: false,
+            preludeExtension: false,
+            venusNextExtension: false,
+            coloniesExtension: false,
+            turmoilExtension: true,
+            boardName: BoardName.ORIGINAL,
+            showOtherPlayersVP: false,
+            customCorporationsList: [],
+            solarPhaseOption: false,
+            promoCardsOption: false,
+            undoOption: false,
+            startingCorporations: 2,
+            soloTR: false,
+            clonedGamedId: undefined
+          } as GameOptions;
+
+        const game = new Game("foobar", [player,player2], player, gameOptions);
+        card.play();
+
+        player.corporationCard = card;
+        player.megaCredits = 0;
+
+        let turmoil = game.turmoil;
+        expect(game.turmoil).not.to.eq(undefined);
+
+        if (turmoil) {
+            turmoil.sendDelegateToParty(player, PartyName.REDS, game);
+            turmoil.sendDelegateToParty(player, PartyName.REDS, game);
+            card.action(player, game);
+            expect(player.megaCredits).to.eq(2);
+
+            player.megaCredits = 0;
+            turmoil.sendDelegateToParty(player, PartyName.KELVINISTS, game);
+            turmoil.sendDelegateToParty(player, PartyName.GREENS, game);
+            card.action(player, game);
+            expect(player.megaCredits).to.eq(6);
+        }
+    });
+
+    it("Cannot act without Turmoil expansion", function () {
+        const card = new SeptumTribus();        
+        const player = new Player("test", Color.BLUE, false);
+        const gameOptions = {
+            draftVariant: false,
+            preludeExtension: false,
+            venusNextExtension: false,
+            coloniesExtension: false,
+            turmoilExtension: true,
+            boardName: BoardName.ORIGINAL,
+            showOtherPlayersVP: false,
+            customCorporationsList: [],
+            solarPhaseOption: false,
+            promoCardsOption: false,
+            undoOption: false,
+            startingCorporations: 2,
+            soloTR: false,
+            clonedGamedId: undefined
+          } as GameOptions;
+
+        const game = new Game("foobar", [player], player, gameOptions);
+        card.play();
+        player.corporationCard = card;
+        expect(card.action(player, game)).to.eq(undefined);
+    });
+});

e112ca6ccdb7ad61a70496f77c5093e17bffc368
waichuan <nwai1990@hotmail.com>
2020-05-31 20:50:52 +0800

    Remove unnecessary click for Inventrix first action

--- src/cards/corporation/Inventrix.ts
+++ src/cards/corporation/Inventrix.ts
@@ -3,7 +3,6 @@ import { CorporationCard } from "./CorporationCard";
 import { Tags } from "../Tags";
 import { Player } from "../../Player";
 import { Game } from "../../Game";
-import { SelectOption } from "../../inputs/SelectOption";
 import { CardName } from '../../CardName';
 
 export class Inventrix implements CorporationCard {
@@ -11,15 +10,13 @@ export class Inventrix implements CorporationCard {
     public tags: Array<Tags> = [Tags.SCIENCE];
     public startingMegaCredits: number = 45;
     public initialAction(player: Player, game: Game) {
-        return new SelectOption("Draw 3 cards", () => {
-            player.cardsInHand.push(
-                game.dealer.dealCard(),
-                game.dealer.dealCard(),
-                game.dealer.dealCard()
-            );
-            game.logCorpFirstAction(player);
-            return undefined;
-        });
+        player.cardsInHand.push(
+            game.dealer.dealCard(),
+            game.dealer.dealCard(),
+            game.dealer.dealCard()
+        );
+        game.logCorpFirstAction(player);
+        return undefined;
     }
     public getRequirementBonus(_player: Player, _game: Game): number {
         return 2;

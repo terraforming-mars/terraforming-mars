fb4ac2f1753d2bedec0aaa200c15f56c156b500e
waichuan <nwai1990@hotmail.com>
2020-06-05 13:05:17 +0800

    Fix edge case for Manutech

--- src/cards/prelude/BusinessEmpire.ts
+++ src/cards/prelude/BusinessEmpire.ts
@@ -5,12 +5,15 @@ import { IProjectCard } from "../IProjectCard";
 import { Resources } from '../../Resources';
 import { CardName } from '../../CardName';
 import { Game } from "../../Game";
+import { CorporationName } from "../../CorporationName";
 
 export class BusinessEmpire extends PreludeCard implements IProjectCard {
     public tags: Array<Tags> = [Tags.EARTH];
     public name: CardName = CardName.BUSINESS_EMPIRE;
     public canPlay(player: Player, _game: Game, bonusMc?: number) {
-        let requiredPayment = 6 - (bonusMc || 0); 
+        let requiredPayment = 6 - (bonusMc || 0);
+        if (player.isCorporation(CorporationName.MANUTECH)) return requiredPayment - 6 <= 0;
+        
         return requiredPayment <= 0 ? true : player.canAfford(requiredPayment);
     }
     public play(player: Player) {

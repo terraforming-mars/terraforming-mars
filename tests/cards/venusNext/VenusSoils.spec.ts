8a85a6a3036ee581ac057bfd096f3dd1bf5c141f
waichuan <nwai1990@hotmail.com>
2020-06-14 14:50:02 +0800

    Improve Venus Soils

--- tests/cards/venusNext/VenusSoils.spec.ts
+++ tests/cards/venusNext/VenusSoils.spec.ts
@@ -6,15 +6,29 @@ import { Resources } from "../../../src/Resources";
 import { SelectCard } from '../../../src/inputs/SelectCard';
 import { Game } from '../../../src/Game';
 import { Thermophiles } from '../../../src/cards/venusNext/Thermophiles';
+import { VenusianInsects } from "../../../src/cards/venusNext/VenusianInsects";
 
 describe("VenusSoils", function () {
-    it("Should play", function () {
+    it("Should play - single target", function () {
         const card = new VenusSoils();
         const card2 = new Thermophiles();
         const player = new Player("test", Color.BLUE, false);
         const game = new Game("foobar", [player,player], player);
 
         player.playedCards.push(card2);
+        card.play(player,game);
+        expect(player.getResourcesOnCard(card2)).to.eq(2);
+        expect(player.getProduction(Resources.PLANTS)).to.eq(1);
+        expect(game.getVenusScaleLevel()).to.eq(2);
+    });
+    it("Should play - multiple targets", function () {
+        const card = new VenusSoils();
+        const card2 = new Thermophiles();
+        const card3 = new VenusianInsects();
+        const player = new Player("test", Color.BLUE, false);
+        const game = new Game("foobar", [player,player], player);
+
+        player.playedCards.push(card2, card3);
         const action = card.play(player,game);
         expect(action instanceof SelectCard).to.eq(true);
         if ( ! (action instanceof SelectCard)) return;

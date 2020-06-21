172722d77bb5d7f6d37048cf65d3d5ad2d66a75a
waichuan <nwai1990@hotmail.com>
2020-06-21 11:38:54 +0800

    Fix edge cases for Moss and Nitrophilic Moss

--- tests/cards/NitrophilicMoss.spec.ts
+++ tests/cards/NitrophilicMoss.spec.ts
@@ -5,6 +5,7 @@ import { Color } from "../../src/Color";
 import { Player } from "../../src/Player";
 import { Game } from "../../src/Game";
 import { Resources } from '../../src/Resources';
+import { ViralEnhancers } from "../../src/cards/ViralEnhancers";
 
 describe("NitrophilicMoss", function () {
     it("Can't play", function () {
@@ -24,6 +25,29 @@ describe("NitrophilicMoss", function () {
         const action = card.play(player);
         expect(action).to.eq(undefined);
         expect(player.plants).to.eq(-2);
+        expect(player.getProduction(Resources.PLANTS)).to.eq(2);
+    });
+    it("Can play with 1 plant if have Viral Enhancers", function () {
+        const card = new NitrophilicMoss();
+        const viralEnhancers = new ViralEnhancers();
+        const player = new Player("test", Color.BLUE, false);
+        const game = new Game("foobar", [player,player], player);
+
+        // meet min. requirements
+        game.addOceanTile(player, "06");
+        game.addOceanTile(player, "07");
+        game.addOceanTile(player, "34");
+
+        // setup player with viral enhancers in play and 1 plant
+        player.plants = 1;
+        player.playedCards.push(viralEnhancers);
+        
+        expect(card.canPlay(player, game)).to.eq(true);
+        card.play(player);
+        
+        expect(player.plants).to.eq(-1);
+        viralEnhancers.onCardPlayed(player, game, card);
+        expect(player.plants).to.eq(0);
         expect(player.getProduction(Resources.PLANTS)).to.eq(2);
     });
 });

5e8617cef2b73c503b6fe65119335745246de69d
waichuan <nwai1990@hotmail.com>
2020-06-25 10:50:34 +0800

    Add Field-Capped City promo card

---
+++ src/cards/promo/FieldCappedCity.ts
@@ -0,0 +1,34 @@
+import {IProjectCard} from '../IProjectCard';
+import {Tags} from '../Tags';
+import {CardType} from '../CardType';
+import {Player} from '../../Player';
+import {Game} from '../../Game';
+import {SelectSpace} from '../../inputs/SelectSpace';
+import {ISpace} from '../../ISpace';
+import { Resources } from '../../Resources';
+import { CardName } from '../../CardName';
+
+export class FieldCappedCity implements IProjectCard {
+    public cost: number = 29;
+    public tags: Array<Tags> = [Tags.CITY, Tags.STEEL, Tags.ENERGY];
+    public name: CardName = CardName.FIELD_CAPPED_CITY;
+    public cardType: CardType = CardType.AUTOMATED;
+
+    public canPlay(player: Player, game: Game): boolean {
+      return game.board.getAvailableSpacesForCity(player).length > 0;
+    }
+
+    public play(player: Player, game: Game) {
+      return new SelectSpace(
+          'Select space for city tile',
+          game.board.getAvailableSpacesForCity(player),
+          (space: ISpace) => {
+            game.addCityTile(player, space.id);
+            player.plants += 3;
+            player.setProduction(Resources.ENERGY,1);
+            player.setProduction(Resources.MEGACREDITS,2);
+            return undefined;
+          }
+      );
+    }
+}

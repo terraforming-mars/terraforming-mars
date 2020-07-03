e0bd776b84692779e33f1bd19d340038fc53270b
Chenyuan Zhang <31570628+zcy921716@users.noreply.github.com>
2020-07-03 16:41:13 +1000

    Update Pristar.ts

--- src/cards/turmoil/Pristar.ts
+++ src/cards/turmoil/Pristar.ts
@@ -11,7 +11,6 @@ export class Pristar implements CorporationCard, IResourceCard {
     public startingMegaCredits: number = 53;
     public resourceType: ResourceType = ResourceType.PRESERVATION;
     public resourceCount: number = 0;
-    public lastGenerationTR: number = 0;
 
     public play(player: Player) {
         player.decreaseTerraformRatingSteps(2);
@@ -24,11 +23,10 @@ export class Pristar implements CorporationCard, IResourceCard {
     }
 
     public onProductionPhase(player: Player) {
-        if (this.lastGenerationTR >= player.getTerraformRating()) {
+        if (!player.increaseTerraformRatingThisGeneration) {
             player.megaCredits += 6;
             this.resourceCount++;
         }
-        this.lastGenerationTR = player.getTerraformRating();
         return undefined;
     }
 }

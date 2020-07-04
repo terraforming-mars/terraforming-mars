b8e2231d9b9e1f9cc790bbdd4fd984b1edffc6fc
waichuan <nwai1990@hotmail.com>
2020-07-04 12:52:28 +0800

    Update test for Great Dam promo

--- tests/cards/promo/GreatDamPromo.spec.ts
+++ tests/cards/promo/GreatDamPromo.spec.ts
@@ -6,6 +6,7 @@ import { Player } from "../../../src/Player";
 import { Game } from "../../../src/Game";
 import { Resources } from '../../../src/Resources';
 import { SelectSpace } from "../../../src/inputs/SelectSpace";
+import { TileType } from "../../../src/TileType";
 
 describe("GreatDamPromo", function () {
     it("Can't play without meeting requirements", function () {
@@ -18,8 +19,14 @@ describe("GreatDamPromo", function () {
         const card = new GreatDamPromo();
         const player = new Player("test", Color.BLUE, false);
         const game = new Game("foobar", [player], player);
-        const action = card.play(player, game);
 
+        // add 4 oceans
+        const oceanSpaces = game.board.getAvailableSpacesForOcean(player);
+        for (let i = 0; i < 4; i++) {
+            oceanSpaces[i].tile = { tileType: TileType.OCEAN };
+        }
+
+        const action = card.play(player, game);
         expect(action instanceof SelectSpace).to.eq(true);
         expect(player.getProduction(Resources.ENERGY)).to.eq(2);
         player.victoryPointsBreakdown.setVictoryPoints('victoryPoints', card.getVictoryPoints());

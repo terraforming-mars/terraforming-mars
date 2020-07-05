5b3c83e0ce21c6a108b55a48b5b7e82d5e0a22dd
Chenyuan Zhang <chenyuanz@3060l-201404-l.mobility.unimelb.net.au>
2020-07-05 15:13:23 +1000

    fix bug:  GlobalEvent  MudSlides

--- src/turmoil/globalEvents/MudSlides.ts
+++ src/turmoil/globalEvents/MudSlides.ts
@@ -1,10 +1,10 @@
-import { IGlobalEvent } from './IGlobalEvent';
-import { GlobalEventName } from './GlobalEventName';
-import { PartyName } from '../parties/PartyName';
-import { Game } from '../../Game';
-import { Resources } from '../../Resources';
-import { Turmoil } from '../Turmoil';
-import { SpaceType } from '../../SpaceType';
+import { IGlobalEvent } from "./IGlobalEvent";
+import { GlobalEventName } from "./GlobalEventName";
+import { PartyName } from "../parties/PartyName";
+import { Game } from "../../Game";
+import { Resources } from "../../Resources";
+import { Turmoil } from "../Turmoil";
+import { TileType } from "../../TileType";
 
 export class MudSlides implements IGlobalEvent {
     public name = GlobalEventName.MUD_SLIDES;
@@ -12,16 +12,17 @@ export class MudSlides implements IGlobalEvent {
     public revealedDelegate = PartyName.KELVINISTS;
     public currentDelegate = PartyName.GREENS;
     public resolve(game: Game, turmoil: Turmoil) {
-        
+
         game.getPlayers().forEach(player => {
-            const ocean = game.board.spaces.filter((space) => (space.player !== undefined && space.player === player && space.tile !== undefined) 
+            const tiles = game.board.spaces.filter((space) => (space.player !== undefined && space.player === player && space.tile !== undefined)
                                && game.board.getAdjacentSpaces(space)
-                               .filter((space) => space.spaceType === SpaceType.OCEAN).length > 0
+                               .filter((space) => (space.tile !== undefined &&
+                                           space.tile.tileType === TileType.OCEAN)).length > 0
                                ).length;
-            const amount = Math.min(5, ocean) - turmoil.getPlayerInfluence(player);
+            const amount = Math.min(5, tiles) - turmoil.getPlayerInfluence(player);
             if (amount > 0) {
                 player.setResource(Resources.MEGACREDITS, -4 * amount, game, undefined, true);
             }
-        });    
+        });
     }
-}    
+}

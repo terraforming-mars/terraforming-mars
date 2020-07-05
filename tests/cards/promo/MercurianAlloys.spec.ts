d08dadd1e72d1b18807d9357ce260d8eb18aca61
waichuan <nwai1990@hotmail.com>
2020-07-05 17:14:50 +0800

    Cleanup Promo tests

--- tests/cards/promo/MercurianAlloys.spec.ts
+++ tests/cards/promo/MercurianAlloys.spec.ts
@@ -3,19 +3,26 @@ import { MercurianAlloys } from "../../../src/cards/promo/MercurianAlloys";
 import { Color } from "../../../src/Color";
 import { Player } from "../../../src/Player";
 import { Game } from '../../../src/Game';
+import { Research } from "../../../src/cards/Research";
 
 describe("MercurianAlloys", function () {
+    let card : MercurianAlloys, player : Player, game : Game;
+
+    beforeEach(function() {
+        card = new MercurianAlloys();
+        player = new Player("test", Color.BLUE, false);
+        game = new Game("foobar", [player], player);
+    });
+
     it("Can't play if not enough science tags available", function () {
-        const card = new MercurianAlloys();
-        const player = new Player("test", Color.BLUE, false);
         expect(card.canPlay(player)).to.eq(false);
     });
+
     it("Should play", function () {
-        const card = new MercurianAlloys();
-        const player = new Player("test", Color.BLUE, false);
-        const game = new Game("foobar", [player,player], player);
-        const play = card.play(player);
-        expect(play).to.eq(undefined);
+        player.playedCards.push(new Research());
+        expect(card.canPlay(player)).to.eq(true);
+        
+        card.play(player);
         expect(player.getTitaniumValue(game)).to.eq(4);
     });
 });

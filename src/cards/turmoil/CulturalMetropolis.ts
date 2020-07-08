2a58a2eb56f4a2d4d3d70e38163b20ebb76aad70
Qiu <757384114@qq.com>
2020-07-08 18:33:34 +1000

    Minor change of the code

--- src/cards/turmoil/CulturalMetropolis.ts
+++ src/cards/turmoil/CulturalMetropolis.ts
@@ -19,7 +19,7 @@ export class CulturalMetropolis implements IProjectCard {
     public canPlay(player: Player, game: Game): boolean {
         if (game.turmoil !== undefined) {
             //This card requires player has 2 delegates available
-            return game.turmoil.canPlay(player, PartyName.UNITY) && player.getProduction(Resources.ENERGY) >= 1 && (game.turmoil!.getDelegates(player.id) > 1 || (game.turmoil!.getDelegates(player.id) === 1 && game.turmoil?.lobby.has(player.id)))
+            return game.turmoil.canPlay(player, PartyName.UNITY) && player.getProduction(Resources.ENERGY) >= 1 && (game.turmoil!.getDelegates(player.id) > 1 || (game.turmoil!.getDelegates(player.id) === 1 && game.turmoil!.lobby.has(player.id)))
         }
         return false;
     }
@@ -28,12 +28,13 @@ export class CulturalMetropolis implements IProjectCard {
         player.setProduction(Resources.ENERGY,-1);
         player.setProduction(Resources.MEGACREDITS,3);
         game.addInterrupt(new SelectCity(player, game));
+        const title = "Select where to send two delegates";
         // To Do: Should allow use the free delegate (in lobby) for this card (only matter in very few secnarios)
         if (game.turmoil!.getDelegates(player.id) > 1){
-          game.addInterrupt(new SelectParty(player, game, "Select where to send two delegates", 2, undefined, undefined, false));
+          game.addInterrupt(new SelectParty(player, game, title, 2, undefined, undefined, false));
         }
-        else if (game.turmoil!.getDelegates(player.id) === 1 && game.turmoil?.lobby.has(player.id)) {
-          game.addInterrupt(new SelectParty(player, game, "Select where to send two delegate", 2, undefined, undefined, true));
+        else if (game.turmoil!.getDelegates(player.id) === 1 && game.turmoil!.lobby.has(player.id)) {
+          game.addInterrupt(new SelectParty(player, game, title, 2, undefined, undefined, true));
         }
         return undefined;
     }

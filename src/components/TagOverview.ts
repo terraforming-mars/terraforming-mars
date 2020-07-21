24a19b9b2c8078fd962364c34e97b197dcd38157
Maarten Meussen <m.meussen@flusso.nl>
2020-07-21 21:34:29 +0200

    - Initial version for tag overview grid - Initial version for displaying corp name under player name

---
+++ src/components/TagOverview.ts
@@ -0,0 +1,41 @@
+import Vue from "vue";
+import {Tags} from "../cards/Tags";
+import {PlayerModel} from "../models/PlayerModel";
+
+export const TagsOverview = Vue.component("tags", {
+    props: ["player"],
+    methods: {
+        getTags: function () {
+            return Tags;
+        },
+        getTagCount: function (player: PlayerModel, iTag: Tags) {
+            let tagCount = player.tags.find(({tag}) => tag === iTag);
+
+            if (tagCount) {
+                return tagCount.count;
+            }
+            return 0;
+        }
+    },
+    template: `
+    <div class="tags_cont" v-trim-whitespace>
+        <div class="tags-grid">
+            <div>
+                <span>Player</span>
+            </div>
+            
+            <div v-for="tag in getTags()" class="tag-count" :class="'tag-'+ tag"></div>
+            
+            <template v-for="player in player.players">
+             <div class="player_name_cont highlighter_box" :class="'player_bg_color_'+player.color">
+                <span class="player_name" >{{player.name}}</span>
+             </div>
+                <template v-for="tag in getTags()">
+                    <div class="grid-item"><span>{{getTagCount(player, tag)}}</span></div>
+                </template>
+            </template>
+        </div>
+    </div>
+    `
+});
+

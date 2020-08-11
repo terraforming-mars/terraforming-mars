5c8bd999e0079ee4ea7b345f6b9d64f19fc7578f
waichuan <nwai1990@hotmail.com>
2020-08-11 15:13:44 +0800

    Improve FloaterTechnology: Auto select if only one target

--- src/cards/colonies/FloaterTechnology.ts
+++ src/cards/colonies/FloaterTechnology.ts
@@ -5,6 +5,7 @@ import { Player } from "../../Player";
 import { CardName } from '../../CardName';
 import { ResourceType } from '../../ResourceType';
 import { Game } from '../../Game';
+import { LogHelper } from "../../components/LogHelper";
 
 export class FloaterTechnology implements IProjectCard {
     public cost: number = 7;
@@ -17,7 +18,15 @@ export class FloaterTechnology implements IProjectCard {
     } 
 
     public action(player: Player, game: Game) {
-        game.addResourceInterrupt(player, ResourceType.FLOATER, 1, undefined);
+        const floaterCards = player.getResourceCards(ResourceType.FLOATER);
+
+        if (floaterCards.length === 1) {
+            player.addResourceTo(floaterCards[0], 1);
+            LogHelper.logAddResource(game, player, floaterCards[0]);
+        } else if (floaterCards.length > 1) {
+            game.addResourceInterrupt(player, ResourceType.FLOATER, 1, undefined);
+        }
+
         return undefined;
     } 
 

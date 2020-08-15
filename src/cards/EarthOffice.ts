3b4861f90c029e038b1cc92d3a253ef97091e405
waichuan <nwai1990@hotmail.com>
2020-08-15 11:11:12 +0800

    Fix discount for Earth Office and Teractor

--- src/cards/EarthOffice.ts
+++ src/cards/EarthOffice.ts
@@ -1,10 +1,9 @@
-
-import {IProjectCard} from './IProjectCard';
-import {Tags} from './Tags';
-import {CardType} from './CardType';
-import {Player} from '../Player';
-import {Game} from '../Game';
-import { CardName } from '../CardName';
+import {IProjectCard} from "./IProjectCard";
+import {Tags} from "./Tags";
+import {CardType} from "./CardType";
+import {Player} from "../Player";
+import {Game} from "../Game";
+import { CardName } from "../CardName";
 
 export class EarthOffice implements IProjectCard {
     public cost: number = 1;
@@ -13,11 +12,9 @@ export class EarthOffice implements IProjectCard {
     public cardType: CardType = CardType.ACTIVE;
 
     public getCardDiscount(_player: Player, _game: Game, card: IProjectCard) {
-      if (card.tags.indexOf(Tags.EARTH) !== -1) {
-        return 3;
-      }
-      return 0;
+      return card.tags.filter(tag => tag === Tags.EARTH).length * 3;
     }
+
     public play() {
       return undefined;
     }

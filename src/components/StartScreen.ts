833339c5c2611213a2c6d8ebabb09869dc91cec8
Simeon Simeonov <s.simeonoff@gmail.com>
2020-08-19 15:19:24 +0200

    names revert

--- src/components/StartScreen.ts
+++ src/components/StartScreen.ts
@@ -15,9 +15,9 @@ export const StartScreen = Vue.component("start-screen", {
                 <a class="start-screen-link start-screen-link--new-game" href="/new-game" v-i18n>New game</a>
                 <a class="start-screen-link start-screen-link--solo" href="/solo" v-i18n>Solo challenge</a>
                 <a class="start-screen-link start-screen-link--cards-list" href="https://ssimeonoff.github.io/cards-list" v-i18n>Cards list</a>
-                <a class="start-screen-link start-screen-link--board-game" href="https://boardgamegeek.com/boardgame/167791/terraforming-mars" v-i18n>The game</a>
+                <a class="start-screen-link start-screen-link--board-game" href="https://boardgamegeek.com/boardgame/167791/terraforming-mars" v-i18n>Board game</a>
                 <a class="start-screen-link start-screen-link--about" href="https://github.com/bafolts/terraforming-mars" v-i18n>About us</a>
-                <a class="start-screen-link start-screen-link--changelog" href="https://github.com/bafolts/terraforming-mars/wiki/Changelog" v-i18n>Changelog</a>
+                <a class="start-screen-link start-screen-link--changelog" href="https://github.com/bafolts/terraforming-mars/wiki/Changelog" v-i18n>Whats new?</a>
                 <a class="start-screen-link start-screen-link--chat" href="https://discord.gg/fWXE53K" target="_blank" v-i18n>Discord</a>
                 <div class="start-screen-header start-screen-link--languages">
                     <language-switcher></language-switcher>

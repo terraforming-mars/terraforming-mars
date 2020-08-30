fb42cde45ebee37acf188f6a577209ea3b1c4790
waichuan <nwai1990@hotmail.com>
2020-08-30 11:28:21 +0800

    Improve Hired Raiders and Sabotage

--- tests/cards/Sabotage.spec.ts
+++ tests/cards/Sabotage.spec.ts
@@ -1,22 +1,34 @@
-
 import { expect } from "chai";
 import { Sabotage } from "../../src/cards/Sabotage";
 import { Color } from "../../src/Color";
 import { Player } from "../../src/Player";
 import { Game } from "../../src/Game";
+import { OrOptions } from "../../src/inputs/OrOptions";
 
 describe("Sabotage", function () {
+    let card : Sabotage, player : Player, player2: Player, game: Game;
+
+    beforeEach(function() {
+        card = new Sabotage();
+        player = new Player("test", Color.BLUE, false);
+        player2 = new Player("test2", Color.RED, false);
+        game = new Game("foobar", [player, player2], player);
+    });
+
     it("Should play", function () {
-        const card = new Sabotage();
-        const player = new Player("test", Color.BLUE, false);
-        const game = new Game("foobar", [player,player], player);
-        const action = card.play(player, game);
-        player.titanium = 3;
-        player.steel = 4;
-        player.megaCredits = 7;
-        if (action !== undefined) {
-            action.options[0].cb(player);
-            expect(player.titanium).to.eq(0);
-        }
+        player2.titanium = 3;
+        player2.steel = 4;
+        player2.megaCredits = 7;
+
+        const action = card.play(player, game) as OrOptions;
+        
+        action.options[0].cb();
+        expect(player2.titanium).to.eq(0);
+
+        action.options[1].cb();
+        expect(player2.steel).to.eq(0);
+
+        action.options[2].cb();
+        expect(player2.megaCredits).to.eq(0);
     });
 });
--- tests/cards/promo/MonsInsurance.spec.ts
+++ tests/cards/promo/MonsInsurance.spec.ts
@@ -38,7 +38,7 @@ describe("MonsInsurance", function () {
         const card2 = new Sabotage();
         const action = card2.play!(player3, game) as OrOptions;
         
-        action.options[0].cb(player2);
+        action.options[1].cb();
         expect(player2.titanium).to.eq(0);
         expect(player2.megaCredits).to.eq(2);
         expect(player.megaCredits).to.eq(0);

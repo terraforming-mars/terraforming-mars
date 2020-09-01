8a895de7de63845c31ce1a4c3506b30d7ced1fa6
Danimir <dnbszbg@gmail.com>
2020-09-01 19:41:53 +0300

    Added tr, vp and cards

--- src/components/overview/PlayerTags.ts
+++ src/components/overview/PlayerTags.ts
@@ -6,9 +6,29 @@ export const PlayerTags = Vue.component("player-tags", {
     components: {
         "tag-count": TagCount,
     },
+    methods: {
+        getCardCount: function (): number {
+            if (this.player.cardsInHandNbr) {
+                return this.player.cardsInHandNbr;
+            }
+            return 0;
+        },
+        getTR: function (): number {
+            return this.player.terraformRating;
+        },
+        getVpCount: function (): number {
+            return this.player.victoryPointsBreakdown.total;
+        },
+        showVpCount: function (): boolean {
+            return this.player.showOtherPlayersVP;
+        },
+    },
     template: `
-        <div class="player-tags"> 
-            <tag-count v-for="tag in player.tags" :tag="tag.tag" :count="tag.count" :size="'big'"/>
+        <div class="player-tags">
+            <tag-count :tag="'tr'" :count="getTR()" :size="'big'" />
+            <tag-count v-if="showVpCount()" :tag="'vp'" :count="getVpCount()" :size="'big'" />
+            <tag-count :tag="'cards'" :count="getCardCount()" :size="'big'" />
+            <tag-count v-for="tag in player.tags" :tag="tag.tag" :count="tag.count" :size="'big'" />
         </div>
     `,
 });

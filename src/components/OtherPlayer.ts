2a441618a3f1985702289eb0e02c7668947ce2b9
Danimir <dnbszbg@gmail.com>
2020-09-15 10:14:36 +0300

    Removed unneccessary styles and elements in OtherPlayer.ts

--- src/components/OtherPlayer.ts
+++ src/components/OtherPlayer.ts
@@ -29,31 +29,6 @@ export const OtherPlayer = Vue.component("other-player", {
         <div> 
             <div v-show="isVisible()" class="other_player_cont menu">
                 <button class="btn btn-lg btn-error other_player_close" v-on:click="hideMe()"><i class="icon icon-cross"></i></button> 
-                <div style="display:none" class="player_home_block">
-                    <span class="player_name" :class="'player_bg_color_' + player.color"> {{ player.name }} : {{player.cardsInHandNbr}} cards in hand </span>
-                </div>
-
-                <div style="display:none" class="tag-display tags_item_cont" :class="player.tags.length > 0 ? 'tag-display-vp': ''">
-                    <div class="tag-display tags_item_cont tag-display-tags" v-if="player.tags.length > 0">
-                        <div v-for="tag in player.tags">
-                            <tag-count v-if="tag.count > 0" :tag="tag.tag" :count="tag.count"> </tag-count>
-                        </div>
-                    </div>
-                </div>
-
-                <div style="display:none" v-if="player.showOtherPlayersVP" class="tag-display tags_item_cont" :class="player.tags.length > 0 ? 'tag-display-vp': ''">
-                    <div>
-                        <div class="tag-display">
-                            <div class="tag-count icon-vp">{{player.victoryPointsBreakdown.total}}</div>
-                        </div>
-                    </div>
-                </div>
-                <div v-else class="nofloat"></div>
-
-                <div style="display:none" class="player_home_block">
-                    <player-resources :player="player"></player-resources>
-                </div>
-
                 <div v-if="player.playedCards.length > 0 || player.corporationCard !== undefined" class="player_home_block">
                     <span class="player_name" :class="'player_bg_color_' + player.color"> {{ player.name }} played cards </span>
                     <div>

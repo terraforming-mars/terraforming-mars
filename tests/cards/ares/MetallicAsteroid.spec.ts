25446cb89715af81ed0f8dcaa13d8b182e2bf464
Robert Konigsberg <konigsberg@gmail.com>
2020-09-19 18:58:36 -0400

    Completing tests for MetallicAsteroid.

--- tests/cards/ares/MetallicAsteroid.spec.ts
+++ tests/cards/ares/MetallicAsteroid.spec.ts
@@ -8,21 +8,29 @@ import { SpaceBonus } from "../../../src/SpaceBonus";
 import { TileType } from "../../../src/TileType";
 
 describe("MetallicAsteroid", function () {
-    let card: MetallicAsteroid, player: Player, game: Game;
+    let card: MetallicAsteroid, player: Player, otherPlayer: Player, game: Game;
 
     beforeEach(function () {
         card = new MetallicAsteroid();
         player = new Player("test", Color.BLUE, false);
-        game = new Game("foobar", [player, player], player);
+        otherPlayer = new Player("other", Color.RED, false);
+        game = new Game("foobar", [player, otherPlayer], player);
     });
 
     it("Play", function () {
+        otherPlayer.plants = 5;
+
+        expect(player.titanium).eq(0);
         expect(game.getTemperature()).eq(-30);
+        expect(game.interrupts).is.length(1);
+
         const action = card.play(player, game);
-        expect(game.getTemperature()).eq(-28);
         expect(player.titanium).eq(1);
-        // TODO(kberg): add test for
-        // game.addResourceDecreaseInterrupt(player, Resources.PLANTS, 4);
+        expect(game.getTemperature()).eq(-28);
+        // This interrupt is for removing four plants. Not going to do further
+        // testing on this because it's beyond the scope of this test without
+        // exposing more from the source method.
+        expect(game.interrupts).is.length(1);
 
         const citySpace = game.board.getAvailableSpacesForCity(player)[0];
         action.cb(citySpace);

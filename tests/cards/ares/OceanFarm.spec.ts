d2df04ba1494b4af4087c968cc59ee121fb0edfc
Robert Konigsberg <konigsberg@gmail.com>
2020-09-22 22:31:37 -0400

    Complete tests for OceanFarm

--- tests/cards/ares/OceanFarm.spec.ts
+++ tests/cards/ares/OceanFarm.spec.ts
@@ -4,6 +4,9 @@ import { Player } from "../../../src/Player";
 import { OceanFarm } from "../../../src/cards/ares/OceanFarm";
 import { AresTestHelper, ARES_GAME_OPTIONS } from "../../ares/AresTestHelper";
 import { expect } from "chai";
+import { TileType } from "../../../src/TileType";
+import { SpaceBonus } from "../../../src/SpaceBonus";
+import { Resources } from "../../../src/Resources";
 
 describe("OceanFarm", function () {
   let card : OceanFarm, player : Player, game : Game;
@@ -12,6 +15,8 @@ describe("OceanFarm", function () {
     card = new OceanFarm();
     player = new Player("test", Color.BLUE, false);
     game = new Game("foobar", [player, player], player, ARES_GAME_OPTIONS);
+    // Clear out spaces so they don't cost anything.
+    game.board.spaces.forEach(space => {space.adjacency = { bonus: [], cost: 0 }});
   });
 
   it("Can play", function () {
@@ -28,8 +33,21 @@ describe("OceanFarm", function () {
     expect(card.canPlay(player, game)).is.true;
   });
 
-});
+  it("Play", function () {
+    expect(player.getProduction(Resources.HEAT)).eq(0);
+    expect(player.getProduction(Resources.PLANTS)).eq(0);
 
-// Increase your heat production 1 step and increase your
-// plant production 1 step.<br>Place this tile on top of an existing ocean tile.
-// The tile grants an <b>adjacency bonus</b> of 1 plant.)
+    const oceanSpace = AresTestHelper.addOcean(game, player);
+    const action = card.play(player, game);
+
+    expect(player.getProduction(Resources.HEAT)).eq(1);
+    expect(player.getProduction(Resources.PLANTS)).eq(1);
+
+    action.cb(oceanSpace);
+
+    expect(oceanSpace.player).to.eq(player);
+    expect(oceanSpace.tile!.tileType).to.eq(TileType.OCEAN_FARM);
+    expect(oceanSpace.adjacency).to.deep.eq({ bonus: [SpaceBonus.PLANT] });
+  });
+
+});

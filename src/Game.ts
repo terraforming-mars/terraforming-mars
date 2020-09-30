f0f4c41ba030b6eba51d74003a1513de66a1968d
Brian Folts <brian.folts@gmail.com>
2020-09-30 09:42:58 -0600

    refactor: show all log entries

--- src/Game.ts
+++ src/Game.ts
@@ -1703,9 +1703,6 @@ export class Game implements ILoadable<SerializedGame, Game> {
       }
       this.gameLog.push(builder.logMessage());
       this.gameAge++;
-      if (this.gameLog.length > 50 ) {
-        (this.gameLog.shift());
-      }
     }
 
     public someoneHasResourceProduction(resource: Resources, minQuantity: number = 1): boolean {

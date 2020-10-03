7e7b14bc83da911b2c7eb6af02eea80e845a838f
Robert Konigsberg <konigsberg@gmail.com>
2020-10-02 23:08:10 -0400

    Use number for Multiset

--- src/utils/Multiset.ts
+++ src/utils/Multiset.ts
@@ -1,5 +1,5 @@
 export class Multiset<T> {
-    private map: Map<T, Number> = new Map();
+    private map: Map<T, number> = new Map();
 
     constructor(items: Array<T> = []) {
         items.forEach((item: T) => this.add(item));
@@ -11,16 +11,13 @@ export class Multiset<T> {
     }
 
     get(key: T): number | undefined {
-        if (this.map.has(key)) {
-            return this.map.get(key)!.valueOf();
-        }
-        return undefined;
+        return this.map.get(key);
     }
 
     entries(): Array<[T, number]> {
         var results: Array<[T, number]> = [];
-        this.map.forEach((count: Number, key: T) => {
-            results.push([key, count.valueOf()]);
+        this.map.forEach((count: number, key: T) => {
+            results.push([key, count]);
         });
         return results;
     }

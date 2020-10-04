a7ef9701d68493bbeb459c7d3bdee37a46711731
Robert Konigsberg <konigsberg@gmail.com>
2020-10-04 01:35:10 -0400

    Add ButterflyEffect test.

--- tests/cards/ares/ButterflyEffect.spec.ts
+++ tests/cards/ares/ButterflyEffect.spec.ts
@@ -2,6 +2,9 @@ import { Color } from "../../../src/Color";
 import { Game } from "../../../src/Game";
 import { Player } from "../../../src/Player";
 import { ButterflyEffect } from "../../../src/cards/ares/ButterflyEffect";
+import { expect } from "chai";
+import { ShiftAresGlobalParametersInterrupt } from "../../../src/interrupts/ShiftAresGlobalParametersInterrupt";
+import { ARES_OPTIONS_WITH_HAZARDS } from "../../ares/AresTestHelper";
 
 describe("ButterflyEffect", function () {
     let card: ButterflyEffect, player: Player, game: Game;
@@ -9,11 +12,36 @@ describe("ButterflyEffect", function () {
     beforeEach(function () {
         card = new ButterflyEffect();
         player = new Player("test", Color.BLUE, false);
-        game = new Game("foobar", [player, player], player);
+        game = new Game("foobar", [player, player], player, ARES_OPTIONS_WITH_HAZARDS);
     });
 
-    // TODO(kberg): implement
-    it("Placeholder test", function () {
+    it("play", function () {
+        const priorTerraformingRating = player.getTerraformRating();
         card.play(player, game);
+        expect(player.getTerraformRating()).eq(priorTerraformingRating + 1);
+        const interrupt = game.interrupts[0];
+        expect(interrupt).instanceOf(ShiftAresGlobalParametersInterrupt);
+
+        const originalHazardData = game.aresData!.hazardData;
+        expect(originalHazardData.erosionOceanCount.threshold).eq(3);
+        expect(originalHazardData.removeDustStormsOceanCount.threshold).eq(6);
+        expect(originalHazardData.severeErosionTemperature.threshold).eq(-4);
+        expect(originalHazardData.severeDustStormOxygen.threshold).eq(5);
+
+        (interrupt as ShiftAresGlobalParametersInterrupt).playerInput.cb(
+            {
+                lowOceanDelta: -1,
+                highOceanDelta: 1,
+                temperatureDelta:  -1,
+                oxygenDelta:  1
+            }
+        );
+
+        const revisedHazardData = game.aresData!.hazardData;
+        expect(revisedHazardData.erosionOceanCount.threshold).eq(2);
+        expect(revisedHazardData.removeDustStormsOceanCount.threshold).eq(7);
+        expect(revisedHazardData.severeErosionTemperature.threshold).eq(-6);
+        expect(revisedHazardData.severeDustStormOxygen.threshold).eq(6);
+
     });
 });

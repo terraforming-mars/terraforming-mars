9c7f4098d28012b36c11c62884586add910283e2
Brian Folts <brian.folts@gmail.com>
2020-10-04 15:39:27 -0700

    refactor: include git log information for version (#1476)

--- server.ts
+++ server.ts
@@ -5,6 +5,8 @@ import * as http from "http";
 import * as fs from "fs";
 import * as path from "path";
 import * as querystring from "querystring";
+import * as child_process from "child_process";
+
 import { AndOptions } from "./src/inputs/AndOptions";
 import { CardModel } from "./src/models/CardModel";
 import { ColonyModel } from "./src/models/ColonyModel";
@@ -48,6 +50,7 @@ const serverId = generateRandomServerId();
 const styles = fs.readFileSync("styles.css");
 const games: Map<string, Game> = new Map<string, Game>();
 const playersToGame: Map<string, Game> = new Map<string, Game>();
+const appVersion = generateAppVersion();
 
 function processRequest(req: http.IncomingMessage, res: http.ServerResponse): void {
     if (req.url !== undefined) {
@@ -84,7 +87,8 @@ function processRequest(req: http.IncomingMessage, res: http.ServerResponse): vo
             } else if (
                 req.url.startsWith("/assets/") ||
                 req.url === "/favicon.ico" ||
-                req.url === "/main.js"
+                req.url === "/main.js" ||
+                req.url === "/main.js.map"
             ) {
                 serveAsset(req, res);
             } else if (req.url.startsWith("/api/games")) {
@@ -169,6 +173,15 @@ function generateRandomGameId(): string {
 
 function generateRandomServerId(): string {
     return generateRandomGameId();
+}
+
+function generateAppVersion(): string {
+    try {
+        return child_process.execSync(`git log -1 --pretty=format:"%h %cD"`).toString();
+    } catch (error) {
+        console.warn("unable to generate app version", error);
+        return "unknown version";
+    }
 }
 
 function processInput(
@@ -1010,24 +1023,31 @@ function isServerIdValid(req: http.IncomingMessage): boolean {
 }
 
 function serveApp(res: http.ServerResponse): void {
-    res.setHeader("Content-Type", "text/html; charset=utf-8");
-    res.write(fs.readFileSync("index.html"));
-    res.end();
+    fs.readFile("index.html", function (err, data) {
+        if (err) {
+            return internalServerError(res, err);
+        }
+        res.setHeader("Content-Type", "text/html; charset=utf-8");
+        res.write(data.toString().replace("$$APP_VERSION$$", appVersion));
+        res.end();
+    });
 }
 
 function serveAsset(req: http.IncomingMessage, res: http.ServerResponse): void {
     if (req.url === undefined) throw new Error("Empty url");
 
+    let file: string | undefined;
+
     if (req.url === "/favicon.ico") {
         res.setHeader("Content-Type", "image/x-icon");
-        res.write(fs.readFileSync("favicon.ico"));
-    } else if (req.url === "/main.js") {
+        file = "favicon.ico";
+    } else if (req.url === "/main.js" || req.url === "/main.js.map") {
         res.setHeader("Content-Type", "text/javascript");
-        res.write(fs.readFileSync("dist/main.js"));
+        file = "dist" + req.url;
     } else if (req.url === "/assets/Prototype.ttf") {
-        res.write(fs.readFileSync("assets/Prototype.ttf"));
+        file = "assets/Prototype.ttf";
     } else if (req.url === "/assets/futureforces.ttf") {
-        res.write(fs.readFileSync("assets/futureforces.ttf"));
+        file = "assets/futureforces.ttf";
     } else if (req.url.endsWith(".png")) {
         const assetsRoot = path.resolve("./assets");
         const reqFile = path.resolve(path.normalize(req.url).slice(1));
@@ -1037,7 +1057,7 @@ function serveAsset(req: http.IncomingMessage, res: http.ServerResponse): void {
             return notFound(req, res);
         }
         res.setHeader("Content-Type", "image/png");
-        res.write(fs.readFileSync(reqFile));
+        file = reqFile;
     } else if (req.url.endsWith(".jpg")) {
         const assetsRoot = path.resolve("./assets");
         const reqFile = path.resolve(path.normalize(req.url).slice(1));
@@ -1047,10 +1067,17 @@ function serveAsset(req: http.IncomingMessage, res: http.ServerResponse): void {
             return notFound(req, res);
         }
         res.setHeader("Content-Type", "image/jpeg");
-        res.write(fs.readFileSync(reqFile));
+        file = reqFile;
+    } else {
+        return notFound(req, res);
     }
-
-    res.end();
+    fs.readFile(file, function (err, data) {
+        if (err) {
+            return internalServerError(res, err);
+        }
+        res.write(data);
+        res.end();
+    });
 }
 
 function serveResource(res: http.ServerResponse, s: Buffer): void {

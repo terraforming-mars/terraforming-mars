227bffad19cc365953c051dd3b899976e683a5d9
vsrisuknimit <14239220+vsrisuknimit@users.noreply.github.com>
2020-10-06 09:49:54 -0400

    remove the ocean debug code

--- src/components/Board.ts
+++ src/components/Board.ts
@@ -111,7 +111,7 @@ export const Board = Vue.component("board", {
         oceansValue: function() {
             const oceans_count = this.oceans_count || 0;
             const leftover = constants.MAX_OCEAN_TILES - oceans_count;
-            if (leftover === 8) {
+            if (leftover === 0) {
                 return `<img width="26" src="/assets/misc/circle-checkmark.png" class="board-ocean-checkmark" :alt="$t('Completed!')">`
             } else {
                 return `${oceans_count}/${constants.MAX_OCEAN_TILES}`

da6237d60bc78649b73f1ec0badfe9b3e1ab0c6e
waichuan <nwai1990@hotmail.com>
2020-10-06 12:47:36 +0800

    Add more cards to debug page

--- src/components/DebugUI.ts
+++ src/components/DebugUI.ts
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import { Card } from "./Card";
-import { ALL_PRELUDE_PROJECTS_CARDS, ALL_VENUS_PROJECTS_CARDS, ALL_COLONIES_PROJECTS_CARDS, ALL_TURMOIL_PROJECTS_CARDS, ALL_PROMO_PROJECTS_CARDS, ALL_PROJECT_CARDS, ALL_CORP_ERA_PROJECT_CARDS } from '../Dealer';
+import { ALL_PRELUDE_PROJECTS_CARDS, ALL_VENUS_PROJECTS_CARDS, ALL_COLONIES_PROJECTS_CARDS, ALL_TURMOIL_PROJECTS_CARDS, ALL_PROMO_PROJECTS_CARDS, ALL_PROJECT_CARDS, ALL_CORP_ERA_PROJECT_CARDS, ALL_CORPORATION_CARDS, ALL_CORP_ERA_CORPORATION_CARDS, ALL_VENUS_CORPORATIONS, ALL_PRELUDE_CORPORATIONS, ALL_COLONIES_CORPORATIONS, ALL_TURMOIL_CORPORATIONS, ALL_PROMO_CORPORATIONS, ALL_COMMUNITY_CORPORATIONS, ALL_PRELUDE_CARDS } from '../Dealer';
 import { CardName } from "../CardName";
 
 export const DebugUI = Vue.component("debug-ui", {
@@ -16,7 +16,16 @@ export const DebugUI = Vue.component("debug-ui", {
                 ...ALL_TURMOIL_PROJECTS_CARDS.map((cf) => cf.cardName),
                 ...ALL_PROMO_PROJECTS_CARDS.map((cf) => cf.cardName),
                 ...ALL_PROJECT_CARDS.map((cf) => cf.cardName),
-                ...ALL_CORP_ERA_PROJECT_CARDS.map((cf) => cf.cardName)
+                ...ALL_CORP_ERA_PROJECT_CARDS.map((cf) => cf.cardName),
+                ...ALL_CORPORATION_CARDS.map((cf) => cf.cardName),
+                ...ALL_CORP_ERA_CORPORATION_CARDS.map((cf) => cf.cardName),
+                ...ALL_VENUS_CORPORATIONS.map((cf) => cf.cardName),
+                ...ALL_PRELUDE_CORPORATIONS.map((cf) => cf.cardName),
+                ...ALL_COLONIES_CORPORATIONS.map((cf) => cf.cardName),
+                ...ALL_TURMOIL_CORPORATIONS.map((cf) => cf.cardName),
+                ...ALL_PROMO_CORPORATIONS.map((cf) => cf.cardName),
+                ...ALL_COMMUNITY_CORPORATIONS.map((cf) => cf.cardName),
+                ...ALL_PRELUDE_CARDS.map((cf) => cf.cardName),
             ].sort();
             return allItems;
         }

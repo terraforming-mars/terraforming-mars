1cb4926a407141fd7b1ea1f7d32e60605dd35689
Brian Folts <brian.folts@gmail.com>
2020-10-06 07:50:04 -0600

    refactor: show all log entries (#1428)
    * refactor: show all log entries
    
    * refactor: provide log route and pull logs from route
    
    * chore: fully merge with master
    
    * refactor: transition to new route with ease

--- src/components/PlayerHome.ts
+++ src/components/PlayerHome.ts
@@ -139,13 +139,13 @@ export const PlayerHome = Vue.component("player-home", {
 
                 <players-overview class="player_home_block player_home_block--players nofloat:" :player="player" v-trim-whitespace />
 
-                <div class="player_home_block player_home_block--log player_home_block--hide_log nofloat" v-if="player.gameLog.length > 0">
+                <div class="player_home_block player_home_block--log player_home_block--hide_log nofloat">
                     <dynamic-title v-if="player.players.length > 1" title="Game log" :color="player.color" :withAdditional="true" :additional="'generation' + player.generation" />
                     <h2 v-else :class="'player_color_'+ player.color">
                         <span v-i18n>Game log</span>
                         <span class="label-additional" v-html="getGenerationText()"></span>
                     </h2>
-                    <log-panel :messages="player.gameLog" :players="player.players"></log-panel>
+                    <log-panel :id="player.id" :players="player.players"></log-panel>
                 </div>
 
                 <div class="player_home_block player_home_block--actions nofloat">

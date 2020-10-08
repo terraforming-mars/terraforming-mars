e057e317abcffa7b517f286cdc8cd52ee2076e2b
Lynesth <lyn@nautile.nc>
2020-10-09 02:32:46 +1100

    Fix resource decrease interrupts

--- src/interrupts/SelectResourceDecrease.ts
+++ src/interrupts/SelectResourceDecrease.ts
@@ -1,9 +1,11 @@
 
 import { Game } from "../Game";
+import { OrOptions } from "../inputs/OrOptions";
 import { PlayerInput } from "../PlayerInput";
 import { Player } from "../Player";
 import { PlayerInterrupt } from "./PlayerInterrupt";
 import { Resources } from "../Resources";
+import { SelectOption } from "../inputs/SelectOption"
 import { SelectPlayer } from "../inputs/SelectPlayer";
 
 export class SelectResourceDecrease implements PlayerInterrupt {
@@ -26,4 +28,58 @@ export class SelectResourceDecrease implements PlayerInterrupt {
           }
         );
     };
-}    
+
+    public beforeAction(): void {
+		if (this.resource === Resources.PLANTS) {
+			this.candidates = this.game.getPlayers().filter((p) => p.id !== this.player.id && !p.plantsAreProtected() && p.getResource(this.resource) > 0);
+		} else {
+			this.candidates = this.game.getPlayers().filter((p) => p.id !== this.player.id && p.getResource(this.resource) > 0);
+		}
+
+		if (this.candidates.length === 0) {
+			return;
+		} else if (this.candidates.length === 1) {
+			const qtyToRemove = Math.min(this.candidates[0].plants, this.count);
+			if (this.resource === Resources.PLANTS) {
+				this.playerInput = new OrOptions(
+					new SelectOption("Remove " + qtyToRemove + " plants from " + this.candidates[0].name, "Remove plants", () => {
+						this.candidates[0].setResource(this.resource, -qtyToRemove, this.game, this.player);
+						return undefined;
+					}),
+					new SelectOption("Skip removing plants", "Confirm", () => {
+						return undefined;
+					})
+				);
+			} else {
+				this.candidates[0].setResource(this.resource, -qtyToRemove, this.game, this.player);
+				return;
+			}
+		} else {
+			if (this.resource === Resources.PLANTS) {
+				const removalOptions = this.candidates.map((candidate) => {
+					const qtyToRemove = Math.min(candidate.plants, this.count);
+					return new SelectOption("Remove " + qtyToRemove + " plants from " + candidate.name, "Remove plants", () => {
+						candidate.setResource(this.resource, -qtyToRemove, this.game, this.player);
+						return undefined;
+					})
+				});
+				this.playerInput = new OrOptions(
+					...removalOptions,
+					new SelectOption("Skip removing plants", "Confirm", () => {
+						return undefined;
+					})
+				);
+			} else {
+				this.playerInput = new SelectPlayer(
+					this.candidates,
+					this.title,
+					"Remove",
+					(found: Player) => {
+						found.setResource(this.resource, -this.count, this.game, this.player);
+						return undefined;
+					}
+				);
+			}
+		}
+    }
+}

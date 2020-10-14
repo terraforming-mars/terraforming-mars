769bb1d3df3e92449f0cde8b2331dff663df16fe
Lynesth <lyn@nautile.nc>
2020-10-15 00:32:10 +1100

    Added comment to explain undefined resourceType

--- src/Player.ts
+++ src/Player.ts
@@ -1103,7 +1103,7 @@ export class Player implements ILoadable<SerializedPlayer, Player>{
             cards.push(
               {
                 resources: targetCard.resourceCount,
-                resourceType: undefined,
+                resourceType: undefined, // Card on SRR cannot gather its own resources (if any)
                 name: targetCard.card.name,
                 calculatedCost: this.getCardCost(game, targetCard.card),
                 cardType: card.cardType,

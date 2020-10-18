2223da164f85090ce88986f77a06c82af70360a5
Brian Folts <brian.folts@gmail.com>
2020-10-18 13:23:05 -0600

    consolidate the mining cards

--- src/cards/MiningRights.ts
+++ src/cards/MiningRights.ts
@@ -1,52 +1,7 @@
-import { IProjectCard } from "./IProjectCard";
-import { Tags } from "./Tags";
-import { CardType } from "./CardType";
-import { Player } from "../Player";
-import { Game } from "../Game";
-import { SelectSpace } from "../inputs/SelectSpace";
-import { SpaceBonus } from "../SpaceBonus";
-import { TileType } from "../TileType";
-import { ISpace } from "../ISpace";
-import { Resources } from "../Resources";
 import { CardName } from "../CardName";
-import { LogHelper } from "../components/LogHelper";
+import { MiningCard } from "./MiningCard";
 
-export class MiningRights implements IProjectCard {
+export class MiningRights extends MiningCard {
     public cost: number = 9;
-    public tags: Array<Tags> = [Tags.STEEL];
-    public name: CardName = CardName.MINING_RIGHTS;
-    public cardType: CardType = CardType.AUTOMATED;
-    public hasRequirements = false;
-    public bonusResource: Resources | undefined = undefined;
-
-    private getAvailableSpaces(player: Player, game: Game): Array<ISpace> {
-        return game.board.getAvailableSpacesOnLand(player)
-                .filter((space) => space.bonus.indexOf(SpaceBonus.STEEL) !== -1 || space.bonus.indexOf(SpaceBonus.TITANIUM) !== -1);
-    }
-    public canPlay(player: Player, game: Game): boolean {
-        return this.getAvailableSpaces(player, game).length > 0
-    }
-    public play(player: Player, game: Game) {
-        return new SelectSpace("Select space with a steel or titanium placement bonus", this.getAvailableSpaces(player, game), (foundSpace: ISpace) => {
-            const bonus = foundSpace.bonus;
-            game.addTile(player, foundSpace.spaceType, foundSpace, { tileType: TileType.MINING_RIGHTS });
-            if (bonus.indexOf(SpaceBonus.STEEL) !== -1) {
-                player.addProduction(Resources.STEEL);
-                this.bonusResource = Resources.STEEL;
-                this.setAdjacencyBonus(foundSpace, SpaceBonus.STEEL);
-                LogHelper.logGainProduction(game, player, Resources.STEEL);
-            } else if (bonus.indexOf(SpaceBonus.TITANIUM) !== -1) {
-                player.addProduction(Resources.TITANIUM);
-                this.bonusResource = Resources.TITANIUM;
-                this.setAdjacencyBonus(foundSpace, SpaceBonus.TITANIUM);
-                LogHelper.logGainProduction(game, player, Resources.TITANIUM);
-            }
-            return undefined;
-        });
-    }
-
-    // This is not the standard way adjacency bonuses are handled, but this is a special
-    // case where the bonus type depends on this subclass. Same goes for Mining Area.
-    protected setAdjacencyBonus(_space: ISpace, _bonusType: SpaceBonus) {
-    }
+    public name = CardName.MINING_RIGHTS;
 }
--- src/cards/ares/MiningRightsAres.ts
+++ src/cards/ares/MiningRightsAres.ts
@@ -1,19 +1,6 @@
 import { CardName } from "../../CardName";
-import { ISpace } from "../../ISpace";
-import { SpaceBonus } from "../../SpaceBonus";
 import { MiningRights } from "../MiningRights";
-import { AresMiningCards } from "./AresMiningCards";
 
 export class MiningRightsAres extends MiningRights {
-    public name: CardName = CardName.MINING_RIGHTS_ARES;
-
-    protected setAdjacencyBonus(space: ISpace, bonusType: SpaceBonus.STEEL | SpaceBonus.TITANIUM) {
-        space.adjacency = { bonus: [bonusType] };
-        // TODO(kberg): this pattern will repeat. Move something to AresHandler.
-        if (space.tile) {
-            AresMiningCards.setTileType(space.tile, bonusType);
-        } else {
-            console.log("Assertion failure: " + space.id + " is expected to have a tile.")
-        }
-    }
+    public name = CardName.MINING_RIGHTS_ARES;
 }

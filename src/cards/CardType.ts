eee6f1e241b884740bdad5535461c4d8cf0ccbf4
Robert Konigsberg <kberg@users.noreply.github.com>
2020-10-20 17:28:14 -0400

    Update CardType.ts

--- src/cards/CardType.ts
+++ src/cards/CardType.ts
@@ -1,9 +1,10 @@
-
+ee
 export enum CardType {
     EVENT = "red",
     ACTIVE = "blue",
     AUTOMATED = "green",
     PRELUDE = "pink",
     CORPORATION = "brown",
-    PROXY = "proxy"
+    // Proxy cards are not real cards, but for operations that need a card-like behavior.
+    PROXY = "proxy",
 }

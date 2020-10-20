f021f948d416379c9d77a19796b1504c7ccfe99f
vsrisuknimit <14239220+vsrisuknimit@users.noreply.github.com>
2020-10-20 08:29:29 -0400

    experimental forest: draw first

--- src/cards/prelude/ExperimentalForest.ts
+++ src/cards/prelude/ExperimentalForest.ts
@@ -11,15 +11,16 @@ export class ExperimentalForest extends PreludeCard implements IProjectCard {
     public tags: Array<Tags> = [Tags.PLANT];
     public name: CardName = CardName.EXPERIMENTAL_FOREST
     public play(player: Player, game: Game) {
+
+        // Draw two plant tag cards first
+        for (const foundCard of game.drawCardsByTag(Tags.PLANT, 2)) {
+            player.cardsInHand.push(foundCard);
+        }
+        const drawnCards = game.getCardsInHandByTag(player, Tags.PLANT).slice(-2);
+        game.log("${0} drew ${1} and ${2}", b => b.player(player).card(drawnCards[0]).card(drawnCards[1]));
+
+        // Then place a greenery
         return new SelectSpace("Select space for greenery tile", game.board.getAvailableSpacesForGreenery(player), (space: ISpace) => {
-	        for (let foundCard of game.drawCardsByTag(Tags.PLANT, 2)) {
-                player.cardsInHand.push(foundCard);
-            }
-
-            const drawnCards = game.getCardsInHandByTag(player, Tags.PLANT).slice(-2);
-
-            game.log("${0} drew ${1} and ${2}", b => b.player(player).card(drawnCards[0]).card(drawnCards[1]));
-
             return game.addGreenery(player, space.id);
         });
     }

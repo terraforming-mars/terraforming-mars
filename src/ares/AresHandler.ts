8a6718319c36cfe3fde4869ea46d19da6ef98ac6
Robert Konigsberg <konigsberg@gmail.com>
2020-10-21 22:22:29 -0400

    More ! removal!

--- src/ares/AresHandler.ts
+++ src/ares/AresHandler.ts
@@ -60,7 +60,8 @@ export class AresHandler {
         if (adjacentSpace.adjacency === undefined || adjacentSpace.adjacency.bonus.length === 0) {
             return false;
         }
-        if (!adjacentSpace.player) {
+        const adjacentPlayer = adjacentSpace.player;
+        if (adjacentPlayer === undefined) {
             throw new Error(`A tile with an adjacency bonus must have an owner (${adjacentSpace.x}, ${adjacentSpace.y}, ${adjacentSpace.adjacency.bonus}`);
         }
 
@@ -118,12 +119,12 @@ export class AresHandler {
         game.log("${0} gains ${1} for placing next to ${2}", b => b.player(player).string(bonusText).string(tileText));
 
         let ownerBonus = 1;
-        if (adjacentSpace.player.cardIsInEffect(CardName.MARKETING_EXPERTS)) {
+        if (adjacentPlayer.cardIsInEffect(CardName.MARKETING_EXPERTS)) {
             ownerBonus = 2;
         };
         
-        adjacentSpace.player.megaCredits += ownerBonus;
-        game.log("${0} gains ${1} M€ for a tile placed next to ${2}", b => b.player(adjacentSpace.player!).number(ownerBonus).string(tileText));
+        adjacentPlayer.megaCredits += ownerBonus;
+        game.log("${0} gains ${1} M€ for a tile placed next to ${2}", b => b.player(adjacentPlayer).number(ownerBonus).string(tileText));
 
         return true;
     }

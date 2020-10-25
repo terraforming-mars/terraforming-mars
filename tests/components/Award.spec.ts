e2fe8e4c24e11008ad36fe09ef04326a6f80fee5
Brian Folts <brian.folts@gmail.com>
2020-10-25 08:25:19 -0700

    vue type checking and unit tests for Milestone and Award component (#1746)

---
+++ tests/components/Award.spec.ts
@@ -0,0 +1,53 @@
+
+import { setup } from "../utils/Vue";
+
+setup();
+
+import { expect } from "chai";
+import { Award } from "../../src/components/Award";
+import { FundedAwardModel } from "../../src/models/FundedAwardModel";
+
+describe("Award", function () {
+    const mockAward: FundedAwardModel = {
+        award: {
+            name: "test",
+            description: "a test",
+            getScore: () => 0
+        },
+        player_name: "foo",
+        player_color: "blue",
+        scores: []
+    };
+    it("getNameCss", function () {
+        expect((Award as any).methods.getNameCss("foo Bar foO")).to.eq("ma-name ma-name--foo-bar-foo");
+    });
+    it("sets up data", function () {
+        const awards_list: Array<FundedAwardModel> = [mockAward];
+        expect((Award as any).data.call({
+            awards_list
+        })).to.deep.eq({
+            showList: true,
+            showDescription: {
+                test: false
+            }
+        });
+    });
+    it("shouldShow", function () {
+        const scope = { showDescription: { test: true } };
+        expect((Award as any).methods.shouldShow.call(scope, mockAward)).to.eq(true);
+    });
+    it("shouldShowList", function () {
+        const scope = { showList: true };
+        expect((Award as any).methods.shouldShowList.call(scope)).to.eq(true);
+    });
+    it("toggle", function () {
+        const scope = { showDescription: { test: false } };
+        (Award as any).methods.toggle.call(scope, mockAward);
+        expect(scope.showDescription.test).to.eq(true);
+    });
+    it("toggleList", function () {
+        const scope = { showList: false };
+        (Award as any).methods.toggleList.call(scope);
+        expect(scope.showList).to.eq(true);
+    });
+});
---
+++ tests/components/Milestone.spec.ts
@@ -0,0 +1,54 @@
+
+import { setup } from "../utils/Vue";
+
+setup();
+
+import { expect } from "chai";
+import { Milestone } from "../../src/components/Milestone";
+import { ClaimedMilestoneModel } from "../../src/models/ClaimedMilestoneModel";
+
+describe("Milestone", function () {
+    const mockMilestone: ClaimedMilestoneModel = {
+        milestone: {
+            name: "test",
+            description: "a test",
+            canClaim: () => true,
+            getScore: () => 0
+        },
+        player_name: "foo",
+        player_color: "blue",
+        scores: []
+    };
+    it("getNameCss", function () {
+        expect((Milestone as any).methods.getNameCss("foo Bar foO")).to.eq("ma-name ma-name--foo-bar-foo");
+    });
+    it("sets up data", function () {
+        const milestones_list: Array<ClaimedMilestoneModel> = [mockMilestone];
+        expect((Milestone as any).data.call({
+            milestones_list
+        })).to.deep.eq({
+            showList: true,
+            showDescription: {
+                test: false
+            }
+        });
+    });
+    it("shouldShow", function () {
+        const scope = { showDescription: { test: true } };
+        expect((Milestone as any).methods.shouldShow.call(scope, mockMilestone)).to.eq(true);
+    });
+    it("shouldShowList", function () {
+        const scope = { showList: true };
+        expect((Milestone as any).methods.shouldShowList.call(scope)).to.eq(true);
+    });
+    it("toggle", function () {
+        const scope = { showDescription: { test: false } };
+        (Milestone as any).methods.toggle.call(scope, mockMilestone);
+        expect(scope.showDescription.test).to.eq(true);
+    });
+    it("toggleList", function () {
+        const scope = { showList: false };
+        (Milestone as any).methods.toggleList.call(scope);
+        expect(scope.showList).to.eq(true);
+    });
+});

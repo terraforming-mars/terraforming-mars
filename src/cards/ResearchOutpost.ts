0ac7207c9a910808a99bef352133bd6ed8248723
Lynesth <lyn@nautile.nc>
2020-11-04 01:20:49 +0000

    Fix research Output and Hazard interaction

--- src/cards/ResearchOutpost.ts
+++ src/cards/ResearchOutpost.ts
@@ -8,6 +8,7 @@ import { SelectSpace } from "../inputs/SelectSpace";
 import { ISpace } from "../ISpace";
 import { PlayerInput } from "../PlayerInput";
 import { CardName } from '../CardName';
+import { AresHandler, HazardSeverity } from "../ares/AresHandler";
 
 export class ResearchOutpost implements IProjectCard {
     public cost = 18;
@@ -15,11 +16,17 @@ export class ResearchOutpost implements IProjectCard {
     public name = CardName.RESEARCH_OUTPOST;
     public cardType = CardType.ACTIVE;
     public hasRequirements = false;
-    private getAvailableSpaces(player: Player, game: Game): Array<ISpace> {
+    private getAvailableSpaces(player: Player, game: Game, addCardCost: boolean = true): Array<ISpace> {
         return game.board.getAvailableSpacesOnLand(player)
                 .filter((space) => {
                     const adjacentSpaces = game.board.getAdjacentSpaces(space);
-                    return adjacentSpaces.filter((space) => space.tile !== undefined).length === 0;
+                    const spaceIsAvailable = adjacentSpaces.filter((space) => space.tile !== undefined).length === 0;
+                    if (game.gameOptions.aresExtension && AresHandler.hasHazardTile(space)) {
+                        const cost = (addCardCost ? this.cost : 0) + (AresHandler.hazardSeverity(space) === HazardSeverity.MILD ? 8 : 16);
+                        return spaceIsAvailable && player.canAfford(cost);
+                    } else {
+                        return spaceIsAvailable;
+                    }
                 });
     }
     public canPlay(player: Player, game: Game): boolean {
@@ -29,7 +36,7 @@ export class ResearchOutpost implements IProjectCard {
         return 1;
     }
     public play(player: Player, game: Game): PlayerInput {
-        return new SelectSpace("Select place next to no other tile for city", this.getAvailableSpaces(player, game), (foundSpace: ISpace) => {
+        return new SelectSpace("Select place next to no other tile for city", this.getAvailableSpaces(player, game, false), (foundSpace: ISpace) => {
             game.addCityTile(player, foundSpace.id);
             return undefined;
         });

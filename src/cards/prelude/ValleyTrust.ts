14420a7d8730ff58b513b4a5a7f0f71f73657411
waichuan <nwai1990@hotmail.com>
2020-11-05 12:22:32 +0800

    Fix: Don't allow Valley Trust to play preludes it cannot afford

--- src/cards/prelude/ValleyTrust.ts
+++ src/cards/prelude/ValleyTrust.ts
@@ -26,8 +26,13 @@ export class ValleyTrust implements CorporationCard {
                 game.dealer.dealPreludeCard(),
                 game.dealer.dealPreludeCard()
             ];
+
             return new SelectCard("Choose prelude card to play", "Play", cardsDrawn, (foundCards: Array<IProjectCard>) => {
-                return player.playCard(game, foundCards[0]);
+                if (foundCards[0].canPlay === undefined || foundCards[0].canPlay(player, game)) {
+                    return player.playCard(game, foundCards[0]);
+                } else {
+                    throw new Error("You cannot pay for this card");
+                }
             }, 1, 1);
         }
         else {

56714ed517478dd716640160f2be61548f4e1974
Robert Konigsberg <konigsberg@gmail.com>
2020-11-08 00:23:01 -0500

    Vestigial change to trigger rebuild.

--- tests/cards/turmoil/UtopiaInvest.spec.ts
+++ tests/cards/turmoil/UtopiaInvest.spec.ts
@@ -4,27 +4,20 @@ import { Color } from "../../../src/Color";
 import { Player } from "../../../src/Player";
 import { Resources } from "../../../src/Resources";
 import { OrOptions } from "../../../src/inputs/OrOptions";
-import { Game } from "../../../src/Game";
-import { setCustomGameOptions } from "../../TestingUtils";
 
 describe("UtopiaInvest", function () {
     it("Should play", function () {
         const card = new UtopiaInvest();
         const player = new Player("test", Color.BLUE, false);
-
-        const gameOptions = setCustomGameOptions();
-        const game = new Game("foobar", [player,player], player, gameOptions);  
-
         const play = card.play(player);
         expect(play).is.undefined;
         expect(player.getProduction(Resources.TITANIUM)).to.eq(1);
         expect(player.getProduction(Resources.STEEL)).to.eq(1);
-        const action = card.action(player, game);
+        const action = card.action(player);
         expect(action).is.not.undefined;
         expect(action instanceof OrOptions).is.true;
         action.options[2].cb();
         expect(player.titanium).to.eq(4);
         expect(player.getProduction(Resources.TITANIUM)).to.eq(0);
-
     });
 });

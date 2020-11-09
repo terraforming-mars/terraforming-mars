cdf8f9de2cf0c42fd9eddd3f90eb695327885550
Danimir <dnbszbg@gmail.com>
2020-11-09 14:07:35 +0200

    Added CardRenderer, CardRenderItem and CardRenderSymbol to renderData metadata functionality

---
+++ src/cards/render/CardRenderItem.ts
@@ -0,0 +1,37 @@
+import { CardRenderItemType } from "./CardRenderItemType";
+
+export class CardRenderItem {
+    private _anyPlayer: boolean = false;
+    private _showDigit: boolean = false;
+    constructor(protected _cardRenderItemType: CardRenderItemType, protected _amount: number = -1) {
+        if (Math.abs(this._amount) > 5) {
+            this._showDigit = true;
+        }
+    }
+
+    public get anyPlayer() {
+        return this._anyPlayer;
+    }
+
+    public get type() {
+        return this._cardRenderItemType;
+    }
+
+    public get amount() {
+        return this._amount;
+    }
+
+    public get showDigit() {
+        return this._showDigit;
+    }
+
+    public any(): CardRenderItem {
+        this._anyPlayer = true;
+        return this;
+    }
+
+    public digit(): CardRenderItem {
+        this._showDigit = true;
+        return this;
+    }
+}
---
+++ src/cards/render/CardRenderSymbol.ts
@@ -0,0 +1,55 @@
+import { CardRenderSymbolType } from "./CardRenderSymbolType";
+import { CardRenderItemSize } from "./CardRenderItemSize";
+
+export class CardRenderSymbol {
+    private constructor(
+        private type: CardRenderSymbolType,
+        private isIcon: boolean = false,
+        private size: CardRenderItemSize = CardRenderItemSize.MEDIUM
+    ) {}
+    public getType(): CardRenderSymbolType {
+        return this.type;
+    }
+    public getIsIcon(): boolean {
+        return this.isIcon;
+    }
+    public getSize(): CardRenderItemSize {
+        return this.size;
+    }
+    public static asterix(): CardRenderSymbol {
+        return new CardRenderSymbol(CardRenderSymbolType.ASTERIX);
+    }
+    public static or(): CardRenderSymbol {
+        return new CardRenderSymbol(CardRenderSymbolType.OR);
+    }
+    public static plus(): CardRenderSymbol {
+        return new CardRenderSymbol(CardRenderSymbolType.PLUS, true);
+    }
+    public static minus(): CardRenderSymbol {
+        return new CardRenderSymbol(CardRenderSymbolType.MINUS, true);
+    }
+    public static empty(): CardRenderSymbol {
+        return new CardRenderSymbol(CardRenderSymbolType.EMPTY);
+    }
+    public static slash(): CardRenderSymbol {
+        return new CardRenderSymbol(CardRenderSymbolType.SLASH);
+    }
+    public static colon(): CardRenderSymbol {
+        return new CardRenderSymbol(CardRenderSymbolType.COLON);
+    }
+    public static arrow(): CardRenderSymbol {
+        return new CardRenderSymbol(CardRenderSymbolType.ARROW, true);
+    }
+    public small(): CardRenderSymbol {
+        this.size = CardRenderItemSize.SMALL;
+        return this;
+    }
+    public medium(): CardRenderSymbol {
+        this.size = CardRenderItemSize.MEDIUM;
+        return this;
+    }
+    public large(): CardRenderSymbol {
+        this.size = CardRenderItemSize.LARGE;
+        return this;
+    }
+}

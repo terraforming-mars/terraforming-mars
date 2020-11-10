90e7fa5836757edeb95410c8e3b2e53c9750acab
bafolts <brian.folts@hbo.com>
2020-11-09 18:00:48 -0800

    unit tests and constants

--- src/cards/colonies/StormCraftIncorporated.ts
+++ src/cards/colonies/StormCraftIncorporated.ts
@@ -1,43 +1,83 @@
+import * as constants from '../../constants';
 import {CorporationCard} from '../corporation/CorporationCard';
+import {Game} from '../../Game';
 import {Player} from '../../Player';
 import {Tags} from '../Tags';
 import {ResourceType} from '../../ResourceType';
 import {ICard, IActionCard, IResourceCard} from '../ICard';
+import {AndOptions} from '../../inputs/AndOptions';
+import {SelectAmount} from '../../inputs/SelectAmount';
 import {SelectCard} from '../../inputs/SelectCard';
+import {SelectOption} from '../../inputs/SelectOption';
 import {CardName} from '../../CardName';
 import {CardType} from '../CardType';
 
 export class StormCraftIncorporated implements IActionCard, CorporationCard, IResourceCard {
-    public name = CardName.STORMCRAFT_INCORPORATED;
-    public tags = [Tags.JOVIAN];
-    public startingMegaCredits: number = 48;
-    public resourceType = ResourceType.FLOATER;
-    public resourceCount: number = 0;
-    public cardType = CardType.CORPORATION;
+  public name = CardName.STORMCRAFT_INCORPORATED;
+  public tags = [Tags.JOVIAN];
+  public startingMegaCredits: number = 48;
+  public resourceType = ResourceType.FLOATER;
+  public resourceCount: number = 0;
+  public cardType = CardType.CORPORATION;
 
-    public play() {
+  public play() {
+    return undefined;
+  }
+
+  public canAct(): boolean {
+    return true;
+  }
+
+  public action(player: Player) {
+    const floaterCards = player.getResourceCards(ResourceType.FLOATER);
+    if (floaterCards.length === 1) {
+      this.resourceCount++;
       return undefined;
     }
 
-    public canAct(): boolean {
-      return true;
-    }
+    return new SelectCard(
+        'Select card to add 1 floater',
+        'Add floater',
+        floaterCards,
+        (foundCards: Array<ICard>) => {
+          player.addResourceTo(foundCards[0], 1);
+          return undefined;
+        },
+    );
+  }
 
-    public action(player: Player) {
-      const floaterCards = player.getResourceCards(ResourceType.FLOATER);
-      if (floaterCards.length === 1) {
-        this.resourceCount++;
-        return undefined;
-      }
+  public convertHeatIntoTemperature(game: Game, player: Player): SelectOption {
+    const floatersToHeat = 2;
+    let heatAmount: number;
+    let floaterAmount: number;
+    const raiseTempOptions = new AndOptions(
+        () => {
+          const total = heatAmount + (floaterAmount * floatersToHeat);
+          if (total < constants.HEAT_FOR_TEMPERATURE) {
+            throw new Error(`Need to pay ${constants.HEAT_FOR_TEMPERATURE} heat`);
+          }
+          if (total > constants.HEAT_FOR_TEMPERATURE) {
+            throw new Error(`Only need to pay ${constants.HEAT_FOR_TEMPERATURE} heat`);
+          }
+          player.removeResourceFrom(this, floaterAmount);
+          player.heat -= heatAmount;
+          game.increaseTemperature(player, 1);
+          game.log('${0} converted heat into temperature', (b) => b.player(player));
+          return undefined;
+        },
+        new SelectAmount('Select amount of heat to spend', 'Spend heat', (amount: number) => {
+          heatAmount = amount;
+          return undefined;
+        }, Math.min(player.heat, constants.HEAT_FOR_TEMPERATURE)),
+        new SelectAmount('Select amount of floaters on corporation to spend', 'Spend floaters', (amount: number) => {
+          floaterAmount = amount;
+          return undefined;
+        }, Math.min(this.resourceCount, constants.HEAT_FOR_TEMPERATURE / floatersToHeat)),
+    );
+    raiseTempOptions.title = 'Select resource amounts to raise temp';
 
-      return new SelectCard(
-          'Select card to add 1 floater',
-          'Add floater',
-          floaterCards,
-          (foundCards: Array<ICard>) => {
-            player.addResourceTo(foundCards[0], 1);
-            return undefined;
-          },
-      );
-    }
+    return new SelectOption(`Convert ${constants.HEAT_FOR_TEMPERATURE} heat into temperature`, 'Convert heat', () => {
+      return raiseTempOptions;
+    });
+  }
 }
--- tests/cards/colonies/StormCraftIncorporated.spec.ts
+++ tests/cards/colonies/StormCraftIncorporated.spec.ts
@@ -1,6 +1,10 @@
+import * as constants from "../../../src/constants";
 import { expect } from "chai";
+import { AndOptions } from "../../../src/inputs/AndOptions";
+import { SelectAmount } from "../../../src/inputs/SelectAmount";
 import { StormCraftIncorporated } from "../../../src/cards/colonies/StormCraftIncorporated";
 import { Color } from "../../../src/Color";
+import { Game } from "../../../src/Game";
 import { Player } from "../../../src/Player";
 
 describe("StormCraftIncorporated", function () {
@@ -15,5 +19,52 @@ describe("StormCraftIncorporated", function () {
         const action = card.action(player);
         expect(action).is.undefined;
         expect(card.resourceCount).to.eq(1);
+    });
+    it("Restricts amounts when converting heat", function () {
+        const card = new StormCraftIncorporated();
+        const player = new Player("test", Color.BLUE, false);
+        const game = new Game("solo1", [player], player);
+        player.heat = 10;
+        card.resourceCount = 10;
+        const action = card.convertHeatIntoTemperature(game, player);
+        const options = action.cb() as AndOptions;
+        expect(options.options.length).to.eq(2);
+        const heatOption = options.options[0] as SelectAmount;
+        expect(heatOption.max).to.eq(constants.HEAT_FOR_TEMPERATURE);
+        const floaterOption = options.options[1] as SelectAmount;
+        expect(floaterOption.max).to.eq(constants.HEAT_FOR_TEMPERATURE / 2);
+    });
+    it("Validates inputs", function () {
+        const card = new StormCraftIncorporated();
+        const player = new Player("test", Color.BLUE, false);
+        const game = new Game("solo1", [player], player);
+        player.heat = 10;
+        card.resourceCount = 10;
+        const action = card.convertHeatIntoTemperature(game, player);
+        const options = action.cb() as AndOptions;
+        const heatOption = options.options[0] as SelectAmount;
+        const floaterOption = options.options[1] as SelectAmount;
+        heatOption.cb(4);
+        floaterOption.cb(0);
+        expect(function () { options.cb(); }).to.throw(`Need to pay ${constants.HEAT_FOR_TEMPERATURE} heat`);
+        heatOption.cb(10);
+        expect(function () { options.cb(); }).to.throw(`Only need to pay ${constants.HEAT_FOR_TEMPERATURE} heat`);
+    });
+    it("Converts heat with floaters and heat", function () {
+        const card = new StormCraftIncorporated();
+        const player = new Player("test", Color.BLUE, false);
+        const game = new Game("solo1", [player], player);
+        player.heat = 10;
+        card.resourceCount = 10;
+        const action = card.convertHeatIntoTemperature(game, player);
+        const options = action.cb() as AndOptions;
+        const heatOption = options.options[0] as SelectAmount;
+        const floaterOption = options.options[1] as SelectAmount;
+        heatOption.cb(2);
+        floaterOption.cb(3);
+        options.cb();
+        expect(player.heat).to.eq(8);
+        expect(card.resourceCount).to.eq(7);
+        expect(game.getTemperature()).to.eq(constants.MIN_TEMPERATURE + 2);
     });
 });

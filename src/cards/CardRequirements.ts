442a24b222cc3fe5a699d539a1de5b565cd2078e
Danimir <dnbszbg@gmail.com>
2020-11-14 10:52:27 +0200

    Formatting

--- src/cards/CardRequirements.ts
+++ src/cards/CardRequirements.ts
@@ -1,11 +1,6 @@
 import {Resources} from '../Resources';
 import {PartyName} from '../turmoil/parties/PartyName';
-import {
-  CardRequirement,
-  PartyCardRequirement,
-  ProductionCardRequirement,
-  TagCardRequirement,
-} from './CardRequirement';
+import {CardRequirement, PartyCardRequirement, ProductionCardRequirement, TagCardRequirement} from './CardRequirement';
 import {RequirementType} from './RequirementType';
 import {Tags} from './Tags';
 
@@ -27,93 +22,93 @@ export class CardRequirements {
 }
 
 class Builder {
-    private reqs: Array<CardRequirement> = [];
+  private reqs: Array<CardRequirement> = [];
 
-    public build(): CardRequirements {
-      return new CardRequirements(this.reqs);
-    }
+  public build(): CardRequirements {
+    return new CardRequirements(this.reqs);
+  }
 
-    public oceans(amount: number = -1): Builder {
-      this.reqs.push(new CardRequirement(RequirementType.OCEANS, amount));
-      return this;
-    }
+  public oceans(amount: number = -1): Builder {
+    this.reqs.push(new CardRequirement(RequirementType.OCEANS, amount));
+    return this;
+  }
 
-    public oxygen(amount: number): Builder {
-      this.reqs.push(new CardRequirement(RequirementType.OXYGEN, amount));
-      return this;
-    }
+  public oxygen(amount: number): Builder {
+    this.reqs.push(new CardRequirement(RequirementType.OXYGEN, amount));
+    return this;
+  }
 
-    public temperature(amount: number): Builder {
-      this.reqs.push(new CardRequirement(RequirementType.TEMPERATURE, amount));
-      return this;
-    }
+  public temperature(amount: number): Builder {
+    this.reqs.push(new CardRequirement(RequirementType.TEMPERATURE, amount));
+    return this;
+  }
 
-    public venus(amount: number): Builder {
-      this.reqs.push(new CardRequirement(RequirementType.VENUS, amount));
-      return this;
-    }
+  public venus(amount: number): Builder {
+    this.reqs.push(new CardRequirement(RequirementType.VENUS, amount));
+    return this;
+  }
 
-    public tr(amount: number): Builder {
-      this.reqs.push(new CardRequirement(RequirementType.TR, amount));
-      return this;
-    }
+  public tr(amount: number): Builder {
+    this.reqs.push(new CardRequirement(RequirementType.TR, amount));
+    return this;
+  }
 
-    public chairman(): Builder {
-      this.reqs.push(new CardRequirement(RequirementType.CHAIRMAN, -1));
-      return this;
-    }
+  public chairman(): Builder {
+    this.reqs.push(new CardRequirement(RequirementType.CHAIRMAN, -1));
+    return this;
+  }
 
-    public resourceTypes(amount: number): Builder {
-      this.reqs.push(new CardRequirement(RequirementType.RESOURCE_TYPES, amount));
-      return this;
-    }
+  public resourceTypes(amount: number): Builder {
+    this.reqs.push(new CardRequirement(RequirementType.RESOURCE_TYPES, amount));
+    return this;
+  }
 
-    public forests(amount: number = -1): Builder {
-      this.reqs.push(new CardRequirement(RequirementType.FORESTS, amount));
-      return this;
-    }
+  public forests(amount: number = -1): Builder {
+    this.reqs.push(new CardRequirement(RequirementType.FORESTS, amount));
+    return this;
+  }
 
-    public cities(amount: number = -1): Builder {
-      this.reqs.push(new CardRequirement(RequirementType.CITIES, amount));
-      return this;
-    }
+  public cities(amount: number = -1): Builder {
+    this.reqs.push(new CardRequirement(RequirementType.CITIES, amount));
+    return this;
+  }
 
-    public colonies(amount: number = -1): Builder {
-      this.reqs.push(new CardRequirement(RequirementType.COLONIES, amount));
-      return this;
-    }
+  public colonies(amount: number = -1): Builder {
+    this.reqs.push(new CardRequirement(RequirementType.COLONIES, amount));
+    return this;
+  }
 
-    public floaters(amount: number = -1): Builder {
-      this.reqs.push(new CardRequirement(RequirementType.FLOATERS, amount));
-      return this;
-    }
+  public floaters(amount: number = -1): Builder {
+    this.reqs.push(new CardRequirement(RequirementType.FLOATERS, amount));
+    return this;
+  }
 
-    public partyLeaders(amount: number = -1): Builder {
-      this.reqs.push(new CardRequirement(RequirementType.PARTY_LEADERS, amount));
-      return this;
-    }
+  public partyLeaders(amount: number = -1): Builder {
+    this.reqs.push(new CardRequirement(RequirementType.PARTY_LEADERS, amount));
+    return this;
+  }
 
-    public tag(tag: Tags, amount: number = -1): Builder {
-      this.reqs.push(new TagCardRequirement(tag, amount));
-      return this;
-    }
+  public tag(tag: Tags, amount: number = -1): Builder {
+    this.reqs.push(new TagCardRequirement(tag, amount));
+    return this;
+  }
 
-    public production(resource: Resources, amount: number = -1): Builder {
-      this.reqs.push(new ProductionCardRequirement(resource, amount));
-      return this;
-    }
+  public production(resource: Resources, amount: number = -1): Builder {
+    this.reqs.push(new ProductionCardRequirement(resource, amount));
+    return this;
+  }
 
-    public party(party: PartyName): Builder {
-      this.reqs.push(new PartyCardRequirement(party));
-      return this;
-    }
+  public party(party: PartyName): Builder {
+    this.reqs.push(new PartyCardRequirement(party));
+    return this;
+  }
 
-    public max(): Builder {
-      const req = this.reqs.pop();
-      if (req === undefined) {
-        throw new Error('Called max without a CardRequirement.');
-      }
-      this.reqs.push(req.max());
-      return this;
+  public max(): Builder {
+    const req = this.reqs.pop();
+    if (req === undefined) {
+      throw new Error('Called max without a CardRequirement.');
     }
+    this.reqs.push(req.max());
+    return this;
+  }
 }

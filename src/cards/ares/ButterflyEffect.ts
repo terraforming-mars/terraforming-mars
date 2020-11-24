2cc54d7a8323993b980e188c3c044b23a9164dbc
Robert Konigsberg <kberg@users.noreply.github.com>
2020-11-23 20:53:48 -0500

    Remove superfluous event tag from Butterfly Effect and Desperate Measures. (#1943)

--- src/cards/ares/ButterflyEffect.ts
+++ src/cards/ares/ButterflyEffect.ts
@@ -4,11 +4,10 @@ import {Game} from '../../Game';
 import {Player} from '../../Player';
 import {CardType} from '../CardType';
 import {IProjectCard} from '../IProjectCard';
-import {Tags} from '../Tags';
 
 export class ButterflyEffect implements IProjectCard {
     public cost = 8;
-    public tags = [Tags.EVENT];
+    public tags = [];
     public cardType = CardType.EVENT;
     public name = CardName.BUTTERFLY_EFFECT;
     public play(player: Player, game: Game) {

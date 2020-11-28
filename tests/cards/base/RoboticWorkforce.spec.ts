80736e3c3c56d3329d822c80a3aff924655853d0
starcalc <gitlab-molly@d00d.de>
2020-11-28 11:06:31 +0100

    Added two test cases for RoboticWorkforce and Ares

--- tests/cards/base/RoboticWorkforce.spec.ts
+++ tests/cards/base/RoboticWorkforce.spec.ts
@@ -8,6 +8,10 @@ import {NoctisFarming} from '../../../src/cards/base/NoctisFarming';
 import {FoodFactory} from '../../../src/cards/base/FoodFactory';
 import {Resources} from '../../../src/Resources';
 import {UtopiaInvest} from '../../../src/cards/turmoil/UtopiaInvest';
+import {Capital} from '../../../src/cards/base/Capital';
+import {CommercialDistrictAres} from '../../../src/cards/ares/CommercialDistrictAres';
+import {CapitalAres} from '../../../src/cards/ares/CapitalAres';
+import {ARES_OPTIONS_NO_HAZARDS} from '../../ares/AresTestHelper';
 
 describe('RoboticWorkforce', function() {
   let card : RoboticWorkforce; let player : Player; let game : Game;
@@ -15,7 +19,8 @@ describe('RoboticWorkforce', function() {
   beforeEach(function() {
     card = new RoboticWorkforce();
     player = new Player('test', Color.BLUE, false);
-    game = new Game('foobar', [player, player], player);
+    game = new Game('foobar', [player, player], player, ARES_OPTIONS_NO_HAZARDS);
+    // game = new Game('foobar', [player, player], player);
   });
 
   it('Can\'t play if no building cards to copy', function() {
@@ -37,7 +42,36 @@ describe('RoboticWorkforce', function() {
         expect(player.getProduction(Resources.MEGACREDITS)).to.eq(1);
   });
 
-  it('Should play with corporation cards', function() {
+  it('Should work with capital', function() {
+    player.playedCards.push(new Capital());
+    player.addProduction(Resources.ENERGY, 2);
+    const action = card.play(player, game);
+    expect(action).is.not.undefined;
+        action!.cb([new Capital()]);
+        expect(player.getProduction(Resources.MEGACREDITS)).to.eq(5);
+  });
+
+  it('Should work with CommercialDistrict (ares expansion)', function() {
+    player.playedCards.push(new CommercialDistrictAres());
+    player.addProduction(Resources.ENERGY, 2);
+    game.addCityTile(player, game.board.getAvailableSpacesOnLand(player)[0].id);
+    expect(game.getCitiesInPlayOnMars()).to.eq(1);
+    const action = card.play(player, game);
+    expect(action).is.not.undefined;
+        action!.cb([new CommercialDistrictAres()]);
+        expect(player.getProduction(Resources.MEGACREDITS)).to.eq(4);
+  });
+
+  it('Should work with Capital (ares expansion)', function() {
+    player.playedCards.push(new CapitalAres());
+    player.addProduction(Resources.ENERGY, 2);
+    const action = card.play(player, game);
+    expect(action).is.not.undefined;
+        action!.cb([new CapitalAres()]);
+        expect(player.getProduction(Resources.MEGACREDITS)).to.eq(5);
+  });
+
+ it('Should play with corporation cards', function() {
     const corporationCard = new UtopiaInvest();
     player.corporationCard = corporationCard;
 

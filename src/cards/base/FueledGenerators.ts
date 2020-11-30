4e7f63e87a63f56cd223a0ea86ef26d4cf335a82
Danimir <dnbszbg@gmail.com>
2020-11-30 19:32:25 +0200

    Added cards with F

--- src/cards/base/FueledGenerators.ts
+++ src/cards/base/FueledGenerators.ts
@@ -1,10 +1,11 @@
-
 import {IProjectCard} from '../IProjectCard';
 import {Tags} from '../Tags';
 import {CardType} from '../CardType';
 import {Player} from '../../Player';
 import {Resources} from '../../Resources';
 import {CardName} from '../../CardName';
+import {CardMetadata} from '../CardMetadata';
+import {CardRenderer} from '../render/CardRenderer';
 
 export class FueledGenerators implements IProjectCard {
   public cost = 1;
@@ -19,5 +20,15 @@ export class FueledGenerators implements IProjectCard {
     player.addProduction(Resources.MEGACREDITS, -1);
     player.addProduction(Resources.ENERGY);
     return undefined;
+  }
+  public metadata: CardMetadata = {
+    cardNumber: '100',
+    renderData: CardRenderer.builder((b) => {
+      b.productionBox((pb) => {
+        pb.minus().megacredits(1).br;
+        pb.plus().energy(1);
+      });
+    }),
+    description: 'Decrease your MC production 1 step and increase your Energy production 1 steps.',
   }
 }

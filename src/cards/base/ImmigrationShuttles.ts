5460c28100b3f8a93489089b203be18854463be2
Danimir <dnbszbg@gmail.com>
2020-12-03 13:08:06 +0200

    First 4

--- src/cards/base/ImmigrationShuttles.ts
+++ src/cards/base/ImmigrationShuttles.ts
@@ -1,4 +1,3 @@
-
 import {IProjectCard} from '../IProjectCard';
 import {Tags} from '../Tags';
 import {CardType} from '../CardType';
@@ -6,6 +5,10 @@ import {Player} from '../../Player';
 import {Game} from '../../Game';
 import {Resources} from '../../Resources';
 import {CardName} from '../../CardName';
+import {CardMetadata} from '../CardMetadata';
+import {CardRenderer} from '../render/CardRenderer';
+import {CardRenderItemSize} from '../render/CardRenderItemSize';
+import {CardRenderDynamicVictoryPoints} from '../render/CardRenderDynamicVictoryPoints';
 
 export class ImmigrationShuttles implements IProjectCard {
     public cost = 31;
@@ -19,5 +22,14 @@ export class ImmigrationShuttles implements IProjectCard {
     public play(player: Player) {
       player.addProduction(Resources.MEGACREDITS, 5);
       return undefined;
+    }
+    public metadata: CardMetadata = {
+      cardNumber: '198',
+      renderData: CardRenderer.builder((b) => {
+        b.productionBox((pb) => pb.megacredits(5)).br;
+        b.text('1 VP for every 3rd City in play', CardRenderItemSize.TINY, true);
+      }),
+      description: 'Increase your MC production 5 steps.',
+      victoryPoints: CardRenderDynamicVictoryPoints.cities(1, 3, true),
     }
 }

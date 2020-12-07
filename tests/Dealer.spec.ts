ab343b7392df0511155fdfb3b98c1c95952b7009
Robert Konigsberg <konigsberg@gmail.com>
2020-12-07 01:54:38 -0500

    Adjust Dealer serialization using the same approach used for Turmoil.
    Two additional changes:
    1. Removed a few really unnecsesary fields in the serialization protocol. So funny.
    2. Moved a couple of Dealer-only tests from `Game.spec.ts` to
    `Dealer.spec.tMoved a couple of Dealer-only tests from `Game.spec.ts` to
    `Dealer.spec.ts`.

--- tests/Dealer.spec.ts
+++ tests/Dealer.spec.ts
@@ -1,9 +1,32 @@
 import {expect} from 'chai';
 import {Dealer} from '../src/Dealer';
+import {COMMUNITY_CARD_MANIFEST} from '../src/cards/community/CommunityCardManifest';
+import {CardFinder} from '../src/CardFinder';
 
 describe('Dealer', function() {
+  it('correctly separates 71 corporate era cards', function() {
+    // include corporate era
+    const dealer = Dealer.newInstance(true, false, false, false, false, false, false);
+    expect(dealer.getDeckSize()).to.eq(208);
+
+    // exclude corporate era
+    const dealer2 = Dealer.newInstance(false, false, false, false, false, false, false);
+    expect(dealer2.getDeckSize()).to.eq(137);
+  });
+
+  it('excludes expansion-specific preludes if those expansions are not selected ', function() {
+    const dealer = Dealer.newInstance(true, false, false, false, false, false, false, true);
+    const preludeDeck = dealer.preludeDeck;
+
+    const turmoilPreludes = COMMUNITY_CARD_MANIFEST.preludeCards.cards.map((c) => c.cardName);
+    turmoilPreludes.forEach((preludeName) => {
+      const preludeCard = new CardFinder().getProjectCardByName(preludeName)!;
+      expect(preludeDeck.includes(preludeCard)).is.not.true;
+    });
+  });
+
   it('serializes every property', function() {
-    const dealer = new Dealer(false, false, false, false, false, false, false, false);
+    const dealer = Dealer.newInstance(false, false, false, false, false, false, false, false);
     const serialized = dealer.serialize();
     const serializedKeys = Object.keys(serialized);
     const dealerKeys = Object.keys(dealer);

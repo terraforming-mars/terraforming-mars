8e28676c2a8023d29d7249c05f7cc6b1f967e651
Danimir <dnbszbg@gmail.com>
2020-12-20 14:20:09 +0200

    Reworked some cards

--- src/cards/ares/MoholeAreaAres.ts
+++ src/cards/ares/MoholeAreaAres.ts
@@ -12,7 +12,7 @@ export class MoholeAreaAres extends MoholeArea {
   public metadata: CardMetadata = {
     cardNumber: 'A16',
     renderData: CardRenderer.builder((b) => {
-      b.productionBox((pb) => pb.heat(4)).br;
+      b.productionBox((pb) => pb.heat(4).digit).br;
       b.tile(TileType.MOHOLE_AREA, false, true);
     }),
     description: 'Increase your heat production 4 steps. Place this tile ON AN AREA RESERVED FOR OCEAN. The tile grants an ADJACENCY BONUS of 2 heat.',

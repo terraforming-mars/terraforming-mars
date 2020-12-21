62677217ef1b31991c2d55619df38a8d69ce97fd
waichuan <nwai1990@hotmail.com>
2020-12-22 01:15:36 +0800

    Clearer BannedDelegate and LunarExports logs + swap option order

--- tests/cards/colonies/LunarExports.spec.ts
+++ tests/cards/colonies/LunarExports.spec.ts
@@ -1,5 +1,6 @@
 import {expect} from 'chai';
 import {LunarExports} from '../../../src/cards/colonies/LunarExports';
+import {Game} from '../../../src/Game';
 import {OrOptions} from '../../../src/inputs/OrOptions';
 import {Resources} from '../../../src/Resources';
 import {TestPlayers} from '../../TestingUtils';
@@ -8,10 +9,12 @@ describe('LunarExports', function() {
   it('Should play', function() {
     const card = new LunarExports();
     const player = TestPlayers.BLUE.newPlayer();
-    const orOptions = card.play(player) as OrOptions;
+    const game = new Game('foobar', [player], player);
+    const orOptions = card.play(player, game) as OrOptions;
+
     expect(orOptions).is.not.undefined;
     expect(orOptions instanceof OrOptions).is.true;
-    orOptions.options[0].cb();
+    orOptions.options[1].cb();
     expect(player.getProduction(Resources.PLANTS)).to.eq(2);
   });
 });

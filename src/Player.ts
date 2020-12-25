9c1d38b5b8d07d4fd18681991eca7d7c433126ef
bafolts <brian.folts@hbo.com>
2020-12-25 12:17:08 -0800

    only show venus tags when on venus

--- src/Player.ts
+++ src/Player.ts
@@ -600,22 +600,21 @@ export class Player implements ISerializable<SerializedPlayer> {
   }
 
   public getAllTags(): Array<ITagCount> {
-    const tags: Array<ITagCount> = [];
-    tags.push({tag: Tags.CITY, count: this.getTagCount(Tags.CITY, false, false)} as ITagCount);
-    tags.push({tag: Tags.EARTH, count: this.getTagCount(Tags.EARTH, false, false)} as ITagCount);
-    tags.push({tag: Tags.ENERGY, count: this.getTagCount(Tags.ENERGY, false, false)} as ITagCount);
-    tags.push({tag: Tags.JOVIAN, count: this.getTagCount(Tags.JOVIAN, false, false)} as ITagCount);
-    tags.push({tag: Tags.MICROBES, count: this.getTagCount(Tags.MICROBES, false, false)} as ITagCount);
-    tags.push({tag: Tags.PLANT, count: this.getTagCount(Tags.PLANT, false, false)} as ITagCount);
-    tags.push({tag: Tags.SCIENCE, count: this.getTagCount(Tags.SCIENCE, false, false)} as ITagCount);
-    tags.push({tag: Tags.SPACE, count: this.getTagCount(Tags.SPACE, false, false)} as ITagCount);
-    tags.push({tag: Tags.STEEL, count: this.getTagCount(Tags.STEEL, false, false)} as ITagCount);
-    tags.push({tag: Tags.VENUS, count: this.getTagCount(Tags.VENUS, false, false)} as ITagCount);
-    tags.push({tag: Tags.WILDCARD, count: this.getTagCount(Tags.WILDCARD, false, false)} as ITagCount);
-    tags.push({tag: Tags.ANIMAL, count: this.getTagCount(Tags.ANIMAL, false, false)} as ITagCount);
-    tags.push({tag: Tags.EVENT, count: this.playedCards.filter((card) => card.cardType === CardType.EVENT).length} as ITagCount);
-
-    return tags.filter((tag) => tag.count > 0);
+    return [
+      {tag: Tags.CITY, count: this.getTagCount(Tags.CITY, false, false)},
+      {tag: Tags.EARTH, count: this.getTagCount(Tags.EARTH, false, false)},
+      {tag: Tags.ENERGY, count: this.getTagCount(Tags.ENERGY, false, false)},
+      {tag: Tags.JOVIAN, count: this.getTagCount(Tags.JOVIAN, false, false)},
+      {tag: Tags.MICROBES, count: this.getTagCount(Tags.MICROBES, false, false)},
+      {tag: Tags.PLANT, count: this.getTagCount(Tags.PLANT, false, false)},
+      {tag: Tags.SCIENCE, count: this.getTagCount(Tags.SCIENCE, false, false)},
+      {tag: Tags.SPACE, count: this.getTagCount(Tags.SPACE, false, false)},
+      {tag: Tags.STEEL, count: this.getTagCount(Tags.STEEL, false, false)},
+      {tag: Tags.VENUS, count: this.getTagCount(Tags.VENUS, false, false)},
+      {tag: Tags.WILDCARD, count: this.getTagCount(Tags.WILDCARD, false, false)},
+      {tag: Tags.ANIMAL, count: this.getTagCount(Tags.ANIMAL, false, false)},
+      {tag: Tags.EVENT, count: this.playedCards.filter((card) => card.cardType === CardType.EVENT).length},
+    ].filter((tag) => tag.count > 0);
   }
 
   public getTagCount(tag: Tags, includeEventsTags:boolean = false, includeWildcardTags:boolean = true): number {

00940d5163b645408ce7e5695896b70d844c0775
sienmich <sienmich@gmail.com>
2020-12-29 17:27:21 +0100

    Improved spending heat with StormCraftIncorporated

--- tests/cards/colonies/StormCraftIncorporated.spec.ts
+++ tests/cards/colonies/StormCraftIncorporated.spec.ts
@@ -2,7 +2,6 @@ import {expect} from 'chai';
 import {StormCraftIncorporated} from '../../../src/cards/colonies/StormCraftIncorporated';
 import * as constants from '../../../src/constants';
 import {Game} from '../../../src/Game';
-import {AndOptions} from '../../../src/inputs/AndOptions';
 import {SelectAmount} from '../../../src/inputs/SelectAmount';
 import {Player} from '../../../src/Player';
 import {TestPlayers} from '../../TestingUtils';
@@ -31,8 +30,7 @@ describe('StormCraftIncorporated', function() {
   it('Restricts amounts when converting heat', function() {
     player.heat = 10;
     card.resourceCount = 10;
-    const action = card.convertHeatIntoTemperature(game, player);
-    const options = action.cb() as AndOptions;
+    const options = card.spendHeat(player, constants.HEAT_FOR_TEMPERATURE);
     expect(options.options.length).to.eq(2);
     const heatOption = options.options[0] as SelectAmount;
     expect(heatOption.max).to.eq(constants.HEAT_FOR_TEMPERATURE);
@@ -43,8 +41,7 @@ describe('StormCraftIncorporated', function() {
   it('Validates inputs', function() {
     player.heat = 10;
     card.resourceCount = 10;
-    const action = card.convertHeatIntoTemperature(game, player);
-    const options = action.cb() as AndOptions;
+    const options = card.spendHeat(player, constants.HEAT_FOR_TEMPERATURE);
     const heatOption = options.options[0] as SelectAmount;
     const floaterOption = options.options[1] as SelectAmount;
     heatOption.cb(4);
@@ -52,17 +49,12 @@ describe('StormCraftIncorporated', function() {
     expect(function() {
       options.cb();
     }).to.throw(`Need to pay ${constants.HEAT_FOR_TEMPERATURE} heat`);
-    heatOption.cb(10);
-    expect(function() {
-      options.cb();
-    }).to.throw(`Only need to pay ${constants.HEAT_FOR_TEMPERATURE} heat`);
   });
 
   it('Converts heat with floaters and heat', function() {
     player.heat = 10;
     card.resourceCount = 10;
-    const action = card.convertHeatIntoTemperature(game, player);
-    const options = action.cb() as AndOptions;
+    const options = card.spendHeat(player, constants.HEAT_FOR_TEMPERATURE);
     const heatOption = options.options[0] as SelectAmount;
     const floaterOption = options.options[1] as SelectAmount;
     heatOption.cb(2);
@@ -70,6 +62,5 @@ describe('StormCraftIncorporated', function() {
     options.cb();
     expect(player.heat).to.eq(8);
     expect(card.resourceCount).to.eq(7);
-    expect(game.getTemperature()).to.eq(constants.MIN_TEMPERATURE + 2);
   });
 });

81a01f6cfbbb8f4fd8284a16f922cd2160bd49ab
Brian Folts <brian.folts@gmail.com>
2020-12-30 18:06:33 -0800

    use getters for corporation cards (#2226)
    Co-authored-by: Robert Konigsberg <kberg@users.noreply.github.com>

---
+++ src/cards/Card.ts
@@ -0,0 +1,46 @@
+
+import {CardMetadata} from './CardMetadata';
+import {CardName} from '../CardName';
+import {CardType} from './CardType';
+import {Tags} from './Tags';
+
+interface StaticCardProperties {
+  cardType: CardType;
+  initialActionText?: string;
+  metadata: CardMetadata;
+  name: CardName;
+  startingMegaCredits?: number;
+  tags?: Array<Tags>;
+}
+
+export abstract class Card {
+  private readonly properties: StaticCardProperties;
+  constructor(properties: StaticCardProperties) {
+    const staticInstance = Object.getPrototypeOf(this).constructor;
+    if (staticInstance.properties === undefined) {
+      if (properties.cardType === CardType.CORPORATION && properties.startingMegaCredits === undefined) {
+        throw new Error('must define startingMegaCredits for corporation cards');
+      }
+      staticInstance.properties = properties;
+    }
+    this.properties = staticInstance.properties;
+  }
+  public get cardType() {
+    return this.properties.cardType;
+  }
+  public get initialActionText() {
+    return this.properties.initialActionText;
+  }
+  public get metadata() {
+    return this.properties.metadata;
+  }
+  public get name() {
+    return this.properties.name;
+  }
+  public get startingMegaCredits() {
+    return this.properties.startingMegaCredits === undefined ? 0 : this.properties.startingMegaCredits;
+  }
+  public get tags() {
+    return this.properties.tags === undefined ? [] : this.properties.tags;
+  }
+}
--- src/cards/corporation/Helion.ts
+++ src/cards/corporation/Helion.ts
@@ -1,36 +1,39 @@
+import {Card} from '../Card';
 import {CorporationCard} from './CorporationCard';
 import {Tags} from '../Tags';
 import {Player} from '../../Player';
 import {Resources} from '../../Resources';
 import {CardName} from '../../CardName';
 import {CardType} from '../CardType';
-import {CardMetadata} from '../CardMetadata';
 import {CardRenderer} from '../render/CardRenderer';
 
-export class Helion implements CorporationCard {
-    public name = CardName.HELION;
-    public tags = [Tags.SPACE];
-    public startingMegaCredits: number = 42;
-    public cardType = CardType.CORPORATION;
+export class Helion extends Card implements CorporationCard {
+  constructor() {
+    super({
+      cardType: CardType.CORPORATION,
+      name: CardName.HELION,
+      tags: [Tags.SPACE],
+      startingMegaCredits: 42,
 
-    public play(player: Player) {
-      player.canUseHeatAsMegaCredits = true;
-      player.addProduction(Resources.HEAT, 3);
-      return undefined;
-    }
-
-    public metadata: CardMetadata = {
-      cardNumber: 'R18',
-      description: 'You start with 3 heat production and 42 MC.',
-      renderData: CardRenderer.builder((b) => {
-        b.br;
-        b.productionBox((pb) => pb.heat(3)).nbsp.megacredits(42);
-        b.corpBox('effect', (ce) => {
-          ce.effectBox((eb) => {
-            eb.text('x').heat(1).startEffect.megacredits(0).multiplier;
-            eb.description('Effect: You may use heat as MC. You may not use MC as heat.');
+      metadata: {
+        cardNumber: 'R18',
+        description: 'You start with 3 heat production and 42 MC.',
+        renderData: CardRenderer.builder((b) => {
+          b.br;
+          b.productionBox((pb) => pb.heat(3)).nbsp.megacredits(42);
+          b.corpBox('effect', (ce) => {
+            ce.effectBox((eb) => {
+              eb.text('x').heat(1).startEffect.megacredits(0).multiplier;
+              eb.description('Effect: You may use heat as MC. You may not use MC as heat.');
+            });
           });
-        });
-      }),
-    }
+        }),
+      },
+    });
+  }
+  public play(player: Player) {
+    player.canUseHeatAsMegaCredits = true;
+    player.addProduction(Resources.HEAT, 3);
+    return undefined;
+  }
 }

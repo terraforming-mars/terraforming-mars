9a5ded1bb6c87ffbec308f272d2f786db5d0dc0c
algorithm99 <brian.folts@gmail.com>
2021-01-02 10:33:11 -0700

    convert the base A cards to less memory pattern

--- src/cards/Card.ts
+++ src/cards/Card.ts
@@ -2,31 +2,52 @@
 import {CardMetadata} from './CardMetadata';
 import {CardName} from '../CardName';
 import {CardType} from './CardType';
+import {IAdjacencyBonus} from '../ares/IAdjacencyBonus';
+import {ResourceType} from '../ResourceType';
 import {Tags} from './Tags';
 
 interface StaticCardProperties {
+  adjacencyBonus?: IAdjacencyBonus;
   cardType: CardType;
+  cost?: number;
+  hasRequirements?: boolean;
   initialActionText?: string;
   metadata: CardMetadata;
   name: CardName;
+  resourceType?: ResourceType;
   startingMegaCredits?: number;
   tags?: Array<Tags>;
 }
 
+const staticCardProperties = new Map<CardName, StaticCardProperties>();
+
 export abstract class Card {
   private readonly properties: StaticCardProperties;
   constructor(properties: StaticCardProperties) {
-    const staticInstance = Object.getPrototypeOf(this).constructor;
-    if (staticInstance.properties === undefined) {
+    let staticInstance = staticCardProperties.get(properties.name);
+    if (staticInstance === undefined) {
       if (properties.cardType === CardType.CORPORATION && properties.startingMegaCredits === undefined) {
         throw new Error('must define startingMegaCredits for corporation cards');
       }
-      staticInstance.properties = properties;
+      if (properties.cardType !== CardType.CORPORATION && properties.cost === undefined) {
+        throw new Error('must define cost for non-corporation cards');
+      }
+      staticCardProperties.set(properties.name, properties);
+      staticInstance = properties;
     }
-    this.properties = staticInstance.properties;
+    this.properties = staticInstance;
+  }
+  public get adjacencyBonus() {
+    return this.properties.adjacencyBonus;
   }
   public get cardType() {
     return this.properties.cardType;
+  }
+  public get cost() {
+    return this.properties.cost === undefined ? 0 : this.properties.cost;
+  }
+  public get hasRequirements() {
+    return this.properties.hasRequirements;
   }
   public get initialActionText() {
     return this.properties.initialActionText;
@@ -36,6 +57,9 @@ export abstract class Card {
   }
   public get name() {
     return this.properties.name;
+  }
+  public get resourceType() {
+    return this.properties.resourceType;
   }
   public get startingMegaCredits() {
     return this.properties.startingMegaCredits === undefined ? 0 : this.properties.startingMegaCredits;

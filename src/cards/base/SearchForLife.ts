788a6d9c7ddfa4711fa34f10fae2378ef762bf3a
bafolts <brian.folts@hbo.com>
2021-01-03 15:11:29 -0800

    another batch of cards with memory change

--- src/cards/base/SearchForLife.ts
+++ src/cards/base/SearchForLife.ts
@@ -1,26 +1,43 @@
 import {IActionCard, IResourceCard} from '../ICard';
 import {IProjectCard} from '../IProjectCard';
 import {Tags} from '../Tags';
+import {Card} from '../Card';
 import {CardType} from '../CardType';
 import {Player} from '../../Player';
 import {Game} from '../../Game';
 import {ResourceType} from '../../ResourceType';
 import {CardName} from '../../CardName';
 import {SelectHowToPayDeferred} from '../../deferredActions/SelectHowToPayDeferred';
-import {CardMetadata} from '../CardMetadata';
 import {CardRequirements} from '../CardRequirements';
 import {CardRenderer} from '../render/CardRenderer';
 import {CardRenderItemSize} from '../render/CardRenderItemSize';
 import {CardRenderDynamicVictoryPoints} from '../render/CardRenderDynamicVictoryPoints';
 import {GlobalParameter} from '../../GlobalParameter';
 
-export class SearchForLife implements IActionCard, IProjectCard, IResourceCard {
-    public cost = 3;
-    public tags = [Tags.SCIENCE];
-    public cardType = CardType.ACTIVE;
-    public resourceType = ResourceType.SCIENCE;
-    public resourceCount: number = 0;
-    public name = CardName.SEARCH_FOR_LIFE;
+export class SearchForLife extends Card implements IActionCard, IProjectCard, IResourceCard {
+  constructor() {
+    super({
+      cardType: CardType.ACTIVE,
+      name: CardName.SEARCH_FOR_LIFE,
+      tags: [Tags.SCIENCE],
+      cost: 3,
+      resourceType: ResourceType.SCIENCE,
+
+      metadata: {
+        cardNumber: '005',
+        requirements: CardRequirements.builder((b) => b.oxygen(6).max()),
+        renderData: CardRenderer.builder((b) => {
+          b.effectBox((eb) => {
+            eb.megacredits(1).startAction.microbes(1).played.asterix().nbsp.colon().nbsp.science();
+            eb.description('Action: Spend 1 MC to reveal the top card of the draw deck. If that card has a Microbe tag, add a Science resource here.');
+          }).br;
+          b.text('3 VPs if you have one or more Science resources here.', CardRenderItemSize.TINY, true);
+        }),
+        victoryPoints: CardRenderDynamicVictoryPoints.searchForLife(),
+      },
+    });
+  }
+    public resourceCount = 0;
     public canPlay(player: Player, game: Game): boolean {
       return game.checkMaxRequirements(player, GlobalParameter.OXYGEN, 6);
     }
@@ -49,18 +66,5 @@ export class SearchForLife implements IActionCard, IProjectCard, IResourceCard {
       game.dealer.discard(topCard);
       game.defer(new SelectHowToPayDeferred(player, 1, false, false, 'Select how to pay for action'));
       return undefined;
-    }
-
-    public metadata: CardMetadata = {
-      cardNumber: '005',
-      requirements: CardRequirements.builder((b) => b.oxygen(6).max()),
-      renderData: CardRenderer.builder((b) => {
-        b.effectBox((eb) => {
-          eb.megacredits(1).startAction.microbes(1).played.asterix().nbsp.colon().nbsp.science();
-          eb.description('Action: Spend 1 MC to reveal the top card of the draw deck. If that card has a Microbe tag, add a Science resource here.');
-        }).br;
-        b.text('3 VPs if you have one or more Science resources here.', CardRenderItemSize.TINY, true);
-      }),
-      victoryPoints: CardRenderDynamicVictoryPoints.searchForLife(),
     }
 }

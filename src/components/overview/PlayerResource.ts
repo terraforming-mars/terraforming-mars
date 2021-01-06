f6eb6e35af202778c303671ca581c4c7e2458be3
Jeep <14239220+vsrisuknimit@users.noreply.github.com>
2021-01-06 11:41:34 -0500

    dot for alloys, shield for protected plants

--- src/components/overview/PlayerResource.ts
+++ src/components/overview/PlayerResource.ts
@@ -1,5 +1,10 @@
 import Vue from 'vue';
+import {DEFAULT_STEEL_VALUE, DEFAULT_TITANIUM_VALUE} from '../../constants';
 import {Resources} from '../../Resources';
+import {TurmoilModel} from '../../models/TurmoilModel';
+import {PartyName} from '../../turmoil/parties/PartyName';
+import {CardModel} from '../../models/CardModel';
+import {CardName} from '../../CardName';
 
 export const PlayerResource = Vue.component('player-resource', {
   props: {
@@ -12,9 +17,34 @@ export const PlayerResource = Vue.component('player-resource', {
     production: {
       type: Number,
     },
+    plantsAreProtected: {
+      type: Boolean || undefined,
+    },
+    steelValue: {
+      type: Number,
+    },
+    titaniumValue: {
+      type: Number,
+    },
+    turmoil: {
+      type: Object as () => TurmoilModel || undefined,
+    },
+    corporationCard: {
+      type: Object as () => CardModel || undefined,
+    },
   },
   data: function() {
-    return {};
+    // TODO: Update logic after PoliticalAgendas merge
+    const unityTitaniumBonusActive: boolean = this.turmoil !== undefined && this.turmoil.ruling === PartyName.UNITY;
+
+    let playerTitaniumValueWithOffset: number = this.titaniumValue;
+    if (unityTitaniumBonusActive) playerTitaniumValueWithOffset -= 1;
+
+    if (this.corporationCard.name === CardName.PHOBOLOG) playerTitaniumValueWithOffset -= 1;
+
+    return {
+      playerAdjustedTitaniumValue: playerTitaniumValueWithOffset,
+    };
   },
   methods: {
     mainCSS: function(): string {
@@ -27,6 +57,21 @@ export const PlayerResource = Vue.component('player-resource', {
       if (this.production > 0) return '+';
       return '';
     },
+    displayPlantsProtectedIcon: function(): boolean {
+      return this.type === Resources.PLANTS && this.plantsAreProtected;
+    },
+    displayPlusOneSteelValueIcon: function(): boolean {
+      return this.type === Resources.STEEL && this.steelValue === DEFAULT_STEEL_VALUE + 1;
+    },
+    displayPlusTwoSteelValueIcon: function(): boolean {
+      return this.type === Resources.STEEL && this.steelValue === DEFAULT_STEEL_VALUE + 2;
+    },
+    displayPlusOneTitaniumValueIcon: function(): boolean {
+      return this.type === Resources.TITANIUM && this.playerAdjustedTitaniumValue === DEFAULT_TITANIUM_VALUE + 1;
+    },
+    displayPlusTwoTitaniumValueIcon: function(): boolean {
+      return this.type === Resources.TITANIUM && this.playerAdjustedTitaniumValue === DEFAULT_TITANIUM_VALUE + 2;
+    },
   },
   template: `
         <div class="resource_item" :class="mainCSS()">
@@ -36,6 +81,11 @@ export const PlayerResource = Vue.component('player-resource', {
             </div>
             <div class="resource_item_prod">
                 <span class="resource_item_prod_count">{{ productionSign() }}{{ production }}</span>
+                <div v-if="displayPlantsProtectedIcon()" class="shield_icon"></div>
+                <div v-if="displayPlusOneSteelValueIcon()" class="alloys">&#9679;</div>
+                <div v-if="displayPlusTwoSteelValueIcon()" class="alloys">&#9679;&#9679;</div>
+                <div v-if="displayPlusOneTitaniumValueIcon()" class="alloys">&#9679;</div>
+                <div v-if="displayPlusTwoTitaniumValueIcon()" class="alloys">&#9679;&#9679;</div>
             </div>
         </div>
     `,

162164789651bb4da581c8be00ee87ac366a33f9
bafolts <brian.folts@hbo.com>
2021-01-07 07:14:48 -0800

    another batch of cards for less memory

--- src/cards/base/TitaniumMine.ts
+++ src/cards/base/TitaniumMine.ts
@@ -1,28 +1,32 @@
 import {IProjectCard} from '../IProjectCard';
 import {Tags} from '../Tags';
+import {Card} from '../Card';
 import {CardType} from '../CardType';
 import {Player} from '../../Player';
 import {Game} from '../../Game';
 import {Resources} from '../../Resources';
 import {CardName} from '../../CardName';
-import {CardMetadata} from '../CardMetadata';
 import {CardRenderer} from '../render/CardRenderer';
 
-export class TitaniumMine implements IProjectCard {
-    public cost = 7;
-    public tags = [Tags.BUILDING];
-    public name = CardName.TITANIUM_MINE;
-    public cardType = CardType.AUTOMATED;
+export class TitaniumMine extends Card implements IProjectCard {
+  constructor() {
+    super({
+      cardType: CardType.AUTOMATED,
+      name: CardName.TITANIUM_MINE,
+      tags: [Tags.BUILDING],
+      cost: 7,
 
-    public play(player: Player, _game: Game) {
-      player.addProduction(Resources.TITANIUM);
-      return undefined;
-    }
-    public metadata: CardMetadata = {
-      cardNumber: '144',
-      renderData: CardRenderer.builder((b) => {
-        b.productionBox((pb) => pb.titanium(1));
-      }),
-      description: 'Increase your titanium production 1 step.',
-    }
+      metadata: {
+        cardNumber: '144',
+        renderData: CardRenderer.builder((b) => {
+          b.productionBox((pb) => pb.titanium(1));
+        }),
+        description: 'Increase your titanium production 1 step.',
+      },
+    });
+  }
+  public play(player: Player, _game: Game) {
+    player.addProduction(Resources.TITANIUM);
+    return undefined;
+  }
 }

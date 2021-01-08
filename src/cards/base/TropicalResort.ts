938b65f2e1e516afc2b130753ced834b156156d2
Brian Folts <brian.folts@gmail.com>
2021-01-07 17:20:05 -0800

    another batch of cards for less memory (#2295)

--- src/cards/base/TropicalResort.ts
+++ src/cards/base/TropicalResort.ts
@@ -1,38 +1,43 @@
 import {IProjectCard} from '../IProjectCard';
 import {Tags} from '../Tags';
+import {Card} from '../Card';
 import {CardType} from '../CardType';
 import {Player} from '../../Player';
 import {Resources} from '../../Resources';
 import {CardName} from '../../CardName';
-import {CardMetadata} from '../CardMetadata';
 import {CardRenderer} from '../render/CardRenderer';
 
-export class TropicalResort implements IProjectCard {
-    public cost = 13;
-    public tags = [Tags.BUILDING];
-    public name = CardName.TROPICAL_RESORT;
-    public cardType = CardType.AUTOMATED;
-    public hasRequirements = false;
-    public canPlay(player: Player): boolean {
-      return player.getProduction(Resources.HEAT) >= 2;
-    }
-    public play(player: Player) {
-      player.addProduction(Resources.HEAT, -2);
-      player.addProduction(Resources.MEGACREDITS, 3);
-      return undefined;
-    }
-    public getVictoryPoints() {
-      return 2;
-    }
-    public metadata: CardMetadata = {
-      cardNumber: '098',
-      renderData: CardRenderer.builder((b) => {
-        b.productionBox((pb) =>{
-          pb.minus().heat(2).br;
-          pb.plus().megacredits(3);
-        });
-      }),
-      description: 'Reduce your heat production 2 steps and increase your MC production 3 steps.',
-      victoryPoints: 2,
-    }
+export class TropicalResort extends Card implements IProjectCard {
+  constructor() {
+    super({
+      cardType: CardType.AUTOMATED,
+      name: CardName.TROPICAL_RESORT,
+      tags: [Tags.BUILDING],
+      cost: 13,
+      hasRequirements: false,
+
+      metadata: {
+        cardNumber: '098',
+        renderData: CardRenderer.builder((b) => {
+          b.productionBox((pb) =>{
+            pb.minus().heat(2).br;
+            pb.plus().megacredits(3);
+          });
+        }),
+        description: 'Reduce your heat production 2 steps and increase your MC production 3 steps.',
+        victoryPoints: 2,
+      },
+    });
+  }
+  public canPlay(player: Player): boolean {
+    return player.getProduction(Resources.HEAT) >= 2;
+  }
+  public play(player: Player) {
+    player.addProduction(Resources.HEAT, -2);
+    player.addProduction(Resources.MEGACREDITS, 3);
+    return undefined;
+  }
+  public getVictoryPoints() {
+    return 2;
+  }
 }

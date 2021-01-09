e36beaba69c9cdcfdc8b1d0a2c83f8d5f59cf908
bafolts <brian.folts@hbo.com>
2021-01-09 08:51:56 -0800

    remaining ares cards for static memory

--- src/cards/ares/OceanSanctuary.ts
+++ src/cards/ares/OceanSanctuary.ts
@@ -1,3 +1,4 @@
+import {Card} from '../Card';
 import {CardName} from '../../CardName';
 import {Game} from '../../Game';
 import {SelectSpace} from '../../inputs/SelectSpace';
@@ -9,19 +10,33 @@ import {TileType} from '../../TileType';
 import {CardType} from '../CardType';
 import {IResourceCard} from '../ICard';
 import {Tags} from '../Tags';
-import {CardMetadata} from '../CardMetadata';
 import {CardRequirements} from '../CardRequirements';
 import {CardRenderer} from '../render/CardRenderer';
 import {CardRenderDynamicVictoryPoints} from '../render/CardRenderDynamicVictoryPoints';
 import {GlobalParameter} from '../../GlobalParameter';
 
-export class OceanSanctuary implements IResourceCard {
-  public cost = 9;
-  public resourceType = ResourceType.ANIMAL;
-  public resourceCount: number = 0;
-  public tags = [Tags.ANIMAL];
-  public cardType = CardType.ACTIVE;
-  public name = CardName.OCEAN_SANCTUARY;
+export class OceanSanctuary extends Card implements IResourceCard {
+  constructor() {
+    super({
+      cardType: CardType.ACTIVE,
+      name: CardName.OCEAN_SANCTUARY,
+      tags: [Tags.ANIMAL],
+      cost: 9,
+      resourceType: ResourceType.ANIMAL,
+
+      metadata: {
+        cardNumber: 'A22',
+        requirements: CardRequirements.builder((b) => b.oceans(5)),
+        renderData: CardRenderer.builder((b) => {
+          b.tile(TileType.OCEAN_SANCTUARY, false, true).nbsp.animals(1).br;
+          b.vpText('1 VP per animal on this card.');
+        }),
+        description: 'Requires 5 ocean tiles. Place this tile on top of an existing ocean tile. The tile grants an ADJACENCY BONUS of 1 animal. Add 1 animal to this card.',
+        victoryPoints: CardRenderDynamicVictoryPoints.animals(1, 1),
+      },
+    });
+  }
+  public resourceCount = 0;
 
   public canPlay(player: Player, game: Game): boolean {
     return game.checkMinRequirements(player, GlobalParameter.OCEANS, 5);
@@ -44,15 +59,5 @@ export class OceanSanctuary implements IResourceCard {
         space.adjacency = {bonus: [SpaceBonus.ANIMAL]};
         return undefined;
       });
-  }
-  public metadata: CardMetadata = {
-    cardNumber: 'A22',
-    requirements: CardRequirements.builder((b) => b.oceans(5)),
-    renderData: CardRenderer.builder((b) => {
-      b.tile(TileType.OCEAN_SANCTUARY, false, true).nbsp.animals(1).br;
-      b.vpText('1 VP per animal on this card.');
-    }),
-    description: 'Requires 5 ocean tiles. Place this tile on top of an existing ocean tile. The tile grants an ADJACENCY BONUS of 1 animal. Add 1 animal to this card.',
-    victoryPoints: CardRenderDynamicVictoryPoints.animals(1, 1),
   }
 }

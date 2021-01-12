8aea33bfc1d36090cf2a2440973abda066130751
Brian Folts <brian.folts@gmail.com>
2021-01-11 17:14:08 -0800

    convert the prelude cards (#2333)

--- src/cards/prelude/GalileanMining.ts
+++ src/cards/prelude/GalileanMining.ts
@@ -5,28 +5,32 @@ import {Resources} from '../../Resources';
 import {CardName} from '../../CardName';
 import {Game} from '../../Game';
 import {SelectHowToPayDeferred} from '../../deferredActions/SelectHowToPayDeferred';
-import {CardMetadata} from '../../cards/CardMetadata';
 import {CardRenderer} from '../../cards/render/CardRenderer';
 
 export class GalileanMining extends PreludeCard {
-    public tags = [Tags.JOVIAN];
-    public name = CardName.GALILEAN_MINING;
-    public canPlay(player: Player, _game: Game) {
-      return player.canAfford(5);
-    }
-    public play(player: Player, game: Game) {
-      player.addProduction(Resources.TITANIUM, 2);
-      game.defer(new SelectHowToPayDeferred(player, 5));
-      return undefined;
-    }
-    public metadata: CardMetadata = {
-      cardNumber: 'P13',
-      renderData: CardRenderer.builder((b) => {
-        b.production((pb) => {
-          pb.titanium(2);
-        }).br;
-        b.megacredits(-5);
-      }),
-      description: 'Increase your titanium production 2 steps. Pay 5 MC.',
-    }
+  constructor() {
+    super({
+      name: CardName.GALILEAN_MINING,
+      tags: [Tags.JOVIAN],
+
+      metadata: {
+        cardNumber: 'P13',
+        renderData: CardRenderer.builder((b) => {
+          b.production((pb) => {
+            pb.titanium(2);
+          }).br;
+          b.megacredits(-5);
+        }),
+        description: 'Increase your titanium production 2 steps. Pay 5 MC.',
+      },
+    });
+  }
+  public canPlay(player: Player, _game: Game) {
+    return player.canAfford(5);
+  }
+  public play(player: Player, game: Game) {
+    player.addProduction(Resources.TITANIUM, 2);
+    game.defer(new SelectHowToPayDeferred(player, 5));
+    return undefined;
+  }
 }

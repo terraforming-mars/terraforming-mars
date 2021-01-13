a4494a1fd6b68a3854ba02cc7638d6a8d80a275a
Jeep <14239220+vsrisuknimit@users.noreply.github.com>
2021-01-13 07:59:36 -0500

    gamelog ref update

--- src/components/PlayerHome.ts
+++ src/components/PlayerHome.ts
@@ -268,8 +268,8 @@ export const PlayerHome = Vue.component('player-home', {
 
                 <players-overview class="player_home_block player_home_block--players nofloat:" :player="player" v-trim-whitespace id="shortkey-playersoverview"/>
 
-                <div class="player_home_block player_home_block--log player_home_block--hide_log nofloat">
-                    <dynamic-title v-if="player.players.length > 1" title="Game log" :color="player.color" :withAdditional="true" :additional="'generation ' + player.generation" id="shortkey-gamelog"/>
+                <div class="player_home_block player_home_block--log player_home_block--hide_log nofloat" id="shortkey-gamelog">
+                    <dynamic-title v-if="player.players.length > 1" title="Game log" :color="player.color" :withAdditional="true" :additional="'generation ' + player.generation"/>
                     <h2 v-else :class="'player_color_'+ player.color">
                         <span v-i18n>Game log</span>
                         <span class="label-additional" v-html="getGenerationText()"></span>

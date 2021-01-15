084a5ffa5be70a04e6f8c924d23018c210fb6b76
Jeep <14239220+vsrisuknimit@users.noreply.github.com>
2021-01-15 07:49:16 -0500

    remove v-html

--- src/components/GameHome.ts
+++ src/components/GameHome.ts
@@ -72,105 +72,41 @@ export const GameHome = Vue.component('game-home', {
     isPlayerUrlCopied: function(playerId: string): boolean {
       return playerId === this.urlCopiedPlayerId;
     },
-    getSoloDetail: function(gameOptions: GameOptions): string {
-      if (this.game !== undefined && this.game.players.length !== 1) return '';
-
-      if (gameOptions.soloTR) {
-        return '<div>Solo mode: 63 TR</div>';
-      } else {
-        return '<div>Solo mode: All parameters</div>';
-      }
+    isPoliticalAgendasOn: function() {
+      return (this.game !== undefined && this.game.gameOptions.politicalAgendasExtension !== AgendaStyle.STANDARD);
     },
-    getExpansionIcons: function(gameOptions: GameOptions): string {
-      let expansionIcons = 'Expansion: ';
-      if (gameOptions.venusNextExtension) {
-        expansionIcons += '<div class="create-game-expansion-icon expansion-icon-venus"></div>';
-      }
-      if (gameOptions.preludeExtension) {
-        expansionIcons += '<div class="create-game-expansion-icon expansion-icon-prelude"></div>';
-      }
-      if (gameOptions.coloniesExtension) {
-        expansionIcons += '<div class="create-game-expansion-icon expansion-icon-colony"></div>';
-      }
-      if (gameOptions.turmoilExtension) {
-        expansionIcons += '<div class="create-game-expansion-icon expansion-icon-turmoil"></div>';
-      }
-      if (gameOptions.promoCardsOption) {
-        expansionIcons += '<div class="create-game-expansion-icon expansion-icon-promo"></div>';
-      }
-      if (gameOptions.aresExtension) {
-        expansionIcons += '<div class="create-game-expansion-icon expansion-icon-ares"></div>';
-      }
-      if (gameOptions.communityCardsOption) {
-        expansionIcons += '<div class="create-game-expansion-icon expansion-icon-community"></div>';
-      }
-      if (gameOptions.politicalAgendasExtension !== AgendaStyle.STANDARD) {
-        expansionIcons += '<div class="create-game-expansion-icon expansion-icon-agendas"></div>';
-      }
-      return expansionIcons;
-    },
-    getBoardDetail: function(gameOptions: GameOptions): string {
-      let boardDetail = 'Board: ';
-      if (gameOptions.boardName === BoardName.ORIGINAL) {
-        boardDetail += '<div class="create-game-board-hexagon create-game-tharsis">&#x2B22;</div>' + gameOptions.boardName;
-      } else if (gameOptions.boardName === BoardName.HELLAS) {
-        boardDetail += '<div class="create-game-board-hexagon create-game-hellas">&#x2B22;</div>' + gameOptions.boardName;
-      } else if (gameOptions.boardName === BoardName.ELYSIUM) {
-        boardDetail += '<div class="create-game-board-hexagon create-game-elysium">&#x2B22;</div>' + gameOptions.boardName;
+    getBoardColorClass: function(boardName: string): string {
+      if (boardName === BoardName.ORIGINAL) {
+        return 'create-game-board-hexagon create-game-tharsis';
+      } else if (boardName === BoardName.HELLAS) {
+        return 'create-game-board-hexagon create-game-hellas';
+      } else if (boardName === BoardName.ELYSIUM) {
+        return 'create-game-board-hexagon create-game-elysium';
       } else {
-        boardDetail += gameOptions.boardName;
+        return 'create-game-board-hexagon create-game-random';
       }
-      if (gameOptions.shuffleMapOption) {
-        boardDetail += ' (randomized tiles)';
-      }
-      return boardDetail;
-    },
-    getVenusDetail: function(gameOptions: GameOptions): string {
-      let venusDetail = '';
-      if (gameOptions.solarPhaseOption) {
-        venusDetail += '<div>WGT: On</div>';
-      } else {
-        venusDetail += '<div>WGT: Off</div>';
-      }
-      if (gameOptions.requiresVenusTrackCompletion) {
-        venusDetail += '<div>Require Terraforming Venus to end the game</div>';
-      }
-      return venusDetail;
     },
     getMilestonesAwardsDetail: function(gameOptions: GameOptions): string {
-      if (this.game !== undefined && this.game.players.length === 1) return '';
-      let MAtype = '';
+      if (this.game !== undefined && this.game.players.length === 1) return 'N/A';
       if (gameOptions.randomMA === RandomMAOptionType.NONE) {
         if (gameOptions.includeVenusMA) {
-          MAtype = 'Board-defined + Hoverlord & Venuphile';
+          return 'Board-defined + Hoverlord & Venuphile';
         } else {
-          MAtype = 'Board-defined';
+          return 'Board-defined';
         }
       } else if (gameOptions.randomMA === RandomMAOptionType.LIMITED) {
         if (gameOptions.includeVenusMA) {
-          MAtype = 'Limited-synergy randomized (6 each)';
+          return 'Limited-synergy randomized (6 each)';
         } else {
-          MAtype = 'Limited-synergy randomized (5 each)';
+          return 'Limited-synergy randomized (5 each)';
         }
       } else {
         if (gameOptions.includeVenusMA) {
-          MAtype = 'Fully randomized (6 each)';
+          return 'Fully randomized (6 each)';
         } else {
-          MAtype = 'Fully randomized (5 each)';
+          return 'Fully randomized (5 each)';
         }
       }
-      return `<div>Milestones and Awards: ${MAtype}</div>`;
-    },
-    getDraftDetail: function(gameOptions: GameOptions): string {
-      let draftType = '';
-      if (gameOptions.draftVariant && gameOptions.initialDraftVariant) {
-        draftType = 'Initial + Research phase';
-      } else if (gameOptions.draftVariant && !gameOptions.initialDraftVariant) {
-        draftType = 'Draft: Research phase only';
-      } else {
-        draftType = 'Off';
-      }
-      return `Draft: ${draftType}`;
     },
     getGameConfigs: function(gameOptions: GameOptions): string {
       const configsDetail: Array<string> = [];
@@ -186,14 +122,7 @@ export const GameHome = Vue.component('game-home', {
       if (gameOptions.undoOption) {
         configsDetail.push('undo');
       }
-      return (configsDetail.length !== 0) ? 'Game configs: ' + configsDetail.join(', ') : '';
-    },
-    getBannedCards: function(gameOptions: GameOptions): string {
-      if (gameOptions.cardsBlackList.length === 0) {
-        return '';
-      } else {
-        return 'Banned cards: ' + gameOptions.cardsBlackList.join(', ');
-      }
+      return (configsDetail.length !== 0) ? configsDetail.join(', ') : '';
     },
   },
   template: `
@@ -211,15 +140,40 @@ export const GameHome = Vue.component('game-home', {
         </ul>
         <br>
         <div v-if="game !== undefined">
-          <h2>Game settings</h2>
-          <div v-html="getSoloDetail(game.gameOptions)"></div>
-          <div v-html="getExpansionIcons(game.gameOptions)"></div>
-          <div v-html="getBoardDetail(game.gameOptions)"></div>
-          <div v-html="getMilestonesAwardsDetail(game.gameOptions)"></div>
-          <div v-html="getVenusDetail(game.gameOptions)"></div>
-          <div v-html="getDraftDetail(game.gameOptions)"></div>
-          <div v-html="getGameConfigs(game.gameOptions)"></div>
-          <div v-html="getBannedCards(game.gameOptions)"></div>
+          
+        <h2>Game settings</h2>
+          <div>Expansion:
+            <div v-if="game.gameOptions.venusNextExtension" class="create-game-expansion-icon expansion-icon-venus"></div>
+            <div v-if="game.gameOptions.preludeExtension" class="create-game-expansion-icon expansion-icon-prelude"></div>
+            <div v-if="game.gameOptions.coloniesExtension" class="create-game-expansion-icon expansion-icon-colony"></div>
+            <div v-if="game.gameOptions.turmoilExtension" class="create-game-expansion-icon expansion-icon-turmoil"></div>
+            <div v-if="game.gameOptions.promoCardsOption" class="create-game-expansion-icon expansion-icon-promo"></div>
+            <div v-if="game.gameOptions.aresExtension" class="create-game-expansion-icon expansion-icon-ares"></div>
+            <div v-if="game.gameOptions.communityCardsOption" class="create-game-expansion-icon expansion-icon-community"></div>
+            <div v-if="isPoliticalAgendasOn()" class="create-game-expansion-icon expansion-icon-agendas"></div>
+          </div>
+          
+          <div>Board:
+            <span>{{ game.gameOptions.boardName }}</span>
+            <span v-if="game.gameOptions.shuffleMapOption" >&nbsp;(randomized tiles)</span>
+            <span :class="getBoardColorClass(game.gameOptions.boardName)">&#x2B22;</span>
+          </div>
+          
+          <div v-if="game.gameOptions.solarPhaseOption">WGT: On</div>
+          <div v-else>WGT: Off</div>
+          <div v-if="game.gameOptions.requiresVenusTrackCompletion">Require Terraforming Venus to end the game</div>
+
+          <div>Milestones and Awards: <span>{{ getMilestonesAwardsDetail(game.gameOptions) }}</span></div>
+          
+          <div>Draft:
+            <span v-if="game.gameOptions.draftVariant && game.gameOptions.initialDraftVariant">Initial + Research phase</span>
+            <span v-else-if="game.gameOptions.draftVariant && !game.gameOptions.initialDraftVariant">Research phase only</span>
+            <span v-else>Off</span>
+          </div>
+
+          <div>Game configs: {{ getGameConfigs(game.gameOptions) }}</div>
+
+          <div>Banned cards: {{ game.gameOptions.cardsBlackList.join(', ') }}</div>
         </div>
       </div>
     `,

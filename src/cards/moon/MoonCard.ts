3cb0d573f36f55c6756582ec3dd83357000cc307
Robert Konigsberg <kberg@users.noreply.github.com>
2021-01-24 21:38:03 -0500

    Update MoonCard.ts

--- src/cards/moon/MoonCard.ts
+++ src/cards/moon/MoonCard.ts
@@ -7,8 +7,7 @@ import {IMoonCard} from './IMoonCard';
 
 export interface MoonCardProperties {
   reserveUnits?: Units,
-  // Really only interested in MOON_COLONY, MOON_MINE and MOON_ROAD.
-  tilesBuilt?: Array<TileType>
+  tilesBuilt?: Array<TileType.MOON_COLONY | TileType.MOON_MINE | TileType.MOON_ROAD>
 }
 
 export abstract class MoonCard extends Card implements IProjectCard, IMoonCard {

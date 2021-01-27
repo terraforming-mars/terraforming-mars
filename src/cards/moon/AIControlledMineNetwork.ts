79b508e675568d3035d6408c0d5fd61e2cfc0087
Robert Konigsberg <konigsberg@gmail.com>
2021-01-26 21:42:42 -0500

    Update AIControlledMineNetwork to the static card pattern.

--- src/cards/moon/AIControlledMineNetwork.ts
+++ src/cards/moon/AIControlledMineNetwork.ts
@@ -1,26 +1,29 @@
 import {CardName} from '../../CardName';
 import {Player} from '../../Player';
 import {CardType} from '../CardType';
-import {IProjectCard} from '../IProjectCard';
 import {Tags} from '../Tags';
-import {CardMetadata} from '../CardMetadata';
 import {MoonExpansion} from '../../moon/MoonExpansion';
 import {CardRenderer} from '../render/CardRenderer';
 import {CardRequirements} from '../CardRequirements';
+import {Card} from '../Card';
 
-export class AIControlledMineNetwork implements IProjectCard {
-  public cost = 6;
-  public tags = [Tags.SCIENCE];
-  public cardType = CardType.AUTOMATED;
-  public name = CardName.AI_CONTROLLED_MINE_NETWORK;
+export class AIControlledMineNetwork extends Card {
+  constructor() {
+    super({
+      name: CardName.AI_CONTROLLED_MINE_NETWORK,
+      cardType: CardType.AUTOMATED,
+      tags: [Tags.SCIENCE],
+      cost: 6,
 
-  public readonly metadata: CardMetadata = {
-    description: 'Requires Logistic Rate to be 2 or higher. Raise Logistic Rate 1 step',
-    cardNumber: 'M32',
-    requirements: CardRequirements.builder((b) => b.logisticRate(2)),
-    renderData: CardRenderer.builder((b) => {
-      b.moonLogisticsRate(1);
-    }),
+      metadata: {
+        description: 'Requires Logistic Rate to be 2 or higher. Raise Logistic Rate 1 step',
+        cardNumber: 'M32',
+        requirements: CardRequirements.builder((b) => b.logisticRate(2)),
+        renderData: CardRenderer.builder((b) => {
+          b.moonLogisticsRate(1);
+        }),
+      },
+    });
   };
 
   public canPlay(player: Player): boolean {

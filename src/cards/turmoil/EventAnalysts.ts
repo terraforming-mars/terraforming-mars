d53d506b40d6fd085aa5cdc604822add4b465cf7
hbofolts1 <brian.folts@hbo.com>
2021-01-27 18:45:04 -0800

    static card properties for first turmoil cards

--- src/cards/turmoil/EventAnalysts.ts
+++ src/cards/turmoil/EventAnalysts.ts
@@ -1,18 +1,31 @@
 import {IProjectCard} from '../IProjectCard';
 import {Tags} from '../Tags';
+import {Card} from '../Card';
 import {CardName} from '../../CardName';
 import {CardType} from '../CardType';
 import {Player} from '../../Player';
 import {PartyName} from '../../turmoil/parties/PartyName';
-import {CardMetadata} from '../CardMetadata';
 import {CardRenderer} from '../render/CardRenderer';
 import {CardRequirements} from '../CardRequirements';
 
-export class EventAnalysts implements IProjectCard {
-  public cost = 5;
-  public tags = [Tags.SCIENCE];
-  public name = CardName.EVENT_ANALYSTS;
-  public cardType = CardType.ACTIVE;
+export class EventAnalysts extends Card implements IProjectCard {
+  constructor() {
+    super({
+      cardType: CardType.ACTIVE,
+      name: CardName.EVENT_ANALYSTS,
+      tags: [Tags.SCIENCE],
+      cost: 5,
+
+      metadata: {
+        description: 'Requires that Scientists are ruling or that you have 2 delegates there.',
+        cardNumber: 'T05',
+        requirements: CardRequirements.builder((b) => b.party(PartyName.SCIENTISTS)),
+        renderData: CardRenderer.builder((b) => b.effect('You have +1 influence.', (be) => {
+          be.startEffect.influence(1);
+        })),
+      },
+    });
+  }
 
   public canPlay(player: Player): boolean {
     if (player.game.turmoil !== undefined) {
@@ -27,13 +40,4 @@ export class EventAnalysts implements IProjectCard {
     }
     return undefined;
   }
-
-  public metadata: CardMetadata = {
-    description: 'Requires that Scientists are ruling or that you have 2 delegates there.',
-    cardNumber: 'T05',
-    requirements: CardRequirements.builder((b) => b.party(PartyName.SCIENTISTS)),
-    renderData: CardRenderer.builder((b) => b.effect('You have +1 influence.', (be) => {
-      be.startEffect.influence(1);
-    })),
-  };
 }

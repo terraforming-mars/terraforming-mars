48626c8335aa2b8156d4c91907e6005d845cf389
Robert Konigsberg <konigsberg@gmail.com>
2021-01-27 19:39:10 -0500

    Add BasicInfrastructure

---
+++ src/moon/MoonCards.ts
@@ -0,0 +1,14 @@
+import {CardName} from '../CardName';
+
+export class MoonCards {
+  public static scienceCardsWith2VPorLess: Array<CardName> = [
+    CardName.OLYMPUS_CONFERENCE,
+    CardName.COPERNICUS_TOWER,
+    CardName.LUNA_ARCHIVES,
+    CardName.PRIDE_OF_THE_EARTH_ARKSHIP,
+  ];
+  otherScienceCards: Array<CardName> = [
+    CardName.PHYSICS_COMPLEX,
+    CardName.SEARCH_FOR_LIFE,
+  ];
+}

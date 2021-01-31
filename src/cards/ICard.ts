59041574ca709fb3f37b90bd818cb0e41f4b5e25
sienmich <sienmich@gmail.com>
2021-01-31 02:01:48 +0100

    Moved requirements from Card.metadata to the Card

--- src/cards/ICard.ts
+++ src/cards/ICard.ts
@@ -17,6 +17,7 @@ import {ResourceType} from '../ResourceType';
 import {CardName} from '../CardName';
 import {CardMetadata} from './CardMetadata';
 import {StandardProjectCard} from './StandardProjectCard';
+import {CardRequirements} from './CardRequirements';
 
 export interface IActionCard {
     action: (player: Player) => OrOptions | SelectOption | AndOptions | SelectAmount | SelectCard<ICard> | SelectCard<IProjectCard> | SelectHowToPay | SelectPlayer | SelectSpace | undefined;
@@ -44,6 +45,7 @@ export interface ICard {
     resourceCount?: number;
     cost?: number;
     cardType: CardType;
+    requirements?: CardRequirements;
     metadata: CardMetadata;
     warning?: string | Message;
 }

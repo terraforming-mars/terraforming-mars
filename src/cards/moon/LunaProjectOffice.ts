1acf822252e84ece48e49deefafea1e285d3b38b
Robert Konigsberg <konigsberg@gmail.com>
2021-01-30 21:07:28 -0500

    Make LunaProjectOffice static.

--- src/cards/moon/LunaProjectOffice.ts
+++ src/cards/moon/LunaProjectOffice.ts
@@ -3,16 +3,26 @@ import {Player} from '../../Player';
 import {CardType} from '../CardType';
 import {IProjectCard} from '../IProjectCard';
 import {Tags} from '../Tags';
-import {CardMetadata} from '../CardMetadata';
 import {CardRenderer} from '../render/CardRenderer';
 import {Cards} from '../Cards';
 import {MoonExpansion} from '../../moon/MoonExpansion';
+import {Card} from '../Card';
 
-export class LunaProjectOffice implements IProjectCard {
-  public cost = 4;
-  public tags = [Tags.SCIENCE];
-  public cardType = CardType.AUTOMATED;
-  public name = CardName.LUNA_PROJECT_OFFICE;
+export class LunaProjectOffice extends Card implements IProjectCard {
+  constructor() {
+    super({
+      name: CardName.LUNA_PROJECT_OFFICE,
+      cardType: CardType.AUTOMATED,
+      tags: [Tags.SCIENCE],
+      cost: 4,
+
+      metadata: {
+        description: 'Requires 2 science tags. / DRAW 5 CARDS DURING THE RESEARCH PHASE FOR THE NEXT 2 GENERATIONS.',
+        cardNumber: 'M20',
+        renderData: CardRenderer.builder((_b) => {}),
+      },
+    });
+  };
   public resourceCount = 0;
 
   public canPlay(player: Player): boolean {
@@ -37,10 +47,4 @@ export class LunaProjectOffice implements IProjectCard {
     this.resourceCount = 2;
     return undefined;
   }
-
-  public readonly metadata: CardMetadata = {
-    description: 'Requires 2 science tags. / DRAW 5 CARDS DURING THE RESEARCH PHASE FOR THE NEXT 2 GENERATIONS.',
-    cardNumber: 'M20',
-    renderData: CardRenderer.builder((_b) => {}),
-  };
 }

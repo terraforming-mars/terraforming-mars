3d1a77016cc788b466602af5fa7d17cfca878dc0
Robert Konigsberg <konigsberg@gmail.com>
2021-02-01 12:52:15 -0500

    Use modern Card definition for ThoriumRush

--- src/cards/moon/ThoriumRush.ts
+++ src/cards/moon/ThoriumRush.ts
@@ -3,18 +3,30 @@ import {Player} from '../../Player';
 import {CardType} from '../CardType';
 import {IProjectCard} from '../IProjectCard';
 import {Tags} from '../Tags';
-import {CardMetadata} from '../CardMetadata';
 import {CardRenderer} from '../render/CardRenderer';
 import {PlaceMoonColonyTile} from '../../moon/PlaceMoonColonyTile';
 import {PlaceMoonRoadTile} from '../../moon/PlaceMoonRoadTile';
 import {PlaceMoonMineTile} from '../../moon/PlaceMoonMineTile';
+import {Card} from '../Card';
 
-export class ThoriumRush implements IProjectCard {
-  public cost = 39;
-  public tags = [Tags.MOON, Tags.BUILDING, Tags.EVENT];
-  public cardType = CardType.EVENT;
-  public name = CardName.THORIUM_RUSH;
+export class ThoriumRush extends Card implements IProjectCard {
+  constructor() {
+    super({
+      name: CardName.THORIUM_RUSH,
+      cardType: CardType.EVENT,
+      tags: [Tags.MOON, Tags.BUILDING, Tags.EVENT],
+      cost: 39,
 
+      metadata: {
+        description: 'Place 1 colony tile, 1 mining tile and 1 road tile on the Moon. ' +
+        'Raise Colony Rate, Mining Rate and Logistic Rate 1 step.',
+        cardNumber: 'M56',
+        renderData: CardRenderer.builder((b) => {
+          b.moonColony().moonMine().moonRoad();
+        }),
+      },
+    });
+  };
   public canPlay(): boolean {
     return true;
   }
@@ -25,13 +37,4 @@ export class ThoriumRush implements IProjectCard {
     player.game.defer(new PlaceMoonRoadTile(player));
     return undefined;
   }
-
-  public readonly metadata: CardMetadata = {
-    description: 'Place 1 colony tile, 1 mining tile and 1 road tile on the Moon. ' +
-      'Raise Colony Rate, Mining Rate and Logistic Rate 1 step.',
-    cardNumber: 'M56',
-    renderData: CardRenderer.builder((b) => {
-      b.moonColony().moonMine().moonRoad();
-    }),
-  };
 }

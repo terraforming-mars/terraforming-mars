1f440f37dfede0953dbcfcbbfadb29218e796188
Robert Konigsberg <konigsberg@gmail.com>
2021-02-01 01:25:48 -0500

    Make LunaConference metadata static

--- src/cards/moon/LunaConference.ts
+++ src/cards/moon/LunaConference.ts
@@ -3,18 +3,33 @@ import {Player} from '../../Player';
 import {CardType} from '../CardType';
 import {IProjectCard} from '../IProjectCard';
 import {Tags} from '../Tags';
-import {CardMetadata} from '../CardMetadata';
 import {CardRenderer} from '../render/CardRenderer';
 import {PartyName} from '../../turmoil/parties/PartyName';
 import {MoonExpansion} from '../../moon/MoonExpansion';
 import {TileType} from '../../TileType';
 import {CardRequirements} from '../CardRequirements';
+import {Card} from '../Card';
 
-export class LunaConference implements IProjectCard {
-  public cost = 5;
-  public tags = [Tags.SCIENCE, Tags.MOON, Tags.EVENT];
-  public cardType = CardType.EVENT;
-  public name = CardName.LUNA_CONFERENCE;
+export class LunaConference extends Card implements IProjectCard {
+  constructor() {
+    super({
+      name: CardName.LUNA_CONFERENCE,
+      cardType: CardType.EVENT,
+      tags: [Tags.SCIENCE, Tags.MOON, Tags.EVENT],
+      cost: 5,
+
+      metadata: {
+        description: 'Requires that Scientists are ruling or that you have 2 delegates there. ' +
+        'Gain 2 MC per road tile on the Moon. Gain 2MC per colony tile on the Moon.',
+        cardNumber: 'M58',
+        requirements: CardRequirements.builder((b) => b.party(PartyName.SCIENTISTS)),
+        renderData: CardRenderer.builder((b) => {
+          b.megacredits(2).slash().tile(TileType.MOON_ROAD, false).br;
+          b.megacredits(2).slash().tile(TileType.MOON_COLONY, false).asterix().br;
+        }),
+      },
+    });
+  };
 
   public canPlay(player: Player): boolean {
     if (player.game.turmoil === undefined) {
@@ -32,15 +47,4 @@ export class LunaConference implements IProjectCard {
     player.megaCredits += (moonRoadCount + moonColonyCount) * 2;
     return undefined;
   }
-
-  public readonly metadata: CardMetadata = {
-    description: 'Requires that Scientists are ruling or that you have 2 delegates there. ' +
-      'Gain 2 MC per road tile on the Moon. Gain 2MC per colony tile on the Moon.',
-    cardNumber: 'M58',
-    requirements: CardRequirements.builder((b) => b.party(PartyName.SCIENTISTS)),
-    renderData: CardRenderer.builder((b) => {
-      b.megacredits(2).slash().tile(TileType.MOON_ROAD, false).br;
-      b.megacredits(2).slash().tile(TileType.MOON_COLONY, false).asterix().br;
-    }),
-  };
 }

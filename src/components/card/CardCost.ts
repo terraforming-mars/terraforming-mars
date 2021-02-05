d036f6e5eafeef51b1d025e5b02dd8b11fb75ea3
sienmich <sienmich@gmail.com>
2021-02-05 21:12:02 +0100

    Jeep's layout suggestion

--- src/components/card/CardCost.ts
+++ src/components/card/CardCost.ts
@@ -12,15 +12,16 @@ export const CardCost = Vue.component('CardCost', {
   methods: {
     getClasses: function(): string {
       const classes = ['card-cost'];
-      if (this.amount === undefined) {
-        classes.push('visibility-hidden');
-      } else if (this.newCost !== undefined && this.newCost !== this.amount) {
-        classes.push('reduced-cost');
-      }
       return classes.join(' ');
+    },
+    displayTwoCosts: function(): boolean {
+      return this.newCost !== undefined && this.newCost !== this.amount;
     },
   },
   template: `
+    <div v-if="amount !== undefined">
         <div :class="getClasses()">{{ newCost !== undefined ? newCost : (amount === null ? 0 : amount) }}</div>
+        <div :class="'card-old-cost'" v-if="displayTwoCosts()">{{amount}}</div>
+    </div>
     `,
 });

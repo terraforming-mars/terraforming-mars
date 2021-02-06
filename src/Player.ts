99855feaffc11b593aeb048516c0943d2d02c689
sienmich <sienmich@gmail.com>
2021-02-06 03:24:04 +0100

    Update src/Player.ts

--- src/Player.ts
+++ src/Player.ts
@@ -834,7 +834,7 @@ export class Player implements ISerializable<SerializedPlayer> {
       const mappedCards: Array<ICard> = [];
       for (const cardName of input[0]) {
         mappedCards.push(this.getCard(pi.cards, cardName));
-        if (pi.enabled?.[pi.cards.map((card) => card.name).indexOf(cardName)] === false) {
+        if (pi.enabled?.[pi.cards.findIndex((card) => card.name === cardName)] === false) {
           throw new Error('Selected unavailable card');
         }
       }

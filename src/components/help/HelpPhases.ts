e58a717626416f0bff08f902bc352895006f9b18
Jeep <14239220+vsrisuknimit@users.noreply.github.com>
2021-02-10 11:13:11 -0500

    add v-i18n support

--- src/components/help/HelpPhases.ts
+++ src/components/help/HelpPhases.ts
@@ -12,13 +12,13 @@ export const HelpPhases = Vue.component('help-phases', {
 
         <li>
         <input type="checkbox" id="initial-draft">
-        <label for="initial-draft" class="first">Initial Drafting (optional variant)</label>
+        <label for="initial-draft" class="first"><span v-i18n>Initial Drafting (optional variant)</span></label>
           <ul>
-            <li>Draw 3 corporation cards</li>
-            <li>Draw 4 prelude cards <div class="expansion-icon expansion-icon-prelude"></li>
-            <li>Draw 5 project cards. Draft. Pick one card and pass to the player above in turn order. Repeat until you have 5 cards.</li>
-            <li>Draw 5 more project cards. Draft and pass to the player below in turn order instead.</li>
-            <li>Draft the prelude cards. Pick one card and pass to the player above in turn order. Repeat until you have 4 preludes.
+            <li v-i18n>Draw 3 corporation cards</li>
+            <li v-i18n>Draw 4 prelude cards <div class="expansion-icon expansion-icon-prelude"></li>
+            <li v-i18n>Draw 5 project cards. Draft. Pick one card and pass to the player above in turn order. Repeat until you have 5 cards.</li>
+            <li v-i18n>Draw 5 more project cards. Draft and pass to the player below in turn order instead.</li>
+            <li><span v-i18n>Draft the prelude cards. Pick one card and pass to the player above in turn order. Repeat until you have 4 preludes.</span>
               <div class="expansion-icon expansion-icon-prelude"></div>
             </li>
           </ul>
@@ -26,84 +26,84 @@ export const HelpPhases = Vue.component('help-phases', {
 
         <li>
           <input type="checkbox" id="initial-research">
-          <label for="initial-research">Initial Research Phase</label>
+          <label for="initial-research" v-i18n><span v-i18n>Initial Research Phase</span></label>
           <ul>
-            <li>Select which corporation, 2 preludes and project cards to keep.</li>
-            <li>In turn order, play the corporation and pay 3 mc for each of the kept project cards.</li>
-            <li>In turn order, play both preludes. <div class="expansion-icon expansion-icon-prelude"></div></li>
+            <li v-i18n>Select which corporation, 2 preludes and project cards to keep.</li>
+            <li v-i18n>In turn order, play the corporation and pay 3 mc for each of the kept project cards.</li>
+            <li v-i18n>In turn order, play both preludes. <div class="expansion-icon expansion-icon-prelude"></div></li>
           </ul>
         </li>
 
         <li>
           <input type="checkbox" id="generation">
-          <label for="generation">Each generation</label>
+          <label for="generation"><span v-i18n>Each generation</span></label>
           <ul>
             <li>
             <input type="checkbox" id="action-phase">
-            <label for="action-phase" class="last">Action phase (Turmoil policy in effect <div class="expansion-icon expansion-icon-turmoil"></div>) </label> 
+            <label for="action-phase" class="last"><span v-i18n>Action phase</span> (<span v-i18n>Turmoil policy in effect</span> <div class="expansion-icon expansion-icon-turmoil"></div>) </label> 
             <ul>
-              <li>Take 1 or 2 actions (mandatory 2 actions if playing with fast mode):
+              <li><span v-i18n>Take 1 or 2 actions (mandatory 2 actions if playing with fast mode):</span>
                 <ul>
-                  <li>play card</li>
-                  <li>use active (blue) card action</li>
-                  <li>standard project</li>
-                  <li>convert plants to greenery</li>
-                  <li>convert heat to temperature</li>
-                  <li>claim milestone</li>
-                  <li>fund award</li>
-                  <li>trade with 9 mc, 3 Ti, or 3 energy <div class="expansion-icon expansion-icon-colony"></div></li>
-                  <li>lobby: one is free, 5 mc per delegate after <div class="expansion-icon expansion-icon-turmoil"></div></li>
+                  <li v-i18n>play card</li>
+                  <li v-i18n>use active (blue) card action</li>
+                  <li v-i18n>standard project</li>
+                  <li v-i18n>convert plants to greenery</li>
+                  <li v-i18n>convert heat to temperature</li>
+                  <li v-i18n>claim milestone</li>
+                  <li v-i18n>fund award</li>
+                  <li><span v-i18n>trade with 9 mc, 3 Ti, or 3 energy<span> <div class="expansion-icon expansion-icon-colony"></div></li>
+                  <li><span v-i18n>lobby: one is free, 5 mc per delegate after<span> <div class="expansion-icon expansion-icon-turmoil"></div></li>
                 </ul>
               </li>
-              <li>Pass for the generation</li>
+              <li v-i18n>Pass for the generation</li>
             </ul>
             </li>
 
             <li>
             <input type="checkbox" id="production-phase">
-            <label for="production-phase">Production Phase</label>
+            <label for="production-phase"><span v-i18n>Production Phase</span></label>
             <ul>
-              <li> Energy becomes heat </li>
-              <li> Produce resources </li>
+              <li v-i18n>Energy becomes heat</li>
+              <li v-i18n>Produce resources</li>
             </ul>
             </li>
 
             <li>
             <input type="checkbox" id="solar-phase">
-            <label for="solar-phase">Solar Phase</label>
+            <label for="solar-phase"><span v-i18n>Solar Phase</span></label>
             <ul>
-              <li> i. Game end check 
+              <li><span v-i18n>i. Game end check</span>
                 <ul>
-                  <li>If temperature, oxygen, and oceans are maxed, skip the rest of solar phase to end game phase. </li>
-                  <li>(If playing solo, Venus track must be completed as well.) </li>
+                  <li v-i18n>If temperature, oxygen, and oceans are maxed, skip the rest of solar phase to end game phase. </li>
+                  <li v-i18n>(If playing solo, Venus track must be completed as well.) </li>
                 </ul>
-              <li> ii. World Government Terraforming (start player chooses) <div class="expansion-icon expansion-icon-venus"></div></li>
-              <li> iii. Colony production
+              <li><span v-i18n>ii. World Government Terraforming (start player chooses)</span> <div class="expansion-icon expansion-icon-venus"></div></li>
+              <li><span v-i18n>iii. Colony production</span>
                 <ul>
-                  <li>Trade fleets return</li>
-                  <li>Each colony marker goes up one step</li>
+                  <li v-i18n>Trade fleets return</li>
+                  <li v-i18n>Each colony marker goes up one step</li>
                 </ul>
               </li>
 
-              <li> iv. Turmoil 
+              <li><span v-i18n>iv. Turmoil</span>
                 <ul>
-                  <li> 1. TR Revision: All players lose 1 TR </li>
-                  <li> 2. Global Event: Perform the current Global Event </li>
-                  <li> 3. New Government
+                  <li v-i18n>1. TR Revision: All players lose 1 TR</li>
+                  <li v-i18n>2. Global Event: Perform the current Global Event</li>
+                  <li><span v-i18n>3. New Government</span>
                     <ul>
-                    <li> Change policy tile </li>
-                    <li> Perform the ruling bonus of the dominant party </li>
-                    <li> The dominant party leader becomes chairman, earning 1 TR. </li>
-                    <li> Move the old chairman and all other delegates from the dominant party to the reserves. </li>
-                    <li> Shift the dominance marker. In case of tie, the party closest to the left of the previous dominant party becomes dominant. </li>
-                    <li> Restore lobby </li>
+                    <li v-i18n>Change policy tile</li>
+                    <li v-i18n>Rerform the ruling bonus of the dominant party</li>
+                    <li v-i18n>The dominant party leader becomes chairman, earning 1 TR.</li>
+                    <li v-i18n>Move the old chairman and all other delegates from the dominant party to the reserves.</li>
+                    <li v-i18n>Shift the dominance marker. In case of tie, the party closest to the left of the previous dominant party becomes dominant.</li>
+                    <li v-i18n>Restore lobby</li>
                     </ul>
                   </li>
-                  <li> 4. Changing Times
+                  <li><span v-i18n>4. Changing Times</span>
                     <ul>
-                      <li> Move the coming (middle) Global Event to current position (right). Add its lower neutral delegate.</li>
-                      <li> Move the distant (left) Global Event to coming position (middle) </li>
-                      <li> Reveal a new Global Event in the distant (left) position. Add its top neutral delegate </li>
+                      <li v-i18n>Move the coming (middle) Global Event to current position (right). Add its lower neutral delegate.</li>
+                      <li v-i18n>Move the distant (left) Global Event to coming position (middle)</li>
+                      <li v-i18n>Reveal a new Global Event in the distant (left) position. Add its top neutral delegate</li>
                     </ul>
                   </li>
                 </ul>
@@ -117,11 +117,11 @@ export const HelpPhases = Vue.component('help-phases', {
 
         <li>
           <input type="checkbox" id="end-game">
-          <label for="end-game">End Game</label>
+          <label for="end-game"><span v-i18n>End Game</span></label>
           <ul>
-            <li> In turn order, convert plants to greeneries. </li>
-            <li> Assign awards </li>
-            <li> Score: TR + tiles + card + milestones + awards + (1 VP per chairman and party leader <div class="expansion-icon expansion-icon-turmoil"></div>)</li>
+            <li v-i18n>In turn order, convert plants to greeneries.</li>
+            <li v-i18n>Assign awards</li>
+            <li><span v-i18n> Score: TR + tiles + card + milestones + awards + (1 VP per chairman and party leader</span> <div class="expansion-icon expansion-icon-turmoil"></div>)</li>
           </ul>
         </li>
 

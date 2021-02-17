3d9bb3262fd8f6fa2600f1763de1e4e965b7729f
Robert Konigsberg <konigsberg@gmail.com>
2021-02-16 23:54:42 -0500

    Add Tutorial Mode preferences.

--- compile-vue-templates.js
+++ compile-vue-templates.js
@@ -171,7 +171,8 @@ checkComponent(
     'ui', 'hide_corporation', 'hide_hand', 'hide_cards', 'hide_awards_and_milestones', 'hide_tag_overview',
     'hide_turnorder', 'hide_corporation_names', , 'hide_top_bar', 'small_cards', 'remove_background', 'magnify_cards',
     'magnify_card_descriptions', 'show_alerts', 'hide_ma_scores', 'hide_non_blue_cards', 'hide_log',
-    'lang', 'langs', 'enable_sounds', 'smooth_scrolling', 'hide_tile_confirmation', 'show_card_number', 'show_discount_on_cards'
+    'lang', 'langs', 'enable_sounds', 'smooth_scrolling', 'hide_tile_confirmation', 'show_card_number', 'show_discount_on_cards',
+    'tutorial_mode',
   ],
 );
 checkComponent(

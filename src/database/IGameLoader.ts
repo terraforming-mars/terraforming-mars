b787f0d283786d54226fe09c93035827ed09da8d
Robert Konigsberg <kberg@users.noreply.github.com>
2021-02-25 21:31:28 -0500

    Write test for ApiHandler (#2827)
    * Add ApiGame as a handler.
    
    * Adding test for ApiGame.
    
    But this required a bunch of changes, including faking the GameLoader.
    So this isn't bad.
    
    * Replace regex parsing with ctx.url.searchParams.

---
+++ src/database/IGameLoader.ts
@@ -0,0 +1,22 @@
+import {Game, GameId} from '../Game';
+import {PlayerId} from '../Player';
+
+type LoadCallback = (game: Game | undefined) => void;
+
+/**
+ * Loads games from javascript memory or database
+ * Loads games from database sequentially as needed
+ */
+export interface IGameLoader {
+  add(game: Game): void;
+  getLoadedGameIds(): Array<string>;
+  /**
+   * Gets a game from javascript memory or pulls from database if needed.
+   * @param {GameId} gameId the id of the game to retrieve
+   * @param {boolean} bypassCache always pull from database
+   * @param {LoadCallback} cb called with game when available
+   */
+  getByGameId(gameId: GameId, bypassCache: boolean, cb: LoadCallback): void;
+  getByPlayerId(playerId: PlayerId, cb: LoadCallback): void;
+  restoreGameAt(gameId: GameId, saveId: number, cb: LoadCallback): void;
+}

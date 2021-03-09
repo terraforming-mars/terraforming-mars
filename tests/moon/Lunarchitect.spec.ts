59402bf5c9f2411a463fd0b8e5d4e19f276badb7
Robert Konigsberg <kberg@users.noreply.github.com>
2021-03-08 21:31:25 -0500

    Milestones and Awards for the Moon (#2912)
    * Add Moon milestones and awards.
    
    * Register Moon milestones and awards.
    
    * Add tests for Moon MAs, and register the awards in the list of ALL_AWARDS.2

---
+++ tests/moon/Lunarchitect.spec.ts
@@ -0,0 +1,56 @@
+import {expect} from 'chai';
+import {Lunarchitect} from '../../src/moon/Lunarchitect';
+import {TestPlayers} from '../TestPlayers';
+import {Game} from '../../src/Game';
+import {MoonExpansion} from '../../src/moon/MoonExpansion';
+import {TestPlayer} from '../TestPlayer';
+import {setCustomGameOptions} from '../TestingUtils';
+import {TileType} from '../../src/TileType';
+
+const MOON_OPTIONS = setCustomGameOptions({moonExpansion: true});
+
+describe('Lunarchitect', function() {
+  let player: TestPlayer;
+  let otherPlayer: TestPlayer;
+
+  beforeEach(() => {
+    player = TestPlayers.BLUE.newPlayer();
+    otherPlayer = TestPlayers.PINK.newPlayer();
+    Game.newInstance('id', [player, otherPlayer], player, MOON_OPTIONS);
+  });
+
+  it('Basic test', function() {
+    const milestone = new Lunarchitect();
+    expect(milestone.canClaim(player)).is.not.true;
+    MoonExpansion.addTile(player, 'm01', {tileType: TileType.MOON_MINE});
+    expect(milestone.canClaim(player)).is.not.true;
+    MoonExpansion.addTile(player, 'm02', {tileType: TileType.MOON_MINE});
+    expect(milestone.canClaim(player)).is.not.true;
+    MoonExpansion.addTile(player, 'm03', {tileType: TileType.MOON_MINE});
+    expect(milestone.canClaim(player)).is.not.true;
+    MoonExpansion.addTile(player, 'm04', {tileType: TileType.MOON_MINE});
+    expect(milestone.canClaim(player)).is.not.true;
+    MoonExpansion.addTile(player, 'm05', {tileType: TileType.MOON_MINE});
+    expect(milestone.canClaim(player)).is.not.true;
+    MoonExpansion.addTile(player, 'm06', {tileType: TileType.MOON_MINE});
+    expect(milestone.canClaim(player)).is.true;
+  });
+
+
+  it('Other player tokens do not count', function() {
+    const milestone = new Lunarchitect();
+    expect(milestone.canClaim(player)).is.not.true;
+    MoonExpansion.addTile(player, 'm01', {tileType: TileType.MOON_MINE});
+    MoonExpansion.addTile(player, 'm02', {tileType: TileType.MOON_MINE});
+    MoonExpansion.addTile(player, 'm03', {tileType: TileType.MOON_MINE});
+    MoonExpansion.addTile(player, 'm04', {tileType: TileType.MOON_MINE});
+    MoonExpansion.addTile(player, 'm05', {tileType: TileType.MOON_MINE});
+    expect(milestone.canClaim(player)).is.not.true;
+
+    MoonExpansion.addTile(otherPlayer, 'm06', {tileType: TileType.MOON_MINE});
+    expect(milestone.canClaim(player)).is.not.true;
+
+    MoonExpansion.addTile(player, 'm07', {tileType: TileType.MOON_MINE});
+    expect(milestone.canClaim(player)).is.true;
+  });
+});

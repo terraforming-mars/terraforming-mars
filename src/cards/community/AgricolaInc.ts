3090bfa29c6a25f6c1b9603849e01766d7bfa2d9
algorithm99 <brian.folts@gmail.com>
2021-03-14 18:24:05 -0600

    more cards to static card properties

--- src/cards/community/AgricolaInc.ts
+++ src/cards/community/AgricolaInc.ts
@@ -2,58 +2,63 @@ import {CorporationCard} from '../corporation/CorporationCard';
 import {Player} from '../../Player';
 import {Tags} from '../Tags';
 import {Resources} from '../../Resources';
+import {Card} from '../Card';
 import {CardName} from '../../CardName';
 import {ITagCount} from '../../ITagCount';
 import {CardType} from '../CardType';
-import {CardMetadata} from '../CardMetadata';
 import {CardRenderer} from '../render/CardRenderer';
 import {CardRenderItemSize} from '../render/CardRenderItemSize';
 import {CardRenderDynamicVictoryPoints} from '../render/CardRenderDynamicVictoryPoints';
 
-export class AgricolaInc implements CorporationCard {
-    public name = CardName.AGRICOLA_INC;
-    public tags = [Tags.PLANT];
-    public startingMegaCredits: number = 40;
-    public cardType = CardType.CORPORATION;
+export class AgricolaInc extends Card implements CorporationCard {
+  constructor() {
+    super({
+      name: CardName.AGRICOLA_INC,
+      tags: [Tags.PLANT],
+      startingMegaCredits: 40,
+      cardType: CardType.CORPORATION,
 
-    public play(player: Player) {
-      player.addProduction(Resources.MEGACREDITS, 1);
-      player.addProduction(Resources.PLANTS, 1);
+      metadata: {
+        cardNumber: 'R36',
+        description: 'You start with 1 plant production, 1 MC production and 40 MC.',
+        renderData: CardRenderer.builder((b) => {
+          b.br.br;
+          b.production((pb) => pb.megacredits(1).plants(1)).nbsp.megacredits(40);
+          b.corpBox('effect', (ce) => {
+            ce.text('Effect: At game end, score -2 / 0 / 1 / 2 VP PER TAG TYPE for 0 / 1-2 / 3-4 / 5+ tags.', CardRenderItemSize.SMALL, true);
+          });
+        }),
+        victoryPoints: CardRenderDynamicVictoryPoints.questionmark(),
+      },
+    });
+  }
 
-      return undefined;
-    }
+  public play(player: Player) {
+    player.addProduction(Resources.MEGACREDITS, 1);
+    player.addProduction(Resources.PLANTS, 1);
 
-    public getVictoryPoints(player: Player): number {
-      const scorableTags : Array<Tags> = [Tags.CITY, Tags.EARTH, Tags.ENERGY, Tags.JOVIAN, Tags.MICROBE, Tags.PLANT, Tags.SCIENCE, Tags.SPACE, Tags.BUILDING, Tags.ANIMAL];
-      if (player.game.gameOptions.venusNextExtension) scorableTags.push(Tags.VENUS);
+    return undefined;
+  }
 
-      const playerTags : ITagCount[] = player.getAllTags();
-      let points = 0;
+  public getVictoryPoints(player: Player): number {
+    const scorableTags : Array<Tags> = [Tags.CITY, Tags.EARTH, Tags.ENERGY, Tags.JOVIAN, Tags.MICROBE, Tags.PLANT, Tags.SCIENCE, Tags.SPACE, Tags.BUILDING, Tags.ANIMAL];
+    if (player.game.gameOptions.venusNextExtension) scorableTags.push(Tags.VENUS);
 
-      scorableTags.forEach((tag) => {
-        const tagData = playerTags.find((data) => data.tag === tag);
+    const playerTags : ITagCount[] = player.getAllTags();
+    let points = 0;
 
-        if (tagData === undefined) {
-          points -= 2;
-        } else if (tagData.count === 3 || tagData.count === 4) {
-          points += 1;
-        } else if (tagData.count > 4) {
-          points += 2;
-        }
-      });
+    scorableTags.forEach((tag) => {
+      const tagData = playerTags.find((data) => data.tag === tag);
 
-      return points;
-    }
-    public metadata: CardMetadata = {
-      cardNumber: 'R36',
-      description: 'You start with 1 plant production, 1 MC production and 40 MC.',
-      renderData: CardRenderer.builder((b) => {
-        b.br.br;
-        b.production((pb) => pb.megacredits(1).plants(1)).nbsp.megacredits(40);
-        b.corpBox('effect', (ce) => {
-          ce.text('Effect: At game end, score -2 / 0 / 1 / 2 VP PER TAG TYPE for 0 / 1-2 / 3-4 / 5+ tags.', CardRenderItemSize.SMALL, true);
-        });
-      }),
-      victoryPoints: CardRenderDynamicVictoryPoints.questionmark(),
-    }
+      if (tagData === undefined) {
+        points -= 2;
+      } else if (tagData.count === 3 || tagData.count === 4) {
+        points += 1;
+      } else if (tagData.count > 4) {
+        points += 2;
+      }
+    });
+
+    return points;
+  }
 }
--- src/cards/venusNext/Viron.ts
+++ src/cards/venusNext/Viron.ts
@@ -3,67 +3,71 @@ import {Player} from '../../Player';
 import {Tags} from '../Tags';
 import {ICard} from '../ICard';
 import {SelectCard} from '../../inputs/SelectCard';
+import {Card} from '../Card';
 import {CardName} from '../../CardName';
 import {CardType} from '../CardType';
-import {CardMetadata} from '../CardMetadata';
 import {CardRenderer} from '../render/CardRenderer';
 
-export class Viron implements ICard, CorporationCard {
-    public name = CardName.VIRON;
-    public tags = [Tags.MICROBE];
-    public startingMegaCredits: number = 48;
-    public cardType = CardType.CORPORATION;
+export class Viron extends Card implements ICard, CorporationCard {
+  constructor() {
+    super({
+      name: CardName.VIRON,
+      tags: [Tags.MICROBE],
+      startingMegaCredits: 48,
+      cardType: CardType.CORPORATION,
 
-    private getActionCards(player: Player):Array<ICard> {
-      const result: Array<ICard> = [];
-      for (const playedCard of player.playedCards) {
-        if (
-          playedCard.action !== undefined &&
+      metadata: {
+        cardNumber: 'R12',
+        description: 'You start with 48 MC.',
+        renderData: CardRenderer.builder((b) => {
+          b.br.br.br;
+          b.megacredits(48);
+          b.corpBox('action', (ce) => {
+            ce.action('Use a blue card action that has already been used this generation.', (eb) => {
+              eb.empty().startAction.empty();
+            });
+          });
+        }),
+      },
+    });
+  }
+
+  private getActionCards(player: Player):Array<ICard> {
+    const result: Array<ICard> = [];
+    for (const playedCard of player.playedCards) {
+      if (
+        playedCard.action !== undefined &&
                     playedCard.canAct !== undefined &&
                     player.getActionsThisGeneration().has(playedCard.name) &&
                     playedCard.canAct(player)) {
-          result.push(playedCard);
-        }
+        result.push(playedCard);
       }
-      return result;
     }
+    return result;
+  }
 
-    public canAct(player: Player): boolean {
-      return this.getActionCards(player).length > 0 && !player.getActionsThisGeneration().has(this.name);
-    }
+  public canAct(player: Player): boolean {
+    return this.getActionCards(player).length > 0 && !player.getActionsThisGeneration().has(this.name);
+  }
 
-    public action(player: Player) {
-      if (this.getActionCards(player).length === 0 ) {
-        return undefined;
-      }
-
-      return new SelectCard(
-        'Perform again an action from a played card',
-        'Take action',
-        this.getActionCards(player),
-        (foundCards: Array<ICard>) => {
-          const foundCard = foundCards[0];
-          player.game.log('${0} used ${1} action with ${2}', (b) => b.player(player).card(foundCard).card(this));
-          return foundCard.action!(player);
-        },
-      );
-    }
-
-    public play() {
+  public action(player: Player) {
+    if (this.getActionCards(player).length === 0 ) {
       return undefined;
     }
 
-    public metadata: CardMetadata = {
-      cardNumber: 'R12',
-      description: 'You start with 48 MC.',
-      renderData: CardRenderer.builder((b) => {
-        b.br.br.br;
-        b.megacredits(48);
-        b.corpBox('action', (ce) => {
-          ce.action('Use a blue card action that has already been used this generation.', (eb) => {
-            eb.empty().startAction.empty();
-          });
-        });
-      }),
-    }
+    return new SelectCard(
+      'Perform again an action from a played card',
+      'Take action',
+      this.getActionCards(player),
+      (foundCards: Array<ICard>) => {
+        const foundCard = foundCards[0];
+        player.game.log('${0} used ${1} action with ${2}', (b) => b.player(player).card(foundCard).card(this));
+        return foundCard.action!(player);
+      },
+    );
+  }
+
+  public play() {
+    return undefined;
+  }
 }

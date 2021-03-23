06e9ab766ea1ecbd7cafcadd731e16f03ab0f59d
Danimir <dnbszbg@gmail.com>
2021-03-23 19:34:06 +0200

    fixed earth tag

--- src/cards/moon/EarthEmbassy.ts
+++ src/cards/moon/EarthEmbassy.ts
@@ -17,8 +17,7 @@ export class EarthEmbassy extends Card implements IProjectCard {
         cardNumber: 'M77',
         renderData: CardRenderer.builder((b) => {
           b.effect('After being played, when you perform an action, your Moon tags count as Earth tags, but not vice versa.', (eb) => {
-            // TODO(kberg): fix this viz. .earth() doesn't work.
-            eb.empty().startEffect.moon().nbsp.text(' = ').nbsp.text('earth');
+            eb.empty().startEffect.moon().equals().earth().played;
           });
         }),
       },

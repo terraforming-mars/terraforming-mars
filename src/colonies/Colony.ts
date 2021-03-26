a75256b41c0d0845f49f88af354b879882434a90
waichuan <nwai1990@hotmail.com>
2021-03-26 08:58:49 +0800

    Fix Leavitt colony bug

--- src/colonies/Colony.ts
+++ src/colonies/Colony.ts
@@ -185,7 +185,7 @@ export abstract class Colony implements SerializedColony {
         break;
 
       case ColonyBenefit.DRAW_CARDS_AND_BUY_ONE:
-        action = DrawCards.keepSome(player, 1, {paying: true});
+        action = DrawCards.keepSome(player, 1, {paying: true, logDrawnCard: true});
         break;
 
       case ColonyBenefit.DRAW_CARDS_AND_DISCARD_ONE:
@@ -194,7 +194,7 @@ export abstract class Colony implements SerializedColony {
         break;
 
       case ColonyBenefit.DRAW_CARDS_AND_KEEP_ONE:
-        action = DrawCards.keepSome(player, quantity, {keepMax: 1, logDrawnCard: true});
+        action = DrawCards.keepSome(player, quantity, {keepMax: 1});
         break;
 
       case ColonyBenefit.GAIN_CARD_DISCOUNT:

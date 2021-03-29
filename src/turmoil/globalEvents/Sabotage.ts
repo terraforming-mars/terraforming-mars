b947eefa3196824e241f1a96a7bf78e603535168
Jeep <14239220+vsrisuknimit@users.noreply.github.com>
2021-03-29 18:58:53 -0400

    Sabotage Global Event reduce only if has any

--- src/turmoil/globalEvents/Sabotage.ts
+++ src/turmoil/globalEvents/Sabotage.ts
@@ -12,8 +12,12 @@ export class Sabotage implements IGlobalEvent {
     public currentDelegate = PartyName.REDS;
     public resolve(game: Game, turmoil: Turmoil) {
       game.getPlayers().forEach((player) => {
-        player.addProduction(Resources.ENERGY, -1, game, undefined, true);
-        player.addProduction(Resources.STEEL, -1, game, undefined, true);
+        if (player.getProduction(Resources.ENERGY) >= 1) {
+          player.addProduction(Resources.ENERGY, -1, game, undefined, true);
+        }
+        if (player.getProduction(Resources.STEEL) >= 1) {
+          player.addProduction(Resources.STEEL, -1, game, undefined, true);
+        }
         player.setResource(Resources.STEEL, turmoil.getPlayerInfluence(player), game, undefined, true);
       });
     }

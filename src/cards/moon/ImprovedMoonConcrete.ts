d91a5f0ee1445ed0d70dd8449120158dd4879ba2
Robert Konigsberg <konigsberg@gmail.com>
2021-03-31 21:19:15 -0400

    Fix Improved Moon Concrete's benefits

--- src/cards/moon/ImprovedMoonConcrete.ts
+++ src/cards/moon/ImprovedMoonConcrete.ts
@@ -18,8 +18,8 @@ export class ImprovedMoonConcrete extends MoonCard implements IProjectCard {
         description: 'Spend 2 steel. Raise the Mining Rate 1 step.',
         cardNumber: 'M37',
         renderData: CardRenderer.builder((b) => {
-          b.effect('When you build a mine on the Moon, you spend 1 steel less.', (eb) => {
-            eb.moonMine().startEffect.minus().steel(1);
+          b.effect('When you build a mine on the Moon, you spend 1 titanium less.', (eb) => {
+            eb.moonMine().startEffect.minus().titanium(1);
           }).br;
           b.minus().steel(2).moonMiningRate();
         }),

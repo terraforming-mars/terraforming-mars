d9a7e9116429acf1b7fdf884c2857c7de8f9f5f9
Robert Konigsberg <konigsberg@gmail.com>
2021-04-01 21:46:52 -0400

    Introduce the notion of a private message to a player.
    This is only going to be used for helping players see what cards they
    were dealt, but as a demo, it's giving an introductory "good luck"
    message.

--- src/LogMessage.ts
+++ src/LogMessage.ts
@@ -1,8 +1,13 @@
 import {LogMessageType} from './LogMessageType';
 import {LogMessageData} from './LogMessageData';
 import {Message} from './Message';
+import {PlayerId} from './Player';
 
 export class LogMessage implements Message {
   public timestamp = Date.now();
-  constructor(public type: LogMessageType, public message: string, public data: Array<LogMessageData>) {}
+  constructor(
+    public type: LogMessageType,
+    public message: string,
+    public data: Array<LogMessageData>,
+    public playerId?: PlayerId) {}
 }

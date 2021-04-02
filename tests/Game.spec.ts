6bcfd67ad373aa0816494d665e0d973f42856073
Robert Konigsberg <kberg@users.noreply.github.com>
2021-04-02 19:29:10 -0400

    Space bonuses are granted en masse. (#3090)

--- tests/Game.spec.ts
+++ tests/Game.spec.ts
@@ -24,6 +24,8 @@ import {CardName} from '../src/CardName';
 import {Player} from '../src/Player';
 import {Color} from '../src/Color';
 import {RandomMAOptionType} from '../src/RandomMAOptionType';
+import {SpaceBonus} from '../src/SpaceBonus';
+import {TileType} from '../src/TileType';
 
 describe('Game', function() {
   it('should initialize with right defaults', function() {
@@ -487,6 +489,23 @@ describe('Game', function() {
     expect(
       () => Game.newInstance('id', [player1, player2], player1))
       .to.throw(Error, /Duplicate color found/);
+  });
+
+  it('grant space bonus sanity test', () => {
+    const player = TestPlayers.BLUE.newPlayer();
+    const game = Game.newInstance('foobar', [player], player);
+    const space = game.board.getAvailableSpacesOnLand()[0];
+
+    space.bonus = [SpaceBonus.DRAW_CARD, SpaceBonus.DRAW_CARD, SpaceBonus.DRAW_CARD, SpaceBonus.DRAW_CARD, SpaceBonus.PLANT, SpaceBonus.TITANIUM];
+    expect(player.cardsInHand).has.length(0);
+    expect(player.plants).eq(0);
+    expect(player.titanium).eq(0);
+
+    game.addTile(player, space.spaceType, space, {tileType: TileType.GREENERY});
+
+    expect(player.cardsInHand).has.length(4);
+    expect(player.plants).eq(1);
+    expect(player.titanium).eq(1);
   });
 
   /**

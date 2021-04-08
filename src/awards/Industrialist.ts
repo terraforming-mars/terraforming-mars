5d7ac3964a5630ea27fabb0e8bf542cb3e351d49
Jeep <14239220+vsrisuknimit@users.noreply.github.com>
2021-04-07 20:23:26 -0400

    calculate award properly (#3101)

--- src/awards/Industrialist.ts
+++ src/awards/Industrialist.ts
@@ -1,10 +1,15 @@
 import {IAward} from './IAward';
 import {Player} from '../Player';
+import {Resources} from '../Resources';
 
 export class Industrialist implements IAward {
     public name: string = 'Industrialist';
-    public description: string = 'Having most steel and energy resources'
+    public description: string = 'Having most steel and energy resources (after final production round)'
     public getScore(player: Player): number {
-      return player.steel + player.energy;
+      if (player.game.isDoneWithFinalProduction()) {
+        return player.steel + player.energy;
+      } else {
+        return player.steel + player.getProduction(Resources.STEEL) + player.getProduction(Resources.ENERGY);
+      }
     }
 }

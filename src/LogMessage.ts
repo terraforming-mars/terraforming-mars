60f71e137f3c8a4e0b45fe4fb6b8d3fe3efeb429
Robert Konigsberg <konigsberg@gmail.com>
2021-04-18 23:23:51 -0400

    Added speech bubble icon for private messages.

--- src/LogMessage.ts
+++ src/LogMessage.ts
@@ -9,5 +9,7 @@ export class LogMessage implements Message {
     public type: LogMessageType,
     public message: string,
     public data: Array<LogMessageData>,
+    // When set, this message is private for the specifed player.
+    // Always filter messages so they're not sent to the wrong player.
     public playerId?: PlayerId) {}
 }

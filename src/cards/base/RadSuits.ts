a198c0cae536fd0d2c5c9cc90acdf91060a00b9f
Robert Konigsberg <konigsberg@gmail.com>
2021-04-19 22:46:26 -0400

    +"production"

--- src/cards/base/RadSuits.ts
+++ src/cards/base/RadSuits.ts
@@ -20,7 +20,7 @@ export class RadSuits extends Card implements IProjectCard {
         renderData: CardRenderer.builder((b) => {
           b.production((pb) => pb.megacredits(1));
         }),
-        description: 'Requires two cities in play. Increase your M€ 1 step.',
+        description: 'Requires two cities in play. Increase your M€ production 1 step.',
         victoryPoints: 1,
       },
     });

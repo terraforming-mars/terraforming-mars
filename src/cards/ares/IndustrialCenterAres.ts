7d92bc3fa08787fa2658c6dd657e57ded84f3356
Robert Konigsberg <kberg@users.noreply.github.com>
2021-04-25 02:43:37 -0400

    Add asterisk to Ares' Industrial Center, just like the main one. (#3206)

--- src/cards/ares/IndustrialCenterAres.ts
+++ src/cards/ares/IndustrialCenterAres.ts
@@ -15,7 +15,7 @@ export class IndustrialCenterAres extends IndustrialCenter {
           b.action('Spend 7 M€ to increase your steel production 1 step.', (eb) => {
             eb.megacredits(7).startAction.production((pb) => pb.steel(1));
           }).br;
-          b.tile(TileType.INDUSTRIAL_CENTER, false, true);
+          b.tile(TileType.INDUSTRIAL_CENTER, false, true).asterix();
         }),
         description: 'Place this tile adjacent to a city tile. This tile grants an ADJACENCY BONUS of 1 steel.',
       });

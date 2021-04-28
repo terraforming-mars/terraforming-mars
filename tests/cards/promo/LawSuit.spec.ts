4cf85ccdef96e4ba7213e4d60bcd86cc74a19744
Robert Konigsberg <konigsberg@gmail.com>
2021-04-27 20:36:29 -0400

    Fix LawSuit's stealing. Add a test.

--- tests/cards/promo/LawSuit.spec.ts
+++ tests/cards/promo/LawSuit.spec.ts
@@ -6,35 +6,48 @@ import {Player} from '../../../src/Player';
 import {Resources} from '../../../src/Resources';
 import {TestPlayers} from '../../TestPlayers';
 
-describe('LawSuit', function() {
+describe('LawSuit', () => {
   let card : LawSuit; let player : Player; let player2 : Player;
 
-  beforeEach(function() {
+  beforeEach(() => {
     card = new LawSuit();
     player = TestPlayers.BLUE.newPlayer();
     player2 = TestPlayers.RED.newPlayer();
     Game.newInstance('foobar', [player, player2], player);
   });
 
-  it('Can\'t play if no resources or production reduced this turn', function() {
+  it('Can\'t play if no resources or production reduced this turn', () => {
     expect(card.canPlay(player)).is.not.true;
   });
 
-  it('Can play if resources removed this turn by other player', function() {
+  it('Can play if resources removed this turn by other player', () => {
     player.addResource(Resources.MEGACREDITS, -1, {log: true, from: player2});
     expect(card.canPlay(player)).is.true;
   });
 
-  it('Can play if production decreased this turn by other player', function() {
+  it('Can play if production decreased this turn by other player', () => {
     player.addProduction(Resources.MEGACREDITS, -1, {log: true, from: player2});
     expect(card.canPlay(player)).is.true;
   });
 
-  it('Should play', function() {
+  it('Should play', () => {
     player.addResource(Resources.MEGACREDITS, -1, {log: true, from: player2});
     player.addProduction(Resources.MEGACREDITS, -1, {log: true, from: player2});
 
     const play = card.play(player);
-    expect(play instanceof SelectPlayer).is.true;
+    expect(play).instanceOf(SelectPlayer);
+  });
+
+  it('Steals resources correctly', () => {
+    // This part sets up player2 as a thief whom you will sue.
+    player.removingPlayers.push(player2.id);
+    // This thief now has has 2MC
+    player2.megaCredits = 2;
+    player.megaCredits = 0;
+    const play = card.play(player);
+    expect(play).instanceOf(SelectPlayer);
+    (play as SelectPlayer).cb(player2);
+    expect(player.megaCredits).eq(2);
+    expect(player2.megaCredits).eq(0);
   });
 });

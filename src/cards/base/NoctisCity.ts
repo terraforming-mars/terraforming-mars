81b8c0663fbf5a45ea72c806f72147d4e34413fa
zinovik <zinovik@gmail.com>
2021-05-08 20:15:28 +0300

    fix noctis city card city asterix (#3265)

--- src/cards/base/NoctisCity.ts
+++ src/cards/base/NoctisCity.ts
@@ -27,7 +27,7 @@ export class NoctisCity extends Card implements IProjectCard {
           b.production((pb) => {
             pb.minus().energy(1).br;
             pb.plus().megacredits(3);
-          }).nbsp.city();
+          }).nbsp.city().asterix();
         }),
         description: 'Decrease your Energy production 1 step and increase your M€ production 3 steps. Place a City tile ON THE RESERVED AREA, disregarding normal placement restrictions.',
       },

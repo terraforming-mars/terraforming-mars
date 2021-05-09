78ddaa29e49f125e7f8b16460f676d8ea3120c9d
Robert Konigsberg <konigsberg@gmail.com>
2021-05-09 12:17:44 -0400

    More static card conversion.

--- src/cards/moon/TheArchaicFoundationInstitute.ts
+++ src/cards/moon/TheArchaicFoundationInstitute.ts
@@ -5,15 +5,34 @@ import {Tags} from '../Tags';
 import {CorporationCard} from '../corporation/CorporationCard';
 import {IProjectCard} from '../IProjectCard';
 import {ResourceType} from '../../ResourceType';
-import {CardMetadata} from '../CardMetadata';
 import {CardRenderer} from '../render/CardRenderer';
+import {Card} from '../Card';
 
-export class TheArchaicFoundationInstitute implements CorporationCard {
-  public name = CardName.THE_ARCHAIC_FOUNDATION_INSTITUTE;
-  public startingMegaCredits = 55;
-  public tags = [Tags.MOON, Tags.MOON];
-  public cardType = CardType.CORPORATION;
-  public readonly resourceType = ResourceType.RESOURCE_CUBE;
+export class TheArchaicFoundationInstitute extends Card implements CorporationCard {
+  constructor() {
+    super({
+      cardType: CardType.CORPORATION,
+      name: CardName.THE_ARCHAIC_FOUNDATION_INSTITUTE,
+      tags: [Tags.MOON, Tags.MOON],
+      startingMegaCredits: 55,
+      resourceType: ResourceType.RESOURCE_CUBE,
+
+      metadata: {
+        description: 'You start with 55 M€.',
+        cardNumber: '',
+        renderData: CardRenderer.builder((b) => {
+          b.megacredits(55).br;
+          b.effect('When you play a Moon tag, add a bronze resource cube on this card.', (eb) => {
+            eb.moon().startEffect.resourceCube();
+          }).br;
+          b.effect('Automatically remove every 3 bronze resource cubes collected here and increase your TR 1 step.', (eb) => {
+            eb.resourceCube(3).startEffect.tr(1);
+          });
+        }),
+      },
+    });
+  }
+
   public resourceCount = 0;
 
   public play() {
@@ -35,18 +54,4 @@ export class TheArchaicFoundationInstitute implements CorporationCard {
     };
     return undefined;
   }
-
-  public readonly metadata: CardMetadata = {
-    description: 'You start with 55 M€.',
-    cardNumber: '',
-    renderData: CardRenderer.builder((b) => {
-      b.megacredits(55).br;
-      b.effect('When you play a Moon tag, add a bronze resource cube on this card.', (eb) => {
-        eb.moon().startEffect.resourceCube();
-      }).br;
-      b.effect('Automatically remove every 3 bronze resource cubes collected here and increase your TR 1 step.', (eb) => {
-        eb.resourceCube(3).startEffect.tr(1);
-      });
-    }),
-  };
 }

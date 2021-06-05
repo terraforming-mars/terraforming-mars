43e0dada1bcd8c557909f77f10a70f3cd75b3c3c
Adrian Wieprzkowicz <adrian.wieprzkowicz@gmail.com>
2021-06-05 18:03:37 +0200

    Handle SFC in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,46 @@
+const {VueLoaderPlugin} = require('vue-loader');
+
 module.exports = {
   mode: 'production',
   entry: './src/main.ts',
   resolve: {
-    extensions: ['.ts', '.tsx', '.js'],
+    extensions: ['.ts', '.tsx', '.js', '.vue'],
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
     },
   },
   module: {
     rules: [
-      {test: /\.tsx?$/, loader: 'ts-loader'},
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+      },
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        options: {appendTsSuffixTo: [/\.vue$/]},
+      },
+      {
+        test: /.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.less$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          'less-loader',
+        ],
+      },
     ],
   },
   output: {
     path: __dirname + '/build',
   },
+  plugins: [
+    new VueLoaderPlugin(),
+  ],
 };

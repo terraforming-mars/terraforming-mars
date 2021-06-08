b766a402de8d90676d9e947cf7e22dbfce9b97dd
Adrian Wieprzkowicz <Argeento@users.noreply.github.com>
2021-06-08 23:07:50 +0200

    Refactor build process (#3343)

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,17 @@
-'use strict';
-
-const process = require('process');
 const CompressionPlugin = require('compression-webpack-plugin');
 const zlib = require('zlib');
 
 module.exports = {
-  devtool: 'source-map',
-  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
-  entry: [
-    './build/src/script.js',
-  ],
+  entry: './src/main.ts',
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js',
+    },
+  },
+  module: {
+    rules: [{test: /\.tsx?$/, loader: 'ts-loader'}],
+  },
   plugins: [
     new CompressionPlugin(),
     new CompressionPlugin({
@@ -18,16 +20,7 @@ module.exports = {
       compressionOptions: {params: {[zlib.constants.BROTLI_PARAM_QUALITY]: zlib.constants.BROTLI_MAX_QUALITY}},
     }),
   ],
-  resolve: {
-    alias: {
-      'vue$': 'vue/dist/vue.esm.js', // 'vue/dist/vue.common.js' for webpack 1
-    },
-  },
   output: {
     path: __dirname + '/build',
   },
-  stats: {
-    warnings: false,
-  },
-  watch: process.env.WATCH_IT !== undefined,
 };

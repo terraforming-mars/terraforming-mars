e445e63e1ef1f4ceedba646295d9cb22de619e78
Robert Konigsberg <konigsberg@gmail.com>
2021-06-15 13:22:47 -0400

    More Philares tests.

--- tests/cards/promo/Philares.spec.ts
+++ tests/cards/promo/Philares.spec.ts
@@ -18,6 +18,7 @@ describe('Philares', () => {
   let game: Game;
   let space: ISpace;
   let adjacentSpace: ISpace;
+  let adjacentSpace2: ISpace;
 
   beforeEach(() => {
     card = new Philares();
@@ -27,6 +28,7 @@ describe('Philares', () => {
     game.board = EmptyBoard.newInstance();
     space = game.board.spaces[4];
     adjacentSpace = game.board.getAdjacentSpaces(space)[0];
+    adjacentSpace2 = game.board.getAdjacentSpaces(space)[2];
 
     player.corporationCard = card;
   });
@@ -78,9 +80,7 @@ describe('Philares', () => {
 
   it('one tile one bonus', () => {
     game.addTile(player, SpaceType.LAND, space, {tileType: TileType.GREENERY});
-    expect(game.deferredActions).has.length(0);
     game.addTile(redPlayer, SpaceType.LAND, adjacentSpace, {tileType: TileType.GREENERY});
-    expect(game.deferredActions).has.length(1);
     const action = game.deferredActions.pop();
     const options = action?.execute() as AndOptions;
     // Options are ordered 0-5, MC to heat.
@@ -90,11 +90,9 @@ describe('Philares', () => {
     expect(player.purse()).deep.eq(Units.of({megacredits: 1}));
   });
 
-  it('one tile one bonus - someone is greedy', () => {
+  it('one tile one bonus - player is greedy', () => {
     game.addTile(player, SpaceType.LAND, space, {tileType: TileType.GREENERY});
-    expect(game.deferredActions).has.length(0);
     game.addTile(redPlayer, SpaceType.LAND, adjacentSpace, {tileType: TileType.GREENERY});
-    expect(game.deferredActions).has.length(1);
     const action = game.deferredActions.pop();
     const options = action?.execute() as AndOptions;
     // Options are ordered 0-5, MC to heat.
@@ -105,8 +103,48 @@ describe('Philares', () => {
   });
 
   it('Multiple bonuses when placing next to multiple tiles', () => {
+    game.addTile(redPlayer, SpaceType.LAND, adjacentSpace, {tileType: TileType.GREENERY});
+    game.addTile(redPlayer, SpaceType.LAND, adjacentSpace2, {tileType: TileType.GREENERY});
+    expect(game.deferredActions).has.length(0);
+    game.addTile(player, SpaceType.LAND, space, {tileType: TileType.GREENERY});
+    expect(game.deferredActions).has.length(1);
+    const action = game.deferredActions.pop();
+    const options = action?.execute() as AndOptions;
+    // Options are ordered 0-5, MC to heat.
+    expect(player.purse()).deep.eq(Units.EMPTY);
+    options.options[0].cb(1);
+    options.options[1].cb(1);
+    options.cb();
+    expect(player.purse()).deep.eq(Units.of({megacredits: 1, steel: 1}));
   });
 
   it('Multiple bonuses when opponent places next to multiple of your tiles', () => {
+    game.addTile(player, SpaceType.LAND, adjacentSpace, {tileType: TileType.GREENERY});
+    game.addTile(player, SpaceType.LAND, adjacentSpace2, {tileType: TileType.GREENERY});
+    expect(game.deferredActions).has.length(0);
+    game.addTile(redPlayer, SpaceType.LAND, space, {tileType: TileType.GREENERY});
+    expect(game.deferredActions).has.length(1);
+    const action = game.deferredActions.pop();
+    const options = action?.execute() as AndOptions;
+    // Options are ordered 0-5, MC to heat.
+    expect(player.purse()).deep.eq(Units.EMPTY);
+    options.options[0].cb(1);
+    options.options[1].cb(1);
+    options.cb();
+    expect(player.purse()).deep.eq(Units.of({megacredits: 1, steel: 1}));
+  });
+
+  it('two tiles two bonuses - player is greedy', () => {
+    game.addTile(redPlayer, SpaceType.LAND, adjacentSpace, {tileType: TileType.GREENERY});
+    game.addTile(redPlayer, SpaceType.LAND, adjacentSpace2, {tileType: TileType.GREENERY});
+    game.addTile(player, SpaceType.LAND, space, {tileType: TileType.GREENERY});
+    const action = game.deferredActions.pop();
+    const options = action?.execute() as AndOptions;
+    // Options are ordered 0-5, MC to heat.
+    expect(player.purse()).deep.eq(Units.EMPTY);
+    options.options[0].cb(1);
+    options.options[1].cb(1);
+    options.options[2].cb(1);
+    expect(() => options.cb()).to.throw('Need to select 2 resource(s)');
   });
 });

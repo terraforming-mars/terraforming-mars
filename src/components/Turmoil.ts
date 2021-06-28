6864bc2b1421884eb4202e0fe2b8dfd0c4e8fc8d
Brian Folts <brian.folts@gmail.com>
2021-06-28 08:47:28 -0700

    single file component: Global Events (#3388)

--- src/components/Turmoil.ts
+++ src/components/Turmoil.ts
@@ -4,147 +4,145 @@ import {$t} from '../directives/i18n';
 import {TurmoilModel} from '../models/TurmoilModel';
 import {BonusId} from '../turmoil/Bonus';
 import {PolicyId} from '../turmoil/Policy';
-import {GlobalEvent} from './GlobalEvent';
+import GlobalEvent from './GlobalEvent.vue';
 
-const AGENDA_HTML: Map<BonusId | PolicyId, string> = new Map();
-{
-  AGENDA_HTML.set('mb01',
+const AGENDA_HTML: Map<BonusId | PolicyId, string> = new Map([
+  ['mb01',
     `<div class="resource money party-resource">1</div> /
-    <div class="resource-tag tag-building party-resource-tag"></div>`);
-  AGENDA_HTML.set('mb02',
+    <div class="resource-tag tag-building party-resource-tag"></div>`],
+  ['mb02',
     `<div class="resource money party-resource">1</div> /
-    <div class="tile empty-tile-small"></div>ON MARS`);
-  AGENDA_HTML.set('sb01',
+    <div class="tile empty-tile-small"></div>ON MARS`],
+  ['sb01',
     `<div class="resource money party-resource">1</div> /
-    <div class="resource-tag tag-science party-resource-tag"></div>`);
-  AGENDA_HTML.set('sb02',
+    <div class="resource-tag tag-science party-resource-tag"></div>`],
+  ['sb02',
     `<div class="resource money party-resource">1</div> / 3
-    <div class="resource card card-small"></div>`);
-  AGENDA_HTML.set('ub01',
+    <div class="resource card card-small"></div>`],
+  ['ub01',
     `<div class="resource money party-resource">1</div> /
     <div class="resource-tag tag-venus party-resource-tag"></div>
     <div class="resource-tag tag-earth party-resource-tag"></div>
-    <div class="resource-tag tag-jovian party-resource-tag"></div>`);
-  AGENDA_HTML.set('ub02',
+    <div class="resource-tag tag-jovian party-resource-tag"></div>`],
+  ['ub02',
     `<div class="resource money party-resource">1</div> /
-    <div class="resource-tag tag-space party-resource-tag"></div>`);
-  AGENDA_HTML.set('kb01',
+    <div class="resource-tag tag-space party-resource-tag"></div>`],
+  ['kb01',
     `<div class="resource money party-resource">1</div> /
     <div class="production-box party-production-box">
       <div class="heat production"></div>
-    </div>`);
-  AGENDA_HTML.set('kb02',
+    </div>`],
+  ['kb02',
     `<div class="resource heat party-resource"></div> /
     <div class="production-box party-production-box">
       <div class="heat production"></div>
-    </div>`);
-  AGENDA_HTML.set('rb01',
+    </div>`],
+  ['rb01',
     `<div class="party-inferior-rating tile party-rating party-tile">&lt;</div> :
-  <div class="rating tile party-rating party-tile"></div>`);
-  AGENDA_HTML.set('rb02',
+  <div class="rating tile party-rating party-tile"></div>`],
+  ['rb02',
     `<div class="party-inferior-rating tile party-rating party-tile">&gt;</div> :
-  <div class="rating tile party-rating party-tile red-outline "></div>`);
-  AGENDA_HTML.set('gb01',
+  <div class="rating tile party-rating party-tile red-outline "></div>`],
+  ['gb01',
     `<div class="resource money party-resource">1</div> /
     <div class="resource-tag tag-plant party-resource-tag"></div>
     <div class="resource-tag tag-microbe party-resource-tag"></div>
-    <div class="resource-tag tag-animal party-resource-tag"></div>`);
-  AGENDA_HTML.set('gb02',
+    <div class="resource-tag tag-animal party-resource-tag"></div>`],
+  ['gb02',
     `<div class="resource money party-resource">2</div> /
-    <div class="tile greenery-tile greenery-tile-small"></div>`);
-
-  AGENDA_HTML.set('mfp01',
+    <div class="tile greenery-tile greenery-tile-small"></div>`],
+  ['mfp01',
     `<div class="policy-top-margin"><div class="tile empty-tile-small"></div> :
-    <span class="steel resource"></span></div>`);
-  AGENDA_HTML.set('mfp02',
-    `<div class="policy-top-margin"><div class="resource-tag tag-building"></div> : <div class="money resource">2</div></div>`);
-  AGENDA_HTML.set('mfp03',
-    `<div class="policy-top-margin"><div class="resource steel"></div> : +<div class="resource money">1</div></div>`);
-  AGENDA_HTML.set('mfp04',
+    <span class="steel resource"></span></div>`],
+  ['mfp02',
+    `<div class="policy-top-margin"><div class="resource-tag tag-building"></div> : <div class="money resource">2</div></div>`],
+  ['mfp03',
+    `<div class="policy-top-margin"><div class="resource steel"></div> : +<div class="resource money">1</div></div>`],
+  ['mfp04',
     `<span class="money resource">4</span>
      <span class="red-arrow-3x"></span>
-    <div class="resource card card-with-border policy-card-with-tag"><div class="card-icon tag-building"></div></div>`);
-  AGENDA_HTML.set('sp01',
+    <div class="resource card card-with-border policy-card-with-tag"><div class="card-icon tag-building"></div></div>`],
+  ['sp01',
     `<span class="money resource">10</span>
     <span class="red-arrow"></span>
     <span class="card card-with-border resource party-resource"></span>
     <span class="card card-with-border resource party-resource"></span>
-    <span class="card card-with-border resource party-resource"></span>`);
-  AGENDA_HTML.set('sp02',
+    <span class="card card-with-border resource party-resource"></span>`],
+  ['sp02',
     `<span>
     <div class="tile oxygen-tile req-tile-small" style="margin: 10px -5px;"></div>
     <div class="tile ocean-tile req-tile-small"></div>
     <div class="tile temperature-tile req-tile-small"></div>
-    : ± 2</span>`);
-  AGENDA_HTML.set('sp03', `<span>
+    : ± 2</span>`],
+  ['sp03', `<span>
     <div class="tile oxygen-tile req-tile-small" style="margin: 10px -5px;"></div>
     <div class="tile ocean-tile req-tile-small"></div>
     <div class="tile temperature-tile req-tile-small"></div>
-    : <div class="resource card card-with-border"></div></span>`);
-  AGENDA_HTML.set('sp04', `<div class="scientists-requisite"><div class="resource-tag tag-science party-resource-tag"></div></div>`);
-  AGENDA_HTML.set('up01',
+    : <div class="resource card card-with-border"></div></span>`],
+  ['sp04', `<div class="scientists-requisite"><div class="resource-tag tag-science party-resource-tag"></div></div>`],
+  ['up01',
     `<div class="policy-top-margin"><div class="resource titanium"></div> :
-    + <div class="resource money">1</div></div>`);
-  AGENDA_HTML.set('up02',
+    + <div class="resource money">1</div></div>`],
+  ['up02',
     `<div class="policy-top-margin">
     <span class="money resource">4</span>
     <span class="red-arrow-3x"></span>2<span class="titanium resource"></span> / 2<span class="floater resource"></span>
-    </div>`);
-  AGENDA_HTML.set('up03',
+    </div>`],
+  ['up03',
     `<span class="money resource">4</span>
     <span class="red-arrow-3x"></span>
-    <div class="resource card card-with-border policy-card-with-tag"><div class="card-icon tag-space"></div></div>`);
-  AGENDA_HTML.set('up04', `<div class="policy-top-margin"><div class="resource-tag tag-space"></div> : <div class="money resource">-2</div></div>`);
-  AGENDA_HTML.set('kp01',
+    <div class="resource card card-with-border policy-card-with-tag"><div class="card-icon tag-space"></div></div>`],
+  ['up04', `<div class="policy-top-margin"><div class="resource-tag tag-space"></div> : <div class="money resource">-2</div></div>`],
+  ['kp01',
     `<span class="money resource">10</span>
     <span class="red-arrow-infinity"></span>
     <div class="production-box production-box-size2">
       <div class="energy production"></div>
       <div class="heat production"></div>
-    </div>`);
-  AGENDA_HTML.set('kp02', `<div class="tile temperature-tile req-tile-small" style="margin-right:5px;"></div> : <span class="money resource">3</span>`);
-  AGENDA_HTML.set('kp03',
+    </div>`],
+  ['kp02', `<div class="tile temperature-tile req-tile-small" style="margin-right:5px;"></div> : <span class="money resource">3</span>`],
+  ['kp03',
     `6 <span class="heat resource"></span>
     <span class="red-arrow-infinity"></span>
-    <div class="tile temperature-tile"></div>`);
-  AGENDA_HTML.set('kp04',
+    <div class="tile temperature-tile"></div>`],
+  ['kp04',
     `<div class="policy-top-margin"><div class="tile empty-tile-small"></div> :
-    <span class="heat resource"></span><span class="heat resource"></span></div>`);
-  AGENDA_HTML.set('rp01',
+    <span class="heat resource"></span><span class="heat resource"></span></div>`],
+  ['rp01',
     `<div class="policy-top-margin">
     <div class="rating tile"></div> :
     <div class="resource money">-3</div>
-    </div>`);
-  AGENDA_HTML.set('rp02', `<div class="policy-top-margin"><div class="tile empty-tile-small"></div> : <span class="money resource">-3</span></div>`);
-  AGENDA_HTML.set('rp03',
+    </div>`],
+  ['rp02', `<div class="policy-top-margin"><div class="tile empty-tile-small"></div> : <span class="money resource">-3</span></div>`],
+  ['rp03',
     `<span class="money resource">4</span>
     <span class="red-arrow-3x"></span>
     <div class="tile oxygen-tile req-tile-small red-outline" style="margin: 10px -5px;"></div> /
     <div class="tile ocean-tile req-tile-small red-outline"></div> /
-    <div class="tile temperature-tile req-tile-small red-outline"></div>`);
-  AGENDA_HTML.set('rp04',
+    <div class="tile temperature-tile req-tile-small red-outline"></div>`],
+  ['rp04',
     `<div class="tile oxygen-tile req-tile-small" style="margin: 10px -5px;"></div>
     <div class="tile ocean-tile req-tile-small"></div>
     <div class="tile temperature-tile req-tile-small"></div>
     : <div class="production-box production-box-size2" style="margin-left:5px;">
       <div class="production-prefix minus"></div><div class="money production">1</div>
-    </div>`);
-  AGENDA_HTML.set('gp01', `<div class="tile greenery-tile"></div> : <div class="resource money">4</div>`);
-  AGENDA_HTML.set('gp02',
+    </div>`],
+  ['gp01', `<div class="tile greenery-tile"></div> : <div class="resource money">4</div>`],
+  ['gp02',
     `<div class="policy-top-margin"><div class="tile empty-tile-small"></div> :
-    <span class="plant resource"></span></div>`);
-  AGENDA_HTML.set('gp03',
+    <span class="plant resource"></span></div>`],
+  ['gp03',
     `<div class="policy-top-margin">
     <div class="resource-tag tag-plant party-resource-tag"></div>
     <div class="resource-tag tag-microbe party-resource-tag"></div>
     <div class="resource-tag tag-animal party-resource-tag"></div> : <div class="resource money">2</div>
-    </div>` );
-  AGENDA_HTML.set('gp04',
+    </div>`],
+  ['gp04',
     `<div class="policy-top-margin">
     <span class="money resource">5</span>
     <span class="red-arrow-3x"></span>3<span class="plant resource"></span> / 2<span class="microbe resource"></span>
-    </div>` );
-}
+    </div>`],
+]);
 
 export const Turmoil = Vue.component('turmoil', {
   props: {

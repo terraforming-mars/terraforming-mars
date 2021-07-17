1ee764edbfa396e14ac55995a7498ac58ffaad56
Robert Konigsberg <konigsberg@gmail.com>
2021-07-17 11:45:33 -0400

    Log the database being connected to.

--- src/database/Database.ts
+++ src/database/Database.ts
@@ -11,10 +11,13 @@ export class Database {
     public static getInstance() {
       if (!Database.instance) {
         if (process.env.POSTGRES_HOST !== undefined) {
+          console.log('Connecting to Postgres database.');
           Database.instance = new PostgreSQL();
         } else if (process.env.LOCAL_FS_DB !== undefined) {
+          console.log('Connecting to local filesystem database.');
           Database.instance = new Localfilesystem();
         } else {
+          console.log('Connecting to SQLite database.');
           Database.instance = new SQLite();
         }
       }

2830ca9ffdb995630945ed4bec912c23f698d003
Brian Folts <brian.folts@gmail.com>
2021-07-26 18:00:32 -0700

    single file component: OrOptions (#3479)

---
+++ src/components/OrOptions.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class='wf-options'>
+    <label v-if="showtitle"><div>{{ $t(playerinput.title) }}</div></label>
+    <div v-for="(option, idx) in displayedOptions" :key="idx">
+      <label class="form-radio">
+        <input v-model="selectedOption" type="radio" :name="radioElementName" :value="option" />
+        <i class="form-icon" />
+        <span>{{ $t(option.title) }}</span>
+      </label>
+      <div v-if="selectedOption === option" style="margin-left: 30px">
+        <player-input-factory ref="inputfactory"
+                              :players="players"
+                              :player="player"
+                              :playerinput="option"
+                              :onsave="playerFactorySaved()"
+                              :showsave="false"
+                              :showtitle="false" />
+      </div>
+    </div>
+    <div v-if="showsave && selectedOption">
+      <div style="margin: 5px 30px 10px" class="wf-action">
+        <Button :title="$t(selectedOption.buttonLabel)" type="submit" size="normal" :onClick="saveData" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+
+import Vue from 'vue';
+import Button from '../components/common/Button.vue';
+import {PlayerModel} from '../models/PlayerModel';
+import {PlayerInputModel} from '../models/PlayerInputModel';
+import {PreferencesManager} from './PreferencesManager';
+import {TranslateMixin} from '../components/TranslateMixin';
+
+let unique = 0;
+
+export default Vue.extend({
+  name: 'or-options',
+  props: {
+    player: {
+      type: Object as () => PlayerModel,
+    },
+    players: {
+      type: Array as () => Array<PlayerModel>,
+    },
+    playerinput: {
+      type: Object as () => PlayerInputModel,
+    },
+    onsave: {
+      type: Function as unknown as () => (out: Array<Array<string>>) => void,
+    },
+    showsave: {
+      type: Boolean,
+    },
+    showtitle: {
+      type: Boolean,
+    },
+  },
+  components: {
+    Button,
+  },
+  data: function() {
+    if (this.playerinput.options === undefined) {
+      throw new Error('no options provided for OrOptions');
+    }
+    const displayedOptions = this.playerinput.options.filter((o) => Boolean(o.showOnlyInLearnerMode) === false || PreferencesManager.loadBoolean('learner_mode'));
+    return {
+      displayedOptions,
+      radioElementName: 'selectOption' + unique++,
+      selectedOption: displayedOptions[0],
+    };
+  },
+  methods: {
+    ...TranslateMixin.methods,
+    playerFactorySaved: function() {
+      const idx = this.playerinput.options?.indexOf(this.selectedOption);
+      if (idx === undefined || idx === -1) {
+        throw new Error('option not found!');
+      }
+      return (out: Array<Array<string>>) => {
+        const copy = [[String(idx)]];
+        for (let i = 0; i < out.length; i++) {
+          copy.push(out[i].slice());
+        }
+        this.onsave(copy);
+      };
+    },
+    saveData: function() {
+      let ref = this.$refs['inputfactory'];
+      if (Array.isArray(ref)) {
+        ref = ref[0];
+      }
+      (ref as any).saveData();
+    },
+  },
+});
+
+</script>
+
---
+++ tests/components/OrOptions.spec.ts
@@ -0,0 +1,82 @@
+
+import {createLocalVue, mount} from '@vue/test-utils';
+
+import {expect} from 'chai';
+import OrOptions from '../../src/components/OrOptions.vue';
+import {PlayerInputTypes} from '../../src/PlayerInputTypes';
+
+describe('OrOptions', function() {
+  function getLocalVue() {
+    const localVue = createLocalVue();
+    localVue.directive('i18n', {});
+    return localVue;
+  }
+  it('saves the options ignoring hidden', async function() {
+    let savedData: Array<Array<string>> | undefined;
+    const component = mount(OrOptions, {
+      localVue: getLocalVue(),
+      propsData: {
+        player: {
+          id: 'foo',
+        },
+        playerinput: {
+          title: 'foo',
+          options: [{
+            inputType: PlayerInputTypes.SELECT_OPTION,
+            title: 'hide this',
+            showOnlyInLearnerMode: true,
+          }, {
+            inputType: PlayerInputTypes.SELECT_OPTION,
+            title: 'select a',
+          }, {
+            title: 'select b',
+            inputType: PlayerInputTypes.SELECT_OPTION,
+          }],
+        },
+        onsave: function(data: Array<Array<string>>) {
+          savedData = data;
+        },
+        showsave: true,
+        showtitle: true,
+      },
+    });
+    const buttons = component.findAllComponents({name: 'Button'});
+    await buttons.at(0).findAllComponents({
+      name: 'button',
+    }).at(0).trigger('click');
+    expect(savedData).to.deep.eq([['1'], ['1']]);
+  });
+  it('moves and selects 2nd option', async function() {
+    let savedData: Array<Array<string>> | undefined;
+    const component = mount(OrOptions, {
+      localVue: getLocalVue(),
+      propsData: {
+        player: {
+          id: 'foo',
+        },
+        playerinput: {
+          title: 'foo',
+          options: [{
+            inputType: PlayerInputTypes.SELECT_OPTION,
+            title: 'select a',
+          }, {
+            title: 'select b',
+            inputType: PlayerInputTypes.SELECT_OPTION,
+          }],
+        },
+        onsave: function(data: Array<Array<string>>) {
+          savedData = data;
+        },
+        showsave: true,
+        showtitle: true,
+      },
+    });
+    const inputs = component.findAll('input');
+    await inputs.at(1).setChecked();
+    const buttons = component.findAllComponents({name: 'Button'});
+    await buttons.at(0).findAllComponents({
+      name: 'button',
+    }).at(0).trigger('click');
+    expect(savedData).to.deep.eq([['1'], ['1']]);
+  });
+});

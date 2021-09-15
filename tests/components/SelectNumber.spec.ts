69d3d3a4f9135bdc6dafc183e0c7f543491859f7
Robert Konigsberg <konigsberg@gmail.com>
2021-09-15 11:47:57 -0400

    Alt Venus track client-side.

---
+++ tests/components/SelectNumber.spec.ts
@@ -0,0 +1,77 @@
+import {createLocalVue, shallowMount} from '@vue/test-utils';
+import {getInputElement, click} from './utils/VueUtils';
+import {expect} from 'chai';
+import SelectNumber from '@/components/SelectNumber.vue';
+
+function getLocalVue() {
+  const localVue = createLocalVue();
+  localVue.directive('trim-whitespace', {});
+  localVue.directive('i18n', {});
+  return localVue;
+}
+
+describe('SelectNumber', () => {
+  it('shows result', () => {
+    const wrapper = shallowMount(SelectNumber, {
+      localVue: getLocalVue(),
+      propsData: {'value': 5},
+    });
+
+    expect(wrapper.props('value')).to.eq(5);
+    expect(getInputElement(wrapper, 'textbox').value).to.eq('5');
+  });
+
+  it('plus works', async () => {
+    const wrapper = shallowMount(SelectNumber, {
+      localVue: getLocalVue(),
+      propsData: {'value': 5},
+    });
+
+    await click(wrapper, 'plus');
+
+    expect(getInputElement(wrapper, 'textbox').value).to.eq('6');
+  });
+
+  it('plus does not exceed max', async () => {
+    const wrapper = shallowMount(SelectNumber, {
+      localVue: getLocalVue(),
+      propsData: {'value': 5, 'max': 7},
+    });
+
+    await click(wrapper, 'plus');
+    expect(getInputElement(wrapper, 'textbox').value).to.eq('6');
+
+    await click(wrapper, 'plus');
+    expect(getInputElement(wrapper, 'textbox').value).to.eq('7');
+
+    await click(wrapper, 'plus');
+    expect(getInputElement(wrapper, 'textbox').value).to.eq('7');
+  });
+
+  it('minus works', async () => {
+    const wrapper = shallowMount(SelectNumber, {
+      localVue: getLocalVue(),
+      propsData: {'value': 5},
+    });
+
+    await click(wrapper, 'minus');
+
+    expect(getInputElement(wrapper, 'textbox').value).to.eq('4');
+  });
+
+  it('minus does not exceed min', async () => {
+    const wrapper = shallowMount(SelectNumber, {
+      localVue: getLocalVue(),
+      propsData: {'value': 5, 'min': 3},
+    });
+
+    await click(wrapper, 'minus');
+    expect(getInputElement(wrapper, 'textbox').value).to.eq('4');
+
+    await click(wrapper, 'minus');
+    expect(getInputElement(wrapper, 'textbox').value).to.eq('3');
+
+    await click(wrapper, 'minus');
+    expect(getInputElement(wrapper, 'textbox').value).to.eq('3');
+  });
+});

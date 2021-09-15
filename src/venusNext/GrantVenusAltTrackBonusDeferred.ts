b2ee9dc416a3625de2c1ef33be7db9474b619eca
Robert Konigsberg <konigsberg@gmail.com>
2021-09-15 16:30:37 -0400

    Completely rewrote alt venus server side handling to reuse Dry Deserts's code.
    That's because I misread the rules; the resources gained by the track
    don't have to be distinct.
    
    Not as elegant, but it'll be much less code in the end.
    
    The SelectDistinctResources action may still be useful later, because
    the rule applied specifically to the Colony, which I have not yet
    written.

--- src/venusNext/GrantVenusAltTrackBonusDeferred.ts
+++ src/venusNext/GrantVenusAltTrackBonusDeferred.ts
@@ -1,10 +1,9 @@
 import {ICard} from '../cards/ICard';
 import {OrOptions} from '../inputs/OrOptions';
 import {SelectCard} from '../inputs/SelectCard';
-import {Units} from '../Units';
 import {DeferredAction, Priority} from '../deferredActions/DeferredAction';
 import {Player} from '../Player';
-import {SelectDistinctResources} from '../inputs/SelectDistinctResources';
+import {SelectResources} from '../inputs/SelectResources';
 
 export class GrantVenusAltTrackBonusDeferred implements DeferredAction {
   public priority = Priority.GAIN_RESOURCE_OR_PRODUCTION;
@@ -14,32 +13,16 @@ export class GrantVenusAltTrackBonusDeferred implements DeferredAction {
     public wildResource: boolean,
   ) { }
 
-  private validate(units: Units, maximum: number) {
-    const array = [units.megacredits, units.steel, units.titanium, units.plants, units.energy, units.heat];
-    if (array.find((count) => count < 0)) {
-      throw new Error('No units should be negative');
-    }
-    const sum = array.reduce((a, b) => a + b, 0);
-    if (sum > maximum) {
-      throw new Error(`Select no more than ${maximum} distinct units.`);
-    }
-  }
-
-  private newDistinctResource(count: number) {
-    return new SelectDistinctResources(
-      count,
+  private newSelectResources(count: number) {
+    return new SelectResources(
       this.player,
-      (units: Units) => {
-        this.validate(units, count);
-        this.player.addUnits(units, {log: true});
-        return undefined;
-      },
-      'Save',
+      count,
+      `Gain ${count} resources for your Venus track bonus.`,
     );
   }
   public execute() {
     if (this.wildResource === false) {
-      return this.newDistinctResource(this.standardResourceCount);
+      return this.newSelectResources(this.standardResourceCount);
     }
 
     const cards = this.player.getResourceCards(undefined);
@@ -49,13 +32,13 @@ export class GrantVenusAltTrackBonusDeferred implements DeferredAction {
         return undefined;
       },
     );
-    const wild = new OrOptions(this.newDistinctResource(1), card);
+    const wild = new OrOptions(this.newSelectResources(1), card);
     wild.title = (this.standardResourceCount === 0) ?
       'Choose your wild resource bonus.':
       `Choose your wild resource bonus, after which you will gain ${this.standardResourceCount} more distinct standard resources.`;
     wild.cb = () => {
       return this.standardResourceCount > 0 ?
-        this.newDistinctResource(this.standardResourceCount) :
+        this.newSelectResources(this.standardResourceCount) :
         undefined;
     };
     return wild;

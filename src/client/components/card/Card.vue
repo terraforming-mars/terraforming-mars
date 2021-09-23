2712536e0006f7efbdcfb95d9649d4387b7b502a
Robert Konigsberg <konigsberg@gmail.com>
2021-09-23 00:25:31 -0400

    prototype: show card resource icons instead of 'RES' in the corner.

--- src/client/components/card/Card.vue
+++ src/client/components/card/Card.vue
@@ -10,7 +10,7 @@
                 <CardNumber v-if="getCardMetadata() !== undefined" :number="getCardNumber()"/>
             </div>
             <CardExpansion :expansion="getCardExpansion()" :isCorporation="isCorporationCard()"/>
-            <CardResourceCounter v-if="card.resources !== undefined" :amount="getResourceAmount(card)" />
+            <CardResourceCounter v-if="card.resources !== undefined" :amount="getResourceAmount(card)" :type="card.resourceType" />
             <CardExtraContent :card="card" />
             <slot/>
         </div>
--- src/client/components/card/CardResourceCounter.vue
+++ src/client/components/card/CardResourceCounter.vue
@@ -1,18 +1,57 @@
 <template>
-        <div class="card-resources-counter">
-            <span class="card-resources-counter-label">res:</span><span class="card-resources-counter-number"> {{ amount }}</span>
-        </div>
+  <div class="card-resources-counter">
+      <span class="card-resource" :class="getClass"></span>
+      <span class="card-resources-counter-label">:</span>
+      <span class="card-resources-counter-number"> {{ amount }}</span>
+  </div>
 </template>
 
 <script lang="ts">
 
 import Vue from 'vue';
+import {ResourceType} from '@/ResourceType';
 export default Vue.extend({
   name: 'CardResourceCounter',
   props: {
     amount: {
       type: Number,
       required: true,
+    },
+    type: {
+      type: String as () => ResourceType,
+      required: true,
+    },
+  },
+  computed: {
+    getClass(): string {
+      switch (this.type) {
+      case ResourceType.ANIMAL:
+        return 'card-resource-animal';
+      case ResourceType.MICROBE:
+        return 'card-resource-microbe';
+      case ResourceType.FIGHTER:
+        return 'card-resource-fighter';
+      case ResourceType.SCIENCE:
+        return 'card-resource-science';
+      case ResourceType.FLOATER:
+        return 'card-resource-floater';
+      case ResourceType.ASTEROID:
+        return 'card-resource-asteroid';
+      case ResourceType.PRESERVATION:
+        return 'card-resource-preservation';
+      case ResourceType.CAMP:
+        return 'card-resource-camp';
+      case ResourceType.DISEASE:
+        return 'card-resource-disease';
+      case ResourceType.RESOURCE_CUBE:
+        return 'card-resource-resource-cube';
+      case ResourceType.DATA:
+        return 'card-resource-data';
+      case ResourceType.SYNDICATE_FLEET:
+        return 'card-resource-syndicate-fleet';
+      default:
+        return '';
+      }
     },
   },
 });

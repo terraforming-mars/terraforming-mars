8fa1b212e94e281b13a692475a5e89bd4cd00a01
Robert Konigsberg <kberg@users.noreply.github.com>
2021-09-22 20:46:12 -0400

    Disable css caching more effectively. (#3615)
    This makes it possible to locally change CSS without having to restart
    the server.

--- tests/routes/ServeAsset.spec.ts
+++ tests/routes/ServeAsset.spec.ts
@@ -1,16 +1,40 @@
 import * as http from 'http';
 import {expect} from 'chai';
-import {ServeAsset} from '../../src/routes/ServeAsset';
+import {FileAPI, ServeAsset} from '../../src/routes/ServeAsset';
 import {Route} from '../../src/routes/Route';
 import {FakeGameLoader} from './FakeGameLoader';
 import {MockResponse} from './HttpMocks';
 import {IContext} from '../../src/routes/IHandler';
+
+class FileApiMock extends FileAPI {
+  public counts = {
+    readFile: 0,
+    readFileSync: 0,
+    existsSync: 0,
+  }
+  public constructor() {
+    super();
+  }
+  public readFileSync(path: string): Buffer {
+    this.counts.readFileSync++;
+    return Buffer.from('data: ' + path);
+  }
+  public readFile(path: string, cb: (err: NodeJS.ErrnoException | null, data: Buffer) => void): void {
+    this.counts.readFile++;
+    cb(null, Buffer.from('data: ' + path));
+  }
+  public existsSync(_path: string): boolean {
+    this.counts.existsSync++;
+    return true;
+  }
+}
 
 describe('ServeAsset', () => {
   let instance: ServeAsset;
   let req: http.IncomingMessage;
   let res: MockResponse;
   let ctx: IContext;
+  let fileApi: FileApiMock;
 
   // Strictly speaking |parameters| can also accept a fragment.
   const setRequest = function(parameters: string, headers: Array<Array<string>> = []) {
@@ -25,6 +49,7 @@ describe('ServeAsset', () => {
     instance = new ServeAsset(undefined, false);
     req = {headers: {}} as http.IncomingMessage;
     res = new MockResponse();
+    fileApi = new FileApiMock();
     ctx = {
       route: new Route(),
       serverId: '1',
@@ -45,4 +70,54 @@ describe('ServeAsset', () => {
     instance.get(req, res.hide(), ctx);
     expect(res.content.startsWith('<!DOCTYPE html>'));
   });
+
+  it('styles.css', () => {
+    instance = new ServeAsset(undefined, false, fileApi);
+    setRequest('/styles.css', [['accept-encoding', '']]);
+    instance.get(req, res.hide(), ctx);
+    expect(res.content).eq('data: build/styles.css');
+  });
+
+  it('styles.css.gz', () => {
+    instance = new ServeAsset(undefined, false, fileApi);
+    setRequest('/styles.css', [['accept-encoding', 'gzip']]);
+    instance.get(req, res.hide(), ctx);
+    expect(res.content).eq('data: build/styles.css.gz');
+  });
+
+  it('styles.css: uncached', () => {
+    instance = new ServeAsset(undefined, false, fileApi);
+    // Primes the cache.
+    expect(fileApi.counts).deep.eq({readFile: 0, readFileSync: 2, existsSync: 0});
+
+    setRequest('/styles.css', [['accept-encoding', '']]);
+    instance.get(req, res.hide(), ctx);
+
+    expect(res.content).eq('data: build/styles.css');
+    expect(fileApi.counts).deep.eq({
+      readFile: 1, // Still read.
+      readFileSync: 2,
+      existsSync: 0,
+    });
+  });
+
+  it('styles.css.gz: cached', () => {
+    instance = new ServeAsset(undefined, true, fileApi);
+    // Primes the cache.
+    expect(fileApi.counts).deep.eq({readFile: 0, readFileSync: 2, existsSync: 0});
+
+    setRequest('/styles.css', [['accept-encoding', 'gzip']]);
+    instance.get(req, res.hide(), ctx);
+
+    expect(res.content).eq('data: build/styles.css.gz');
+    expect(fileApi.counts).deep.eq({
+      readFile: 0, // Does not change
+      readFileSync: 2,
+      existsSync: 0,
+    });
+  });
+
+
+  // Cached CSS
+  // Uncached CSS
 });

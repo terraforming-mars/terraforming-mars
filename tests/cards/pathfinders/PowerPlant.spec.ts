956d24e23d20ffe549f1c4a2c13ee006f7e46f2e
Robert Konigsberg <konigsberg@gmail.com>
2021-10-03 13:43:28 -0400

    Add Pathfinders Power Plant

---
+++ tests/cards/pathfinders/PowerPlant.spec.ts
@@ -0,0 +1,25 @@
+import {expect} from 'chai';
+import {PowerPlant} from '../../../src/cards/pathfinders/PowerPlant';
+import {Game} from '../../../src/Game';
+import {TestPlayer} from '../../TestPlayer';
+import {TestPlayers} from '../../TestPlayers';
+import {Units} from '../../../src/Units';
+
+describe('PowerPlant', function() {
+  let card: PowerPlant;
+  let player: TestPlayer;
+  let game: Game;
+
+  beforeEach(function() {
+    card = new PowerPlant();
+    player = TestPlayers.BLUE.newPlayer();
+    game = Game.newInstance('foobar', [player], player);
+    player.playedCards.push(card);
+  });
+
+  it('play', function() {
+    const action = card.play(player);
+    expect(action).is.undefined;
+    expect(player.getProductionForTest()).deep.eq(Units.of({heat: 2, energy: 1}));
+  });
+});

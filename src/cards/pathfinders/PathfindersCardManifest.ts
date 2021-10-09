0ffd0cb95b41440194b6295016d37cbe4bf2cc34
Robert Konigsberg <konigsberg@gmail.com>
2021-10-09 00:28:59 -0400

    Add Pathfinders cards Data Leak and Small Open Pit Mine

--- src/cards/pathfinders/PathfindersCardManifest.ts
+++ src/cards/pathfinders/PathfindersCardManifest.ts
@@ -17,7 +17,7 @@ import {CeresSpaceport} from './CeresSpaceport';
 // import {Cryptocurrency} from './Cryptocurrency';
 // import {CultivationofVenus} from './CultivationofVenus';
 // import {Cyanobacteria} from './Cyanobacteria';
-// import {DataLeak} from './DataLeak';
+import {DataLeak} from './DataLeak';
 // import {DeclarationofIndependence} from './DeclarationofIndependence';
 // import {DeepSpaceOperations} from './DeepSpaceOperations';
 // import {DesignCompany} from './DesignCompany';
@@ -61,7 +61,7 @@ import {ReturntoAbandonedTechnology} from './ReturntoAbandonedTechnology';
 // import {RichDeposits} from './RichDeposits';
 // import {SecretLabs} from './SecretLabs';
 // import {SmallComet} from './SmallComet';
-// import {SmallOpenPitMine} from './SmallOpenPitMine';
+import {SmallOpenPitMine} from './SmallOpenPitMine';
 // import {SocialEvents} from './SocialEvents';
 // import {SoilDetoxification} from './SoilDetoxification';
 // import {Solarpedia} from './Solarpedia';
@@ -106,8 +106,8 @@ export const PATHFINDERS_CARD_MANIFEST = new CardManifest({
     // {cardName: CardName.CYANOBACTERIA, Factory: Cyanobacteria},
     // {cardName: CardName.COMMUNICATION_CENTER, Factory: CommunicationCenter},
     // {cardName: CardName.MARTIAN_REPOSITORY, Factory: MartianRepository},
-    // {cardName: CardName.DATA_LEAK, Factory: DataLeak},
-    // {cardName: CardName.SMALL_OPEN_PIT_MINE, Factory: SmallOpenPitMine},
+    {cardName: CardName.DATA_LEAK, Factory: DataLeak},
+    {cardName: CardName.SMALL_OPEN_PIT_MINE, Factory: SmallOpenPitMine},
     // {cardName: CardName.SOLAR_STORM, Factory: SolarStorm},
     // {cardName: CardName.SPACE_RELAY, Factory: SpaceRelay},
     // {cardName: CardName.DECLARATION_OF_INDEPENDENCE, Factory: DeclarationofIndependence, compatibility: GameModule.Turmoil},

368885e00fffd0215c909e9a9732051db4fac34c
Robert Konigsberg <konigsberg@gmail.com>
2021-10-09 19:35:30 -0400

    Remove oceans reds tax checks from most cards that place oceans.

--- src/cards/colonies/IceMoonColony.ts
+++ src/cards/colonies/IceMoonColony.ts
@@ -5,9 +5,6 @@ import {Player} from '../../Player';
 import {CardName} from '../../CardName';
 import {BuildColony} from '../../deferredActions/BuildColony';
 import {PlaceOceanTile} from '../../deferredActions/PlaceOceanTile';
-import {MAX_OCEAN_TILES, REDS_RULING_POLICY_COST} from '../../constants';
-import {PartyHooks} from '../../turmoil/parties/PartyHooks';
-import {PartyName} from '../../turmoil/parties/PartyName';
 import {Card} from '../Card';
 import {CardRenderer} from '../render/CardRenderer';
 
@@ -18,6 +15,7 @@ export class IceMoonColony extends Card implements IProjectCard {
       tags: [Tags.SPACE],
       name: CardName.ICE_MOON_COLONY,
       cardType: CardType.AUTOMATED,
+      tr: {oceans: 1},
 
       metadata: {
         cardNumber: 'C15',
@@ -28,15 +26,7 @@ export class IceMoonColony extends Card implements IProjectCard {
   }
 
   public canPlay(player: Player): boolean {
-    const oceansMaxed = player.game.board.getOceansOnBoard() === MAX_OCEAN_TILES;
-    const hasAvailableColonyTile = player.hasAvailableColonyTileToBuildOn();
-    const canPayForReds = player.canAfford(player.getCardCost(this) + REDS_RULING_POLICY_COST, {titanium: true});
-
-    if (PartyHooks.shouldApplyPolicy(player, PartyName.REDS) && !oceansMaxed) {
-      return hasAvailableColonyTile && canPayForReds;
-    }
-
-    return hasAvailableColonyTile;
+    return player.hasAvailableColonyTileToBuildOn();
   }
 
   public play(player: Player) {

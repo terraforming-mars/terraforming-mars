e7f59f15e66ba305bc19ce614e7ccafb7ff4621d
Robert Konigsberg <konigsberg@gmail.com>
2021-10-16 22:41:54 -0400

    The simplest uses of canAct that uses Reds.

--- src/cards/base/OreProcessor.ts
+++ src/cards/base/OreProcessor.ts
@@ -5,9 +5,6 @@ import {Card} from '../Card';
 import {CardType} from '../CardType';
 import {Player} from '../../Player';
 import {CardName} from '../../CardName';
-import {MAX_OXYGEN_LEVEL, REDS_RULING_POLICY_COST} from '../../constants';
-import {PartyHooks} from '../../turmoil/parties/PartyHooks';
-import {PartyName} from '../../turmoil/parties/PartyName';
 import {CardRenderer} from '../render/CardRenderer';
 import {digit} from '../Options';
 
@@ -34,14 +31,7 @@ export class OreProcessor extends Card implements IActionCard, IProjectCard {
     return undefined;
   }
   public canAct(player: Player): boolean {
-    const hasEnoughEnergy = player.energy >= 4;
-    const oxygenMaxed = player.game.getOxygenLevel() === MAX_OXYGEN_LEVEL;
-
-    if (PartyHooks.shouldApplyPolicy(player, PartyName.REDS) && !oxygenMaxed) {
-      return player.canAfford(REDS_RULING_POLICY_COST) && hasEnoughEnergy;
-    }
-
-    return hasEnoughEnergy;
+    return player.energy >= 4 && player.canAfford(0, {tr: {oxygen: 1}});
   }
   public action(player: Player) {
     player.energy -= 4;
--- src/cards/venusNext/VenusMagnetizer.ts
+++ src/cards/venusNext/VenusMagnetizer.ts
@@ -3,10 +3,7 @@ import {Tags} from '../Tags';
 import {CardType} from '../CardType';
 import {Player} from '../../Player';
 import {Resources} from '../../Resources';
-import {MAX_VENUS_SCALE, REDS_RULING_POLICY_COST} from '../../constants';
 import {CardName} from '../../CardName';
-import {PartyHooks} from '../../turmoil/parties/PartyHooks';
-import {PartyName} from '../../turmoil/parties/PartyName';
 import {CardRequirements} from '../CardRequirements';
 import {CardRenderer} from '../render/CardRenderer';
 import {Card} from '../Card';
@@ -36,14 +33,7 @@ export class VenusMagnetizer extends Card implements IActionCard {
     return undefined;
   }
   public canAct(player: Player): boolean {
-    const venusMaxed = player.game.getVenusScaleLevel() === MAX_VENUS_SCALE;
-    const hasEnergyProduction = player.getProduction(Resources.ENERGY) > 0;
-
-    if (PartyHooks.shouldApplyPolicy(player, PartyName.REDS)) {
-      return player.canAfford(REDS_RULING_POLICY_COST) && hasEnergyProduction && !venusMaxed;
-    }
-
-    return hasEnergyProduction && !venusMaxed;
+    return player.getProduction(Resources.ENERGY) > 0 && player.canAfford(0, {tr: {venus: 1}});
   }
   public action(player: Player) {
     player.addProduction(Resources.ENERGY, -1);

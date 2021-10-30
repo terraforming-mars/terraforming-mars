36df0e362d98db1244d96112cf0fc84fc191b372
Robert Konigsberg <konigsberg@gmail.com>
2021-10-30 17:17:22 -0400

    Three more pathfinders cards.

---
+++ src/cards/pathfinders/Anthozoa.ts
@@ -0,0 +1,58 @@
+import {IProjectCard} from '../IProjectCard';
+import {Player} from '../../Player';
+import {Card} from '../Card';
+import {CardType} from '../CardType';
+import {CardName} from '../../CardName';
+import {CardRenderer} from '../render/CardRenderer';
+import {IActionCard} from '../ICard';
+import {Resources} from '../../Resources';
+import {Tags} from '../Tags';
+import {CardRequirements} from '../CardRequirements';
+import {CardRenderDynamicVictoryPoints} from '../render/CardRenderDynamicVictoryPoints';
+import {ResourceType} from '../../ResourceType';
+
+export class Anthozoa extends Card implements IProjectCard, IActionCard {
+  constructor() {
+    super({
+      cardType: CardType.ACTIVE,
+      name: CardName.ANTHOZOA,
+      cost: 9,
+      tags: [Tags.PLANT, Tags.ANIMAL, Tags.MARS],
+      requirements: CardRequirements.builder((b) => b.oceans(3)),
+      resourceType: ResourceType.ANIMAL,
+
+      metadata: {
+        cardNumber: 'Pf55',
+        renderData: CardRenderer.builder((b) => {
+          b.action('Spend 1 plant to add an aimal to this card..', (eb) => {
+            eb.plants(1).startAction.animals(1);
+          });
+        }),
+        victoryPoints: CardRenderDynamicVictoryPoints.animals(1, 2),
+        description: 'Requires 3 oceans on Mars. 1 VP per 2 animals on this card',
+      },
+    });
+  }
+
+  public resourceCount = 0;
+
+  public canAct(player: Player) {
+    return player.plants > 0;
+  }
+
+  public action(player: Player) {
+    player.addResource(Resources.PLANTS, -1);
+    player.addResourceTo(this);
+    player.game.log('${0} spent 1 plant to place an animal on ${1}.', (b) => b.player(player).card(this));
+    return undefined;
+  }
+
+  public play() {
+    return undefined;
+  }
+
+  public getVictoryPoints() {
+    return Math.floor(this.resourceCount / 2);
+  }
+}
+

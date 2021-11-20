2998d95014837a145a728bd6d58584a9673f89a2
Robert Konigsberg <konigsberg@gmail.com>
2021-11-20 10:52:06 -0500

    Include Pathfinders corps Ringcom and Martian Insurance Group.

---
+++ tests/cards/pathfinders/MartianInsuranceGroup.spec.ts
@@ -0,0 +1,42 @@
+import {expect} from 'chai';
+import {MartianInsuranceGroup} from '../../../src/cards/pathfinders/MartianInsuranceGroup';
+import {Game} from '../../../src/Game';
+import {TestPlayer} from '../../TestPlayer';
+import {newTestGame} from '../../TestGame';
+import {CardName} from '../../../src/CardName';
+import {TestingUtils} from '../../TestingUtils';
+import {CardType} from '../../../src/cards/CardType';
+import {Resources} from '../../../src/Resources';
+
+describe('MartianInsuranceGroup', function() {
+  let card: MartianInsuranceGroup;
+  let player: TestPlayer;
+  let player2: TestPlayer;
+  let game: Game;
+
+  beforeEach(function() {
+    card = new MartianInsuranceGroup();
+    game = newTestGame(2);
+    player = game.getPlayers()[0] as TestPlayer;
+    player2 = game.getPlayers()[1] as TestPlayer;
+    player.corporationCard = card;
+  });
+
+  it('when you play an event', function() {
+    const event = TestingUtils.fakeCard({name: 'A' as CardName, cardType: CardType.EVENT});
+    expect(player.getProduction(Resources.MEGACREDITS)).eq(0);
+    expect(player2.getProduction(Resources.MEGACREDITS)).eq(0);
+    player.playCard(event);
+    expect(player.getProduction(Resources.MEGACREDITS)).eq(1);
+    expect(player2.getProduction(Resources.MEGACREDITS)).eq(0);
+  });
+
+  it('when opponent plays an event', function() {
+    const event = TestingUtils.fakeCard({name: 'A' as CardName, cardType: CardType.EVENT});
+    expect(player.getProduction(Resources.MEGACREDITS)).eq(0);
+    expect(player2.getProduction(Resources.MEGACREDITS)).eq(0);
+    player2.playCard(event);
+    expect(player.getProduction(Resources.MEGACREDITS)).eq(0);
+    expect(player2.getProduction(Resources.MEGACREDITS)).eq(0);
+  });
+});
---
+++ tests/cards/pathfinders/Ringcom.spec.ts
@@ -0,0 +1,49 @@
+import {expect} from 'chai';
+import {Ringcom} from '../../../src/cards/pathfinders/Ringcom';
+import {Game} from '../../../src/Game';
+import {TestPlayer} from '../../TestPlayer';
+import {newTestGame} from '../../TestGame';
+import {CardName} from '../../../src/CardName';
+import {TestingUtils} from '../../TestingUtils';
+import {Tags} from '../../../src/cards/Tags';
+
+describe('Ringcom', function() {
+  let card: Ringcom;
+  let player: TestPlayer;
+  let player2: TestPlayer;
+  let game: Game;
+
+  beforeEach(function() {
+    card = new Ringcom();
+    game = newTestGame(2);
+    player = game.getPlayers()[0] as TestPlayer;
+    player2 = game.getPlayers()[1] as TestPlayer;
+    player.corporationCard = card;
+  });
+
+  it('initialAction', function() {
+    const a = TestingUtils.fakeCard({name: 'A' as CardName, tags: [Tags.JOVIAN]});
+    const b = TestingUtils.fakeCard({name: 'B' as CardName, tags: []});
+    const c = TestingUtils.fakeCard({name: 'C' as CardName, tags: [Tags.EARTH]});
+    const d = TestingUtils.fakeCard({name: 'D' as CardName, tags: [Tags.JOVIAN]});
+    game.dealer.deck.push(a, b, c, d);
+
+    card.initialAction(player);
+
+    expect(player.cardsInHand).has.members([a, d]);
+  });
+
+  it('when you play a jovian tag', function() {
+    const a = TestingUtils.fakeCard({name: 'A' as CardName, tags: [Tags.JOVIAN]});
+    expect(player.titanium).eq(0);
+    player.playCard(a);
+    expect(player.titanium).eq(1);
+  });
+
+  it('when opponent plays a jovian tag', function() {
+    const a = TestingUtils.fakeCard({name: 'A' as CardName, tags: [Tags.JOVIAN]});
+    expect(player.titanium).eq(0);
+    player2.playCard(a);
+    expect(player.titanium).eq(1);
+  });
+});

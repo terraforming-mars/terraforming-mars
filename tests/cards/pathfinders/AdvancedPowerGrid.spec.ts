af4ca17bfc0faa9c0882958b967074c82649d9a1
Robert Konigsberg <konigsberg@gmail.com>
2021-12-05 13:38:10 -0500

    Advanced Power Grid doesn't include its tags.

--- tests/cards/pathfinders/AdvancedPowerGrid.spec.ts
+++ tests/cards/pathfinders/AdvancedPowerGrid.spec.ts
@@ -18,16 +18,16 @@ describe('AdvancedPowerGrid', function() {
   it('play', function() {
     player.setProductionForTest(Units.EMPTY);
     card.play(player);
-    expect(player.getProductionForTest()).deep.eq(Units.of({energy: 2}));
+    expect(player.getProductionForTest()).deep.eq(Units.of({energy: 2, megacredits: 1}));
 
     player.setProductionForTest(Units.EMPTY);
     player.tagsForTest = {power: 1};
     card.play(player);
-    expect(player.getProductionForTest()).deep.eq(Units.of({energy: 2, megacredits: 1}));
+    expect(player.getProductionForTest()).deep.eq(Units.of({energy: 2, megacredits: 2}));
 
     player.setProductionForTest(Units.EMPTY);
     player.tagsForTest = {power: 4};
     card.play(player);
-    expect(player.getProductionForTest()).deep.eq(Units.of({energy: 2, megacredits: 4}));
+    expect(player.getProductionForTest()).deep.eq(Units.of({energy: 2, megacredits: 5}));
   });
 });

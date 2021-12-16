e14206683cfc78a7a726c0fb90032d2320887431
Robert Konigsberg <konigsberg@gmail.com>
2021-12-16 07:36:19 -0800

    Remove unused code from Prefabrication of Human Habitats.

--- src/cards/pathfinders/PrefabricationofHumanHabitats.ts
+++ src/cards/pathfinders/PrefabricationofHumanHabitats.ts
@@ -4,16 +4,13 @@ import {Card} from '../Card';
 import {CardType} from '../CardType';
 import {CardName} from '../../CardName';
 import {CardRenderer} from '../render/CardRenderer';
-import {IActionCard} from '../ICard';
-import {ResourceType} from '../../ResourceType';
-import {SelectCard} from '../../inputs/SelectCard';
 import {Resources} from '../../Resources';
 import {CardRequirements} from '../CardRequirements';
 import {Tags} from '../Tags';
 import {Size} from '../render/Size';
 import {played} from '../Options';
 
-export class PrefabricationofHumanHabitats extends Card implements IProjectCard, IActionCard {
+export class PrefabricationofHumanHabitats extends Card implements IProjectCard {
   constructor() {
     super({
       cardType: CardType.ACTIVE,
@@ -42,24 +39,7 @@ export class PrefabricationofHumanHabitats extends Card implements IProjectCard,
     return card.tags.includes(Tags.CITY) ? 2 : 0;
   }
 
-  public canAct(player: Player) {
-    return player.plants > 0 && player.getResourceCards(ResourceType.ANIMAL).length > 0;
-  }
-
-  public action(player: Player) {
-    return new SelectCard(
-      'Select a card to gain an animal resource',
-      'Spend 1 plant',
-      player.getResourceCards(ResourceType.ANIMAL),
-      (cards) => {
-        player.addResource(Resources.PLANTS, -1);
-        player.addResourceTo(cards[0], {log: true});
-        return undefined;
-      });
-  }
-
-  public play(player: Player) {
-    player.game.increaseTemperature(player, 1);
+  public play() {
     return undefined;
   }
 }

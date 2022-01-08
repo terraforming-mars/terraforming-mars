9700e66681b9da80032997ad896ad7aaf8f72dc3
Robert Konigsberg <konigsberg@gmail.com>
2022-01-08 17:44:26 -0500

    Add track-name to PlanetaryTracks

--- src/client/components/pathfinders/PlanetaryTracks.vue
+++ src/client/components/pathfinders/PlanetaryTracks.vue
@@ -4,45 +4,45 @@
     <div class="track track-background-venus" v-if="gameOptions.venusNextExtension">
       <div class="track-line"></div>
       <table class="table-track track-venus">
-        <PlanetaryTrack type='risingPlayer' :val="tracks.venus" :rewards="rewards.venus" :gameOptions="gameOptions" />
-        <PlanetaryTrack trackName="venus" type='middle' :val="tracks.venus" :rewards="rewards.venus" :gameOptions="gameOptions" />
-        <PlanetaryTrack type='everyone' :val="tracks.venus" :rewards="rewards.venus" :gameOptions="gameOptions" />
+        <PlanetaryTrack track-name="venus" type='risingPlayer' :val="tracks.venus" :rewards="rewards.venus" :gameOptions="gameOptions" />
+        <PlanetaryTrack track-name="venus" type='middle' :val="tracks.venus" :rewards="rewards.venus" :gameOptions="gameOptions" />
+        <PlanetaryTrack track-name="venus" type='everyone' :val="tracks.venus" :rewards="rewards.venus" :gameOptions="gameOptions" />
       </table>
     </div>
 
     <div class="track track-background-earth">
       <div class="track-line"></div>
       <table class="table-track track-earth">
-        <PlanetaryTrack type='risingPlayer' :val="tracks.earth" :rewards="rewards.earth" :gameOptions="gameOptions" />
-        <PlanetaryTrack trackName="earth" type='middle' :val="tracks.earth" :rewards="rewards.earth" :gameOptions="gameOptions" />
-        <PlanetaryTrack type='everyone' :val="tracks.earth" :rewards="rewards.earth" :gameOptions="gameOptions" />
+        <PlanetaryTrack track-name="earth" type='risingPlayer' :val="tracks.earth" :rewards="rewards.earth" :gameOptions="gameOptions" />
+        <PlanetaryTrack track-name="earth" type='middle' :val="tracks.earth" :rewards="rewards.earth" :gameOptions="gameOptions" />
+        <PlanetaryTrack track-name="earth" type='everyone' :val="tracks.earth" :rewards="rewards.earth" :gameOptions="gameOptions" />
       </table>
     </div>
 
     <div class="track track-background-mars">
       <div class="track-line"></div>
       <table class="table-track track-mars">
-        <PlanetaryTrack type='risingPlayer' :val="tracks.mars" :rewards="rewards.mars" :gameOptions="gameOptions" />
-        <PlanetaryTrack trackName="mars" type='middle' :val="tracks.mars" :rewards="rewards.mars" :gameOptions="gameOptions" />
-        <PlanetaryTrack type='everyone' :val="tracks.mars" :rewards="rewards.mars" :gameOptions="gameOptions" />
+        <PlanetaryTrack track-name="mars" type='risingPlayer' :val="tracks.mars" :rewards="rewards.mars" :gameOptions="gameOptions" />
+        <PlanetaryTrack track-name="mars" type='middle' :val="tracks.mars" :rewards="rewards.mars" :gameOptions="gameOptions" />
+        <PlanetaryTrack track-name="mars" type='everyone' :val="tracks.mars" :rewards="rewards.mars" :gameOptions="gameOptions" />
       </table>
     </div>
 
     <div class="track track-background-jovian">
       <div class="track-line"></div>
       <table class="table-track track-jovian">
-        <PlanetaryTrack type='risingPlayer' :val="tracks.jovian" :rewards="rewards.jovian" :gameOptions="gameOptions" />
-        <PlanetaryTrack trackName="jovian" type='middle' :val="tracks.jovian" :rewards="rewards.jovian" :gameOptions="gameOptions" />
-        <PlanetaryTrack type='everyone' :val="tracks.jovian" :rewards="rewards.jovian" :gameOptions="gameOptions" />
+        <PlanetaryTrack track-name="jovian" type='risingPlayer' :val="tracks.jovian" :rewards="rewards.jovian" :gameOptions="gameOptions" />
+        <PlanetaryTrack track-name="jovian" type='middle' :val="tracks.jovian" :rewards="rewards.jovian" :gameOptions="gameOptions" />
+        <PlanetaryTrack track-name="jovian" type='everyone' :val="tracks.jovian" :rewards="rewards.jovian" :gameOptions="gameOptions" />
       </table>
     </div>
 
     <div class="track track-background-moon" v-if="gameOptions.moonExpansion">
       <div class="track-line"></div>
       <table class="table-track track-moon">
-        <PlanetaryTrack type='risingPlayer' v-if="gameOptions.moonExpansion" :val="tracks.moon" :rewards="rewards.moon" :gameOptions="gameOptions" />
-        <PlanetaryTrack trackName="moon" type='middle' v-if="gameOptions.moonExpansion" :val="tracks.moon" :rewards="rewards.moon" :gameOptions="gameOptions" />
-        <PlanetaryTrack type='everyone' v-if="gameOptions.moonExpansion" :val="tracks.moon" :rewards="rewards.moon" :gameOptions="gameOptions" />
+        <PlanetaryTrack track-name="moon" type='risingPlayer' v-if="gameOptions.moonExpansion" :val="tracks.moon" :rewards="rewards.moon" :gameOptions="gameOptions" />
+        <PlanetaryTrack track-name="moon" type='middle' v-if="gameOptions.moonExpansion" :val="tracks.moon" :rewards="rewards.moon" :gameOptions="gameOptions" />
+        <PlanetaryTrack track-name="moon" type='everyone' v-if="gameOptions.moonExpansion" :val="tracks.moon" :rewards="rewards.moon" :gameOptions="gameOptions" />
       </table>
     </div>
 

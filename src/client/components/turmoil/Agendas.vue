07cba158ff8f8907d5e26628c821f501a933e8ca
Robert Konigsberg <konigsberg@gmail.com>
2022-02-01 17:47:33 -0500

    Add v-else safety and use v-else-if for optimization.

--- src/client/components/turmoil/Agendas.vue
+++ src/client/components/turmoil/Agendas.vue
@@ -4,80 +4,80 @@
       <div class="resource money party-resource">1</div> /
       <div class="resource-tag tag-building party-resource-tag"></div>
     </template>
-    <template v-if="id === 'mb02'">
+    <template v-else-if="id === 'mb02'">
       <div class="resource money party-resource">1</div> /
       <div class="tile empty-tile-small"></div>ON MARS
     </template>
-    <template v-if="id === 'sb01'">
+    <template v-else-if="id === 'sb01'">
       <div class="resource money party-resource">1</div> /
       <div class="resource-tag tag-science party-resource-tag"></div>
     </template>
-    <template v-if="id === 'sb02'">
+    <template v-else-if="id === 'sb02'">
       <div class="resource money party-resource">1</div> / 3
       <div class="resource card card-small"></div>
     </template>
-    <template v-if="id === 'ub01'">
+    <template v-else-if="id === 'ub01'">
       <div class="resource money party-resource">1</div> /
       <div class="resource-tag tag-venus party-resource-tag"></div>
       <div class="resource-tag tag-earth party-resource-tag"></div>
       <div class="resource-tag tag-jovian party-resource-tag"></div>
     </template>
-    <template v-if="id === 'ub02'">
+    <template v-else-if="id === 'ub02'">
       <div class="resource money party-resource">1</div> /
       <div class="resource-tag tag-space party-resource-tag"></div>
     </template>
-    <template v-if="id === 'kb01'">
+    <template v-else-if="id === 'kb01'">
       <div class="resource money party-resource">1</div> /
       <div class="production-box party-production-box">
         <div class="heat production"></div>
       </div>
     </template>
-    <template v-if="id === 'kb02'">
+    <template v-else-if="id === 'kb02'">
       <div class="resource heat party-resource"></div> /
       <div class="production-box party-production-box">
         <div class="heat production"></div>
       </div>
     </template>
-    <template v-if="id === 'rb01'">
+    <template v-else-if="id === 'rb01'">
       <div class="party-inferior-rating tile party-rating party-tile">&lt;</div> :
       <div class="rating tile party-rating party-tile"></div>
     </template>
-    <template v-if="id === 'rb02'">
+    <template v-else-if="id === 'rb02'">
       <div class="party-inferior-rating tile party-rating party-tile">&gt;</div> :
       <div class="rating tile party-rating party-tile red-outline "></div>
     </template>
-    <template v-if="id === 'gb01'">
+    <template v-else-if="id === 'gb01'">
       <div class="resource money party-resource">1</div> /
       <div class="resource-tag tag-plant party-resource-tag"></div>
       <div class="resource-tag tag-microbe party-resource-tag"></div>
       <div class="resource-tag tag-animal party-resource-tag"></div>
     </template>
-    <template v-if="id === 'gb02'">
+    <template v-else-if="id === 'gb02'">
       <div class="resource money party-resource">2</div> /
       <div class="tile greenery-tile greenery-tile-small"></div>
     </template>
-    <template v-if="id === 'mfp01'">
+    <template v-else-if="id === 'mfp01'">
       <div class="policy-top-margin"><div class="tile empty-tile-small"></div> : <span class="steel resource"></span></div>
     </template>
-    <template v-if="id === 'mfp02'">
+    <template v-else-if="id === 'mfp02'">
       <div class="policy-top-margin"><div class="resource-tag tag-building"></div> : <div class="money resource">2</div></div>
     </template>
-    <template v-if="id === 'mfp03'">
+    <template v-else-if="id === 'mfp03'">
       <div class="policy-top-margin"><div class="resource steel"></div> : +<div class="resource money">1</div></div>
     </template>
-    <template v-if="id === 'mfp04'">
+    <template v-else-if="id === 'mfp04'">
       <span class="money resource">4</span>
       <span class="red-arrow-3x"></span>
       <div class="resource card card-with-border policy-card-with-tag"><div class="card-icon tag-building"></div></div>
     </template>
-    <template v-if="id === 'sp01'">
+    <template v-else-if="id === 'sp01'">
       <span class="money resource">10</span>
       <span class="red-arrow"></span>
       <span class="card card-with-border resource party-resource"></span>
       <span class="card card-with-border resource party-resource"></span>
       <span class="card card-with-border resource party-resource"></span>
     </template>
-    <template v-if="id === 'sp02'">
+    <template v-else-if="id === 'sp02'">
       <span>
         <div class="tile oxygen-tile req-tile-small" style="margin: 10px -5px;"></div>
         <div class="tile ocean-tile req-tile-small"></div>
@@ -85,7 +85,7 @@
         : ± 2
       </span>
     </template>
-    <template v-if="id === 'sp03'">
+    <template v-else-if="id === 'sp03'">
       <span>
         <div class="tile oxygen-tile req-tile-small" style="margin: 10px -5px;"></div>
         <div class="tile ocean-tile req-tile-small"></div>
@@ -93,26 +93,26 @@
         : <div class="resource card card-with-border"></div>
       </span>
     </template>
-    <template v-if="id === 'sp04'">
+    <template v-else-if="id === 'sp04'">
       <div class="scientists-requisite"><div class="resource-tag tag-science party-resource-tag"></div></div>
     </template>
-    <template v-if="id === 'up01'">
+    <template v-else-if="id === 'up01'">
       <div class="policy-top-margin"><div class="resource titanium"></div> : + <div class="resource money">1</div></div>
     </template>
-    <template v-if="id === 'up02'">
+    <template v-else-if="id === 'up02'">
       <div class="policy-top-margin">
       <span class="money resource">4</span>
       <span class="red-arrow-3x"></span>2<span class="titanium resource"></span> / 2<span class="floater resource"></span>
       </div>
     </template>
-    <template v-if="id === 'up03'">
+    <template v-else-if="id === 'up03'">
       <span class="money resource">4</span>
       <span class="red-arrow-3x"></span>
       <div class="resource card card-with-border policy-card-with-tag"><div class="card-icon tag-space"></div></div>
     </template>
-    <template v-if="id === 'up04'"> `<div class="policy-top-margin"><div class="resource-tag tag-space"></div> : <div class="money resource">-2</div></div>
+    <template v-else-if="id === 'up04'"> `<div class="policy-top-margin"><div class="resource-tag tag-space"></div> : <div class="money resource">-2</div></div>
     </template>
-    <template v-if="id === 'kp01'">
+    <template v-else-if="id === 'kp01'">
       <span class="money resource">10</span>
       <span class="red-arrow-infinity"></span>
       <div class="production-box production-box-size2">
@@ -120,35 +120,35 @@
         <div class="heat production"></div>
       </div>
     </template>
-    <template v-if="id === 'kp02'">
+    <template v-else-if="id === 'kp02'">
       <div class="tile temperature-tile req-tile-small" style="margin-right:5px;"></div> : <span class="money resource">3</span>
     </template>
-    <template v-if="id === 'kp03'">
+    <template v-else-if="id === 'kp03'">
       6 <span class="heat resource"></span>
       <span class="red-arrow-infinity"></span>
       <div class="tile temperature-tile"></div>
     </template>
-    <template v-if="id === 'kp04'">
+    <template v-else-if="id === 'kp04'">
       <div class="policy-top-margin"><div class="tile empty-tile-small"></div> :
       <span class="heat resource"></span><span class="heat resource"></span></div>
     </template>
-    <template v-if="id === 'rp01'">
+    <template v-else-if="id === 'rp01'">
       <div class="policy-top-margin">
       <div class="rating tile"></div> :
       <div class="resource money">-3</div>
       </div>
     </template>
-    <template v-if="id === 'rp02'">
+    <template v-else-if="id === 'rp02'">
       <div class="policy-top-margin"><div class="tile empty-tile-small"></div> : <span class="money resource">-3</span></div>
     </template>
-    <template v-if="id === 'rp03'">
+    <template v-else-if="id === 'rp03'">
       <span class="money resource">4</span>
       <span class="red-arrow-3x"></span>
       <div class="tile oxygen-tile req-tile-small red-outline" style="margin: 10px -5px;"></div> /
       <div class="tile ocean-tile req-tile-small red-outline"></div> /
       <div class="tile temperature-tile req-tile-small red-outline"></div>
     </template>
-    <template v-if="id === 'rp04'">
+    <template v-else-if="id === 'rp04'">
       <div class="tile oxygen-tile req-tile-small" style="margin: 10px -5px;"></div>
       <div class="tile ocean-tile req-tile-small"></div>
       <div class="tile temperature-tile req-tile-small"></div>
@@ -156,24 +156,27 @@
         <div class="production-prefix minus"></div><div class="money production">1</div>
       </div>
     </template>
-    <template v-if="id === 'gp01'">
+    <template v-else-if="id === 'gp01'">
       <div class="tile greenery-tile"></div> : <div class="resource money">4</div>
     </template>
-    <template v-if="id === 'gp02'">
+    <template v-else-if="id === 'gp02'">
       <div class="policy-top-margin"><div class="tile empty-tile-small"></div> : <span class="plant resource"></span></div>
     </template>
-    <template v-if="id === 'gp03'">
+    <template v-else-if="id === 'gp03'">
       <div class="policy-top-margin">
       <div class="resource-tag tag-plant party-resource-tag"></div>
       <div class="resource-tag tag-microbe party-resource-tag"></div>
       <div class="resource-tag tag-animal party-resource-tag"></div> : <div class="resource money">2</div>
       </div>
     </template>
-    <template v-if="id === 'gp04'">
+    <template v-else-if="id === 'gp04'">
       <div class="policy-top-margin">
       <span class="money resource">5</span>
       <span class="red-arrow-3x"></span>3<span class="plant resource"></span> / 2<span class="microbe resource"></span>
       </div>
+    </template>
+    <template v-else>
+      <div>Unknown agenda ID {{id}}</div>
     </template>
   </div>
 </template>

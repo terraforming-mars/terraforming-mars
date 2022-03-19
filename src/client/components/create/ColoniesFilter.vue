478aabc61b9b6331216fda1e155f5ed2ae0e0a9e
Robert Konigsberg <konigsberg@gmail.com>
2022-03-18 22:27:51 -0400

    Add all/none/invert options to ColoniesFilter

--- src/client/components/create/ColoniesFilter.vue
+++ src/client/components/create/ColoniesFilter.vue
@@ -2,17 +2,18 @@
     <div class="colonies-filter">
         <div>
             <h2 v-i18n>Colonies</h2>
+            <div class="corporations-filter-toolbox corporations-filter-toolbox--topmost">
+                <a href="#" v-i18n v-on:click.prevent="selectAll('All')">All*</a> |
+                <a href="#" v-i18n v-on:click.prevent="selectNone('All')">None*</a> |
+                <a href="#" v-i18n v-on:click.prevent="invertSelection('All')">Invert*</a>
+            </div>
         </div>
-        <div class="colonies-filter-list">
-            <h2 v-i18n>Official</h2>
-            <label class="form-checkbox" v-for="colony in officialColonies" v-bind:key="colony">
-                <input type="checkbox" v-model="selectedColonies" :value="colony">
-                <i class="form-icon"></i><span v-i18n>{{ colony }} - ({{ description(colony) }})</span>
-            </label>
-        </div>
-        <div class="colonies-filter-list">
-            <h2 v-i18n>Community</h2>
-            <label class="form-checkbox" v-for="colony in communityColonies" v-bind:key="colony">
+        <div class="colonies-filter-list" v-for="module in modules" v-bind:key="module">
+            <h2 v-i18n>{{title(module)}}</h2>
+              <a href="#" v-i18n v-on:click.prevent="selectAll(module)">All</a> |
+              <a href="#" v-i18n v-on:click.prevent="selectNone(module)">None</a> |
+              <a href="#" v-i18n v-on:click.prevent="invertSelection(module)">Invert</a>
+            <label class="form-checkbox" v-for="colony in getColonies(module)" v-bind:key="colony">
                 <input type="checkbox" v-model="selectedColonies" :value="colony">
                 <i class="form-icon"></i><span v-i18n>{{ colony }} - ({{ description(colony) }})</span>
             </label>
@@ -25,13 +26,18 @@ import Vue from 'vue';
 import {ColonyName} from '@/common/colonies/ColonyName';
 import {COLONY_DESCRIPTIONS} from '@/common/colonies/ColonyDescription';
 import {OFFICIAL_COLONY_NAMES, COMMUNITY_COLONY_NAMES} from '@/common/colonies/AllColonies';
+import {GameModule} from '@/common/cards/GameModule';
 
 type Data = {
   allColonies: Array<ColonyName>,
   officialColonies: Array<ColonyName>,
   communityColonies: Array<ColonyName>,
   selectedColonies: Array<ColonyName>,
+  modules: Array<ColonyModule>,
 }
+type ColonyModule = GameModule.Colonies | GameModule.Community;
+type Group = ColonyModule | 'All';
+
 export default Vue.extend({
   name: 'ColoniesFilter',
   props: {
@@ -54,12 +60,62 @@ export default Vue.extend({
         ...OFFICIAL_COLONY_NAMES,
         ...this.communityCardsOption ? COMMUNITY_COLONY_NAMES: [],
       ],
+      modules: [GameModule.Colonies, GameModule.Community],
     };
     return data;
   },
   methods: {
     description(colonyName: ColonyName): string {
       return COLONY_DESCRIPTIONS.get(colonyName) ?? 'unknown';
+    },
+    getItemsByGroup(group: Group): Array<ColonyName> {
+      switch (group) {
+      case 'All': return this.allColonies;
+      case GameModule.Colonies: return this.officialColonies;
+      case GameModule.Community: return this.communityColonies;
+      default: return [];
+      }
+    },
+    selectAll(group: Group) {
+      const items = this.getItemsByGroup(group);
+      for (const item of items) {
+        if (this.selectedColonies.includes(item) === false) {
+          this.selectedColonies.push(item);
+        }
+      }
+    },
+    removeFromSelection(colonyName: ColonyName) {
+      const itemIdx = this.selectedColonies.indexOf(colonyName);
+      if (itemIdx !== -1) {
+        this.selectedColonies.splice(itemIdx, 1);
+      }
+    },
+    selectNone(group: Group) {
+      const items = this.getItemsByGroup(group);
+      for (const item of items) {
+        this.removeFromSelection(item);
+      }
+    },
+    invertSelection(group: Group) {
+      const items = this.getItemsByGroup(group);
+
+      for (const idx in items) {
+        if (this.selectedColonies.includes(items[idx])) {
+          this.removeFromSelection(items[idx]);
+        } else {
+          this.selectedColonies.push(items[idx]);
+        }
+      }
+    },
+    title(module: ColonyModule) {
+      if (module === GameModule.Colonies) return 'Official';
+      if (module === GameModule.Community) return 'Community';
+      return module;
+    },
+    getColonies(module: ColonyModule) {
+      if (module === GameModule.Colonies) return this.officialColonies;
+      if (module === GameModule.Community) return this.communityColonies;
+      return [];
     },
   },
   watch: {

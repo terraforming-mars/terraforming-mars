f1af4afcc6d97698d6740ac4bb1b4016fbfd006d
Robert Konigsberg <konigsberg@gmail.com>
2022-03-26 13:18:14 -0400

    Improve Mars U and Artificial Photosynthesis priorities.
    Also introducing shorthand code for deferring actions.

--- tests/cards/base/ArtificialPhotosynthesis.spec.ts
+++ tests/cards/base/ArtificialPhotosynthesis.spec.ts
@@ -1,23 +1,25 @@
-
 import {expect} from 'chai';
 import {ArtificialPhotosynthesis} from '../../../src/cards/base/ArtificialPhotosynthesis';
 import {Game} from '../../../src/Game';
 import {OrOptions} from '../../../src/inputs/OrOptions';
 import {Resources} from '../../../src/common/Resources';
 import {TestPlayers} from '../../TestPlayers';
+import {TestingUtils} from '../../TestingUtils';
 
 describe('ArtificialPhotosynthesis', () => {
   it('Should play', () => {
     const card = new ArtificialPhotosynthesis();
     const player = TestPlayers.BLUE.newPlayer();
-    Game.newInstance('foobar', [player], player);
+    const game = Game.newInstance('foobar', [player], player);
+    player.popWaitingFor(); // Remove SelectInitialCards.
     const action = card.play(player);
-    expect(action).is.not.undefined;
-    expect(action).instanceOf(OrOptions);
-    expect(action.options).has.lengthOf(2);
-    action.options[0].cb();
+    expect(action).is.undefined;
+    TestingUtils.runAllActions(game);
+    const options = TestingUtils.cast(player.popWaitingFor(), OrOptions);
+    expect(options.options).has.lengthOf(2);
+    options.options[0].cb();
     expect(player.getProduction(Resources.ENERGY)).to.eq(2);
-    action.options[1].cb();
+    options.options[1].cb();
     expect(player.getProduction(Resources.PLANTS)).to.eq(1);
   });
 });

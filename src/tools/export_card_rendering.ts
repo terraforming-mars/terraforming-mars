2cac3fbd024ceddc0329b199444cb66ed90dfc63
Robert Konigsberg <konigsberg@gmail.com>
2022-03-27 11:31:22 -0400

    Dump global events in export.

--- src/tools/export_card_rendering.ts
+++ src/tools/export_card_rendering.ts
@@ -6,46 +6,74 @@ import {CardManifest} from '../cards/CardManifest';
 import {ICard} from '../cards/ICard';
 import {Deck} from '../Deck';
 import {GameModule} from '../common/cards/GameModule';
+import {IGlobalEvent} from '../turmoil/globalEvents/IGlobalEvent';
+import {ALL_EVENTS} from '../turmoil/globalEvents/GlobalEventDealer';
+import {IClientGlobalEvent} from '../common/turmoil/IClientGlobalEvent';
 
-const json: Array<any> = [];
+class ProjectCardProcessor {
+  public static json: Array<any> = [];
+  public static makeJson() {
+    ALL_CARD_MANIFESTS.forEach(this.processManifest);
+  }
 
-console.log('Starting');
-ALL_CARD_MANIFESTS.forEach(processManifest);
-console.log('Done');
+  private static processManifest(manifest: CardManifest) {
+    for (const deck of [manifest.projectCards, manifest.corporationCards, manifest.preludeCards, manifest.standardActions, manifest.standardProjects]) {
+      ProjectCardProcessor.processDeck(manifest.module, deck);
+    }
+  }
 
-function processManifest(manifest: CardManifest) {
-  processDeck(manifest.module, manifest.projectCards);
-  processDeck(manifest.module, manifest.corporationCards);
-  processDeck(manifest.module, manifest.preludeCards);
-  processDeck(manifest.module, manifest.standardActions);
-  processDeck(manifest.module, manifest.standardProjects);
+  private static processDeck(module: GameModule, deck: Deck<ICard>) {
+    deck.factories.forEach((factory) => {
+      ProjectCardProcessor.processCard(module, new factory.Factory());
+    });
+  }
+
+  private static processCard(module: GameModule, card: ICard) {
+    const vizCardData = {
+      module: module,
+      name: card.name,
+      tags: card.tags,
+      cardDiscount: card.cardDiscount,
+      resourceType: card.resourceType,
+      cost: card.cost,
+      cardType: card.cardType,
+      requirements: card.requirements,
+      metadata: card.metadata,
+      warning: card.warning,
+    };
+
+    ProjectCardProcessor.json.push(vizCardData);
+  }
 }
 
-function processDeck(module: GameModule, deck: Deck<ICard>) {
-  deck.factories.forEach((factory) => {
-    processCard(module, new factory.Factory());
-  });
-}
+class GlobalEventProcessor {
+  public static json: Array<any> = [];
+  public static makeJson() {
+    ALL_EVENTS.forEach((Factory) => {
+      const globalEvent = new Factory();
+      GlobalEventProcessor.processGlobalEvent(globalEvent);
+    });
+  }
 
-function processCard(module: GameModule, card: ICard) {
-  const vizCardData = {
-    module: module,
-    name: card.name,
-    tags: card.tags,
-    cardDiscount: card.cardDiscount,
-    resourceType: card.resourceType,
-    cost: card.cost,
-    cardType: card.cardType,
-    requirements: card.requirements,
-    metadata: card.metadata,
-    warning: card.warning,
-  };
+  private static processGlobalEvent(globalEvent: IGlobalEvent) {
+    const vizCardData: IClientGlobalEvent = {
+      name: globalEvent.name,
+      description: globalEvent.description,
+      revealedDelegate: globalEvent.revealedDelegate,
+      currentDelegate: globalEvent.currentDelegate,
+      renderData: globalEvent.renderData,
+    };
 
-  json.push(vizCardData);
+    GlobalEventProcessor.json.push(vizCardData);
+  }
 }
 
 if (!fs.existsSync('src/genfiles')) {
   fs.mkdirSync('src/genfiles');
 }
 
-fs.writeFileSync('src/genfiles/cards.json', JSON.stringify(json, null, 2));
+ProjectCardProcessor.makeJson();
+GlobalEventProcessor.makeJson();
+
+fs.writeFileSync('src/genfiles/cards.json', JSON.stringify(ProjectCardProcessor.json, null, 2));
+fs.writeFileSync('src/genfiles/events.json', JSON.stringify(GlobalEventProcessor.json, null, 2));

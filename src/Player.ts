239b5db61f5cd11b6a6a0753829aad8e75f8c7b1
Robert Konigsberg <konigsberg@gmail.com>
2022-04-01 12:51:01 -0400

    Log error when discarding card is not in a player's played cards.

--- src/Player.ts
+++ src/Player.ts
@@ -1653,8 +1653,11 @@ export class Player {
   }
 
   public discardPlayedCard(card: IProjectCard) {
-    // This is copied straight out of Project Workshop.
     const cardIndex = this.playedCards.findIndex((c) => c.name === card.name);
+    if (cardIndex === -1) {
+      console.log(`Error: card ${card.name} not in ${this.id}'s hand`);
+      return;
+    }
     this.playedCards.splice(cardIndex, 1);
     this.game.dealer.discard(card);
     card.onDiscard?.(this);

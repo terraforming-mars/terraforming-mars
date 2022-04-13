3395082b68744e01a8ca315bbe669a1108726c00
Robert Konigsberg <konigsberg@gmail.com>
2022-04-13 14:57:10 -0400

    Centralize common route testing code.

---
+++ tests/routes/RouteTestScaffolding.ts
@@ -0,0 +1,29 @@
+import * as http from 'http';
+import {IContext} from '../../src/routes/IHandler';
+import {Route} from '../../src/routes/Route';
+import {FakeGameLoader} from './FakeGameLoader';
+
+export type Header = 'accept-encoding';
+
+// Reusable components for testing routes.
+export class RouteTestScaffolding {
+  public req: http.IncomingMessage;
+  public ctx: IContext;
+
+  constructor(req: Partial<http.IncomingMessage> = {}) {
+    this.req = req as http.IncomingMessage;
+    this.ctx = {
+      route: new Route(),
+      serverId: '1',
+      url: new URL('http://boo.com'),
+      gameLoader: new FakeGameLoader(),
+    };
+    if (!this.req.headers) this.req.headers = {};
+  }
+
+  // Strictly speaking |url| can also accept a fragment.
+  public set url(headlessUri: string) {
+    this.req.url = headlessUri;
+    this.ctx.url = new URL('http://boo.com' + headlessUri);
+  }
+}

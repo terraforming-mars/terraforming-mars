2f6d5d05dc7255f923610d4bc5f96fd79a6159d2
Robert Konigsberg <konigsberg@gmail.com>
2022-04-24 15:18:43 -0400

    Remove server-side colonies and events. webpack-ed code down 50%
    Before:
    
    asset main.js 2.9 MiB [emitted] [minimized] [big] (name: main) 4 related assets
    orphan modules 363 KiB [orphan] 295 modules
    runtime modules 1.13 KiB 5 modules
    modules by path ./src/ 4.14 MiB 1187 modules
    modules by path ./node_modules/ 1.01 MiB
      modules by path ./node_modules/mnemonist/*.js 244 KiB 39 modules
      modules by path ./node_modules/mnemonist/utils/*.js 25.9 KiB 7 modules
      modules by path ./node_modules/css-loader/dist/runtime/*.js 3.79 KiB
        ./node_modules/css-loader/dist/runtime/cssWithMappingToString.js 2.22 KiB [built] [code generated]
        ./node_modules/css-loader/dist/runtime/api.js 1.57 KiB [built] [code generated]
      modules by path ./node_modules/obliterator/*.js 5.18 KiB
        ./node_modules/obliterator/foreach.js 3.45 KiB [built] [code generated]
        ./node_modules/obliterator/iterator.js 1.73 KiB [built] [code generated]
      6 modules
    
    WARNING in asset size limit: The following asset(s) exceed the recommended size limit (244 KiB).
    This can impact web performance.
    Assets:
      main.js (2.9 MiB)
      main.js.gz (495 KiB)
      main.js.map.gz (1.26 MiB)
      main.js.br (351 KiB)
      main.js.map.br (969 KiB)
    
    WARNING in entrypoint size limit: The following entrypoint(s) combined asset size exceeds the recommended limit (244 KiB). This can impact web performance.
    Entrypoints:
      main (2.9 MiB)
          main.js
    
    After:
    
    asset main.js 1.48 MiB [emitted] [minimized] [big] (name: main) 4 related assets
    orphan modules 363 KiB [orphan] 295 modules
    runtime modules 1.13 KiB 5 modules
    javascript modules 1.84 MiB
      modules by path ./src/client/ 764 KiB 265 modules
      modules by path ./node_modules/ 1.01 MiB
        modules by path ./node_modules/mnemonist/*.js 244 KiB 39 modules
        modules by path ./node_modules/mnemonist/utils/*.js 25.9 KiB 7 modules
        modules by path ./node_modules/css-loader/dist/runtime/*.js 3.79 KiB 2 modules
        modules by path ./node_modules/obliterator/*.js 5.18 KiB 2 modules
      modules by path ./src/common/ 82.1 KiB 37 modules
    json modules 622 KiB
      ./src/genfiles/settings.json 105 bytes [built] [code generated]
      ./src/genfiles/cards.json 590 KiB [built] [code generated]
      ./src/genfiles/events.json 28.1 KiB [built] [code generated]
      ./src/genfiles/colonies.json 3.97 KiB [built] [code generated]
    
    WARNING in asset size limit: The following asset(s) exceed the recommended size limit (244 KiB).
    This can impact web performance.
    Assets:
      main.js (1.48 MiB)
      main.js.gz (280 KiB)
      main.js.map.gz (698 KiB)
      main.js.map.br (575 KiB)
    
    WARNING in entrypoint size limit: The following entrypoint(s) combined asset size exceeds the recommended limit (244 KiB). This can impact web performance.
    Entrypoints:
      main (1.48 MiB)
          main.js

--- src/client/turmoil/ClientGlobalEventManifest.ts
+++ src/client/turmoil/ClientGlobalEventManifest.ts
@@ -1,35 +1,21 @@
-import {ALL_EVENTS} from '@/turmoil/globalEvents/GlobalEventDealer';
 import {GlobalEventModel} from '@/common/models/TurmoilModel';
 import {GlobalEventName} from '@/common/turmoil/globalEvents/GlobalEventName';
 import {PartyName} from '@/common/turmoil/PartyName';
 import {IClientGlobalEvent} from '@/common/turmoil/IClientGlobalEvent';
 // @ts-ignore events.json doesn't exist during npm run build
 import * as eventJson from '@/genfiles/events.json';
-import {getPreferences} from '../utils/PreferencesManager';
 
 const events: Map<GlobalEventName, IClientGlobalEvent> = new Map();
-if (getPreferences().experimental_ui) {
-  (eventJson as any as Array<IClientGlobalEvent>).forEach((card) => {
-    events.set(card.name, card);
-  });
-}
+(eventJson as any as Array<IClientGlobalEvent>).forEach((card) => {
+  events.set(card.name, card);
+});
 
 export function allGlobalEventNames() {
-  if (getPreferences().experimental_ui) {
-    return events.keys();
-  } else {
-    return ALL_EVENTS.keys();
-  }
+  return events.keys();
 }
 
 export function getGlobalEvent(globalEventName: GlobalEventName): IClientGlobalEvent | undefined {
-  if (getPreferences().experimental_ui) {
-    return events.get(globalEventName);
-  } else {
-    const Factory = ALL_EVENTS.get(globalEventName);
-    if (Factory !== undefined) return new Factory();
-    return undefined;
-  }
+  return events.get(globalEventName);
 }
 
 export function getGlobalEventModel(globalEventName: GlobalEventName): GlobalEventModel {

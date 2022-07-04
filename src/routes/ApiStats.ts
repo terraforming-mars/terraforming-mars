1a4bd2b82c81819d332de44016e5a4cb1e3a6371
Robert Konigsberg <konigsberg@gmail.com>
2022-07-04 19:24:05 -0400

    Add more runtime metrics.
    Split out some of the startup processing to make startup easier to read.

--- src/routes/ApiStats.ts
+++ src/routes/ApiStats.ts
@@ -2,6 +2,7 @@ import * as http from 'http';
 import {AsyncHandler} from './Handler';
 import {IContext} from './IHandler';
 import {Database} from '../database/Database';
+import {Metrics} from '../server/metrics';
 
 
 export class ApiStats extends AsyncHandler {
@@ -12,8 +13,9 @@ export class ApiStats extends AsyncHandler {
 
   public override async get(req: http.IncomingMessage, res: http.ServerResponse, ctx: IContext): Promise<void> {
     try {
+      const metrics = Metrics.INSTANCE.get();
       const stats = await Database.getInstance().stats();
-      ctx.route.writeJson(res, stats);
+      ctx.route.writeJson(res, {metrics, stats}, 2);
     } catch (err) {
       console.error(err);
       ctx.route.badRequest(req, res, 'could not load admin stats');

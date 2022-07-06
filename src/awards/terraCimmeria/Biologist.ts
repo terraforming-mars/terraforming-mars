2e60436805e8a572bc8266c36e4b177dad649448
Robert Konigsberg <kberg@users.noreply.github.com>
2022-07-05 21:56:18 -0400

    Update Biologist.ts

--- src/awards/terraCimmeria/Biologist.ts
+++ src/awards/terraCimmeria/Biologist.ts
@@ -4,7 +4,7 @@ import {IAward} from '../IAward';
 
 export class Biologist implements IAward {
   public name: string = 'Biologist';
-  public description: string = 'Most Animal, Plant, and Microbe tags';
+  public description: string = 'Having the most Animal, Plant, and Microbe tags in play';
 
   public getScore(player: Player): number {
     return player.getTagCount(Tags.MICROBE, 'award') + player.getTagCount(Tags.PLANT, 'award') + player.getTagCount(Tags.ANIMAL, 'award');

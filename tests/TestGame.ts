68e44341e57f536c9813f7d16a3f14cff22e60fb
Robert Konigsberg <konigsberg@gmail.com>
2022-07-11 22:05:41 -0400

    Add the ability to tag Test player ids and game ids with a suffix.
    Will help when dealing with GameLoader and GameIds.

--- tests/TestGame.ts
+++ tests/TestGame.ts
@@ -3,22 +3,22 @@ import {TestPlayers} from './TestPlayers';
 import {setCustomGameOptions} from './TestingUtils';
 import {TestPlayer} from './TestPlayer';
 
-export function newTestGame(count: number, customOptions?: Partial<GameOptions>): Game {
+export function newTestGame(count: number, customOptions?: Partial<GameOptions>, idSuffix = ''): Game {
   const players = [
-    TestPlayers.BLUE.newPlayer(),
-    TestPlayers.RED.newPlayer(),
-    TestPlayers.YELLOW.newPlayer(),
-    TestPlayers.GREEN.newPlayer(),
-    TestPlayers.BLACK.newPlayer(),
-    TestPlayers.PURPLE.newPlayer(),
-    TestPlayers.ORANGE.newPlayer(),
-    TestPlayers.PINK.newPlayer(),
+    TestPlayers.BLUE.newPlayer(false, idSuffix),
+    TestPlayers.RED.newPlayer(false, idSuffix),
+    TestPlayers.YELLOW.newPlayer(false, idSuffix),
+    TestPlayers.GREEN.newPlayer(false, idSuffix),
+    TestPlayers.BLACK.newPlayer(false, idSuffix),
+    TestPlayers.PURPLE.newPlayer(false, idSuffix),
+    TestPlayers.ORANGE.newPlayer(false, idSuffix),
+    TestPlayers.PINK.newPlayer(false, idSuffix),
   ].slice(0, count);
 
   const options: GameOptions | undefined = customOptions === undefined ?
     undefined :
     setCustomGameOptions(customOptions);
-  return Game.newInstance('game-id', players, players[0], options);
+  return Game.newInstance(`game-id${idSuffix}`, players, players[0], options);
 }
 
 export function getTestPlayer(game: Game, idx: number): TestPlayer {

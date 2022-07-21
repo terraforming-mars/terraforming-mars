6cf1a310513829606d7e7c98f5c56eaa4301dd80
Robert Konigsberg <konigsberg@gmail.com>
2022-07-21 03:01:13 -0400

    Rewrite Odyssey to be include paying for the card.
    This required touching quite a lot of the code for the
    select-a-card-to-play,pay-for-it,play-it action sequence.

--- src/colonies/Colony.ts
+++ src/colonies/Colony.ts
@@ -229,7 +229,7 @@ export abstract class Colony implements IColony {
 
       case ColonyBenefit.GAIN_SCIENCE_TAG:
         player.scienceTagCount += 1;
-        player.playCard(new ScienceTagCard(), undefined, false);
+        player.playCard(new ScienceTagCard(), undefined, 'nothing');
         game.log('${0} gained 1 Science tag', (b) => b.player(player));
         break;
 

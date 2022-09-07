8a4a9afc0946cd3c53acf0dd3322b009676aed30
Robert Konigsberg <kberg@users.noreply.github.com>
2022-09-07 11:03:59 -0400

    Update some tests. (#4903)
    There's a little bit of improved testing, but as this service
    moves toward the Behavior/bespoke API, tests with Player instances and
    not Game instances will break. So this is pre-emptive.

--- tests/cards/prelude/Mohole.spec.ts
+++ tests/cards/prelude/Mohole.spec.ts
@@ -1,11 +1,12 @@
 import {expect} from 'chai';
 import {Mohole} from '../../../src/server/cards/prelude/Mohole';
-import {TestPlayer} from '../../TestPlayer';
+import {getTestPlayer, newTestGame} from '../../TestGame';
 
 describe('Mohole', function() {
   it('Should play', function() {
     const card = new Mohole();
-    const player = TestPlayer.BLUE.newPlayer();
+    const game = newTestGame(1);
+    const player = getTestPlayer(game, 0);
     const action = card.play(player);
     expect(action).is.undefined;
     expect(player.production.heat).to.eq(3);
--- tests/cards/venusNext/TerraformingContract.spec.ts
+++ tests/cards/venusNext/TerraformingContract.spec.ts
@@ -1,12 +1,18 @@
 import {expect} from 'chai';
 import {TerraformingContract} from '../../../src/server/cards/venusNext/TerraformingContract';
-import {TestPlayer} from '../../TestPlayer';
+import {getTestPlayer, newTestGame} from '../../TestGame';
 
 describe('TerraformingContract', function() {
   it('Should play', function() {
     const card = new TerraformingContract();
-    const player = TestPlayer.BLUE.newPlayer();
+    const game = newTestGame(1);
+    const player = getTestPlayer(game, 0);
+
+    player.setTerraformRating(24);
     expect(player.canPlayIgnoringCost(card)).is.not.true;
+    player.setTerraformRating(25);
+    expect(player.canPlayIgnoringCost(card)).is.true;
+
     const action = card.play(player);
     expect(action).is.undefined;
     expect(player.production.megacredits).to.eq(4);

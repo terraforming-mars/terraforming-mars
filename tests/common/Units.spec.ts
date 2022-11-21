1ef269895d248b86e33d23cf24b11f0bf6bdaaa2
Robert Konigsberg <konigsberg@gmail.com>
2022-11-21 10:50:48 -0500

    Add Units.partial. Add some more Units tests.

--- tests/common/Units.spec.ts
+++ tests/common/Units.spec.ts
@@ -66,4 +66,41 @@ describe('Units', () => {
       heat: 1,
     });
   });
+
+  // todo: test for isUnits
+
+  it('isEmpty', () => {
+    expect(Units.isEmpty({})).is.true;
+    expect(Units.isEmpty(Units.EMPTY)).is.true;
+    expect(Units.isEmpty(Units.negative(Units.EMPTY))).is.true;
+    expect(Units.isEmpty({steel: 0})).is.true;
+    expect(Units.isEmpty({steel: 1})).is.false;
+  });
+
+  it('negative', () => {
+    expect(Units.negative(Units.EMPTY)).deep.eq(Units.EMPTY);
+    expect(Units.negative(Units.of({
+      megacredits: -2,
+      steel: -1,
+      titanium: 0,
+      plants: 1,
+      energy: 2,
+      heat: 3,
+    }))).deep.eq(Units.of({
+      megacredits: 2,
+      steel: 1,
+      titanium: 0,
+      plants: -1,
+      energy: -2,
+      heat: -3,
+    }));
+  });
+
+  it('partial', () => {
+    expect(Units.partial({})).deep.eq({});
+    expect(Units.partial({megacredits: 0})).deep.eq({});
+    expect(Units.partial({megacredits: undefined})).deep.eq({});
+    expect(Units.partial({megacredits: 1})).deep.eq({megacredits: 1});
+    expect(Units.partial({megacredits: -1})).deep.eq({megacredits: -1});
+  });
 });

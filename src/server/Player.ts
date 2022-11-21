e263a94ef83670042fcb78d6fe1f06cd995aff3f
d-little <david.n.little@gmail.com>
2022-11-21 16:41:19 +1100

    Revert "add a reminder that you'll draw an additional Merger"
    This reverts commit 8f7d8eaffa3e3feca59b4c3a5ea50036269f7c17.

--- src/server/Player.ts
+++ src/server/Player.ts
@@ -902,15 +902,9 @@ export class Player {
       cards = passedCards;
     }
 
-    let tmpmsg = cardsToKeep === 1 ?
+    const message = cardsToKeep === 1 ?
       'Select a card to keep and pass the rest to ${0}' :
       'Select two cards to keep and pass the rest to ${0}';
-
-    if (this.game.gameOptions.twoCorpsVariant) {
-      tmpmsg = tmpmsg.concat(' (an additional Merger Prelude will be drawn at the end of all drafting)');
-    }
-
-    const message = tmpmsg;
 
     this.setWaitingFor(
       new SelectCard({

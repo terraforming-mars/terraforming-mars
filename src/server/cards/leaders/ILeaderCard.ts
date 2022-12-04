023e3247fed2f9af27405c70fca7ef2abd6f8ee5
d-little <david.n.little@gmail.com>
2022-12-04 20:08:30 +1100

    Re-add checks to fix manifest complaints

--- src/server/cards/leaders/ILeaderCard.ts
+++ src/server/cards/leaders/ILeaderCard.ts
@@ -1,15 +1,14 @@
-// import {ICard} from '../ICard';
+import {ICard} from '../ICard';
 import {Player} from '../../Player'
-import {IProjectCard} from '../IProjectCard';
-// import {CardType} from '../../../common/cards/CardType';
+import {CardType} from '../../../common/cards/CardType';
 
-export interface ILeaderCard extends IProjectCard {
+export interface ILeaderCard extends ICard {
   isDisabled?: boolean;
   opgActionIsActive?: boolean;
   generationUsed?: number;
   onTRIncrease?: (player: Player) => undefined;
 }
 
-// export function isLeaderCard(card: ICard): card is ILeaderCard {
-//   return card.cardType === CardType.LEADER;
-// }
+export function isLeaderCard(card: ICard): card is ILeaderCard {
+  return card.cardType === CardType.LEADER;
+}

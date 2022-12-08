757b667cf78baff7098a7b161c487109f9bbbe38
Robert Konigsberg <kberg@users.noreply.github.com>
2022-12-08 00:07:28 -0500

    Fix regression with Bactoviral research. (#5177)

--- tests/cards/promo/BactoviralResearch.spec.ts
+++ tests/cards/promo/BactoviralResearch.spec.ts
@@ -8,6 +8,8 @@ import {BactoviralResearch} from '../../../src/server/cards/promo/BactoviralRese
 import {Game} from '../../../src/server/Game';
 import {SelectCard} from '../../../src/server/inputs/SelectCard';
 import {TestPlayer} from '../../TestPlayer';
+import {SecurityFleet} from '../../../src/server/cards/base/SecurityFleet';
+import {PharmacyUnion} from '../../../src/server/cards/promo/PharmacyUnion';
 
 describe('BactoviralResearch', function() {
   let card: BactoviralResearch;
@@ -36,13 +38,13 @@ describe('BactoviralResearch', function() {
   });
 
   it('Should play with single microbe card', function() {
-    const card2 = new RegolithEaters();
-    player.playedCards.push(card2);
+    const microbeCard = new RegolithEaters();
+    player.playedCards.push(microbeCard);
     expect(card.play(player)).is.undefined;
 
     runAllActions(game);
 
-    expect(card2.resourceCount).to.eq(2);
+    expect(microbeCard.resourceCount).to.eq(2);
     expect(player.cardsInHand.length).to.eq(1);
   });
 
@@ -50,6 +52,28 @@ describe('BactoviralResearch', function() {
     expect(card.play(player)).is.undefined;
     runAllActions(game);
 
+    expect(player.cardsInHand.length).to.eq(1);
+  });
+
+  it('Should ignore non-microbe cards', () => {
+    const securityFleetCard = new SecurityFleet();
+    player.playedCards.push(securityFleetCard);
+    expect(card.play(player)).is.undefined;
+
+    runAllActions(game);
+
+    expect(securityFleetCard.resourceCount).to.eq(0);
+    expect(player.cardsInHand.length).to.eq(1);
+  });
+
+  it('Should ignore cards with microbe tags but does not collect microbe resources.', () => {
+    const pharmacyUnion = new PharmacyUnion();
+    player.corporations.push(pharmacyUnion);
+    expect(card.play(player)).is.undefined;
+
+    runAllActions(game);
+
+    expect(pharmacyUnion.resourceCount).to.eq(0);
     expect(player.cardsInHand.length).to.eq(1);
   });
 });

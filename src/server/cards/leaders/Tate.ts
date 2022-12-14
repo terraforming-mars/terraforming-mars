f4b9fcd89acfaa81c67ad69facc7aa1bb7d8e050
d-little <david.n.little@gmail.com>
2022-12-14 16:47:21 +1100

    Remove Event tags, switch to array, git blame tightens

--- src/server/cards/leaders/Tate.ts
+++ src/server/cards/leaders/Tate.ts
@@ -42,7 +42,12 @@ export class Tate extends Card implements LeaderCard {
     const game = player.game;
 
     // There's only one card with a Wild tag in the game right now... we shouldn't use that
-    const tags = game.getAllValidTags().filter(tag => tag !== Tag.WILD);
+    // Also remove Event tags.  
+    const excludedTags = [
+      Tag.WILD,
+      Tag.EVENT,
+    ]
+    const tags = game.getAllValidTags().filter(tag => !excludedTags.includes(tag));
     const options = tags.map((tag) => {
       return new SelectOption('Search for ' + tag + ' tags', 'Search', () => {
         game.log('${0} searched for ${1} tags', (b) => b.player(player).string(tag));

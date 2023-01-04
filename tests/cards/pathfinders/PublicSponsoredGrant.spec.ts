2da9426f46a3d57258ef5ac26f80fb3720e01735
d-little <david.n.little@gmail.com>
2023-01-04 15:19:53 +1100

    Update Testing for Public Sponsored Grant to catch bad-Event tags

--- tests/cards/pathfinders/PublicSponsoredGrant.spec.ts
+++ tests/cards/pathfinders/PublicSponsoredGrant.spec.ts
@@ -4,8 +4,8 @@ import {Game} from '../../../src/server/Game';
 import {Turmoil} from '../../../src/server/turmoil/Turmoil';
 import {TestPlayer} from '../../TestPlayer';
 import {getTestPlayer, newTestGame} from '../../TestGame';
-import {BiomassCombustors} from '../../../src/server/cards/base/BiomassCombustors';
 import {SearchForLife} from '../../../src/server/cards/base/SearchForLife';
+import {BiomassCombustors} from '../../../src/server/cards/base/BiomassCombustors';
 import {ColonizerTrainingCamp} from '../../../src/server/cards/base/ColonizerTrainingCamp';
 import {Unity} from '../../../src/server/turmoil/parties/Unity';
 import {Scientists} from '../../../src/server/turmoil/parties/Scientists';
@@ -39,19 +39,26 @@ describe('PublicSponsoredGrant', function() {
     expect(player.canPlay(card)).is.true;
   });
 
-  it('play', function() {
+  it('Credit deductions work as expected', function() {
     player.megaCredits = 4;
     player2.megaCredits = 1;
     player3.megaCredits = 0;
 
-    const options = cast(card.play(player), OrOptions);
+    cast(card.play(player), OrOptions);
 
     expect(player.megaCredits).eq(2);
     expect(player2.megaCredits).eq(0);
     expect(player3.megaCredits).eq(0);
+  });
 
+  it('There are only 8 Tag-options to choose from.', function() {
+    const options = cast(card.play(player), OrOptions);
+    expect(options.options.length).eq(8);
+  });
+
+  it('Find Specially Crafted BUILDING Tags', function() {
+    const options = cast(card.play(player), OrOptions);
     expect(options.options[0].title).eq(Tag.BUILDING);
-    expect(player.cardsInHand).is.empty;
 
     const scienceCard = new SearchForLife();
     const buildingCard1 = new BiomassCombustors();
@@ -59,11 +66,39 @@ describe('PublicSponsoredGrant', function() {
     game.projectDeck.drawPile.push(buildingCard1, scienceCard, buildingCard2);
     game.projectDeck.discardPile = [];
 
+    expect(player.cardsInHand).is.empty;
     options.options[0].cb();
-
+    expect(player.cardsInHand.length).eq(2)
     expect(player.cardsInHand.map((c) => c.name)).has.members([CardName.BIOMASS_COMBUSTORS, CardName.COLONIZER_TRAINING_CAMP]);
     expect(game.projectDeck.discardPile.map((c) => c.name)).deep.eq([CardName.SEARCH_FOR_LIFE]);
   });
+
+  //
+  // Individual tests for every TAG
+  //
+  const TAGS = [
+    Tag.BUILDING,
+    Tag.SPACE,
+    Tag.SCIENCE,
+    Tag.POWER,
+    Tag.PLANT,
+    Tag.MICROBE,
+    Tag.ANIMAL,
+    Tag.EVENT,
+  ]
+  for(let i=0; i<=TAGS.length -1; i++){
+    it('Option "['+ i +']" is "' + TAGS[i] + '" Tag', function() {
+      const options = cast(card.play(player), OrOptions);
+      expect(options.options[7].title).eq(Tag.EVENT);
+    });
+    it('Find Generic "' + TAGS[i] + '" Tags', function() {
+      const options = cast(card.play(player), OrOptions);
+      expect(options.options[i].title).eq(TAGS[i]);
+      expect(player.cardsInHand).is.empty;
+      options.options[i].cb();
+      expect(player.cardsInHand.length).eq(2)
+    });
+  }
 
   it('compatible with Mons Insurance', function() {
     player2.setCorporationForTest(new MonsInsurance());

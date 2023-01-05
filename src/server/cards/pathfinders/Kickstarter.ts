c91b0f224571e054d3cc61ec7578f542c6c4de83
d-little <david.n.little@gmail.com>
2023-01-05 14:09:09 +1100

    Whoops, kickstarter only 1 tag, reverting

--- src/server/cards/pathfinders/Kickstarter.ts
+++ src/server/cards/pathfinders/Kickstarter.ts
@@ -29,7 +29,7 @@ export class Kickstarter extends Card implements IProjectCard, ICloneTagCard {
   public cloneTag: Tag = Tag.CLONE;
 
   public override get tags(): Array<Tag> {
-    return [this.cloneTag, this.cloneTag];
+    return [this.cloneTag];
   }
 
   public override bespokePlay(player: Player) {

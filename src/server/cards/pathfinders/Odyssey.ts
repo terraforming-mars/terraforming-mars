7bcc1a8c744faef352e29869aeeeb2a329011fd7
Robert Konigsberg <konigsberg@gmail.com>
2023-01-07 11:36:23 -0500

    Add text for Playrights and Odyssey confirming that special tiles can be duplicated.
    Fixes #4929

--- src/server/cards/pathfinders/Odyssey.ts
+++ src/server/cards/pathfinders/Odyssey.ts
@@ -25,7 +25,7 @@ export class Odyssey extends Card implements ICorporationCard, IActionCard {
           b.colon().cards(1, {secondaryTag: Tag.EVENT}).asterix().br;
           b.text('(Effect: Your event cards stay face up, and their tags are in use as if those were automated (green) cards.)',
             Size.TINY, false, false).br;
-          b.action('Pay for and play an event card you have already played that has a base cost of 16M€ or less, after which discard that card.', (e) => {
+          b.action('Pay for and play an event card you have already played that has a base cost of 16M€ or less (INCLUDING events that place special tiles,) after which discard that card.', (e) => {
             e.empty().startAction.event({played}).asterix().nbsp.text('≤').nbsp.megacredits(16);
           });
         }),

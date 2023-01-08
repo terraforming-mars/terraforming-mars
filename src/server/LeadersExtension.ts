c088dcabd8b70261d6804c6e101dcbbcffbe0e6f
d-little <david.n.little@gmail.com>
2023-01-08 21:25:22 +1100

    Comment these out until we push in Xavier and Duncan

--- src/server/LeadersExtension.ts
+++ src/server/LeadersExtension.ts
@@ -1,23 +1,23 @@
-import {CardName} from '../common/cards/CardName';
 import {Player} from './Player';
-import {VictoryPointsBreakdown} from './VictoryPointsBreakdown';
 import {isLeaderCard} from './cards/leaders/LeaderCard';
+// import {CardName} from '../common/cards/CardName';
+// import {VictoryPointsBreakdown} from './VictoryPointsBreakdown';
 
 export class LeadersExtension {
-  public static calculateVictoryPoints(player: Player, vpb: VictoryPointsBreakdown): void {
-    const duncan = player.getLeader(CardName.DUNCAN);
-    if (duncan?.isDisabled === true && duncan.generationUsed !== undefined) {
-      vpb.setVictoryPoints('victoryPoints', 6 - duncan.generationUsed, 'Leaders VP');
-    }
-  }
+  // public static calculateVictoryPoints(player: Player, vpb: VictoryPointsBreakdown): void {
+  //   const duncan = player.getLeader(CardName.DUNCAN);
+  //   if (duncan?.isDisabled === true && duncan.generationUsed !== undefined) {
+  //     vpb.setVictoryPoints('victoryPoints', 6 - duncan.generationUsed, 'Leaders VP');
+  //   }
+  // }
 
-  public static getBonusWildTags(player: Player) {
-    const xavier = player.getLeader(CardName.XAVIER);
-    if (xavier?.opgActionIsActive === true) {
-      return 2;
-    }
-    return 0;
-  }
+  // public static getBonusWildTags(player: Player) {
+  //   const xavier = player.getLeader(CardName.XAVIER);
+  //   if (xavier?.opgActionIsActive === true) {
+  //     return 2;
+  //   }
+  //   return 0;
+  // }
 
   public static leaderActionIsUsable(player: Player): boolean {
     for (const card of player.playedCards) {

fe8e09f0315150a8444cbd365e50fbd0de3577cc
Robert Konigsberg <kberg@users.noreply.github.com>
2023-01-08 23:06:28 -0500

    Update LeaderCard.ts
    Adding some comments to describe the fields.

--- src/server/cards/leaders/LeaderCard.ts
+++ src/server/cards/leaders/LeaderCard.ts
@@ -4,10 +4,16 @@ import {CardType} from '../../../common/cards/CardType';
 import {ICard} from '../ICard';
 
 export interface LeaderCard extends IProjectCard {
+  // TODO: Rename to something that indicates that it's usable even when this value is true.
+  /** When true, the card cannot be activated again. */
   isDisabled?: boolean;
+  
+  /** If this card is active this generation. */
   opgActionIsActive?: boolean;
+
+  /** The generation the card was activated. Used for Duncan. */
   generationUsed?: number;
-  onTRIncrease?: (player: Player) => undefined;
+
   canAct: (player: Player) => boolean;
 }
 

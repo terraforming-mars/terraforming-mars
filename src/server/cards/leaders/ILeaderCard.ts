9afae91aa5d2ae3e3eefedff2693a2fe2f04a896
Robert Konigsberg <kberg@users.noreply.github.com>
2023-01-12 09:50:03 -0500

    Update ILeaderCard.ts

--- src/server/cards/leaders/ILeaderCard.ts
+++ src/server/cards/leaders/ILeaderCard.ts
@@ -2,7 +2,7 @@ import {ICard} from '../ICard';
 import {Player} from '../../Player';
 
 export interface ILeaderCard extends ICard {
-  canPlay: (player: Player) => boolean;
+  canPlay(player: Player): boolean;
   cost: number;
   isDisabled?: boolean;
   opgActionIsActive?: boolean;

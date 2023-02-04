dede4b5249c81c5a4599840df6f6ee66a1ebe743
Robert Konigsberg <kberg@users.noreply.github.com>
2023-02-04 00:29:44 -0500

    Add unplayed corporation cards to the discard pile. (#5280)
    This makes them available for Merger.
    
    Also, this is moved to SelectInitialCards as it puts most of the initial
    card setup in one place. There's probably a cleaner way to do this by
    moving _all_ the selection out of SIC, but this is an easy way to test.
    
    Addresses #5277

--- src/server/inputs/SelectInitialCards.ts
+++ src/server/inputs/SelectInitialCards.ts
@@ -6,11 +6,13 @@ import {Player} from '../Player';
 import {PlayerInputType} from '../../common/input/PlayerInputType';
 import {SelectCard} from './SelectCard';
 import {Merger} from '../cards/promo/Merger';
+import {CardName} from '../../common/cards/CardName';
 
 export class SelectInitialCards extends AndOptions {
   public override readonly inputType = PlayerInputType.SELECT_INITIAL_CARDS;
-  constructor(player: Player, cb: (corporation: ICorporationCard) => undefined) {
+  constructor(private player: Player, cb: (corporation: ICorporationCard) => undefined) {
     super(() => {
+      this.completed(corporation);
       cb(corporation);
       return undefined;
     });
@@ -60,5 +62,28 @@ export class SelectInitialCards extends AndOptions {
         }, {min: 0, max: 10},
       ),
     );
+  }
+
+  private completed(corporation: ICorporationCard) {
+    const player = this.player;
+    // Check for negative M€
+    const cardCost = corporation.cardCost !== undefined ? corporation.cardCost : player.cardCost;
+    if (corporation.name !== CardName.BEGINNER_CORPORATION && player.cardsInHand.length * cardCost > corporation.startingMegaCredits) {
+      player.cardsInHand = [];
+      player.preludeCardsInHand = [];
+      throw new Error('Too many cards selected');
+    }
+    // discard all unpurchased cards
+    player.dealtProjectCards.forEach((card) => {
+      if (player.cardsInHand.includes(card) === false) {
+        player.game.projectDeck.discard(card);
+      }
+    });
+
+    player.dealtCorporationCards.forEach((card) => {
+      if (card.name !== corporation.name) {
+        player.game.corporationDeck.discard(card);
+      }
+    });
   }
 }

b2aac1fa05f0ff1c2048bbc08ebae5bb46599bdf
d-little <david.n.little@gmail.com>
2023-02-08 00:49:38 +1100

    Per #5293

--- src/server/deferredActions/PlaceHazardTile.ts
+++ src/server/deferredActions/PlaceHazardTile.ts
@@ -6,33 +6,31 @@ import {_AresHazardPlacement} from '../ares/AresHazards';
 import {TileType} from '../../common/TileType';
 import {PlacementType} from '../boards/PlacementType';
 
-
 export class PlaceHazardTile extends DeferredAction {
   constructor(
     player: Player,
+    public hazardType: TileType.DUST_STORM_MILD | TileType.EROSION_MILD,
     private options?: {
       on?: PlacementType,
       title?: string,
-      hazardType?: TileType,
     }) {
     super(player, Priority.DEFAULT);
   }
 
   public execute() {
-    // TODO: Confirm, would this ever NOT be land?
-    const type = this.options?.on || 'land';
+    const type = 'land';
     const availableSpaces = this.player.game.board.getAvailableSpacesForType(this.player, type);
     if (availableSpaces.length === 0) {
       return undefined;
     }
-    const hazardType = this.options?.hazardType || Math.floor(Math.random() * 2) === 0 ? TileType.DUST_STORM_MILD : TileType.EROSION_MILD;
+    const hazardType = this.hazardType;
     const title = this.options?.title || 'Select space for ' + TileType.toString(hazardType);
 
     return new SelectSpace(
       title,
       availableSpaces,
       (space: ISpace) => {
-        this.player.game.addHazardTile(this.player, space, hazardType);
+        _AresHazardPlacement.putHazardAt(space, hazardType);
         return undefined;
       },
     );

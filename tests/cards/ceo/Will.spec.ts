9edf5c63be2c8b12f0e6c1bf8ab75db8e856f5ca
d-little <david.n.little@gmail.com>
2023-02-07 13:04:40 +1100

    Add CommCenter tests

--- tests/cards/ceo/Will.spec.ts
+++ tests/cards/ceo/Will.spec.ts
@@ -9,6 +9,7 @@ import {TestPlayer} from '../../TestPlayer';
 import {Ants} from '../../../src/server/cards/base/Ants';
 import {Birds} from '../../../src/server/cards/base/Birds';
 import {AsteroidRights} from '../../../src/server/cards/promo/AsteroidRights';
+import {CommunicationCenter} from '../../../src/server/cards/pathfinders/CommunicationCenter';
 
 import {Will} from '../../../src/server/cards/ceos/Will';
 
@@ -53,6 +54,30 @@ describe('Will', function() {
     const selectCard = game.deferredActions.pop()!.execute() as SelectCard<ICard>;
     selectCard.cb([selectCard.cards[1]]);
     expect(ants.resourceCount).eq(3);
+  });
+
+  it('Takes OPG w/ Communication Center', function() {
+    const comms = new CommunicationCenter();
+    player.playedCards.push(comms);
+    comms.resourceCount = 2; // Put 2 data onto CommCenter
+
+    // Sanity
+    expect(comms.resourceCount).eq(2);
+    expect(player.cardsInHand.length).to.eq(0);
+
+    // Action
+    card.action(player);
+    expect(game.deferredActions).has.length(6);
+    game.deferredActions.runNext(); // No animals
+    game.deferredActions.runNext(); // No bugs
+    game.deferredActions.runNext(); // No Science
+    game.deferredActions.runNext(); // No Floaters
+    game.deferredActions.runNext(); // No Asteroid
+    game.deferredActions.runNext(); // One Wild on comms
+
+    // We should have drawn a card here
+    expect(comms.resourceCount).eq(0);
+    expect(player.cardsInHand.length).to.eq(1);
   });
 
   it('Can only act once per game', function() {

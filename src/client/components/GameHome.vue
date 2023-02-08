65b35b6de93989dd30b56018deab3f6b941c2517
Santtu Hietamäki <santtu.hietamaki@gmail.com>
2023-02-08 23:34:20 +0200

    Add comment

--- src/client/components/GameHome.vue
+++ src/client/components/GameHome.vue
@@ -107,7 +107,7 @@ export default Vue.extend({
     },
     copyUrl(playerId: ParticipantId | undefined): void {
       if (playerId === undefined) return;
-
+      // Get current location path without game?id=xxxxxxx
       const path = window.location.href.replace(/game\?id=.*/, "");
       copyToClipboard(path + this.getHref(playerId));
       this.urlCopiedPlayerId = playerId;

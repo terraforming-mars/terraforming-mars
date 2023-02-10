e9d13884e27b579514fd73199dd3a88327c7a075
d-little <david.n.little@gmail.com>
2023-02-10 14:06:48 +1100

    CEO PR# 5.5 - CEOs Quill, Rogers, Ryu (#5326)
    * init CEO Quill
    
    * init ceo Roger
    
    * lint quill
    
    * init ceo Ryu
    
    * fix unused Rogers test player2
    
    * fixes per #5327
    
    * activate Quill Rogers Ryu

---
+++ src/server/cards/ceos/Quill.ts
@@ -0,0 +1,36 @@
+import {CardName} from '../../../common/cards/CardName';
+import {Player} from '../../Player';
+import {PlayerInput} from '../../PlayerInput';
+import {CardRenderer} from '../render/CardRenderer';
+import {CeoCard} from './CeoCard';
+
+import {CardResource} from '../../../common/CardResource';
+import {AddResourcesToCard} from '../../deferredActions/AddResourcesToCard';
+import {AltSecondaryTag} from '../../../common/cards/render/AltSecondaryTag';
+
+
+export class Quill extends CeoCard {
+  constructor() {
+    super({
+      name: CardName.QUILL,
+      metadata: {
+        cardNumber: 'L17',
+        renderData: CardRenderer.builder((b) => {
+          b.opgArrow().cards(1, {secondaryTag: AltSecondaryTag.FLOATER}).colon().nbsp;
+          b.plus().floaters(2).asterix();
+        }),
+        description: 'Once per game, add 2 floaters to each of your cards that collect floaters, then add 2 floaters to ANY card.',
+      },
+    });
+  }
+
+  public action(player: Player): PlayerInput | undefined {
+    const resourceCards = player.getResourceCards(CardResource.FLOATER);
+    resourceCards.forEach((card) => player.addResourceTo(card, {qty: 2, log: true}));
+
+    player.game.defer(new AddResourcesToCard(player, CardResource.FLOATER, {count: 2}));
+
+    this.isDisabled = true;
+    return undefined;
+  }
+}

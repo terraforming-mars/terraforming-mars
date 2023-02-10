07a09d30970047155ea65c1acca5a10355cf3458
Aloïs de Gouvello <aloisdegouvello@live.fr>
2023-02-10 10:58:03 +0100

    Use partition instead of two filters

--- src/client/utils/CardOrderStorage.ts
+++ src/client/utils/CardOrderStorage.ts
@@ -1,5 +1,6 @@
 
 import {CardModel} from '@/common/models/CardModel';
+import {partition} from 'utils';
 
 const STORAGE_PREFIX = 'cardOrder';
 
@@ -16,14 +17,15 @@ export class CardOrderStorage {
       return {};
     }
   }
+
   public static getOrdered(order: {[x: string]: number}, cards: Array<CardModel>): Array<CardModel> {
-    const misses = cards.filter((card) => order[card.name] === undefined);
-    const hits = cards.filter((card) => order[card.name] !== undefined);
+    const [misses, hits] = partition(cards, (card) => order[card.name] === undefined);
     hits.sort((a, b) => {
       return order[a.name] - order[b.name];
     });
     return hits.concat(misses);
   }
+
   public static updateCardOrder(playerId: string, order: {[x: string]: number}): void {
     try {
       localStorage.setItem(`${STORAGE_PREFIX}${playerId}`, JSON.stringify(order));
---
+++ src/client/utils/utils.ts
@@ -0,0 +1,18 @@
+/**
+ * Creates an array of elements split into two groups,
+ * the first of which contains elements predicate returns truthy for,
+ * the second of which contains elements predicate returns falsey for.
+ * The predicate is invoked with one argument: (value).
+ *
+ * @param {Array<T>} source The collection to iterate over.
+ * @param {Function} predicate The function invoked per iteration.
+ * @returns {Array<Array<T>>} Returns the array of grouped elements. Failing group come first.
+ */
+export function partition<T>(source: Array<T>, predicate: (t: T) => boolean): Array<Array<T>> {
+  return source
+    .reduce((result, element) => {
+      result[predicate(element) ^ 1].push(element);
+      return result;
+    },
+    [[], []]);
+}

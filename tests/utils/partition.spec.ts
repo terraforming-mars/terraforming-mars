4d22f84b9e8af839e4628127a5e7860a19ee4327
aloisdg <aloisdegouvello@live.fr>
2023-02-11 13:36:17 +0100

    Use deep equality

--- tests/utils/partition.spec.ts
+++ tests/utils/partition.spec.ts
@@ -3,14 +3,15 @@ import {partition} from '../../src/client/utils/utils';
 
 describe('partitions', () => {
   it('sanity', () => {
-    expect(partition([true, true], (x) => x)).eq([[true, true], []]);
-    expect(partition([true, false], (x) => x)).eq([[true], [false]]);
-    expect(partition([false, true], (x) => x)).eq([[true], [false]]); // matching come first
-    expect(partition([0, 1, 2, 3], (x) => x % 2 === 0)).eq([[0, 2], [1, 3]]);
-    expect(partition([{a: 0}, {a: 1}, {a: 2}, {a: 3}], (x) => x.a % 2 === 0)).eq([[{a: 0}, {a: 2}], [{a: 1}, {a: 3}]]);
+    expect(partition([true], (x) => x)).eql([[true], []]);
+    expect(partition([true, true], (x) => x)).eql([[true, true], []]);
+    expect(partition([true, false], (x) => x)).eql([[true], [false]]);
+    expect(partition([false, true], (x) => x)).eql([[true], [false]]); // matching come first
+    expect(partition([0, 1, 2, 3], (x) => x % 2 === 0)).eql([[0, 2], [1, 3]]);
+    expect(partition([{a: 0}, {a: 1}, {a: 2}, {a: 3}], (x) => x.a % 2 === 0)).eql([[{a: 0}, {a: 2}], [{a: 1}, {a: 3}]]);
   });
 
   it('error cases', () => {
-    expect(partition([], (x) => x)).eq([[], []]);
+    expect(partition([], (x) => x)).eql([[], []]);
   });
 });

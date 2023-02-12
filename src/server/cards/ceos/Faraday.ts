6251de16f89e858d467785ec1d42a13f090e215c
d-little <david.n.little@gmail.com>
2023-02-12 15:24:19 +1100

    init ceo Faraday

---
+++ src/server/cards/ceos/Faraday.ts
@@ -0,0 +1,99 @@
+import {CardName} from '../../../common/cards/CardName';
+import {CardType} from '../../../common/cards/CardType';
+import {Player} from '../../Player';
+// import {PlayerInput} from '../../PlayerInput';
+import {CardRenderer} from '../render/CardRenderer';
+import {CeoCard} from './CeoCard';
+
+import {IProjectCard} from '../IProjectCard';
+import {Size} from '../../../common/cards/render/Size';
+import {AltSecondaryTag} from '../../../common/cards/render/AltSecondaryTag';
+import {Tag} from '../../../common/cards/Tag';
+import {SelectOption} from '../../inputs/SelectOption';
+import {OrOptions} from '../../inputs/OrOptions';
+import {SimpleDeferredAction} from '../../deferredActions/DeferredAction';
+import {SelectPaymentDeferred} from '../../deferredActions/SelectPaymentDeferred';
+
+export class Faraday extends CeoCard {
+  constructor() {
+    super({
+      name: CardName.FARADAY,
+      metadata: {
+        cardNumber: 'L27',
+        renderData: CardRenderer.builder((b) => {
+          b.br;
+          b.text('5', Size.LARGE).diverseTag(1).colon().megacredits(-2).cards(1, {secondaryTag: AltSecondaryTag.DIVERSE}).asterix();
+          b.br.br;
+        }),
+        description: 'When you gain a multiple of 5 for any tag type IN PLAY, you may pay 2 M€ to draw a card with that tag. Wild tags do not count for this effect.',
+      },
+    });
+  }
+
+  public override canAct(): boolean {
+    return false;
+  }
+
+  public onCardPlayed(player: Player, card: IProjectCard) {
+    if (card.tags.length === 0 || card.cardType === CardType.EVENT || !player.canAfford(2)) return;
+
+    // Get all 'valid' tags that Player has in play after playing the card.
+    const allTags = this.getAllValidTags(player);
+    const validTags = allTags
+      .filter((item) => card.tags.includes(item));
+
+    const uniqueCardTags = new Set(card.tags);
+    uniqueCardTags.forEach((tag) => {
+      if (!validTags.includes(tag)) return;
+      const playerTagCount = player.tags.count(tag, 'raw'); // Raw here means no Wild tags
+      const cardTagCount = card.tags.filter((cardTag) => cardTag === tag).length;
+      const playerTagCountPrePlay = playerTagCount - cardTagCount;
+      // Modulo 5 what the tag count was before the card was played.
+      // Sum that pre-played count with the new cards tags.  If this sum is >=5, offer a card draw.
+      //  this wont work if someone makes a card with > 5 tags of one type, but...
+      if (playerTagCountPrePlay%5 + cardTagCount >= 5) {
+        player.game.defer(new SimpleDeferredAction(player, () => this.effectOptions(player, tag)));
+      }
+    });
+  }
+
+  public effectOptions(player: Player, tag: Tag) {
+    return new OrOptions(
+      new SelectOption(`Pay 2 M€ to draw a ${tag} card`, 'Confirm', () => {
+        player.game.defer(new SelectPaymentDeferred(player, 2, {title: 'Select how to pay for action'}));
+        player.drawCard(1, {tag: tag});
+        return undefined;
+      }),
+      new SelectOption('Do nothing', 'Confirm', () => {
+        return undefined;
+      }),
+    );
+  }
+
+  private getAllValidTags(player: Player): Array<Tag> {
+    const tags = [
+      Tag.BUILDING,
+      Tag.SPACE,
+      Tag.SCIENCE,
+      Tag.POWER,
+      Tag.EARTH,
+      Tag.JOVIAN,
+      Tag.PLANT,
+      Tag.MICROBE,
+      Tag.ANIMAL,
+      Tag.CITY,
+      // Tag.WILD,  // No Wild
+      // Tag.EVENT, // No Event
+      // Tag.CLONE,
+    ];
+
+    // On the players 'Tag Bar' VENUS is in between JOVIAN and PLANTS
+    // It's nice to keep these in order for the player's own tag prompts
+    const gOpts = player.game.gameOptions;
+    if (gOpts.venusNextExtension) tags.splice(tags.indexOf(Tag.JOVIAN) + 1, 0, Tag.VENUS);
+    if (gOpts.moonExpansion) tags.push(Tag.MOON);
+    if (gOpts.aresExtension) tags.push(Tag.MARS);
+
+    return tags;
+  }
+}
---
+++ tests/cards/ceo/Faraday.spec.ts
@@ -0,0 +1,150 @@
+import {expect} from 'chai';
+import {Faraday} from '../../../src/server/cards/ceos/Faraday';
+import {Game} from '../../../src/server/Game';
+import {OrOptions} from '../../../src/server/inputs/OrOptions';
+import {TestPlayer} from '../../TestPlayer';
+import {Tag} from '../../../src/common/cards/Tag';
+import {getTestPlayer, newTestGame} from '../../TestGame';
+import {cast, fakeCard, runAllActions} from '../../TestingUtils';
+import {CardType} from '../../../src/common/cards/CardType';
+
+
+describe('Faraday', function() {
+  let card: Faraday;
+  let player: TestPlayer;
+  let game: Game;
+
+  beforeEach(() => {
+    card = new Faraday();
+    game = newTestGame(1);
+    player = getTestPlayer(game, 0);
+
+    player.megaCredits = 10;
+    player.playedCards.push(card);
+  });
+
+  it('Nothing happens when playing the 7th tag', function() {
+    // 7th here is arbitrary, just a number that isnt a multiple of 5
+    player.playedCards.push(fakeCard({tags: [Tag.SCIENCE, Tag.SCIENCE, Tag.SCIENCE, Tag.SCIENCE, Tag.SCIENCE, Tag.SCIENCE]}));
+    player.playCard(fakeCard({tags: [Tag.SCIENCE]}));
+
+    expect(game.deferredActions).has.lengthOf(0);
+    expect(player.cardsInHand).has.length(0);
+    expect(player.megaCredits).to.eq(10);
+  });
+
+
+  it('Can draw a card when reaching a multiple of 5 for a tag', function() {
+    player.playedCards.push(fakeCard({tags: [Tag.SCIENCE, Tag.SCIENCE, Tag.SCIENCE, Tag.SCIENCE]}));
+    // 4 tags: Not sufficient
+    expect(game.deferredActions).has.lengthOf(0);
+    // 5 tags: Draw a card with a Science tag
+    player.playCard(fakeCard({tags: [Tag.SCIENCE]}));
+
+    expect(game.deferredActions).has.lengthOf(1);
+    const orOptions = cast(game.deferredActions.pop()!.execute(), OrOptions);
+    orOptions.options[0].cb();
+    runAllActions(game);
+    expect(player.megaCredits).to.eq(8);
+    expect(player.cardsInHand).has.length(1);
+    expect(player.cardsInHand[0].tags.includes(Tag.SCIENCE)).is.true;
+  });
+
+  it('Can choose to do nothing when reaching a multiple of 5 for a tag', function() {
+    player.playedCards.push(fakeCard({tags: [Tag.SCIENCE, Tag.SCIENCE, Tag.SCIENCE, Tag.SCIENCE]}));
+    player.playCard(fakeCard({tags: [Tag.SCIENCE]}));
+
+    const orOpations = cast(game.deferredActions.pop()!.execute(), OrOptions);
+    orOpations.options[1].cb();
+    runAllActions(game);
+    expect(player.cardsInHand).has.length(0);
+    expect(player.megaCredits).to.eq(10);
+  });
+
+  it('No prompt if player cannot afford to pay for card', function() {
+    player.megaCredits = 1;
+    player.playedCards.push(fakeCard({tags: [Tag.SCIENCE, Tag.SCIENCE, Tag.SCIENCE, Tag.SCIENCE]}));
+    player.playCard(fakeCard({tags: [Tag.SCIENCE]}));
+
+    expect(game.deferredActions).has.length(0);
+    expect(player.cardsInHand).has.length(0);
+    expect(player.megaCredits).to.eq(1);
+  });
+
+  it('Play a card with two of the same tag', function() {
+    player.playedCards.push(fakeCard({tags: [Tag.SCIENCE, Tag.SCIENCE, Tag.SCIENCE, Tag.SCIENCE]}));
+    player.playCard(fakeCard({tags: [Tag.SCIENCE, Tag.SCIENCE]}));
+
+    expect(game.deferredActions).has.lengthOf(1);
+    const orOptions = cast(game.deferredActions.pop()!.execute(), OrOptions);
+    orOptions.options[0].cb();
+    runAllActions(game);
+    expect(player.megaCredits).to.eq(8);
+    expect(player.cardsInHand).has.length(1);
+    expect(player.cardsInHand[0].tags.includes(Tag.SCIENCE)).is.true;
+  });
+
+  it('Play a card that puts two tags at 5 count, buy both', function() {
+    player.playedCards.push(fakeCard({tags: [Tag.SCIENCE, Tag.SCIENCE, Tag.SCIENCE, Tag.SCIENCE]}));
+    player.playedCards.push(fakeCard({tags: [Tag.EARTH, Tag.EARTH, Tag.EARTH, Tag.EARTH]}));
+
+    player.playCard(fakeCard({tags: [Tag.EARTH, Tag.SCIENCE]}));
+
+    expect(game.deferredActions).has.lengthOf(2);
+    const orOptions = cast(game.deferredActions.pop()!.execute(), OrOptions);
+    orOptions.options[0].cb();
+    runAllActions(game);
+    orOptions.options[0].cb();
+    runAllActions(game);
+
+    expect(player.cardsInHand).has.length(2);
+    expect(player.megaCredits).to.eq(6);
+  });
+
+  it('Play a card that puts two tags at 5 count, buy one', function() {
+    player.playedCards.push(fakeCard({tags: [Tag.SCIENCE, Tag.SCIENCE, Tag.SCIENCE, Tag.SCIENCE]}));
+    player.playedCards.push(fakeCard({tags: [Tag.EARTH, Tag.EARTH, Tag.EARTH, Tag.EARTH]}));
+
+    player.playCard(fakeCard({tags: [Tag.EARTH, Tag.SCIENCE]}));
+
+    expect(game.deferredActions).has.lengthOf(2);
+    const orOptions = cast(game.deferredActions.pop()!.execute(), OrOptions);
+    orOptions.options[0].cb();
+    runAllActions(game);
+    orOptions.options[1].cb();
+    runAllActions(game);
+
+    expect(player.cardsInHand).has.length(1);
+    expect(player.megaCredits).to.eq(8);
+  });
+
+  it('Play a card that puts two tags at 5 count, buy none', function() {
+    player.playedCards.push(fakeCard({tags: [Tag.SCIENCE, Tag.SCIENCE, Tag.SCIENCE, Tag.SCIENCE]}));
+    player.playedCards.push(fakeCard({tags: [Tag.EARTH, Tag.EARTH, Tag.EARTH, Tag.EARTH]}));
+
+    player.playCard(fakeCard({tags: [Tag.EARTH, Tag.SCIENCE]}));
+
+    expect(game.deferredActions).has.lengthOf(2);
+    const orOptions = cast(game.deferredActions.pop()!.execute(), OrOptions);
+    orOptions.options[1].cb();
+    runAllActions(game);
+
+    expect(player.cardsInHand).has.length(0);
+    expect(player.megaCredits).to.eq(10);
+  });
+
+  it('Wild tags dont count', function() {
+    player.playedCards.push(fakeCard({tags: [Tag.WILD, Tag.WILD]}));
+    player.playedCards.push(fakeCard({tags: [Tag.SCIENCE, Tag.SCIENCE]}));
+    player.playCard(fakeCard({tags: [Tag.SCIENCE]}));
+    expect(game.deferredActions).has.length(0);
+    expect(player.cardsInHand).has.length(0);
+  });
+
+  it('Does not trigger on event cards', function() {
+    player.playedCards.push(fakeCard({tags: [Tag.SCIENCE, Tag.SCIENCE, Tag.SCIENCE, Tag.SCIENCE]}));
+    player.playCard(fakeCard({tags: [Tag.SCIENCE], cardType: CardType.EVENT}));
+    expect(game.deferredActions).has.length(0);
+    expect(player.cardsInHand).has.length(0);
+  });
+});

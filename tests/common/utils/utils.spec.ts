98b6c519059eabfe264617f6b8d9c38f5917e560
Aloïs de Gouvello <aloisdegouvello@live.fr>
2023-02-12 05:46:34 +0100

    Use partition instead of two filters (#5335)
    * Use partition instead of two filters
    
    * Fix comment
    
    * Lint
    
    * Recover code
    
    * Use deep equality
    
    * Move file around
    
    ---------
    
    Co-authored-by: Robert Konigsberg <kberg@users.noreply.github.com>

--- tests/common/utils/utils.spec.ts
+++ tests/common/utils/utils.spec.ts
@@ -35,6 +35,16 @@ describe('utils', function() {
     expect(array).deep.eq(['Wednesday', 'Thursday']);
   });
 
+  it('partitions', () => {
+    expect(utils.partition([], (x) => x)).eql([[], []]);
+    expect(utils.partition([true], (x) => x)).eql([[true], []]);
+    expect(utils.partition([true, true], (x) => x)).eql([[true, true], []]);
+    expect(utils.partition([true, false], (x) => x)).eql([[true], [false]]);
+    expect(utils.partition([false, true], (x) => x)).eql([[true], [false]]); // matching come first
+    expect(utils.partition([0, 1, 2, 3], (x) => x % 2 === 0)).eql([[0, 2], [1, 3]]);
+    expect(utils.partition([{a: 0}, {a: 1}, {a: 2}, {a: 3}], (x) => x.a % 2 === 0)).eql([[{a: 0}, {a: 2}], [{a: 1}, {a: 3}]]);
+  });
+
   it('sum', () => {
     expect(utils.sum([1, 3, 4])).eq(8);
     expect(utils.sum([1, 4])).eq(5);

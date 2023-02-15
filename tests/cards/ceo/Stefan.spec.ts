9c861bd3853da949e56c21ceaab21a81fefa9742
d-little <david.n.little@gmail.com>
2023-02-15 14:03:27 +1100

    CEO: Stefan, Tate (#5357)
    * init CEO Stefan
    
    * init CEO Tate
    
    * Remove unused import
    
    * Resolving per #5357

---
+++ tests/cards/ceo/Stefan.spec.ts
@@ -0,0 +1,65 @@
+import {expect} from 'chai';
+import {ICard} from '../../../src/server/cards/ICard';
+import {Stefan} from '../../../src/server/cards/ceos/Stefan';
+import {Game} from '../../../src/server/Game';
+import {SelectCard} from '../../../src/server/inputs/SelectCard';
+import {forceGenerationEnd, fakeCard} from '../../TestingUtils';
+import {TestPlayer} from '../../TestPlayer';
+import {getTestPlayer, newTestGame} from '../../TestGame';
+
+
+describe('Stefan', function() {
+  let card: Stefan;
+  let player: TestPlayer;
+  let game: Game;
+
+  beforeEach(() => {
+    card = new Stefan();
+    game = newTestGame(1);
+    player = getTestPlayer(game, 0);
+  });
+
+  it('Cannot act without cards', function() {
+    expect(card.canAct(player)).is.false;
+  });
+
+  it('Takes action, sells all cards', function() {
+    const fake1 = fakeCard({tags: []});
+    const fake2 = fakeCard({tags: []});
+    player.cardsInHand.push(fake1, fake2);
+    expect(card.canAct(player)).is.true;
+
+    const selectCard = card.action(player) as SelectCard<ICard>;
+    game.deferredActions.runNext();
+    selectCard.cb([fake1, fake2]);
+    game.deferredActions.runAll(() => {});
+    expect(player.cardsInHand).has.length(0);
+    expect(player.megaCredits).eq(6);
+  });
+
+  it('Takes action, sells only 1 card', function() {
+    const fake1 = fakeCard({tags: []});
+    const fake2 = fakeCard({tags: []});
+    player.cardsInHand.push(fake1, fake2);
+    expect(card.canAct(player)).is.true;
+
+    const selectCard = card.action(player) as SelectCard<ICard>;
+    game.deferredActions.runNext();
+    selectCard.cb([fake1]);
+    game.deferredActions.runAll(() => {});
+    expect(player.cardsInHand).has.length(1);
+    expect(player.megaCredits).eq(3);
+  });
+
+  it('Can only act once per game', function() {
+    const fake1 = fakeCard({tags: []});
+    player.cardsInHand.push(fake1);
+
+    const selectCard = card.action(player) as SelectCard<ICard>;
+    game.deferredActions.runNext();
+    selectCard.cb([fake1]);
+    forceGenerationEnd(game);
+    expect(card.isDisabled).is.true;
+    expect(card.canAct(player)).is.false;
+  });
+});

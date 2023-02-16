e8a683a036d36e6444e8e9519b58c5e59fcc625b
d-little <david.n.little@gmail.com>
2023-02-16 11:59:50 +1100

    Van Allen init

---
+++ src/server/cards/ceos/VanAllen.ts
@@ -0,0 +1,26 @@
+import {CardName} from '../../../common/cards/CardName';
+import {CardRenderer} from '../render/CardRenderer';
+import {CeoCard} from './CeoCard';
+
+export class VanAllen extends CeoCard {
+  constructor() {
+    super({
+      name: CardName.VANALLEN,
+      metadata: {
+        cardNumber: 'L22',
+        renderData: CardRenderer.builder((b) => {
+          b.br;
+          b.text('MILESTONES: ').megacredits(0, {cancelled: true}).megacredits(3).asterix();
+          b.br.br;
+        }),
+        description: 'You may claim milestones for free (you must still meet the requirements). When any milestone is claimed, gain 3 M€.',
+      },
+    });
+  }
+
+  // All of the Van Allen hook is done in src/server/Player.ts : claimMilestone()
+
+  public override canAct(): boolean {
+    return false;
+  }
+}

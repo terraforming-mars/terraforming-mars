80d7f1248d86117ec60b7c1b8cd29910a5ac74a0
Robert Konigsberg <kberg@users.noreply.github.com>
2023-02-17 00:26:07 -0500

    Make it easier to use messages in logs and things. (#5372)
    This introduces a simple way to make Message objects, and ties it into the logging API without duplication (though it's not the most amazing class structure I've ever written.)
    
    This simplifies some of the calls in Player.ts.

---
+++ src/server/logs/MessageBuilder.ts
@@ -0,0 +1,111 @@
+import {LogMessageDataType} from '../../common/logs/LogMessageDataType';
+import {Player} from '../Player';
+import {CardName} from '../../common/cards/CardName';
+import {ICard} from '../cards/ICard';
+import {IAward} from '../awards/IAward';
+import {IMilestone} from '../milestones/IMilestone';
+import {IParty} from '../turmoil/parties/IParty';
+import {TileType} from '../../common/TileType';
+import {SpaceBonus} from '../../common/boards/SpaceBonus';
+import {IGlobalEvent} from '../turmoil/globalEvents/IGlobalEvent';
+import {GlobalEventName} from '../../common/turmoil/globalEvents/GlobalEventName';
+import {PartyName} from '../../common/turmoil/PartyName';
+import {IColony} from '../colonies/IColony';
+import {Message} from '../../common/logs/Message';
+
+export class MessageBuilder {
+  protected message: Message;
+
+  constructor(message: string) {
+    this.message = {
+      data: [],
+      message: message,
+    };
+  }
+
+  public string(value: string): MessageBuilder {
+    this.message.data.push({type: LogMessageDataType.STRING, value});
+    return this;
+  }
+
+  public rawString(value: string): MessageBuilder {
+    this.message.data.push({type: LogMessageDataType.RAW_STRING, value});
+    return this;
+  }
+
+  public number(value: number): MessageBuilder {
+    this.message.data.push({type: LogMessageDataType.RAW_STRING, value: value.toString()});
+    return this;
+  }
+
+  public player(value: Player): MessageBuilder {
+    return this.playerId(value.color);
+  }
+
+  public playerId(value: string): MessageBuilder {
+    this.message.data.push({type: LogMessageDataType.PLAYER, value});
+    return this;
+  }
+
+  public card(value: ICard): MessageBuilder {
+    return this.cardName(value.name);
+  }
+
+  public cardName(value: CardName): MessageBuilder {
+    this.message.data.push({type: LogMessageDataType.CARD, value});
+    return this;
+  }
+
+  public award(value: IAward): MessageBuilder {
+    this.message.data.push({type: LogMessageDataType.AWARD, value: value.name});
+    return this;
+  }
+
+  public milestone(value: IMilestone): MessageBuilder {
+    this.message.data.push({type: LogMessageDataType.MILESTONE, value: value.name});
+    return this;
+  }
+
+  public colony(value: IColony): MessageBuilder {
+    this.message.data.push({type: LogMessageDataType.COLONY, value: value.name});
+    return this;
+  }
+
+  public standardProject(value: string): MessageBuilder {
+    this.message.data.push({type: LogMessageDataType.STANDARD_PROJECT, value});
+    return this;
+  }
+
+  public party(value: IParty): MessageBuilder {
+    this.message.data.push({type: LogMessageDataType.PARTY, value: value.name});
+    return this;
+  }
+
+  public partyName(value: PartyName): MessageBuilder {
+    this.message.data.push({type: LogMessageDataType.PARTY, value});
+    return this;
+  }
+
+  public tileType(value: TileType): MessageBuilder {
+    this.message.data.push({type: LogMessageDataType.TILE_TYPE, value: value.toString()});
+    return this;
+  }
+
+  public spaceBonus(value: SpaceBonus): MessageBuilder {
+    this.message.data.push({type: LogMessageDataType.SPACE_BONUS, value: value.toString()});
+    return this;
+  }
+
+  public globalEvent(value: IGlobalEvent): MessageBuilder {
+    return this.globalEventName(value.name);
+  }
+
+  public globalEventName(value: GlobalEventName): MessageBuilder {
+    this.message.data.push({type: LogMessageDataType.GLOBAL_EVENT, value: value.toString()});
+    return this;
+  }
+
+  public getMessage(): Message {
+    return this.message;
+  }
+}

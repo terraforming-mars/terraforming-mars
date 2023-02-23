203664bcdfeafcd9cb23b9f1a0cdc5323264efac
d-little <david.n.little@gmail.com>
2023-02-24 10:40:02 +1100

    Update tests

--- tests/cards/ceo/Oscar.spec.ts
+++ tests/cards/ceo/Oscar.spec.ts
@@ -3,42 +3,63 @@ import {Game} from '../../../src/server/Game';
 import {getTestPlayer, newTestGame} from '../../TestGame';
 import {forceGenerationEnd} from '../../TestingUtils';
 import {TestPlayer} from '../../TestPlayer';
-
 import {Turmoil} from '../../../src/server/turmoil/Turmoil';
 
 import {Oscar} from '../../../src/server/cards/ceos/Oscar';
+import {Politician} from '../../../src/server/awards/terraCimmeria/Politician';
 
 describe('Oscar', function() {
   let card: Oscar;
   let player: TestPlayer;
   let game: Game;
+  let turmoil: Turmoil;
 
   beforeEach(() => {
     card = new Oscar();
-    game = newTestGame(1);
+    game = newTestGame(1, {ceoExtension: true, turmoilExtension: true});
     player = getTestPlayer(game, 0);
     player.playedCards.push(card);
+    turmoil = Turmoil.getTurmoil(player.game);
   });
 
   it('Has +1 influence', function() {
     card.play(player);
-    expect(player.game.turmoil?.getPlayerInfluence(player)).eq(1);
+    expect(turmoil?.getPlayerInfluence(player)).eq(1);
   });
 
   it('Takes OPG action', function() {
+    turmoil.chairman = 'NEUTRAL';
     card.action(player);
-    const turmoil = Turmoil.getTurmoil(player.game);
     expect(turmoil.chairman).eq(player.id);
   });
 
   it('Can only act once per game', function() {
+    expect(card.canAct(player)).is.true;
     card.action(player);
     forceGenerationEnd(game);
-
     expect(card.isDisabled).is.true;
     expect(card.canAct(player)).is.false;
   });
 
-  // TODO(dl) (low hanging fruit):
-  // Test to see if action increases POLITICIAN Milestone
+  it('Cannot take OPG if no delegates in reserve', function() {
+    expect(card.canAct(player)).is.true;
+    turmoil.delegateReserve.clear();
+    expect(card.canAct(player)).is.false;
+  });
+
+  it('Cannot take OPG if already chairman', function() {
+    expect(card.canAct(player)).is.true;
+    turmoil.chairman = player.id;
+    expect(card.canAct(player)).is.false;
+  });
+
+
+  it('OPG Counts for POLITICAN Award', function() {
+    const politician = new Politician();
+    game.awards = [];
+    game.awards.push(politician);
+    const preOPGScore = game.awards[0].getScore(player);
+    card.action(player);
+    expect(game.awards[0].getScore(player)).eq(preOPGScore+1);
+  });
 });

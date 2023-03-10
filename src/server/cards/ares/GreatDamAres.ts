a66aa8b697b7a337170c54f165c9a67b4b4dba91
d-little <david.n.little@gmail.com>
2023-03-10 18:29:57 +1100

    Render

--- src/server/cards/ares/GreatDamAres.ts
+++ src/server/cards/ares/GreatDamAres.ts
@@ -12,7 +12,7 @@ export class GreatDamAres extends GreatDamPromo {
       {
         cardNumber: 'A25',
         renderData: CardRenderer.builder((b) => {
-          b.production((pb) => pb.energy(2)).tile(TileType.GREAT_DAM, true, true).asterix();
+          b.production((pb) => pb.energy(2)).tile(TileType.GREAT_DAM, false, true).asterix();
         }),
         description: 'Requires 4 ocean tiles. Increase your energy production 2 steps. Place this tile ADJACENT TO an ocean tile. The tile grants an ADJACENCY BONUS of 2 Energy',
       },

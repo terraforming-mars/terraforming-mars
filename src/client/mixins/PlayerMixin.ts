ea1e0df5296360e2683d211527fdbcf341532aef
Robert Konigsberg <konigsberg@gmail.com>
2023-03-14 00:59:50 -0400

    Remove CardModel.type

--- src/client/mixins/PlayerMixin.ts
+++ src/client/mixins/PlayerMixin.ts
@@ -2,6 +2,7 @@ import {CardModel} from '@/common/models/CardModel';
 import {CardType} from '@/common/cards/CardType';
 import {PublicPlayerModel} from '@/common/models/PlayerModel';
 import {sortActiveCards} from '@/client/utils/ActiveCardsSortingOrder';
+import {getCardOrThrow} from '@/client/cards/ClientCardManifest';
 
 // Common code for player layouts
 
@@ -9,11 +10,8 @@ export const PlayerMixin = {
   name: 'PlayerMixin',
   methods: {
     sortActiveCards: sortActiveCards,
-    getCardsByType(
-      inCards: Array<CardModel>,
-      cardTypes: Array<CardType>,
-    ) {
-      const cards: Array<CardModel> = inCards.filter((card) => cardTypes.includes(card.type));
+    getCardsByType(inCards: Array<CardModel>, cardTypes: Array<CardType>): Array<CardModel> {
+      const cards: Array<CardModel> = inCards.filter((card) => cardTypes.includes(getCardOrThrow(card.name).type));
       return cards.reverse();
     },
     isCardActivated(card: CardModel, player: PublicPlayerModel): boolean {

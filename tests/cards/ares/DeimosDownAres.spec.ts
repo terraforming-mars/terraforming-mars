139cda8c6b60f3d5f63a5ff3f29645d9c9c0f623
d-little <david.n.little@gmail.com>
2023-04-02 10:17:12 +1000

    No it isnt

--- tests/cards/ares/DeimosDownAres.spec.ts
+++ tests/cards/ares/DeimosDownAres.spec.ts
@@ -65,7 +65,6 @@ describe('DeimosDownAres', function() {
     expect(player.plants).to.eq(15); // not removed
   });
 
-  // Identical to the Deimos Down Promo test
   it('Adjacency bonus', function() {
     card.play(player);
     runAllActions(game);

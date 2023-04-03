995f99df029764722cdff9eb32090c9b74617f78
Robert Konigsberg <konigsberg@gmail.com>
2023-04-03 12:02:56 -0400

    Roll back Extreme Cold Fungus

--- src/server/cards/base/ExtremeColdFungus.ts
+++ src/server/cards/base/ExtremeColdFungus.ts
@@ -1,38 +1,26 @@
+import {IActionCard} from '../ICard';
 import {Tag} from '../../../common/cards/Tag';
+import {Card} from '../Card';
 import {CardType} from '../../../common/cards/CardType';
+import {Player} from '../../Player';
+import {OrOptions} from '../../inputs/OrOptions';
+import {SelectOption} from '../../inputs/SelectOption';
+import {SelectCard} from '../../inputs/SelectCard';
+import {IProjectCard} from '../IProjectCard';
 import {CardResource} from '../../../common/CardResource';
 import {CardName} from '../../../common/cards/CardName';
+import {Resources} from '../../../common/Resources';
 import {CardRequirements} from '../CardRequirements';
 import {CardRenderer} from '../render/CardRenderer';
 import {max} from '../Options';
-import {ActionCard} from '../ActionCard';
 
-export class ExtremeColdFungus extends ActionCard {
+export class ExtremeColdFungus extends Card implements IActionCard, IProjectCard {
   constructor() {
     super({
       type: CardType.ACTIVE,
       name: CardName.EXTREME_COLD_FUNGUS,
       tags: [Tag.MICROBE],
       cost: 13,
-
-      action: {
-        or: {
-          autoSelect: true,
-          behaviors: [
-            {
-              addResourcesToAnyCard: {
-                type: CardResource.MICROBE,
-                count: 2,
-              },
-              title: 'Select card to add 2 microbes',
-            },
-            {
-              stock: {plants: 1},
-              title: 'Gain 1 plant',
-            },
-          ],
-        },
-      },
 
       requirements: CardRequirements.builder((b) => b.temperature(-10, {max})),
       metadata: {
@@ -49,5 +37,46 @@ export class ExtremeColdFungus extends ActionCard {
         }),
       },
     });
+  }
+  public canAct(): boolean {
+    return true;
+  }
+  public action(player: Player) {
+    const otherMicrobeCards = player.getResourceCards(CardResource.MICROBE);
+
+    if (otherMicrobeCards.length === 0) {
+      player.addResource(Resources.PLANTS, 1, {log: true});
+      return undefined;
+    }
+
+    const gainPlantOption = new SelectOption('Gain 1 plant', 'Gain plant', () => {
+      player.addResource(Resources.PLANTS, 1, {log: true});
+      return undefined;
+    });
+
+    if (otherMicrobeCards.length === 1) {
+      const targetCard = otherMicrobeCards[0];
+
+      return new OrOptions(
+        new SelectOption('Add 2 microbes to ' + targetCard.name, 'Add microbes', () => {
+          player.addResourceTo(targetCard, {qty: 2, log: true});
+          return undefined;
+        }),
+        gainPlantOption,
+      );
+    }
+
+    return new OrOptions(
+      new SelectCard(
+        'Select card to add 2 microbes',
+        'Add microbes',
+        otherMicrobeCards,
+        ([card]) => {
+          player.addResourceTo(card, {qty: 2, log: true});
+          return undefined;
+        },
+      ),
+      gainPlantOption,
+    );
   }
 }

f1ad8b0da88d7008211127548ee1cae5f2548f05
d-little <david.n.little@gmail.com>
2023-04-04 11:54:10 +1000

    Opponent Claims Test

--- tests/cards/ceo/VanAllen.spec.ts
+++ tests/cards/ceo/VanAllen.spec.ts
@@ -10,11 +10,13 @@ import {VanAllen} from '../../../src/server/cards/ceos/VanAllen';
 describe('Van Allen', function() {
   let card: VanAllen;
   let player: TestPlayer;
+  let player2: TestPlayer;
   let game: Game;
+
 
   beforeEach(() => {
     card = new VanAllen();
-    [game, player] = testGame(2, {ceoExtension: true});
+    [game, player, player2] = testGame(2, {ceoExtension: true});
   });
 
   it('Cannot claim for free if VanAllen is not in play', function() {
@@ -24,7 +26,6 @@ describe('Van Allen', function() {
     const claimMilestoneAction = actions.options.find((option) => option.title === 'Claim a milestone');
     expect(claimMilestoneAction).is.undefined;
   });
-
 
   it('Can claim milestones for free, and gains 3 M€ upon claim', function() {
     player.playedCards.push(card);
@@ -43,5 +44,24 @@ describe('Van Allen', function() {
     expect(player.megaCredits).eq(3); // No M€ cost incurred, gains 3 M€ instead
     const claimedMilestone = player.game.claimedMilestones;
     expect(claimedMilestone.find((cm) => cm.milestone.name === 'Terraformer' && cm.player === player)).is.not.undefined;
+  });
+
+  it('Gains 3 M€ when an opponent claims', function() {
+    player.playedCards.push(card);
+
+    player.megaCredits = 0;
+    player2.megaCredits = 8;
+    player.setTerraformRating(35); // Can claim Terraformer milestone
+    player2.setTerraformRating(35); // Can claim Terraformer milestone
+
+    const actions = cast(player2.getActions(), OrOptions);
+    const claimMilestoneAction = cast(actions.options.find((option) => option.title === 'Claim a milestone'), OrOptions);
+
+    expect(claimMilestoneAction).is.not.undefined;
+    claimMilestoneAction!.options![0].cb();
+    game.deferredActions.runAll(() => {});
+    expect(player.megaCredits).eq(3); // No M€ cost incurred, gains 3 M€ instead
+    const claimedMilestone = player.game.claimedMilestones;
+    expect(claimedMilestone.find((cm) => cm.milestone.name === 'Terraformer' && cm.player === player2)).is.not.undefined;
   });
 });

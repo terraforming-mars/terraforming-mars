28306a2870293010167dee7ef8c01090dff3c19c
d-little <david.n.little@gmail.com>
2023-04-06 13:40:23 +1000

    Rebalance Will bonus resouce gain

--- src/server/cards/ceos/Will.ts
+++ src/server/cards/ceos/Will.ts
@@ -16,10 +16,10 @@ export class Will extends CeoCard {
         renderData: CardRenderer.builder((b) => {
           b.opgArrow().text('GAIN THESE RESOURCES').br;
           b.animals(1).animals(1).microbes(1).microbes(1).br;
-          b.science().floaters(1).asteroids(1).wild(1);
+          b.floaters(1).floaters(1).wild(1).wild(1);
           b.br;
         }),
-        description: 'Once per game, add the following resources to your cards: 2 animals, 2 microbes, 1 science, 1 floater, 1 asteroid, 1 wild.',
+        description: 'Once per game, add the following resources to your cards: 2 animals, 2 microbes, 2 floaters, 2 wild.',
       },
     });
   }
@@ -28,10 +28,8 @@ export class Will extends CeoCard {
     this.isDisabled = true;
     player.game.defer(new AddResourcesToCard(player, CardResource.ANIMAL, {count: 2}));
     player.game.defer(new AddResourcesToCard(player, CardResource.MICROBE, {count: 2}));
-    player.game.defer(new AddResourcesToCard(player, CardResource.SCIENCE, {count: 1}));
-    player.game.defer(new AddResourcesToCard(player, CardResource.FLOATER, {count: 1}));
-    player.game.defer(new AddResourcesToCard(player, CardResource.ASTEROID, {count: 1}));
-    player.game.defer(new AddResourcesToCard(player, undefined, {count: 1}));
+    player.game.defer(new AddResourcesToCard(player, CardResource.FLOATER, {count: 2}));
+    player.game.defer(new AddResourcesToCard(player, undefined, {count: 2}));
 
     return undefined;
   }

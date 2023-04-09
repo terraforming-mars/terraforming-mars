aa74deebbbee6d898c05bbb7b79c4cfaa099e6e9
Robert Konigsberg <konigsberg@gmail.com>
2023-04-09 15:23:34 -0400

    Make tradding mesasges more dynamic and easier to internationalize. Fixes #5579

--- src/server/colonies/IColonyTrader.ts
+++ src/server/colonies/IColonyTrader.ts
@@ -1,8 +1,9 @@
+import {Message} from '@/common/logs/Message';
 import {IColony} from './IColony';
 
 // Something that can pay for trading with colonies.
 export interface IColonyTrader {
   canUse(): boolean;
-  optionText(): string;
+  optionText(): string | Message;
   trade(colony: IColony): void;
 }

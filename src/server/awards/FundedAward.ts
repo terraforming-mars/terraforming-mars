6ccac1d1c22393cb9bb14ba9e2e1203944d44fb9
Robert Konigsberg <kberg@users.noreply.github.com>
2023-04-11 19:16:31 -0400

    Extra defense to prevent claiming milestones twice. (#5588)
    * Log the game ID at the end of an export.
    
    * Extra defense to prevent claiming milestones twice.

--- src/server/awards/FundedAward.ts
+++ src/server/awards/FundedAward.ts
@@ -26,20 +26,36 @@ export function deserializeFundedAwards(
   fundedAwards: Array<SerializedFundedAward>,
   players: Array<Player>,
   awards: Array<IAward>): Array<FundedAward> {
-  return fundedAwards.map((element: SerializedFundedAward) => {
-    const awardName = element.name;
-    if (awardName === undefined) {
+  // Remove duplicates
+  const aw = new Set<AwardName>();
+  const filtered: Array<Required<SerializedFundedAward>> = [];
+  for (const fundedAward of fundedAwards) {
+    const name = fundedAward.name;
+    if (name === undefined) {
       throw new Error('Award name not found');
     }
+    const playerId = fundedAward.playerId;
+    if (playerId === undefined) {
+      throw new Error(`Player ID not found when rebuilding funded award ${name}`);
+    }
+
+    if (aw.has(name)) {
+      console.error('Found duplicate award: ' + name);
+      continue;
+    } else {
+      filtered.push({name, playerId});
+      aw.add(name);
+    }
+  }
+
+  return filtered.map((element: SerializedFundedAward) => {
+    const awardName = element.name;
     const award: IAward | undefined = awards.find((award) => award.name === awardName);
     if (award === undefined) {
       throw new Error(`Award ${awardName} not found when rebuilding Funded Award`);
     }
 
     const playerId = element.playerId;
-    if (playerId === undefined) {
-      throw new Error(`Player ID not found when rebuilding Funded Award ${awardName}`);
-    }
     const player = players.find((player) => player.id === playerId);
     if (player === undefined) {
       throw new Error(`Player ${playerId} not found when rebuilding Funded Award ${awardName}`);

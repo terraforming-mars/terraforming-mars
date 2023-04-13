1fd426172cab852190f430841f7c24631fc5a47b
Robert Konigsberg <konigsberg@gmail.com>
2023-04-12 20:48:53 -0400

    Atmo Collectors action is now a Behavior.

--- tests/cards/colonies/AtmoCollectors.spec.ts
+++ tests/cards/colonies/AtmoCollectors.spec.ts
@@ -1,17 +1,20 @@
 import {expect} from 'chai';
-import {cast} from '../../TestingUtils';
+import {cast, runAllActions} from '../../TestingUtils';
 import {AtmoCollectors} from '../../../src/server/cards/colonies/AtmoCollectors';
 import {testGame} from '../../TestGame';
 import {OrOptions} from '../../../src/server/inputs/OrOptions';
 import {TestPlayer} from '../../TestPlayer';
+import {Game} from '../../../src/server/Game';
+import {Units} from '../../../src/common/Units';
 
 describe('AtmoCollectors', function() {
   let card: AtmoCollectors;
   let player: TestPlayer;
+  let game: Game;
 
   beforeEach(function() {
     card = new AtmoCollectors();
-    [/* skipped */, player] = testGame(2);
+    [game, player] = testGame(2);
   });
 
   it('Should play', function() {
@@ -21,14 +24,43 @@ describe('AtmoCollectors', function() {
 
   it('Should act', function() {
     player.playedCards.push(card);
-    const action = card.action(player);
-    expect(action).is.undefined;
-    expect(card.resourceCount).to.eq(1);
 
-    const orOptions = cast(card.action(player), OrOptions);
+    card.action(player);
+    runAllActions(game);
+    expect(player.popWaitingFor()).is.undefined;
+    expect(card.resourceCount).to.eq(1);
+    expect(player.purse()).deep.eq(Units.EMPTY);
+
+    card.resourceCount = 3;
+
+    card.action(player);
+    runAllActions(game);
+    const orOptions = cast(player.popWaitingFor(), OrOptions);
 
     orOptions.options[0].cb();
+    runAllActions(game);
+    expect(card.resourceCount).to.eq(2);
+    expect(player.purse()).deep.eq(Units.of({titanium: 2}));
+
+    player.titanium = 0;
+
+    orOptions.options[1].cb();
+    runAllActions(game);
+    expect(card.resourceCount).to.eq(1);
+    expect(player.purse()).deep.eq(Units.of({energy: 3}));
+
+    player.energy = 0;
+
+    orOptions.options[2].cb();
+    runAllActions(game);
     expect(card.resourceCount).to.eq(0);
-    expect(player.titanium).to.eq(2);
+    expect(player.purse()).deep.eq(Units.of({heat: 4}));
+
+    player.heat = 0;
+
+    orOptions.options[3].cb();
+    runAllActions(game);
+    expect(card.resourceCount).to.eq(1);
+    expect(player.purse()).deep.eq(Units.EMPTY);
   });
 });

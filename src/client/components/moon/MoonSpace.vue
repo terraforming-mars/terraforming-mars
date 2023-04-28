ed5b1b7f1d80a9b25874544504289aa9b098a0c9
Robert Konigsberg <kberg@users.noreply.github.com>
2023-04-27 23:21:22 -0400

    Consolidate BoardSpace tile generation. (#5629)

--- src/client/components/moon/MoonSpace.vue
+++ src/client/components/moon/MoonSpace.vue
@@ -1,6 +1,13 @@
 <template>
-  <div :class="mainClass" :data_space_id="space.id" :title="verboseTitle">
-    <div :class="tileClass" data-test="tile"/>
+  <div :class="mainClass" :data_space_id="space.id">
+    <board-space-tile
+      :tileType="space.tileType"
+      :spaceType="space.spaceType"
+      :aresExtension="false"
+      :tileView="tileView"
+      :highlight="space.highlight"
+      :restricted="false"
+    ></board-space-tile>
     <div class="board-space-text" v-if="text" v-i18n>{{ text }}</div>
     <bonus v-if="space.tileType === undefined || tileView === 'hide'" :bonus="space.bonus" />
     <template v-if="tileView === 'coords'">
@@ -17,21 +24,10 @@
 <script lang="ts">
 import Vue from 'vue';
 import {SpaceModel} from '@/common/models/SpaceModel';
-import {TileType} from '@/common/TileType';
 import Bonus from '@/client/components/Bonus.vue';
 import {TileView} from '../board/TileView';
+import BoardSpaceTile from '@/client/components/board/BoardSpaceTile.vue';
 
-const tileTypeToCssClass = new Map<TileType, string>([
-  [TileType.MOON_ROAD, 'road'],
-  [TileType.MOON_HABITAT, 'colony'],
-  [TileType.MOON_MINE, 'mine'],
-  [TileType.LUNA_TRADE_STATION, 'luna-trade-station'],
-  [TileType.LUNA_MINING_HUB, 'luna-mining-hub'],
-  [TileType.LUNA_TRAIN_STATION, 'luna-train-station'],
-  [TileType.LUNAR_MINE_URBANIZATION, 'lunar-mine-urbanization'],
-]);
-
-// TODO(kberg): Can part of this become BoardSpaceTile?
 export default Vue.extend({
   name: 'MoonSpace',
   props: {
@@ -51,19 +47,9 @@ export default Vue.extend({
   },
   components: {
     Bonus,
+    'board-space-tile': BoardSpaceTile,
   },
   computed: {
-    verboseTitle(): string {
-      let ret = '';
-      if (this.space.tileType === TileType.LUNA_TRADE_STATION) {
-        ret = 'Luna Trade Station';
-      } else if (this.space.tileType === TileType.LUNA_MINING_HUB) {
-        ret = 'Luna Mining Hub';
-      } else if (this.space.tileType === TileType.LUNA_TRAIN_STATION) {
-        ret = 'Luna Train Station';
-      }
-      return this.$t(ret);
-    },
     mainClass(): string {
       let css = 'board-space moon-space-' + this.space.id.toString();
 
@@ -75,33 +61,6 @@ export default Vue.extend({
         css += ' moon-space-type-mine';
       } else {
         css += ' moon-space-type-other';
-      }
-
-      return css;
-    },
-    tileClass(): string {
-      let css = 'board-space';
-      const tileType = this.space.tileType;
-
-      if (tileType !== undefined) {
-        switch (this.space.tileType) {
-        case TileType.MOON_HABITAT:
-          css += ' board-space-tile--colony';
-          break;
-        case TileType.MOON_ROAD:
-          css += ' board-space-tile--road';
-          break;
-        case TileType.MOON_MINE:
-          css += ' board-space-tile--mine';
-          break;
-        default:
-          const cssClass = tileTypeToCssClass.get(tileType);
-          css += ' board-space-tile--' + cssClass;
-        }
-      }
-
-      if (this.tileView === 'hide') {
-        css += ' board-hidden-tile';
       }
 
       return css;

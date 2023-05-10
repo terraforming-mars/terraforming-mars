a8363f60b30c5d6e9a96ec09ba575603e6fee242
Robert Konigsberg <konigsberg@gmail.com>
2023-05-10 00:17:03 -0400

    Coordinator should use getPlayedEventsCount.

--- src/server/awards/Coordinator.ts
+++ src/server/awards/Coordinator.ts
@@ -1,11 +1,11 @@
 import {IAward} from './IAward';
 import {Player} from '../Player';
-import {CardType} from '../../common/cards/CardType';
 
 export class Coordinator implements IAward {
   public readonly name = 'Coordinator';
   public readonly description = 'Having the most event cards in play';
+
   public getScore(player: Player): number {
-    return player.getCardsByCardType(CardType.EVENT).length;
+    return player.getPlayedEventsCount();
   }
 }

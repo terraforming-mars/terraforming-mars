43cd9565dd6bb7f47d869762b446ec68e0e31c42
Robert Konigsberg <kberg@users.noreply.github.com>
2023-05-12 00:17:25 -0400

    Coordinator should use getPlayedEventsCount. (#5675)

--- src/server/cards/community/ProjectWorkshop.ts
+++ src/server/cards/community/ProjectWorkshop.ts
@@ -57,11 +57,15 @@ export class ProjectWorkshop extends Card implements ICorporationCard {
   }
 
   private getEligibleCards(player: Player) {
-    const cards = player.getCardsByCardType(CardType.ACTIVE);
-    if (!PartyHooks.shouldApplyPolicy(player, PartyName.REDS)) return cards;
+    const cards = player.playedCards.filter((card) => card.type === CardType.ACTIVE);
+    if (!PartyHooks.shouldApplyPolicy(player, PartyName.REDS)) {
+      return cards;
+    }
     return cards.filter((card) => {
       const vp = card.getVictoryPoints(player);
-      if (vp <= 0) return true;
+      if (vp <= 0) {
+        return true;
+      }
       return player.canAfford(REDS_RULING_POLICY_COST * vp);
     });
   }

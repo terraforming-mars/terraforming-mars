236e974ff9e1902c2dc5d069db49a51b353ec8ff
Robert Konigsberg <konigsberg@gmail.com>
2023-05-14 10:30:28 -0400

    Minor crashlanding tweaks

--- src/server/cards/pathfinders/Crashlanding.ts
+++ src/server/cards/pathfinders/Crashlanding.ts
@@ -32,11 +32,11 @@ export class Crashlanding extends Card implements IProjectCard {
       metadata: {
         cardNumber: 'Pf48',
         renderData: CardRenderer.builder((b) => {
-          b.tile(TileType.CRASHLANDING, false, true).asterix().data().wild(1).br;
+          b.tile(TileType.CRASHLANDING, false, true).asterix().data().asterix().wild(1).asterix().br;
           b.text('(Place AND ORIENT this tile on a non-reserved space adjacent to no more than 1 city.', Size.TINY, false, false).br;
           b.text('Crashsite grants an ADJACENCY BONUS of 1 data, and either 1 steel or 1 titanium.', Size.TINY, false, false).br;
           b.text('When placing Crashsite, for each tile you own next it, gain those bonuses.', Size.TINY, false, false).br;
-          b.text('Place 1 data on any card. Place 1 resource on any card.)', Size.TINY, false, false).br;
+          b.text('Add 1 data to any card. Add 1 resource to any card.)', Size.TINY, false, false).br;
         }),
       },
     });

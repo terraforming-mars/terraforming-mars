7781687c51114b836e6d1e0340a7cd9c1360cb29
Robert Konigsberg <konigsberg@gmail.com>
2023-05-29 22:17:13 -0400

    Clarify Floater Urbanism

--- src/server/cards/pathfinders/FloaterUrbanism.ts
+++ src/server/cards/pathfinders/FloaterUrbanism.ts
@@ -23,8 +23,8 @@ export class FloaterUrbanism extends Card implements IProjectCard, IActionCard {
 
       metadata: {
         renderData: CardRenderer.builder((b) => {
-          b.action('Remove 1 floater from any card and add 1 Venusian habitat on this card.', (ab) => {
-            ab.floaters(1).startAction.venusianHabitat(1);
+          b.action('Spend 1 floater from any card to add 1 Venusian habitat on this card.', (ab) => {
+            ab.minus().floaters(1).startAction.venusianHabitat(1);
           }).br;
           b.vpText('1 VP for every Venusian habitat on this card.');
         }),

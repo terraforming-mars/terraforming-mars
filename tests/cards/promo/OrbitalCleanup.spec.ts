a5b0eaee42ff7da5a8233dae729fb347fe50e836
d-little <david.n.little@gmail.com>
2023-06-02 15:33:03 +1000

    Add check for OrbitalCleanup + Scientists Policy 4 (#5742)
    * add check for OrbitalCleanup + Scientists
    
    * fixes

--- tests/cards/promo/OrbitalCleanup.spec.ts
+++ tests/cards/promo/OrbitalCleanup.spec.ts
@@ -6,6 +6,11 @@ import {OrbitalCleanup} from '../../../src/server/cards/promo/OrbitalCleanup';
 import {Game} from '../../../src/server/Game';
 import {Resource} from '../../../src/common/Resource';
 import {TestPlayer} from '../../TestPlayer';
+import {testGame} from '../../TestGame';
+import {setRulingPartyAndRulingPolicy} from '../../TestingUtils';
+import {BioengineeringEnclosure} from '../../../src/server/cards/ares/BioengineeringEnclosure';
+import {PartyName} from '../../../src/common/turmoil/PartyName';
+import {SCIENTISTS_POLICY_4} from '../../../src/server/turmoil/parties/Scientists';
 
 describe('OrbitalCleanup', function() {
   let card: OrbitalCleanup;
@@ -40,5 +45,27 @@ describe('OrbitalCleanup', function() {
   it('Should give victory points', function() {
     card.play(player);
     expect(card.getVictoryPoints(player)).to.eq(2);
+  });
+
+  it('Turmoil Science Tag Requirements doesnt increase Income', function() {
+    const [game, player] = testGame(2, {ceoExtension: true, turmoilExtension: true});
+    const turmoil = game.turmoil!;
+    const scientists = turmoil.getPartyByName(PartyName.SCIENTISTS);
+
+    // Sanity check that Science Ruling Policy is working as intended:
+    const bioengineeringEnclosure = new BioengineeringEnclosure(); // Requires 1 science tag
+    expect(player.simpleCanPlay(bioengineeringEnclosure)).is.false;
+    setRulingPartyAndRulingPolicy(game, turmoil, scientists, SCIENTISTS_POLICY_4.id); // Reduce science tag requirements by 1
+    SCIENTISTS_POLICY_4.apply(game);
+    expect(player.simpleCanPlay(bioengineeringEnclosure)).is.true;
+
+    // Make sure that we do not get 1MC for the Science Ruling Policy
+    player.playedCards.push(new Research());
+    player.playedCards.push(new AdvancedAlloys());
+    player.playedCards.push(new ResearchCoordination());
+
+    expect(player.megaCredits).to.eq(0);
+    card.action(player);
+    expect(player.megaCredits).to.eq(4);
   });
 });

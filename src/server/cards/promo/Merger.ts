d2f08f4480b37c28ad99e2bb8a6e47993703758b
Robert Konigsberg <kberg@users.noreply.github.com>
2023-06-15 10:20:58 -0400

    First fizzle. Implement for Merger. (#5796)

--- src/server/cards/promo/Merger.ts
+++ src/server/cards/promo/Merger.ts
@@ -33,12 +33,14 @@ export class Merger extends PreludeCard {
   public override bespokePlay(player: IPlayer) {
     const game = player.game;
     const dealtCorps = Merger.dealCorporations(player, game.corporationDeck);
+    LogHelper.logDrawnCards(player, dealtCorps, true);
     const enabled = dealtCorps.map((corp) => {
       return player.canAfford(Merger.mergerCost - this.spendableMegacredits(player, corp));
     });
     if (enabled.some((v) => v === true) === false) {
-      game.log('None of the four drawn corporation cards are affordable.');
+      player.fizzle(this);
       dealtCorps.forEach((corp) => game.corporationDeck.discard(corp));
+      return undefined;
     }
     game.defer(new SimpleDeferredAction(player, () => {
       return new SelectCard('Choose corporation card to play', 'Play', dealtCorps, ([card]) => {

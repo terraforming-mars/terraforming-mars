6da6777619ccb5a87165667e3856124991691a31
Robert Konigsberg <kberg@users.noreply.github.com>
2023-06-23 21:12:00 -0400

    Make Eccentric Sponsor fizzle (#5816)

--- tests/cards/prelude/EccentricSponsor.spec.ts
+++ tests/cards/prelude/EccentricSponsor.spec.ts
@@ -1,25 +1,24 @@
 import {SelectProjectCardToPlay} from '../../../src/server/inputs/SelectProjectCardToPlay';
 import {expect} from 'chai';
 import {EccentricSponsor} from '../../../src/server/cards/prelude/EccentricSponsor';
-import {Game} from '../../../src/server/Game';
 import {TestPlayer} from '../../TestPlayer';
 import {cast, runAllActions} from '../../TestingUtils';
 import {NitrogenRichAsteroid} from '../../../src/server/cards/base/NitrogenRichAsteroid';
+import {testGame} from '../../TestGame';
 
 describe('EccentricSponsor', function() {
-  let card: EccentricSponsor;
+  let eccentricSponsor: EccentricSponsor;
   let player: TestPlayer;
 
   beforeEach(function() {
-    card = new EccentricSponsor();
-    player = TestPlayer.BLUE.newPlayer();
-    Game.newInstance('gameid', [player], player);
+    eccentricSponsor = new EccentricSponsor();
+    [, player] = testGame(1);
   });
 
   it('Gets card discount', function() {
-    expect(card.getCardDiscount(player)).to.eq(0);
-    player.lastCardPlayed = card.name;
-    expect(card.getCardDiscount(player)).to.eq(25);
+    expect(eccentricSponsor.getCardDiscount(player)).to.eq(0);
+    player.lastCardPlayed = eccentricSponsor.name;
+    expect(eccentricSponsor.getCardDiscount(player)).to.eq(25);
   });
 
   it('Should play', function() {
@@ -30,12 +29,27 @@ describe('EccentricSponsor', function() {
     expect(player.getCardCost(nitrogenRichAsteroid)).eq(31);
     expect(player.canPlay(nitrogenRichAsteroid)).is.false;
 
-    player.playCard(card);
+    player.playCard(eccentricSponsor);
     runAllActions(player.game);
     const selectProjectCardToPlay = cast(player.popWaitingFor(), SelectProjectCardToPlay);
     expect(selectProjectCardToPlay.cards).deep.eq([nitrogenRichAsteroid]);
 
     expect(player.getCardCost(nitrogenRichAsteroid)).eq(6);
     expect(player.canPlay(nitrogenRichAsteroid)).is.true;
+  });
+
+  it('Fizzle', function() {
+    const nitrogenRichAsteroid = new NitrogenRichAsteroid();
+    player.cardsInHand = [nitrogenRichAsteroid];
+    player.megaCredits = 0;
+
+    expect(player.getCardCost(nitrogenRichAsteroid)).eq(31);
+    expect(player.canPlay(nitrogenRichAsteroid)).is.false;
+
+    player.playCard(eccentricSponsor);
+    runAllActions(player.game);
+    cast(player.popWaitingFor(), undefined);
+    expect(player.megaCredits).eq(15);
+    expect(player.cardsInHand).deep.eq([nitrogenRichAsteroid]);
   });
 });

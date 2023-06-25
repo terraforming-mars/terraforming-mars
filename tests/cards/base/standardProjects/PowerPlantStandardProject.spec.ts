011c9f7c82bfdb984e112820fb90a1284eb27f93
Robert Konigsberg <kberg@users.noreply.github.com>
2023-06-25 12:44:10 -0400

    Update PowerPlantStandardProject.spec.ts

--- tests/cards/base/standardProjects/PowerPlantStandardProject.spec.ts
+++ tests/cards/base/standardProjects/PowerPlantStandardProject.spec.ts
@@ -19,7 +19,7 @@ describe('PowerPlantStandardProjects', function() {
   it('Should act', function() {
     player.megaCredits = 11;
     player.playedCards.push(new StandardTechnology());
-    expect(game.deferredActions).has.length;
+    expect(game.deferredActionss.length).eq(0);
     card.action(player);
     expect(game.deferredActions.length).eq(1);
     expect(player.megaCredits).eq(11);

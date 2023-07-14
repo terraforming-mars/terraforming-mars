45ae03226cc866ff84ce0b23999cc85b5cad25bb
ligvigfui <ligvigfui@gmailcom>
2023-07-14 22:33:07 +0200

    translated a bit and wondered around

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -56,7 +56,7 @@ export const DEFAULT_ESCAPE_VELOCITY_PENALTY = 1;
 // Leaders/CEOs
 export const ASIMOV_AWARD_BONUS = 2;
 
-export const ALL_LANGUAGES = ['en', 'de', 'fr', 'ru', 'cn', 'pl', 'es', 'br', 'it', 'ko', 'nl'] as const;
+export const ALL_LANGUAGES = ['en', 'de', 'fr', 'ru', 'cn', 'pl', 'es', 'br', 'it', 'ko', 'nl', 'hu'] as const;
 
 export const LANGUAGES: Record<typeof ALL_LANGUAGES[number], string> = {
   en: 'English',
@@ -70,6 +70,7 @@ export const LANGUAGES: Record<typeof ALL_LANGUAGES[number], string> = {
   it: 'Italiano',
   ko: '한국어',
   nl: 'Nederlands',
+  hu: 'Magyar',
 };
 
 export const APP_NAME = 'Terraforming Mars';

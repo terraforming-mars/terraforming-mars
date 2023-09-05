65935dec8720ad049bfb1c8ebfb163c00734dce0
Robert Konigsberg <kberg@users.noreply.github.com>
2023-09-05 00:53:57 -0400

    Use this as return type. (#5904)
    Will faciliate subclassing, an issue I had in the past.

--- src/server/cards/requirements/CardRequirements.ts
+++ src/server/cards/requirements/CardRequirements.ts
@@ -72,112 +72,112 @@ class Builder {
     return new CardRequirements(this.reqs);
   }
 
-  public oceans(amount: number = 1, options?: Options): Builder {
+  public oceans(amount: number = 1, options?: Options): this {
     this.reqs.push(new OceanRequirement(amount, options));
     return this;
   }
 
-  public oxygen(amount: number = 1, options?: Options): Builder {
+  public oxygen(amount: number = 1, options?: Options): this {
     this.reqs.push(new OxygenRequirement(amount, options));
     return this;
   }
 
-  public temperature(amount: number = 1, options?: Options): Builder {
+  public temperature(amount: number = 1, options?: Options): this {
     this.reqs.push(new TemperatureRequirement(amount, options));
     return this;
   }
 
-  public venus(amount: number = 1, options?: Options): Builder {
+  public venus(amount: number = 1, options?: Options): this {
     this.reqs.push(new VenusRequirement(amount, options));
     return this;
   }
 
-  public tr(amount: number = 1, options?: Options): Builder {
+  public tr(amount: number = 1, options?: Options): this {
     this.reqs.push(new TRRequirement(amount, options));
     return this;
   }
 
-  public chairman(): Builder {
+  public chairman(): this {
     this.reqs.push(new ChairmanRequirement());
     return this;
   }
 
-  public resourceTypes(amount: number = 1, options?: Options): Builder {
+  public resourceTypes(amount: number = 1, options?: Options): this {
     this.reqs.push(new ResourceTypeRequirement(amount, options));
     return this;
   }
 
-  public greeneries(amount: number = 1, options?: Options): Builder {
+  public greeneries(amount: number = 1, options?: Options): this {
     this.reqs.push(new GreeneriesRequirement(amount, options));
     return this;
   }
 
-  public cities(amount: number = 1, options?: Options): Builder {
+  public cities(amount: number = 1, options?: Options): this {
     this.reqs.push(new CitiesRequirement(amount, options));
     return this;
   }
 
-  public colonies(amount: number = 1, options?: Options): Builder {
+  public colonies(amount: number = 1, options?: Options): this {
     this.reqs.push(new ColoniesRequirement(amount, options));
     return this;
   }
 
-  public floaters(amount: number = 1, options?: Options): Builder {
+  public floaters(amount: number = 1, options?: Options): this {
     this.reqs.push(new FloatersRequirement(amount, options));
     return this;
   }
 
-  public partyLeaders(amount: number = 1, options?: Options): Builder {
+  public partyLeaders(amount: number = 1, options?: Options): this {
     this.reqs.push(new PartyLeadersRequirement(amount, options));
     return this;
   }
 
-  public tag(tag: Tag, amount: number = 1, options?: Options): Builder {
+  public tag(tag: Tag, amount: number = 1, options?: Options): this {
     this.reqs.push(new TagCardRequirement(tag, amount, options));
     return this;
   }
 
-  public production(resource: Resource, amount: number = 1, options?: Options): Builder {
+  public production(resource: Resource, amount: number = 1, options?: Options): this {
     this.reqs.push(new ProductionRequirement(resource, amount, options));
     return this;
   }
 
-  public party(party: PartyName): Builder {
+  public party(party: PartyName): this {
     this.reqs.push(new PartyRequirement(party));
     return this;
   }
 
-  public plantsRemoved(): Builder {
+  public plantsRemoved(): this {
     this.reqs.push(new RemovedPlantsRequirement());
     return this;
   }
 
-  public habitatRate(amount: number = 1, options?: Options): Builder {
+  public habitatRate(amount: number = 1, options?: Options): this {
     this.reqs.push(new HabitatRateRequirement(amount, options));
     return this;
   }
 
-  public miningRate(amount: number = 1, options?: Options): Builder {
+  public miningRate(amount: number = 1, options?: Options): this {
     this.reqs.push(new MiningRateRequirement(amount, options));
     return this;
   }
 
-  public logisticRate(amount: number = 1, options?: Options): Builder {
+  public logisticRate(amount: number = 1, options?: Options): this {
     this.reqs.push(new LogisticsRateRequirement(amount, options));
     return this;
   }
 
-  public habitatTiles(amount: number = 1, options?: Options): Builder {
+  public habitatTiles(amount: number = 1, options?: Options): this {
     this.reqs.push(new HabitatTilesRequirement(amount, options));
     return this;
   }
 
-  public miningTiles(amount: number = 1, options?: Options): Builder {
+  public miningTiles(amount: number = 1, options?: Options): this {
     this.reqs.push(new MiningTilesRequirement(amount, options));
     return this;
   }
 
-  public roadTiles(amount: number = 1, options?: Options): Builder {
+  public roadTiles(amount: number = 1, options?: Options): this {
     this.reqs.push(new RoadTilesRequirement(amount, options));
     return this;
   }

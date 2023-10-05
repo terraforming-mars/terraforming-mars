0fdc7ed627dd8d9f881a7db380d242578abb44e0
Robert Konigsberg <konigsberg@gmail.com>
2023-10-05 08:28:50 -0400

    Show megacredits in the log when revealing tile placement.

--- src/common/logs/LogMessageData.ts
+++ src/common/logs/LogMessageData.ts
@@ -2,6 +2,7 @@ import {LogMessageDataType} from './LogMessageDataType';
 
 export type LogMessageDataAttrs = {
   tags?: boolean,
+  cost?: boolean,
 }
 export type LogMessageData = {
   type: LogMessageDataType;

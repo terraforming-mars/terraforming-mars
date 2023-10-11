99ef5d464c89445435c7b93730aa9b631ba0ac4e
Robert Konigsberg <konigsberg@gmail.com>
2023-10-11 12:11:41 -0400

    Serializing and deserializing board spaces

--- tests/underworld/UnderworldExpansion.spec.ts
+++ tests/underworld/UnderworldExpansion.spec.ts
@@ -67,4 +67,37 @@ describe('UnderworldExpansion', function() {
       'p-player1-id - p-player2-id - 03',
     ]);
   });
+
+  it('serializing and deserializing board spaces', () => {
+    const spaces = game.board.getAvailableSpacesOnLand(player1);
+    spaces[0].undergroundResources = 'data3';
+    spaces[1].undergroundResources = 'corruption2';
+    spaces[2].undergroundResources = 'ocean';
+    spaces[0].excavator = player1;
+    spaces[1].excavator = player2;
+
+    const spaceIds = spaces.map((s) => s.id);
+    const serialized = game.serialize();
+    const serializedSpaces = serialized.board.spaces.filter((space) => spaceIds.includes(space.id));
+
+    expect(serializedSpaces[0].undergroundResources).eq('data3');
+    expect(serializedSpaces[1].undergroundResources).eq('corruption2');
+    expect(serializedSpaces[2].undergroundResources).eq('ocean');
+    expect(serializedSpaces[3]).does.not.haveOwnPropertyDescriptor('undergroundResources');
+    expect(serializedSpaces[0].excavator).eq('p-player1-id');
+    expect(serializedSpaces[1].excavator).eq('p-player2-id');
+    expect(serializedSpaces[2]).does.not.haveOwnPropertyDescriptor('excavator');
+    expect(serializedSpaces[3]).does.not.haveOwnPropertyDescriptor('excavator');
+
+    const game2 = Game.deserialize(serialized);
+    const deserializedSpaces = game2.board.spaces.filter((space) => spaceIds.includes(space.id));
+    expect(deserializedSpaces[0].undergroundResources).eq('data3');
+    expect(deserializedSpaces[1].undergroundResources).eq('corruption2');
+    expect(deserializedSpaces[2].undergroundResources).eq('ocean');
+    expect(deserializedSpaces[3]).does.not.haveOwnPropertyDescriptor('undergroundResources');
+    expect(deserializedSpaces[0].excavator?.id).eq('p-player1-id');
+    expect(deserializedSpaces[1].excavator?.id).eq('p-player2-id');
+    expect(deserializedSpaces[2]).does.not.haveOwnPropertyDescriptor('excavator');
+    expect(deserializedSpaces[3]).does.not.haveOwnPropertyDescriptor('excavator');
+  });
 });

e031bc5e91f630d39b41f22dc0392404c5fccc65
maserion <maserion@gmail.com>
2023-10-12 14:38:06 +0200

    Moon cards update (#6107)
    Lunar Industry Complex - added missing secondary tag
    Mare Serenitatis Mine - added missing secondary tag
    Luna Mining Hub - cost adjusted
    Luna Train Station - cost adjusted
    Revolting Colonists - symbology consistent

--- src/server/cards/moon/MareSerenitatisMine.ts
+++ src/server/cards/moon/MareSerenitatisMine.ts
@@ -32,7 +32,7 @@ export class MareSerenitatisMine extends Card {
         renderData: CardRenderer.builder((b) => {
           b.minus().titanium(2).minus().steel(1).br;
           b.production((pb) => pb.steel(1).titanium(1)).br;
-          b.moonMine({secondaryTag: AltSecondaryTag.MOON_MINING_RATE}).asterix().nbsp.moonRoad({secondaryTag: AltSecondaryTag.MOON_MINING_RATE}).asterix();
+          b.moonMine({secondaryTag: AltSecondaryTag.MOON_MINING_RATE}).asterix().nbsp.moonRoad({secondaryTag: AltSecondaryTag.MOON_LOGISTICS_RATE}).asterix();
         }),
       },
       tilesBuilt: [TileType.MOON_MINE, TileType.MOON_ROAD],

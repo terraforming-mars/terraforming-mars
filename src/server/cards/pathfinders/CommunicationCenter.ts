8e8c2c18e90151e009019efc1ac6efeddec6dcdc
Robert Konigsberg <konigsberg@gmail.com>
2023-10-17 01:18:59 -0400

    Make CEO's Favorite Project compatible with Communication Center.

--- src/server/cards/pathfinders/CommunicationCenter.ts
+++ src/server/cards/pathfinders/CommunicationCenter.ts
@@ -9,6 +9,7 @@ import {CardResource} from '../../../common/CardResource';
 import {all, played} from '../Options';
 import {Size} from '../../../common/cards/render/Size';
 import {ICard} from '../ICard';
+import {Priority, SimpleDeferredAction} from '../../deferredActions/DeferredAction';
 
 export class CommunicationCenter extends Card implements IProjectCard {
   constructor() {
@@ -50,7 +51,12 @@ export class CommunicationCenter extends Card implements IProjectCard {
 
   public onCardPlayedFromAnyPlayer(thisCardOwner: IPlayer, _playedCardOwner: IPlayer, card: IProjectCard) {
     if (card.type === CardType.EVENT) {
-      thisCardOwner.addResourceTo(this, {qty: 1, log: true});
+      // Resolve CEO's Favorite Project before adding the resource.
+      const priority = (card.name === CardName.CEOS_FAVORITE_PROJECT) ? Priority.BACK_OF_THE_LINE : Priority.DEFAULT;
+      thisCardOwner.game.defer(new SimpleDeferredAction(thisCardOwner, () => {
+        thisCardOwner.addResourceTo(this, {qty: 1, log: true});
+        return undefined;
+      }), priority);
     }
     return undefined;
   }

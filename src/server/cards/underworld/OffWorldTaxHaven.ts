af3a4d4962f59ac38eedeb83dd5e3d8c19798498
Robert Konigsberg <konigsberg@gmail.com>
2023-10-27 13:58:17 -0400

    Underworld card Off-World Tax Haven

---
+++ src/server/cards/underworld/OffWorldTaxHaven.ts
@@ -0,0 +1,32 @@
+import {CardType} from '../../../common/cards/CardType';
+import {IProjectCard} from '../IProjectCard';
+import {CardName} from '../../../common/cards/CardName';
+import {CardRenderer} from '../render/CardRenderer';
+import {Card} from '../Card';
+import {Tag} from '../../../common/cards/Tag';
+
+export class OffWorldTaxHaven extends Card implements IProjectCard {
+  constructor() {
+    super({
+      type: CardType.AUTOMATED,
+      name: CardName.OFF_WORLD_TAX_HAVEN,
+      cost: 8,
+      tags: [Tag.EARTH, Tag.SPACE],
+
+      victoryPoints: -1,
+      requirements: {corruption: 2},
+
+      behavior: {
+        production: {megacredits: 2},
+      },
+
+      metadata: {
+        cardNumber: 'U10',
+        renderData: CardRenderer.builder((b) => {
+          b.production((pb) => pb.megacredits(2));
+        }),
+        description: 'Requires 2 corruption. Increase your M€ production 2 steps.',
+      },
+    });
+  }
+}
---
+++ tests/cards/underworld/OffWorldTaxHaven.spec.ts
@@ -0,0 +1,27 @@
+import {expect} from 'chai';
+import {OffWorldTaxHaven} from '../../../src/server/cards/underworld/OffWorldTaxHaven';
+import {testGame} from '../../TestGame';
+import {cast, runAllActions} from '../../TestingUtils';
+
+describe('OffWorldTaxHaven', () => {
+  it('canPlay', () => {
+    const card = new OffWorldTaxHaven();
+    const [, player] = testGame(1);
+
+    player.underworldData.corruption = 1;
+    expect(card.canPlay(player)).is.false;
+
+    player.underworldData.corruption = 2;
+    expect(card.canPlay(player)).is.true;
+  });
+
+  it('play', () => {
+    const card = new OffWorldTaxHaven();
+    const [game, player] = testGame(1);
+
+    cast(card.play(player), undefined);
+    runAllActions(game);
+
+    expect(player.production.megacredits).eq(2);
+  });
+});

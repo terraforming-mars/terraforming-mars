f59eb7d1c696709f09ac74958c20af83cae57919
Robert Konigsberg <konigsberg@gmail.com>
2023-10-29 00:20:04 -0400

    Underworld prelude Ganymede Trading Company

---
+++ tests/cards/underworld/GanymedeTradingCompany.spec.ts
@@ -0,0 +1,20 @@
+import {expect} from 'chai';
+import {GanymedeTradingCompany} from '../../../src/server/cards/underworld/GanymedeTradingCompany';
+import {testGame} from '../../TestGame';
+import {cast} from '../../TestingUtils';
+import {Units} from '../../../src/common/Units';
+
+describe('GanymedeTradingCompany', () => {
+  it('play', () => {
+    const card = new GanymedeTradingCompany();
+    const [/* skipped */, player] = testGame(2, {coloniesExtension: true});
+
+    expect(player.colonies.getFleetSize()).eq(1);
+
+    cast(card.play(player), undefined);
+
+    expect(player.stock.asUnits()).deep.eq(Units.of({titanium: 3}));
+    expect(player.underworldData.corruption).eq(1);
+    expect(player.colonies.getFleetSize()).eq(2);
+  });
+});

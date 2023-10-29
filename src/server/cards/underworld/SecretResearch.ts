b4fc93b9e9292110ae9997c0e20d8feaa6e71b6f
Robert Konigsberg <konigsberg@gmail.com>
2023-10-29 00:22:12 -0400

    Underworld prelude Secret Research

---
+++ src/server/cards/underworld/SecretResearch.ts
@@ -0,0 +1,27 @@
+import {PreludeCard} from '../prelude/PreludeCard';
+import {CardName} from '../../../common/cards/CardName';
+import {CardRenderer} from '../render/CardRenderer';
+import {Tag} from '../../../common/cards/Tag';
+
+export class SecretResearch extends PreludeCard {
+  constructor() {
+    super({
+      name: CardName.SECRET_RESEARCH,
+      tags: [Tag.JOVIAN, Tag.SPACE],
+
+      behavior: {
+        drawCard: 3,
+        underworld: {corruption: 1},
+      },
+
+      metadata: {
+        cardNumber: 'UP12',
+        renderData: CardRenderer.builder((b) => {
+          b.corruption().cards(3);
+        }),
+        description: 'Gain 1 corruption and draw 3 cards.',
+      },
+    });
+  }
+}
+
---
+++ tests/cards/underworld/SecretResearch.spec.ts
@@ -0,0 +1,19 @@
+import {expect} from 'chai';
+import {SecretResearch} from '../../../src/server/cards/underworld/SecretResearch';
+import {testGame} from '../../TestGame';
+import {cast} from '../../TestingUtils';
+
+describe('SecretResearch', () => {
+  it('play', () => {
+    const card = new SecretResearch();
+    const [/* skipped */, player] = testGame(2, {coloniesExtension: true});
+
+    expect(player.cardsInHand).is.empty;
+    expect(player.underworldData.corruption).eq(0);
+
+    cast(card.play(player), undefined);
+
+    expect(player.underworldData.corruption).eq(1);
+    expect(player.cardsInHand).has.length(3);
+  });
+});

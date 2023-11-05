7a45e3dca060418fec17cf35c384f6b28cfcb79a
Robert Konigsberg <konigsberg@gmail.com>
2023-11-05 11:08:53 -0500

    Add tile placement assertions

--- tests/cards/prelude/EarlySettlement.spec.ts
+++ tests/cards/prelude/EarlySettlement.spec.ts
@@ -1,9 +1,7 @@
-import {expect} from 'chai';
 import {EarlySettlement} from '../../../src/server/cards/prelude/EarlySettlement';
-import {SelectSpace} from '../../../src/server/inputs/SelectSpace';
-import {TileType} from '../../../src/common/TileType';
-import {cast} from '../../TestingUtils';
+import {runAllActions} from '../../TestingUtils';
 import {testGame} from '../../TestGame';
+import {UnderworldTestHelper} from '../../underworld/UnderworldTestHelper';
 
 describe('EarlySettlement', function() {
   it('Should play', function() {
@@ -11,12 +9,8 @@ describe('EarlySettlement', function() {
     const [game, player] = testGame(1);
 
     card.play(player);
-    const selectSpace = cast(game.deferredActions.peek()!.execute(), SelectSpace);
+    runAllActions(game);
 
-    expect(player.production.plants).to.eq(1);
-    expect(selectSpace.cb(selectSpace.spaces[0])).is.undefined;
-    expect(selectSpace.spaces[0].player).to.eq(player);
-    expect(selectSpace.spaces[0].tile).is.not.undefined;
-    expect(selectSpace.spaces[0].tile!.tileType).to.eq(TileType.CITY);
+    UnderworldTestHelper.assertPlaceCity(player, player.popWaitingFor());
   });
 });

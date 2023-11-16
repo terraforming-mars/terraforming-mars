1fe6957607d48c99aa869fed6eaff887332897ea
Ethan Dobbs <ejdobbs@gmail.com>
2023-11-16 11:16:36 -0600

    Fixed rendering cutoffs

--- src/server/turmoil/globalEvents/Election.ts
+++ src/server/turmoil/globalEvents/Election.ts
@@ -12,8 +12,8 @@ import {played} from '../../cards/Options';
 import {Size} from '../../../common/cards/render/Size';
 
 const RENDER_DATA = CardRenderer.builder((b) => {
-  b.influence({size: Size.SMALL}).plus().building(1, {played, size: Size.SMALL}).plus().city({size: Size.SMALL}).colon();
-  b.text('1st:', Size.SMALL).tr(2, {size: Size.TINY, digit: true}).text('2nd:', Size.SMALL).tr(1, {size: Size.TINY});
+  b.influence().plus().building(1, {played, size: Size.SMALL}).plus().city({size: Size.SMALL}).colon().br;
+  b.text('1st:', Size.SMALL).tr(2, {size: Size.TINY}).nbsp.text('2nd:', Size.SMALL).tr(1, {size: Size.TINY});
 });
 
 export class Election extends GlobalEvent implements IGlobalEvent {

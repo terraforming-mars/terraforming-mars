3ccf72c79a96eefe3dd938022375b11c47cf860d
Robert Konigsberg <konigsberg@gmail.com>
2023-11-17 12:40:24 -0500

    Underworld card Deepnuking

--- tests/cards/underworld/Deepnuking.spec.ts
+++ tests/cards/underworld/Deepnuking.spec.ts
@@ -1,20 +1,22 @@
 import {expect} from 'chai';
 
-import {TunnelBoringMachine} from '../../../src/server/cards/underworld/TunnelBoringMachine';
+import {Deepnuking} from '../../../src/server/cards/underworld/Deepnuking';
 import {TestPlayer} from '../../TestPlayer';
 import {testGame} from '../../TestGame';
-import {runAllActions} from '../../TestingUtils';
+import {cast, runAllActions} from '../../TestingUtils';
 import {IGame} from '../../../src/server/IGame';
 import {UnderworldTestHelper} from '../../underworld/UnderworldTestHelper';
+import {OrOptions} from '../../../src/server/inputs/OrOptions';
 
-describe('TunnelBoringMachine', () => {
+describe('Deepnuking', () => {
   let game: IGame;
   let player: TestPlayer;
-  let card: TunnelBoringMachine;
+  let player2: TestPlayer;
+  let card: Deepnuking;
 
   beforeEach(() => {
-    [game, player] = testGame(1, {underworldExpansion: true});
-    card = new TunnelBoringMachine();
+    [game, player, player2] = testGame(2, {underworldExpansion: true});
+    card = new Deepnuking();
   });
 
   it('can play', () => {
@@ -22,26 +24,20 @@ describe('TunnelBoringMachine', () => {
   });
 
   it('play', () => {
+    player2.plants = 5;
+
     expect(card.play(player)).is.undefined;
-  });
-
-  it('can act', () => {
-    player.energy = 2;
-    expect(card.canAct(player)).is.false;
-    player.energy = 3;
-    expect(card.canAct(player)).is.true;
-  });
-
-  it('action', () => {
-    player.energy = 3;
-
-    expect(card.action(player)).is.undefined;
     runAllActions(game);
-
-    expect(player.energy).eq(0);
     UnderworldTestHelper.assertIsExcavationAction(player, player.popWaitingFor());
     runAllActions(game);
     UnderworldTestHelper.assertIsExcavationAction(player, player.popWaitingFor());
+    runAllActions(game);
+
+    const orOptions = cast(player.popWaitingFor(), OrOptions);
+    expect(orOptions.options).has.length(2);
+    orOptions.options[0].cb();
+    expect(player2.plants).to.eq(2);
+
     runAllActions(game);
     expect(player.popWaitingFor()).is.undefined;
   });

9151e4e3366c2c754de50eb149f19ec799ea960f
Robert Konigsberg <kberg@users.noreply.github.com>
2023-12-15 11:30:57 -0800

    Fix bonus for Gagarin Mobile Base player (#6293)

--- src/server/cards/pathfinders/GagarinMobileBase.ts
+++ src/server/cards/pathfinders/GagarinMobileBase.ts
@@ -107,7 +107,7 @@ export class GagarinMobileBase extends CorporationCard implements IActionCard {
       return;
     }
     if (space.id === activePlayer.game.gagarinBase[0]) {
-      cardOwner.defer(this.action(activePlayer));
+      cardOwner.defer(this.action(cardOwner));
     }
   }
 }

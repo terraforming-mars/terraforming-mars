69a7733e21e6c04d5b199b6afff019a66d75466b
Robert Konigsberg <konigsberg@gmail.com>
2023-12-30 14:42:55 -0500

    Add comments to all requirement evaluators

--- src/server/cards/requirements/CardRequirement.ts
+++ src/server/cards/requirements/CardRequirement.ts
@@ -14,10 +14,25 @@ export type YesAnd = {
   thinkTankResources?: number
 }
 
+/**
+ * Information to evaluate a card requirement.
+ *
+ * This represents requirements such as global parameters and tags.
+ *
+ * This base class contains some common attributes, but subclasses will contain
+ * new data.
+ */
 export abstract class CardRequirement {
+  /** The type of requirement this represents, e.g. Oxygen. */
   public abstract readonly type: RequirementType;
+  /** The count for this requirement (e.g. 5%) */
   public readonly count: number;
+  /** When true, this requirement is a maximum vs minimum requirement (e.g. max 5%) */
   public readonly max: boolean;
+  /**
+   * When true, requirement applies to everyone's tiles or tags, etc.
+   * (e.g. you have 2 cities vs all players have 2 cities.)
+   */
   public readonly all: boolean;
   /** Used during card rendering. */
   public readonly text: string | undefined;
@@ -32,5 +47,13 @@ export abstract class CardRequirement {
     this.text = options?.text;
   }
 
+  /**
+   * Evaluate whether |player| satisfies this requirement. It takes into account any
+   * player modifiers (e.g. Adaptation Technology, and fan-based Think Tank.)
+   *
+   * Returns true if |player| can satisfy the requirement, false if it cannot, and
+   * YesAnd if it can only do that under certain conditions (e.g. spend 2 Think Tank
+   * resources.)
+   */
   public abstract satisfies(player: IPlayer, thinkTankResources?: number) : boolean | YesAnd;
 }

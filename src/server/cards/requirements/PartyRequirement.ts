a87f56aebd01c54f21cb775a882f10a1b66e64ba
Robert Konigsberg <konigsberg@gmail.com>
2023-12-30 14:42:42 -0500

    Inline Turmoil.canPlay in PartyRequirement

--- src/server/cards/requirements/PartyRequirement.ts
+++ src/server/cards/requirements/PartyRequirement.ts
@@ -4,13 +4,26 @@ import {Turmoil} from '../../turmoil/Turmoil';
 import {CardRequirement} from './CardRequirement';
 import {RequirementType} from '../../../common/cards/RequirementType';
 
+
+/**
+ * Evaluate whether a player can satisfy a Turmoil party requirement.
+ *
+ * A player satisfies a party requirement if the party is currently ruling, or if
+ * contains two of the player's delegates.
+ */
 export class PartyRequirement extends CardRequirement {
   public readonly type = RequirementType.PARTY;
   constructor(public readonly party: PartyName) {
     super();
-    this.party = party;
   }
+
   public satisfies(player: IPlayer): boolean {
-    return Turmoil.getTurmoil(player.game).canPlay(player, this.party);
+    const turmoil = Turmoil.getTurmoil(player.game);
+    if (turmoil.rulingParty.name === this.party) {
+      return true;
+    }
+
+    const party = turmoil.getPartyByName(this.party);
+    return party.delegates.count(player) >= 2;
   }
 }

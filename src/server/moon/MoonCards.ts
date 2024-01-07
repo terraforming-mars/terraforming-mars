db592b7a7971d28129df466b22a52aa366fa4798
Robert Konigsberg <konigsberg@gmail.com>
2024-01-07 00:07:00 -0500

    Add new cards to Moon's list of science resource cards.
    Add a test to ensure this gets caught in the future.

--- src/server/moon/MoonCards.ts
+++ src/server/moon/MoonCards.ts
@@ -7,9 +7,12 @@ export class MoonCards {
     CardName.LUNA_ARCHIVES,
     CardName.PRIDE_OF_THE_EARTH_ARKSHIP,
     CardName.NANOTECH_INDUSTRIES,
+    CardName.APPLIED_SCIENCE,
+    CardName.SPIRE,
   ]);
-  otherScienceCards: Set<CardName> = new Set([
+  public static otherScienceCards: Set<CardName> = new Set([
     CardName.PHYSICS_COMPLEX,
     CardName.SEARCH_FOR_LIFE,
+    CardName.SEARCH_FOR_LIFE_UNDERGROUND,
   ]);
 }

5aee0a6e2e19ce6b23bfe652cec112c9603934e5
Robert Konigsberg <konigsberg@gmail.com>
2024-02-04 13:04:58 -0500

    Fix MC rendering, show a number inside.

--- src/client/components/card/CardRenderItemComponent.vue
+++ src/client/components/card/CardRenderItemComponent.vue
@@ -420,7 +420,7 @@ export default Vue.extend({
       if (isICardRenderItem(this.item) && this.item.amountInside) {
         if (this.item.questionMark === true) {
           result += '?';
-        } else if (this.item.amount !== 0) {
+        } else {
           result += this.item.amount.toString();
         }
 

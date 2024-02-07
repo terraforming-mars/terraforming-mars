16f3264ecf8bc3f34fa7097b023fba2b2d97bf2c
Robert Konigsberg <konigsberg@gmail.com>
2024-02-07 16:11:40 -0500

    Move viewing a single log message to its own component

--- src/client/components/LogPanel.vue
+++ src/client/components/LogPanel.vue
@@ -15,7 +15,7 @@
         <div class="panel log-panel">
           <div id="logpanel-scrollable" class="panel-body">
             <ul v-if="messages">
-              <li v-for="(message, index) in messages" :key="index" v-on:click.prevent="messageClicked(message)" v-html="messageToHTML(message)"></li>
+              <log-message-component v-for="(message, index) in messages" :key="index" :message="message" :players="players" v-on:click="messageClicked(message)"></log-message-component>
             </ul>
           </div>
           <div class='debugid'>(debugid {{step}})</div>
@@ -40,15 +40,12 @@
 import Vue from 'vue';
 import {paths} from '@/common/app/paths';
 import {statusCode} from '@/common/http/statusCode';
-import {CardType} from '@/common/cards/CardType';
 import {LogMessage} from '@/common/logs/LogMessage';
-import {LogMessageType} from '@/common/logs/LogMessageType';
-import {LogMessageData, LogMessageDataAttrs} from '@/common/logs/LogMessageData';
+import {LogMessageData} from '@/common/logs/LogMessageData';
 import {LogMessageDataType} from '@/common/logs/LogMessageDataType';
 import {PublicPlayerModel} from '@/common/models/PlayerModel';
 import Card from '@/client/components/card/Card.vue';
 import {CardName} from '@/common/cards/CardName';
-import {TileType, tileTypeToString} from '@/common/TileType';
 import {playerColorClass} from '@/common/utils/utils';
 import {Color} from '@/common/Color';
 import {SoundManager} from '@/client/utils/SoundManager';
@@ -56,37 +53,13 @@ import {getPreferences} from '@/client/utils/PreferencesManager';
 import {GlobalEventName} from '@/common/turmoil/globalEvents/GlobalEventName';
 import GlobalEvent from '@/client/components/turmoil/GlobalEvent.vue';
 import AppButton from '@/client/components/common/AppButton.vue';
-import {Log} from '@/common/logs/Log';
-import {getCard} from '@/client/cards/ClientCardManifest';
+import LogMessageComponent from '@/client/components/LogMessageComponent.vue';
 import {ParticipantId} from '@/common/Types';
 import {ColonyName} from '@/common/colonies/ColonyName';
 import Colony from '@/client/components/colonies/Colony.vue';
 import {ColonyModel} from '@/common/models/ColonyModel';
-import {ClientCard} from '@/common/cards/ClientCard';
 
 let logRequest: XMLHttpRequest | undefined;
-
-const cardTypeToCss: Record<CardType, string | undefined> = {
-  event: 'background-color-events',
-  corporation: 'background-color-global-event',
-  active: 'background-color-active',
-  automated: 'background-color-automated',
-  prelude: 'background-color-prelude',
-  ceo: 'background-color-ceo',
-  standard_project: 'background-color-standard-project',
-  standard_action: 'background-color-standard-project',
-  proxy: undefined,
-};
-
-const translatableMessageDataTypes = new Set([
-  LogMessageDataType.STRING,
-  LogMessageDataType.STANDARD_PROJECT,
-  LogMessageDataType.MILESTONE,
-  LogMessageDataType.AWARD,
-  LogMessageDataType.COLONY,
-  LogMessageDataType.PARTY,
-  LogMessageDataType.TILE_TYPE,
-  LogMessageDataType.GLOBAL_EVENT]);
 
 class ToggleSet<T> {
   public elements: Array<T> = [];
@@ -153,8 +126,9 @@ export default Vue.extend({
   components: {
     AppButton,
     Card,
-    GlobalEvent,
     Colony,
+    GlobalEvent,
+    LogMessageComponent,
   },
   methods: {
     scrollToEnd() {
@@ -162,106 +136,6 @@ export default Vue.extend({
       if (scrollablePanel !== null) {
         scrollablePanel.scrollTop = scrollablePanel.scrollHeight;
       }
-    },
-    cardToHtml(card: ClientCard, attrs: LogMessageDataAttrs | undefined) {
-      const suffixFreeCardName = card.name.split(':')[0];
-      const className = cardTypeToCss[card.type];
-
-      if (className === undefined) {
-        return suffixFreeCardName;
-      }
-      let tagHTML = '';
-      if (attrs?.tags === true) {
-        tagHTML = '&nbsp;' + (card.tags.map((tag) => `<div class="log-tag tag-${tag}"></div>`).join(' '));
-      }
-
-      let costHTML = '';
-      if (attrs?.cost === true) {
-        costHTML = `<span>&nbsp;<div class="log-resource-megacredits">${card.cost}</div></span>`;
-      }
-      return '<span class="log-card '+ className + '">' + this.$t(suffixFreeCardName) + tagHTML + costHTML +'</span>';
-    },
-    messageDataToHTML(data: LogMessageData): string {
-      if (data.type === undefined || data.value === undefined) {
-        return '';
-      }
-
-      switch (data.type) {
-      case LogMessageDataType.PLAYER:
-        for (const player of this.players) {
-          if (data.value === player.color) {
-            return '<span class="log-player player_bg_color_'+player.color+'">'+player.name+'</span>';
-          }
-        }
-        break;
-
-      case LogMessageDataType.CARD:
-        const cardName = data.value as CardName;
-        const card = getCard(cardName);
-        if (card !== undefined) {
-          return this.cardToHtml(card, data.attrs);
-        } else {
-          console.log(`Cannot render ${cardName}`);
-        }
-        break;
-
-      case LogMessageDataType.GLOBAL_EVENT:
-        const globalEventName = data.value as GlobalEventName;
-        return '<span class="log-card background-color-global-event">' + this.$t(globalEventName) + '</span>';
-
-      case LogMessageDataType.TILE_TYPE:
-        const tileType: TileType = Number(data.value);
-        return this.$t(tileTypeToString[tileType]);
-
-      case LogMessageDataType.COLONY:
-        const colonyName = data.value as ColonyName;
-        return '<span class="log-card background-color-colony">' + this.$t(colonyName) + '</span>';
-
-      default:
-        if (translatableMessageDataTypes.has(data.type)) {
-          return this.$t(data.value);
-        }
-      }
-      return data.value;
-    },
-    // Called in the event that a bad log message comes down. Does its best to return something.
-    safeMessage(message: LogMessage) {
-      try {
-        if (message === undefined) {
-          return 'undefined';
-        }
-        if (message.data === undefined) {
-          return `BUG: Unparseable message: ${message.message}`;
-        }
-        const data = message.data.map((datum) => {
-          return (datum === undefined) ?
-            'undefined' :
-            ('(' + datum.type + ') ' + datum.value);
-        });
-        return `BUG: Unparseable message: ${message.message}, (${data.join(', ')})`;
-      } catch (err) {
-        return `BUG: Unparseable message: ${message.message} ${String(err)}`;
-      }
-    },
-    messageToHTML(message: LogMessage) {
-      try {
-        let logEntryBullet = '';
-
-        if (message.type !== LogMessageType.NEW_GENERATION) {
-          const when = new Date(message.timestamp).toLocaleString();
-          // clock or speaking.
-          const icon = message.playerId === undefined ? '&#x1f551;' : '&#x1f4ac;';
-          logEntryBullet = `<span title="${when}">${icon}</span>`;
-        }
-        if (message.message !== undefined) {
-          message.message = this.$t(message.message);
-          return logEntryBullet + Log.applyData(message, this.messageDataToHTML);
-        }
-      } catch (err) {
-        console.log(err);
-        return this.safeMessage(message);
-      }
-      return '';
     },
     messageClicked(message: LogMessage) {
       // TODO(kberg): add global event here, too.
@@ -369,4 +243,3 @@ export default Vue.extend({
 });
 
 </script>
-

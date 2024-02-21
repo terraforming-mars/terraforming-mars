bb4ab07e5d50a813a5d9233c3c0a32b2087ecb4c
Robert Konigsberg <konigsberg@gmail.com>
2024-02-21 13:07:01 -0500

    Underworld card Monopoly

---
+++ src/server/cards/underworld/Monopoly.ts
@@ -0,0 +1,77 @@
+import {IProjectCard} from '../IProjectCard';
+import {Card} from '../Card';
+import {CardType} from '../../../common/cards/CardType';
+import {IPlayer} from '../../IPlayer';
+import {CardName} from '../../../common/cards/CardName';
+import {SelectResource} from '../../inputs/SelectResource';
+import {CardRenderer} from '../render/CardRenderer';
+import {all} from '../Options';
+import {Units} from '../../../common/Units';
+
+export class Monopoly extends Card implements IProjectCard {
+  constructor() {
+    super({
+      type: CardType.EVENT,
+      name: CardName.MONOPOLY,
+      cost: 12,
+      requirements: {corruption: 3},
+      victoryPoints: -2,
+
+      metadata: {
+        cardNumber: 'U65',
+        renderData: CardRenderer.builder((b) => {
+          b.text('STEAL').production((pb) => pb.wild(1, {all})).br;
+        }),
+        description: 'Requires 3 corruption. Choose a standard production type. ' +
+          'Steal 1 unit of that production from EACH OTHER player. They can block this with corruption.',
+      },
+    });
+  }
+
+  private availableProductions(player: IPlayer): Array<keyof Units> {
+    const units = new Set<keyof Units>();
+    const targets = player.game.getPlayers().filter((p) => p !== player);
+    for (const unit of Units.keys) {
+      const resource = Units.ResourceMap[unit];
+      if (targets.some((target) => target.canHaveProductionReduced(resource, 1, player))) {
+        units.add(unit);
+      }
+    }
+    return Array.from(units);
+  }
+
+  public override bespokeCanPlay(player: IPlayer) {
+    return this.availableProductions(player).length > 0;
+  }
+
+  public override bespokePlay(player: IPlayer) {
+    return new SelectResource(
+      'Select which resource type to steal from all other players.',
+      this.availableProductions(player),
+      (unitKey) => {
+        const resource = Units.ResourceMap[unitKey];
+        if (player.game.isSoloMode()) {
+          player.production.add(resource, 1, {log: true});
+          player.resolveInsuranceInSoloGame();
+          return undefined;
+        }
+        for (const target of player.game.getPlayers()) {
+          if (target === player) {
+            continue;
+          }
+          if (target.canHaveProductionReduced(resource, 1, player)) {
+            target.maybeBlockAttack(player, (proceed: boolean) => {
+              if (proceed) {
+                target.production.add(resource, -1, {log: true, from: player, stealing: true});
+                player.production.add(resource, 1, {log: false});
+                target.resolveInsurance();
+              }
+              return undefined;
+            });
+          }
+        }
+        return undefined;
+      });
+  }
+}
+

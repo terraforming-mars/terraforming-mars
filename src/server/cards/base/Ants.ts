e4ba2e0ff2c79312f8e6f86b3cfc7012951dd534
Robert Konigsberg <konigsberg@gmail.com>
2024-02-22 11:38:50 -0500

    Minor tweak to Ants

--- src/server/cards/base/Ants.ts
+++ src/server/cards/base/Ants.ts
@@ -38,7 +38,7 @@ export class Ants extends Card implements IActionCard, IProjectCard {
 
   public canAct(player: IPlayer): boolean {
     if (player.game.isSoloMode()) return true;
-    return RemoveResourcesFromCard.getAvailableTargetCards(player, this.resourceType).length > 0;
+    return RemoveResourcesFromCard.getAvailableTargetCards(player, CardResource.MICROBE).length > 0;
   }
 
   public action(player: IPlayer) {

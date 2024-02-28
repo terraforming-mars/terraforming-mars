144214d7461553ed2d728b240f64ebc5307f4159
Robert Konigsberg <konigsberg@gmail.com>
2024-02-28 07:03:07 -0500

    Underworld card viz feedback

--- src/server/cards/underworld/SpacePrivateers.ts
+++ src/server/cards/underworld/SpacePrivateers.ts
@@ -34,7 +34,8 @@ export class SpacePrivateers extends Card implements IProjectCard, IActionCard {
           b.effect(
             'If 1 or more targets block this with corruption, remove 1 fighter from here.',
             (eb) => eb.corruptionShield().startEffect.minus().fighter()).br;
-          b.plainText('(Solo: Gain 2 M€ and remove 1 fighter from this card.)');
+          b.plainText('(Solo: Gain 2 M€ and remove 1 fighter from this card.)').br;
+          b.fighter(3);
         }),
         description: 'Requires 3 corruption. Put 3 fighter resources on this card.',
       },

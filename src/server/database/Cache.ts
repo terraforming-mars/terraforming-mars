fe510fc4c4503b0a493f208e65c325f540031bde
Robert Konigsberg <konigsberg@gmail.com>
2024-03-12 13:10:04 -0400

    Remove TODO

--- src/server/database/Cache.ts
+++ src/server/database/Cache.ts
@@ -2,24 +2,9 @@ import {IGame} from '../IGame';
 import {GameId, ParticipantId} from '../../common/Types';
 import {once} from 'events';
 import {EventEmitter} from 'events';
-import * as prometheus from 'prom-client';
 import {Database} from './Database';
 import {CacheConfig} from './CacheConfig';
 import {Clock} from '../../common/Timer';
-import {SerializedGame} from '../SerializedGame';
-
-const metrics = {
-  start: new prometheus.Gauge({
-    name: 'game_ids_get_all_instances_started',
-    help: 'Time getAllInstances started',
-    registers: [prometheus.register],
-  }),
-  end: new prometheus.Gauge({
-    name: 'game_ids_get_all_instances_finished',
-    help: 'Time getAllInstances finished',
-    registers: [prometheus.register],
-  }),
-};
 
 export class Cache extends EventEmitter {
   private loaded = false;
@@ -38,46 +23,6 @@ export class Cache extends EventEmitter {
     this.clock = clock;
   }
 
-  private async getInstance(gameId: GameId) : Promise<void> {
-    let game: SerializedGame;
-    try {
-      game = await this.db.getGame(gameId);
-    } catch (e) {
-      console.error(`getInstance for ${gameId}`, e);
-      return;
-    }
-    if (this.games.get(gameId) === undefined) {
-      this.games.set(gameId, undefined);
-      const participantIds: Array<ParticipantId> = [];
-      if (game.spectatorId !== undefined) {
-        this.participantIds.set(game.spectatorId, gameId);
-        participantIds.push(game.spectatorId);
-      }
-      for (const player of game.players) {
-        this.participantIds.set(player.id, gameId);
-        participantIds.push(player.id);
-      }
-      try {
-        this.db.storeParticipants({gameId, participantIds});
-      } catch (e) {
-        console.log(`Failed to store ${gameId}`);
-      }
-    }
-  }
-
-  private async getAllInstances(allGameIds: Array<GameId>): Promise<void> {
-    metrics.start.set(this.clock.now());
-
-    const sliceSize = 1000;
-    for (let i = 0; i < allGameIds.length; i += sliceSize) {
-      const slice = allGameIds.slice(i, i + sliceSize);
-      await Promise.all(slice.map((x) => this.getInstance(x))).then(() => {
-        console.log(`Loaded ${i} to ${i + slice.length} of ${allGameIds.length}`);
-      });
-    }
-    metrics.end.set(this.clock.now());
-  }
-
   public async load(): Promise<void> {
     try {
       console.log('Preloading IDs.');
@@ -90,12 +35,6 @@ export class Cache extends EventEmitter {
         }
       }
       console.log(`Preloaded ${entries.length} IDs.`);
-
-      // TODO(kberg): Eventually these three lines will be unnecessary. They're actually
-      // unnecessary after the first run.
-      const allGameIds = await this.db.getGameIds();
-      const filtered = allGameIds.filter((id) => !this.games.has(id));
-      await this.getAllInstances(filtered);
     } catch (err) {
       console.error('error loading all games', err);
     }

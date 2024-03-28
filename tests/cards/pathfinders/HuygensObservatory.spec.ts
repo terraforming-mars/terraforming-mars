terraforming-mars/terraforming-mars

commit 4f3f26f152941a6a38be1b4f2b762d347b73ecd5

Make Huygens Observatory and Darkside Smugglers Union compatible with Trade Embargo
Fixes #6473

src/server/cards/pathfinders/HuygensObservatory.ts
@@ -94,7 +94,10 @@ export class HuygensObservatory extends Card implements IProjectCard {
     }
   }
   public override bespokeCanPlay(player: IPlayer): boolean {
-    return player.colonies.getPlayableColonies(/** allowDuplicate = */true).length > 0 || ColoniesHandler.tradeableColonies(player.game).length > 0;
+    if (!player.colonies.canTrade()) {
+      return false;
+    }
+    return player.colonies.getPlayableColonies(/** allowDuplicate = */true).length > 0;
   }
 
   public override bespokePlay(player: IPlayer) {

src/server/cards/moon/DarksideSmugglersUnion.ts
@@ -37,7 +37,7 @@ export class DarksideSmugglersUnion extends Card implements IProjectCard, IActio
   }
 
   public canAct(player: IPlayer): boolean {
-    return player.colonies.canTrade() && ColoniesHandler.tradeableColonies(player.game).length > 0;
+    return player.colonies.canTrade();
   }
 
   public action(player: IPlayer) {

commit 96caa23b724a932c528d22971644a07315f22be3

Restore Huygens behavior.
This was broken in 4f3f26f152941a6a38be1b4f2b762d347b73ecd5 and would
have been caught with the right canPlay tests. Those tests are now
in place.

src/server/cards/pathfinders/HuygensObservatory.ts
@@ -94,10 +94,17 @@ export class HuygensObservatory extends Card implements IProjectCard {
     }
   }
   public override bespokeCanPlay(player: IPlayer): boolean {
-    if (!player.colonies.canTrade()) {
+    // NOTE: Don't use canTrade.
+    if (player.game.tradeEmbargo === true) {
       return false;
     }
-    return player.colonies.getPlayableColonies(/** allowDuplicate = */true).length > 0;
+    if (player.colonies.getPlayableColonies(/** allowDuplicate = */true).length === 0) {
+      return false;
+    }
+    if (ColoniesHandler.tradeableColonies(player.game).length === 0) {
+      return false;
+    }
+    return true;
   }
 
   public override bespokePlay(player: IPlayer) {

tests/cards/pathfinders/HuygensObservatory.spec.ts
@@ -21,7 +21,7 @@ describe('HuygensObservatory', function() {
     card = new HuygensObservatory();
     // By choosing 2 players I don't have to pay attention to the first action which
     // removes a colony tile.
-    [game, player] = testGame(2, {
+    [game, player/* , player2*/] = testGame(2, {
       coloniesExtension: true,
       customColoniesList: [
         ColonyName.GANYMEDE,
@@ -32,6 +32,50 @@ describe('HuygensObservatory', function() {
       ],
     });
     ganymede = game.colonies.find((colony) => colony.name === ColonyName.GANYMEDE)!;
+  });
+
+  it('can play, trade fleet home', () => {
+    expect(card.canPlay(player)).is.true;
+  });
+
+  it('can play, trade fleet on a colony tile', () => {
+    ganymede.trade(player);
+
+    expect(card.canPlay(player)).is.true;
+  });
+
+  it('Cannot play, cannot place a colony', () => {
+    for (const colony of player.game.colonies) {
+      colony.colonies = [player.id, player.id, player.id];
+    }
+
+    expect(card.canPlay(player)).is.false;
+  });
+
+  it('Can play even if you have a colony where you already have one.', () => {
+    for (const colony of player.game.colonies) {
+      colony.colonies = [player.id, player.id, player.id];
+    }
+
+    expect(card.canPlay(player)).is.false;
+
+    ganymede.colonies.pop();
+
+    expect(card.canPlay(player)).is.true;
+  });
+
+  it('Cannot play, cannot trade', () => {
+    for (const colony of player.game.colonies) {
+      expect(card.canPlay(player)).is.true;
+      colony.visitor = player.id;
+    }
+
+    expect(card.canPlay(player)).is.false;
+  });
+
+  it('Cannot play, trade embargo', () => {
+    player.game.tradeEmbargo = true;
+    expect(card.canPlay(player)).is.false;
   });
 
   it('play, simple case (place colony, trade with it)', function() {
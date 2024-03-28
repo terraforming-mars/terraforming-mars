a8c87ddfda0daa3ac9ee2e14e74ecc990493b227
Robert Konigsberg <konigsberg@gmail.com>
2024-03-28 14:24:37 -0400

    Update Public Sponsored Grants behavior to match printed card.
    According to Google Translate, most of the translations already
    referenced "Every other player."

--- tests/cards/pathfinders/PublicSponsoredGrant.spec.ts
+++ tests/cards/pathfinders/PublicSponsoredGrant.spec.ts
@@ -43,7 +43,7 @@ describe('PublicSponsoredGrant', function() {
 
     const options = cast(card.play(player), OrOptions);
 
-    expect(player.megaCredits).eq(2);
+    expect(player.megaCredits).eq(4);
     expect(player2.megaCredits).eq(0);
     expect(player3.megaCredits).eq(0);
 
@@ -74,7 +74,7 @@ describe('PublicSponsoredGrant', function() {
 
     // This is a great test, because player1 instigated the loss, so does not get an insurance
     // payout. Player 2 loses the payout and player 3 gets it.
-    expect(player.megaCredits).eq(8);
+    expect(player.megaCredits).eq(10);
     expect(player2.megaCredits).eq(5);
     expect(player3.megaCredits).eq(11);
   });

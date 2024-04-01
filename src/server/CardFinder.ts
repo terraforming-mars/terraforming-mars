84058621469bac48ca3ca4af87962a9fcfabebb2
Robert Konigsberg <konigsberg@gmail.com>
2024-04-01 11:00:43 -0400

    Turn CardFinder into functions
    These functions are pretty much factory functions, so they're called
    "new". In the next commit CardFinder will become CardFactory or
    something.

--- src/server/CardFinder.ts
+++ src/server/CardFinder.ts
@@ -9,119 +9,116 @@ import {ALL_MODULE_MANIFESTS} from './cards/AllManifests';
 
 const CARD_RENAMES = new Map<string, CardName>([
   // When renaming a card, add the old name here (like the example below), and add a TODO (like the example below)
-  // And remember to add a test in CardFinder.spec.ts.
+  // And remember to add a test in spec.ts.
 
   // TODO(yournamehere): remove after 2021-04-05
   // ['Earth Embasy', CardName.EARTH_EMBASSY],
 ]);
 
-// TODO(kberg): This doesn't need to be a class.
-export class CardFinder {
-  private getCard<T extends ICard>(cardName: CardName, cardManifestNames: Array<keyof ModuleManifest>): T | undefined {
-    const standardizedCardName = CARD_RENAMES.get(cardName) || cardName;
+function _createCard<T extends ICard>(cardName: CardName, cardManifestNames: Array<keyof ModuleManifest>): T | undefined {
+  const standardizedCardName = CARD_RENAMES.get(cardName) || cardName;
 
-    for (const moduleManifest of ALL_MODULE_MANIFESTS) {
-      for (const manifestName of cardManifestNames) {
-        const cardManifest = <CardManifest<T>> moduleManifest[manifestName];
-        const factory = cardManifest[standardizedCardName];
-        if (factory !== undefined) {
-          return new factory.Factory();
-        }
+  for (const moduleManifest of ALL_MODULE_MANIFESTS) {
+    for (const manifestName of cardManifestNames) {
+      const cardManifest = <CardManifest<T>> moduleManifest[manifestName];
+      const factory = cardManifest[standardizedCardName];
+      if (factory !== undefined) {
+        return new factory.Factory();
       }
     }
-    console.warn(`card not found ${cardName}`);
-    return undefined;
   }
+  console.warn(`card not found ${cardName}`);
+  return undefined;
+}
 
-  public getCardByName(cardName: CardName): ICard | undefined {
-    return this.getCard(cardName, ['corporationCards', 'projectCards', 'preludeCards', 'ceoCards']);
+export function newCard(cardName: CardName): ICard | undefined {
+  return _createCard(cardName, ['corporationCards', 'projectCards', 'preludeCards', 'ceoCards']);
+}
+
+export function newCorporationCard(cardName: CardName): ICorporationCard | undefined {
+  return _createCard(cardName, ['corporationCards']);
+}
+
+// Function to return a card object by its name
+// NOTE(kberg): This replaces a larger function which searched for both Prelude cards amidst project cards
+// TODO(kberg+dl): Find the use cases where this is used to find Prelude+CEO cards and filter them out to
+//              another function, perhaps?
+export function newProjectCard(cardName: CardName): IProjectCard | undefined {
+  return _createCard(cardName, ['projectCards', 'preludeCards', 'ceoCards']);
+}
+
+export function newPrelude(cardName: CardName): IPreludeCard | undefined {
+  return _createCard(cardName, ['preludeCards']);
+}
+
+export function newCeo(cardName: CardName): ICeoCard | undefined {
+  return _createCard(cardName, ['ceoCards']);
+}
+
+export function preludesFromJSON(cards: Array<CardName>): Array<IPreludeCard> {
+  if (cards === undefined) {
+    console.warn('missing cards calling cardsFromJSON');
+    return [];
   }
-
-  public getCorporationCardByName(cardName: CardName): ICorporationCard | undefined {
-    return this.getCard(cardName, ['corporationCards']);
-  }
-
-  // Function to return a card object by its name
-  // NOTE(kberg): This replaces a larger function which searched for both Prelude cards amidst project cards
-  // TODO(kberg+dl): Find the use cases where this is used to find Prelude+CEO cards and filter them out to
-  //              another function, perhaps?
-  public getProjectCardByName(cardName: CardName): IProjectCard | undefined {
-    return this.getCard(cardName, ['projectCards', 'preludeCards', 'ceoCards']);
-  }
-
-  public getPreludeByName(cardName: CardName): IPreludeCard | undefined {
-    return this.getCard(cardName, ['preludeCards']);
-  }
-
-  public getCeoByName(cardName: CardName): ICeoCard | undefined {
-    return this.getCard(cardName, ['ceoCards']);
-  }
-
-  public preludesFromJSON(cards: Array<CardName>): Array<IPreludeCard> {
-    if (cards === undefined) {
-      console.warn('missing cards calling cardsFromJSON');
-      return [];
+  const result: Array<IPreludeCard> = [];
+  cards.forEach((element: CardName) => {
+    const card = newPrelude(element);
+    if (card !== undefined) {
+      result.push(card);
+    } else {
+      console.warn(`card ${element} not found while loading game.`);
     }
-    const result: Array<IPreludeCard> = [];
-    cards.forEach((element: CardName) => {
-      const card = this.getPreludeByName(element);
-      if (card !== undefined) {
-        result.push(card);
-      } else {
-        console.warn(`card ${element} not found while loading game.`);
-      }
-    });
-    return result;
-  }
+  });
+  return result;
+}
 
-  public ceosFromJSON(cards: Array<CardName>): Array<ICeoCard> {
-    if (cards === undefined) {
-      console.warn('missing cards calling ceosFromJSON');
-      return [];
-    }
-    const result: Array<ICeoCard> = [];
-    cards.forEach((element: CardName) => {
-      const card = this.getCeoByName(element);
-      if (card !== undefined) {
-        result.push(card);
-      } else {
-        console.warn(`card ${element} not found while loading game.`);
-      }
-    });
-    return result;
+export function ceosFromJSON(cards: Array<CardName>): Array<ICeoCard> {
+  if (cards === undefined) {
+    console.warn('missing cards calling ceosFromJSON');
+    return [];
   }
+  const result: Array<ICeoCard> = [];
+  cards.forEach((element: CardName) => {
+    const card = newCeo(element);
+    if (card !== undefined) {
+      result.push(card);
+    } else {
+      console.warn(`card ${element} not found while loading game.`);
+    }
+  });
+  return result;
+}
 
-  public cardsFromJSON(cards: Array<CardName>): Array<IProjectCard> {
-    if (cards === undefined) {
-      console.warn('missing cards calling cardsFromJSON');
-      return [];
-    }
-    const result: Array<IProjectCard> = [];
-    cards.forEach((element: CardName) => {
-      const card = this.getProjectCardByName(element);
-      if (card !== undefined) {
-        result.push(card);
-      } else {
-        console.warn(`card ${element} not found while loading game.`);
-      }
-    });
-    return result;
+export function cardsFromJSON(cards: Array<CardName>): Array<IProjectCard> {
+  if (cards === undefined) {
+    console.warn('missing cards calling cardsFromJSON');
+    return [];
   }
+  const result: Array<IProjectCard> = [];
+  cards.forEach((element: CardName) => {
+    const card = newProjectCard(element);
+    if (card !== undefined) {
+      result.push(card);
+    } else {
+      console.warn(`card ${element} not found while loading game.`);
+    }
+  });
+  return result;
+}
 
-  public corporationCardsFromJSON(cards: Array<CardName>): Array<ICorporationCard> {
-    if (cards === undefined) {
-      console.warn('missing cards calling corporationCardsFromJSON');
-      return [];
-    }
-    const result: Array<ICorporationCard> = [];
-    cards.forEach((element: CardName) => {
-      const card = this.getCorporationCardByName(element);
-      if (card !== undefined) {
-        result.push(card);
-      } else {
-        console.warn(`corporation ${element} not found while loading game.`);
-      }
-    });
-    return result;
+export function corporationCardsFromJSON(cards: Array<CardName>): Array<ICorporationCard> {
+  if (cards === undefined) {
+    console.warn('missing cards calling corporationCardsFromJSON');
+    return [];
   }
+  const result: Array<ICorporationCard> = [];
+  cards.forEach((element: CardName) => {
+    const card = newCorporationCard(element);
+    if (card !== undefined) {
+      result.push(card);
+    } else {
+      console.warn(`corporation ${element} not found while loading game.`);
+    }
+  });
+  return result;
 }
--- tests/CardFinder.spec.ts
+++ tests/CardFinder.spec.ts
@@ -1,26 +1,27 @@
 import {CardName} from '../src/common/cards/CardName';
 import {expect} from 'chai';
-import {CardFinder} from '../src/server/CardFinder';
+import {newCeo, newProjectCard} from '../src/server/CardFinder';
 
 describe('CardFinder', function() {
-  it('getProjectCardByName: success', function() {
-    expect(new CardFinder().getProjectCardByName(CardName.AI_CENTRAL)?.name).eq(CardName.AI_CENTRAL);
+  it('findProjectCard: success', function() {
+    expect(newProjectCard(CardName.AI_CENTRAL)?.name).eq(CardName.AI_CENTRAL);
   });
-  it('getProjectCardByName: failure', function() {
-    expect(new CardFinder().getProjectCardByName(CardName.ECOLINE)).is.undefined;
+  it('findProjectCard: failure', function() {
+    expect(newProjectCard(CardName.ECOLINE)).is.undefined;
   });
-  it('getProjectCardByName prelude: success', function() {
-    expect(new CardFinder().getProjectCardByName(CardName.ALLIED_BANK)?.name).eq(CardName.ALLIED_BANK);
+  it('findProjectCard prelude: success', function() {
+    expect(newProjectCard(CardName.ALLIED_BANK)?.name).eq(CardName.ALLIED_BANK);
   });
-  it('getProjectCardByName ceo: success', function() {
-    expect(new CardFinder().getProjectCardByName(CardName.HAL9000)?.name).eq(CardName.HAL9000);
+  it('findProjectCard ceo: success', function() {
+    expect(newProjectCard(CardName.HAL9000)?.name).eq(CardName.HAL9000);
   });
   it('getCeoByName ceo: success', function() {
-    expect(new CardFinder().getCeoByName(CardName.HAL9000)?.name).eq(CardName.HAL9000);
+    expect(newCeo(CardName.HAL9000)?.name).eq(CardName.HAL9000);
   });
-  // Dont' remove this test. It's a placeholder for card renames.
+
+  // Don't remove this test. It's a placeholder for card renames.
   it('finds renamed cards', function() {
-    // expect(new CardFinder().getProjectCardByName('Designed Micr-organisms'as CardName)?.name).to.equal(CardName.DESIGNED_MICROORGANISMS);
-    // expect(new CardFinder().getProjectCardByName('Cryo Sleep' as CardName)?.name).to.equal(CardName.CRYO_SLEEP);
+    // expect(findProjectCard('Designed Micr-organisms'as CardName)?.name).to.equal(CardName.DESIGNED_MICROORGANISMS);
+    // expect(findProjectCard('Cryo Sleep' as CardName)?.name).to.equal(CardName.CRYO_SLEEP);
   });
 });

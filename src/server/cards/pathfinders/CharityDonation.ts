1c910ff6ad91f1ef9257d3bfb3dd57648c22eafc
Robert Konigsberg <konigsberg@gmail.com>
2024-04-02 09:58:17 -0400

    Remove TODO

--- src/server/cards/pathfinders/CharityDonation.ts
+++ src/server/cards/pathfinders/CharityDonation.ts
@@ -34,7 +34,6 @@ export class CharityDonation extends Card implements IProjectCard {
     const players = game.getPlayersInGenerationOrder();
     const thisIdx = players.findIndex((p) => p === player);
     const cards = game.projectDeck.drawByCondition(game, players.length + 1, () => true);
-    // TODO(kberg): log the drawn cards after raising the planetary track.
     LogHelper.logDrawnCards(player, cards);
     game.defer(new SelectCharityDonationCard(players, thisIdx, thisIdx, cards));
     return undefined;

09f3356c90fb0d1085d17b11b6554fc729410abb
Jan Terje Bringe <jan.terje.bringe@gmail.com>
2024-04-16 14:48:41 +0200

    Comma dangle -_-

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -72,7 +72,7 @@ export const ASIMOV_AWARD_BONUS = 2;
 // Underworld
 export const CORRUPTION_VALUE = 10;
 
-export const ALL_LANGUAGES = ['en', 'de', 'fr', 'ru', 'cn', 'pl', 'es', 'br', 'it', 'ko', 'nl', 'hu', 'jp', 'bg', 'nb',] as const;
+export const ALL_LANGUAGES = ['en', 'de', 'fr', 'ru', 'cn', 'pl', 'es', 'br', 'it', 'ko', 'nl', 'hu', 'jp', 'bg', 'nb'] as const;
 
 export const LANGUAGES: Record<typeof ALL_LANGUAGES[number], string> = {
   en: 'English',
@@ -89,7 +89,7 @@ export const LANGUAGES: Record<typeof ALL_LANGUAGES[number], string> = {
   hu: 'Magyar',
   jp: '日本語',
   bg: 'Български',
-  nb: 'Norsk',
+  nb: 'Norsk'
 };
 
 export const APP_NAME = 'Terraforming Mars';

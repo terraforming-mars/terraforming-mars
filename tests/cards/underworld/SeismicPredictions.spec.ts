4470b13fb52ac0170298e6f9f0809c7e81958abf
Robert Konigsberg <konigsberg@gmail.com>
2024-04-23 13:31:40 -0400

    Seismic productions should not apply to space colonies

--- tests/cards/underworld/SeismicPredictions.spec.ts
+++ tests/cards/underworld/SeismicPredictions.spec.ts
@@ -6,6 +6,7 @@ import {TestPlayer} from '../../TestPlayer';
 import {testGame} from '../../TestGame';
 import {TileType} from '../../../src/common/TileType';
 import {UnderworldExpansion} from '../../../src/server/underworld/UnderworldExpansion';
+import {SpaceName} from '../../../src/server/SpaceName';
 
 describe('SeismicPredictions', function() {
   let card: SeismicPredictions;
@@ -20,17 +21,18 @@ describe('SeismicPredictions', function() {
   });
 
   const resolveTests = [
-    {mc: 10, ownedTiles: 0, claimed: 0, influence: 0, expect: {mc: 10}},
-    {mc: 10, ownedTiles: 0, claimed: 0, influence: 1, expect: {mc: 10}},
-    {mc: 10, ownedTiles: 1, claimed: 1, influence: 0, expect: {mc: 10}},
-    {mc: 10, ownedTiles: 1, claimed: 0, influence: 1, expect: {mc: 10}},
-    {mc: 10, ownedTiles: 0, claimed: 0, influence: 1, expect: {mc: 10}},
-    {mc: 10, ownedTiles: 4, claimed: 3, influence: 0, expect: {mc: 8}},
-    {mc: 10, ownedTiles: 4, claimed: 0, influence: 0, expect: {mc: 2}},
-    {mc: 10, ownedTiles: 6, claimed: 0, influence: 0, expect: {mc: 0}},
-    {mc: 10, ownedTiles: 6, claimed: 0, influence: 1, expect: {mc: 2}},
-    {mc: 10, ownedTiles: 6, claimed: 0, influence: 2, expect: {mc: 4}},
-    {mc: 10, ownedTiles: 6, claimed: 0, influence: 3, expect: {mc: 6}},
+    {mc: 10, ownedTiles: 0, claimed: 0, influence: 0, spaceColony: false, expect: {mc: 10}},
+    {mc: 10, ownedTiles: 0, claimed: 0, influence: 0, spaceColony: false, expect: {mc: 10}},
+    {mc: 10, ownedTiles: 0, claimed: 0, influence: 1, spaceColony: false, expect: {mc: 10}},
+    {mc: 10, ownedTiles: 1, claimed: 1, influence: 0, spaceColony: false, expect: {mc: 10}},
+    {mc: 10, ownedTiles: 1, claimed: 0, influence: 1, spaceColony: false, expect: {mc: 10}},
+    {mc: 10, ownedTiles: 0, claimed: 0, influence: 1, spaceColony: false, expect: {mc: 10}},
+    {mc: 10, ownedTiles: 4, claimed: 3, influence: 0, spaceColony: false, expect: {mc: 8}},
+    {mc: 10, ownedTiles: 4, claimed: 0, influence: 0, spaceColony: false, expect: {mc: 2}},
+    {mc: 10, ownedTiles: 6, claimed: 0, influence: 0, spaceColony: false, expect: {mc: 0}},
+    {mc: 10, ownedTiles: 6, claimed: 0, influence: 1, spaceColony: false, expect: {mc: 2}},
+    {mc: 10, ownedTiles: 6, claimed: 0, influence: 2, spaceColony: false, expect: {mc: 4}},
+    {mc: 10, ownedTiles: 6, claimed: 0, influence: 3, spaceColony: false, expect: {mc: 6}},
   ] as const;
 
   resolveTests.forEach((run, idx) => {
@@ -51,6 +53,10 @@ describe('SeismicPredictions', function() {
           space.excavator = player;
         }
       });
+
+      if (run.spaceColony) {
+        game.simpleAddTile(player, board.getSpaceOrThrow(SpaceName.GANYMEDE_COLONY), {tileType: TileType.CITY});
+      }
 
       card.resolve(game, turmoil);
 

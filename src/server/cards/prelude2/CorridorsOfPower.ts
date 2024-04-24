a7ffe6acfbd59d5c32b3e3705d8d56006feccbac
Robert Konigsberg <konigsberg@gmail.com>
2024-04-24 05:52:50 -0400

    Prelude 2 card Corridors of Power

---
+++ src/server/cards/prelude2/CorridorsOfPower.ts
@@ -0,0 +1,32 @@
+import {Tag} from '../../../common/cards/Tag';
+import {CardName} from '../../../common/cards/CardName';
+import {CardRenderer} from '../render/CardRenderer';
+import {PreludeCard} from '../prelude/PreludeCard';
+
+export class CorridorsOfPower extends PreludeCard {
+  constructor() {
+    super({
+      name: CardName.CORRIDORS_OF_POWER,
+      tags: [Tag.EARTH],
+
+      behavior: {
+        tr: 1,
+        stock: {megacredits: 4},
+      },
+
+      metadata: {
+        cardNumber: '',
+        renderData: CardRenderer.builder((b) => {
+          b.effect('Whenever one of your delegates becomes a Party Leader, draw a card', (ab) => {
+            ab.partyLeaders(1).startAction.cards(1);
+          }).br;
+          b.tr(1).megacredits(4).br;
+          b.plainText('Increase your TR one step. Gain 4 M€');
+        }),
+      },
+    });
+
+    // Behavior in Party.ts
+  }
+}
+
---
+++ tests/cards/prelude2/CorridorsOfPower.spec.ts
@@ -0,0 +1,62 @@
+import {expect} from 'chai';
+import {IGame} from '../../../src/server/IGame';
+import {testGame} from '../../TestGame';
+import {CorridorsOfPower} from '../../../src/server/cards/prelude2/CorridorsOfPower';
+import {TestPlayer} from '../../TestPlayer';
+import {Delegate, Turmoil} from '../../../src/server/turmoil/Turmoil';
+import {PartyName} from '../../../src/common/turmoil/PartyName';
+import {Color, ColorWithNeutral} from '../../../src/common/Color';
+
+describe('CorridorsOfPower', () => {
+  let player: TestPlayer;
+  let game: IGame;
+  let turmoil: Turmoil;
+  let card: CorridorsOfPower;
+
+  beforeEach(() => {
+    [game, player] = testGame(2, {turmoilExtension: true});
+    turmoil = game.turmoil!;
+    card = new CorridorsOfPower();
+  });
+
+  it('play', () => {
+    card.play(player);
+    expect(player.getTerraformRating()).eq(21);
+    expect(player.megaCredits).eq(4);
+  });
+
+  function getDelegate(color: ColorWithNeutral | undefined): Delegate | undefined {
+    if (color === undefined) {
+      return undefined;
+    }
+    return game.getPlayers().find((player) => player.color === color) ?? 'NEUTRAL';
+  }
+
+  const effectRuns = [
+    {playedCard: false, delegates: [], initialLeader: undefined, newDelegate: Color.BLUE, newLeader: Color.BLUE, expected: 0},
+    {playedCard: true, delegates: [], initialLeader: undefined, newDelegate: Color.BLUE, newLeader: Color.BLUE, expected: 4},
+    {playedCard: true, delegates: [], initialLeader: undefined, newDelegate: Color.RED, newLeader: Color.RED, expected: 0},
+    {playedCard: true, delegates: [], initialLeader: undefined, newDelegate: 'NEUTRAL', newLeader: 'NEUTRAL', expected: 0},
+    {playedCard: true, delegates: [Color.BLUE], initialLeader: Color.BLUE, newDelegate: Color.BLUE, newLeader: Color.BLUE, expected: 0},
+    {playedCard: true, delegates: [Color.RED, Color.BLUE], initialLeader: Color.RED, newDelegate: Color.BLUE, newLeader: Color.BLUE, expected: 4},
+  ] as const;
+  for (const run of effectRuns) {
+    it('effect ' + JSON.stringify(run), () => {
+      const unity = turmoil.getPartyByName(PartyName.UNITY);
+      unity.delegates.forEachMultiplicity((count, key) => turmoil.delegateReserve.add(key, count));
+      unity.delegates.clear();
+      unity.partyLeader = undefined;
+      run.delegates.forEach((delegate) => unity.sendDelegate(getDelegate(delegate)!, game));
+      expect(unity.partyLeader).eq(getDelegate(run.initialLeader));
+
+      if (!run.hasOwnProperty('playedCard') || run['playedCard'] !== false) {
+        player.playedCards.push(card);
+      }
+
+      unity.sendDelegate(getDelegate(run.newDelegate)!, game);
+      expect(unity.partyLeader).eq(getDelegate(run.newLeader));
+
+      expect(player.megaCredits).eq(run.expected);
+    });
+  }
+});

10d157f41a834777cd4dd460dc31cc69263f5dbc
Robert Konigsberg <kberg@users.noreply.github.com>
2024-04-24 12:04:45 -0400

    Update IVictoryPointsBreakdown.ts

--- src/common/game/IVictoryPointsBreakdown.ts
+++ src/common/game/IVictoryPointsBreakdown.ts
@@ -1,5 +1,7 @@
 import {Tag} from '../cards/Tag';
 
+type MADetail = {message: string, messageArgs?: Array<string>, victoryPoint: number};
+
 export interface IVictoryPointsBreakdown {
   terraformRating: number;
   milestones: number;
@@ -14,7 +16,7 @@ export interface IVictoryPointsBreakdown {
   victoryPoints: number;
   total: number;
   detailsCards: Array<{cardName: string, victoryPoint: number}>;
-  detailsMilestones: Array<{message: string, messageArgs?: Array<string>, victoryPoint: number}>;
-  detailsAwards: Array<{message: string, messageArgs?: Array<string>, victoryPoint: number}>;
+  detailsMilestones: Array<MADetail>;
+  detailsAwards: Array<MADetail>;
   detailsPlanetaryTracks: Array<{tag: Tag, points: number}>;
 }

e1b648d861e7f56b44956bf8dbe2ccc108415ea2
Robert Konigsberg <konigsberg@gmail.com>
2024-05-02 16:08:56 -0400

    Add initial action test for Poseidon

--- tests/cards/colonies/Poseidon.spec.ts
+++ tests/cards/colonies/Poseidon.spec.ts
@@ -1,23 +1,38 @@
 import {expect} from 'chai';
 import {Poseidon} from '../../../src/server/cards/colonies/Poseidon';
 import {Ceres} from '../../../src/server/colonies/Ceres';
-import {Game} from '../../../src/server/Game';
-import {TestPlayer} from '../../TestPlayer';
-
-// TODO(kberg): add a test for Posideon's initial action.
+import {testGame} from '../../TestGame';
+import {cast, runAllActions} from '../../TestingUtils';
+import {SelectColony} from '../../../src/server/inputs/SelectColony';
 
 describe('Poseidon', function() {
   it('Should play', function() {
     const card = new Poseidon();
-    const player = TestPlayer.BLUE.newPlayer();
-    const player2 = TestPlayer.RED.newPlayer();
-    Game.newInstance('gameid', [player, player2], player);
-    const play = card.play(player);
-    expect(play).is.undefined;
+    const [/* game */, player/* , player2 */] = testGame(2);
+
+    cast(card.play(player), undefined);
+
     player.setCorporationForTest(card);
     const ceres = new Ceres();
     ceres.addColony(player);
+
     expect(player.production.megacredits).to.eq(1);
     expect(player.production.steel).to.eq(1);
+  });
+
+  it('initial action', () => {
+    const card = new Poseidon();
+    const [game, player/* , player2 */] = testGame(2, {coloniesExtension: true});
+
+    player.deferInitialAction(card);
+    runAllActions(game);
+
+    const selectColony = cast(player.popWaitingFor(), SelectColony);
+    const colony = selectColony.colonies[0];
+    expect(colony.colonies).is.empty;
+
+    selectColony.cb(colony);
+
+    expect(colony.colonies).deep.eq([player.id]);
   });
 });

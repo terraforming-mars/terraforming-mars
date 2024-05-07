e4fd8927645e5e338b4677e01466021946058bd1
Robert Konigsberg <konigsberg@gmail.com>
2024-05-06 23:01:04 -0400

    Fix Poseidon regression

--- tests/cards/colonies/Poseidon.spec.ts
+++ tests/cards/colonies/Poseidon.spec.ts
@@ -4,11 +4,12 @@ import {Ceres} from '../../../src/server/colonies/Ceres';
 import {testGame} from '../../TestGame';
 import {cast, runAllActions} from '../../TestingUtils';
 import {SelectColony} from '../../../src/server/inputs/SelectColony';
+import {Units} from '../../../src/common/Units';
 
 describe('Poseidon', function() {
   it('Should play', function() {
     const card = new Poseidon();
-    const [/* game */, player/* , player2 */] = testGame(2);
+    const [/* game */, player, player2] = testGame(2);
 
     cast(card.play(player), undefined);
 
@@ -16,8 +17,13 @@ describe('Poseidon', function() {
     const ceres = new Ceres();
     ceres.addColony(player);
 
-    expect(player.production.megacredits).to.eq(1);
-    expect(player.production.steel).to.eq(1);
+    expect(player.production.asUnits()).deep.eq(Units.of({megacredits: 1, steel: 1}));
+    expect(player2.production.asUnits()).deep.eq(Units.of({}));
+
+    ceres.addColony(player2);
+
+    expect(player.production.asUnits()).deep.eq(Units.of({megacredits: 2, steel: 1}));
+    expect(player2.production.asUnits()).deep.eq(Units.of({steel: 1}));
   });
 
   it('initial action', () => {

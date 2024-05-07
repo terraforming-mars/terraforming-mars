c7bf3d44c817a1a499dcbc303b41c3f931f19396
Robert Konigsberg <konigsberg@gmail.com>
2024-05-07 10:45:35 -0400

    Turn UnderworldTestHelper from a class to a set of functions

--- tests/underworld/UnderworldTestHelper.ts
+++ tests/underworld/UnderworldTestHelper.ts
@@ -8,49 +8,47 @@ import {UnderworldExpansion} from '../../src/server/underworld/UnderworldExpansi
 import {oneWayDifference} from '../../src/common/utils/utils';
 import {Space} from '../../src/server/boards/Space';
 
-export class UnderworldTestHelper {
-  public static assertIsExcavationAction(player: TestPlayer, input: PlayerInput | undefined, ignorePlacementRestrictions: boolean = false, _space: Space | undefined = undefined) {
-    const selectSpace = cast(input, SelectSpace);
-    const candidateSpaces = selectSpace.spaces;
+export function assertIsExcavationAction(player: TestPlayer, input: PlayerInput | undefined, ignorePlacementRestrictions: boolean = false, _space: Space | undefined = undefined) {
+  const selectSpace = cast(input, SelectSpace);
+  const candidateSpaces = selectSpace.spaces;
 
-    if (ignorePlacementRestrictions) {
-      const strictlyExcavatableSpaces = UnderworldExpansion.excavatableSpaces(player, false, true);
-      expect(oneWayDifference(candidateSpaces, strictlyExcavatableSpaces)).is.not.empty;
-    }
-
-    if (_space !== undefined) {
-      expect(selectSpace.spaces).includes(_space);
-    }
-    const space = _space ?? selectSpace.spaces[0];
-
-    expect(space.excavator).is.undefined;
-
-    const plants = player.plants;
-    space.undergroundResources = 'plant1';
-    const pi = selectSpace.cb(space);
-
-    expect(space.excavator).eq(player);
-    expect(player.plants - plants).eq(1);
-    return pi;
+  if (ignorePlacementRestrictions) {
+    const strictlyExcavatableSpaces = UnderworldExpansion.excavatableSpaces(player, false, true);
+    expect(oneWayDifference(candidateSpaces, strictlyExcavatableSpaces)).is.not.empty;
   }
 
-  public static assertIsIdentificationAction(player: TestPlayer, input: PlayerInput | undefined) {
-    const selectSpace = cast(input, SelectSpace);
-    const space = selectSpace.spaces[0];
-
-    expect(space.undergroundResources).is.undefined;
-
-    player.defer(selectSpace.cb(space));
-
-    expect(space.undergroundResources).is.not.undefined;
+  if (_space !== undefined) {
+    expect(selectSpace.spaces).includes(_space);
   }
+  const space = _space ?? selectSpace.spaces[0];
 
-  public static assertIsMaybeBlock(_player :TestPlayer, input: PlayerInput | undefined, choice: 'fighters' | 'corruption' | 'do not block') {
-    const orOptions = cast(input, OrOptions);
+  expect(space.excavator).is.undefined;
 
-    expect(formatMessage(orOptions.title)).contains('Spend 1 corruption to block an attack by');
+  const plants = player.plants;
+  space.undergroundResources = 'plant1';
+  const pi = selectSpace.cb(space);
 
-    const option = orOptions.options.find((o) => formatMessage(o.title).toLowerCase().indexOf(choice) > -1);
+  expect(space.excavator).eq(player);
+  expect(player.plants - plants).eq(1);
+  return pi;
+}
+
+export function assertIsIdentificationAction(player: TestPlayer, input: PlayerInput | undefined) {
+  const selectSpace = cast(input, SelectSpace);
+  const space = selectSpace.spaces[0];
+
+  expect(space.undergroundResources).is.undefined;
+
+  player.defer(selectSpace.cb(space));
+
+  expect(space.undergroundResources).is.not.undefined;
+}
+
+export function assertIsMaybeBlock(_player :TestPlayer, input: PlayerInput | undefined, choice: 'fighters' | 'corruption' | 'do not block') {
+  const orOptions = cast(input, OrOptions);
+
+  expect(formatMessage(orOptions.title)).contains('Spend 1 corruption to block an attack by');
+
+  const option = orOptions.options.find((o) => formatMessage(o.title).toLowerCase().indexOf(choice) > -1);
     option!.cb();
-  }
 }

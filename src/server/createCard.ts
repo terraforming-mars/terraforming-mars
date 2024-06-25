7f60f451cc5d73c3bd909cff25362833fa2fe139
Robert Konigsberg <konigsberg@gmail.com>
2024-06-24 20:13:35 -0400

    Remove TODO

--- src/server/createCard.ts
+++ src/server/createCard.ts
@@ -12,9 +12,6 @@ const CARD_RENAMES = new Map<string, CardName>([
   // And remember to add a test in spec.ts.
 
   // TODO(yournamehere): remove after 2021-04-05
-  // TODO(konigsberg): remove after 2024-06-15
-  ['New Colony Planning Initiaitives', CardName.NEW_COLONY_PLANNING_INITIAITIVES],
-  ['Sinus Irdium Road Network', CardName.SINUS_IRDIUM_ROAD_NETWORK],
 ]);
 
 function _createCard<T extends ICard>(cardName: CardName, cardManifestNames: Array<keyof ModuleManifest>): T | undefined {

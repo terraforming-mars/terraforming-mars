bccc3fe109fd1059665fcc3116edf13cd24c695d
Robert Konigsberg <konigsberg@gmail.com>
2024-06-28 13:37:49 -0400

    Combine code for Lowell and Co Leadership, test unplayable CEO behavior.

--- src/server/cards/ceos/Lowell.ts
+++ src/server/cards/ceos/Lowell.ts
@@ -4,9 +4,10 @@ import {PlayerInput} from '../../PlayerInput';
 import {CardRenderer} from '../render/CardRenderer';
 import {CeoCard} from './CeoCard';
 import {Tag} from '../../../common/cards/Tag';
-import {SelectCard} from '../../inputs/SelectCard';
 import {SelectPaymentDeferred} from '../../deferredActions/SelectPaymentDeferred';
 import {TITLES} from '../../inputs/titles';
+import {DrawCeoCardFromDeck} from '../../deferredActions/DrawCeoCardFromDeck';
+import {inplaceRemove} from '../../../common/utils/utils';
 
 export class Lowell extends CeoCard {
   constructor() {
@@ -34,36 +35,22 @@ export class Lowell extends CeoCard {
     return player.canAfford(8);
   }
 
-
   public action(player: IPlayer): PlayerInput | undefined {
     this.isDisabled = true;
     const game = player.game;
-    let ceosDrawn = game.ceoDeck.drawN(game, 3);
 
-    // TODO(d-little): This is not being tested, but currently every CEO is always playable
-    ceosDrawn = ceosDrawn.filter((ceo) => {
-      if (ceo.canPlay?.(player) === false) {
-        game.ceoDeck.discard(ceo);
-        game.log('${0} was discarded as ${1} could not play it.', (b) => b.card(ceo).player(player), {reservedFor: player});
-        return false;
-      }
-      return true;
-    });
-
-    player.game.defer(new SelectPaymentDeferred(player, 8, {title: TITLES.payForCardAction(this.name)}));
-
-    return new SelectCard('Choose CEO card to play', 'Play', ceosDrawn)
-      .andThen(([chosenCeo]) => {
-      // Discard unchosen CEOs
-        ceosDrawn.filter((c) => c !== chosenCeo).forEach((c) => game.ceoDeck.discard(c));
-        // Remove Lowell from played cards
-        const lowellIndex = player.playedCards.findIndex((c) => c.name === this.name);
-        player.playedCards.splice(lowellIndex, 1);
-        // Add Lowell to Discard pile
-        game.ceoDeck.discard(this);
-        // Play the new CEO
-        player.playCard(chosenCeo);
-        return undefined;
+    game.defer(new SelectPaymentDeferred(player, 8, {title: TITLES.payForCardAction(this.name)}))
+      .andThen(() => {
+        player.game.defer(new DrawCeoCardFromDeck(player, 3)).andThen((newCeo) => {
+          if (newCeo !== undefined) {
+            // Move Lowell to the discard pile
+            inplaceRemove(player.playedCards, this);
+            game.ceoDeck.discard(this);
+            // Play the new CEO
+            player.playCard(newCeo);
+          }
+        });
       });
+    return undefined;
   }
 }

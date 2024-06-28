1aa0d2e5e39461aade30f61f78b63a064475b682
Robert Konigsberg <konigsberg@gmail.com>
2024-06-28 11:09:22 -0400

    clean up todos

--- src/server/cards/StandardProjectCard.ts
+++ src/server/cards/StandardProjectCard.ts
@@ -75,7 +75,6 @@ export abstract class StandardProjectCard extends Card implements IStandardProje
     return player.canAfford(this.canPlayOptions(player));
   }
 
-  // TODO(kberg): Replace with abstract method.
   public canPayWith(_player: IPlayer): StandardProjectCanPayWith {
     return {};
   }

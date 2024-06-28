03943670c3d15edb1613baa75d67b16069f60b22
Robert Konigsberg <konigsberg@gmail.com>
2024-06-27 22:05:01 -0400

    Clean up Self Sufficient Settlement test

--- tests/cards/prelude/SelfSufficientSettlement.spec.ts
+++ tests/cards/prelude/SelfSufficientSettlement.spec.ts
@@ -3,8 +3,7 @@ import {testGame} from '../../TestGame';
 import {SelfSufficientSettlement} from '../../../src/server/cards/prelude/SelfSufficientSettlement';
 import {Units} from '../../../src/common/Units';
 import {cast, runAllActions} from '../../TestingUtils';
-import {SelectSpace} from '../../../src/server/inputs/SelectSpace';
-import {TileType} from '../../../src/common/TileType';
+import {assertPlaceCity} from '../../assertions';
 
 describe('SelfSufficientSettlement', function() {
   it('Should play', function() {
@@ -18,15 +17,6 @@ describe('SelfSufficientSettlement', function() {
     cast(action, undefined);
 
     expect(player.production.asUnits()).deep.eq(Units.of({megacredits: 2}));
-    const selectSpace = cast(player.popWaitingFor(), SelectSpace);
-    const space = selectSpace.spaces[0];
-
-    expect(space.player).is.undefined;
-    expect(space.tile).is.undefined;
-
-    selectSpace.cb(space);
-
-    expect(space.player).eq(player);
-    expect(space.tile?.tileType).eq(TileType.CITY);
+    assertPlaceCity(player, player.popWaitingFor());
   });
 });

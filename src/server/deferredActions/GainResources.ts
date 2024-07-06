89aee6433718e16045781c94ac714c7b7065076d
Robert Konigsberg <konigsberg@gmail.com>
2024-07-06 11:04:59 -0400

    Clarify when players get money from Splice

--- src/server/deferredActions/GainResources.ts
+++ src/server/deferredActions/GainResources.ts
@@ -1,4 +1,4 @@
-import {IPlayer} from '../IPlayer';
+import {IPlayer, ResourceSource} from '../IPlayer';
 import {Resource} from '../../common/Resource';
 import {DeferredAction} from './DeferredAction';
 import {Priority} from './Priority';
@@ -6,6 +6,7 @@ import {Priority} from './Priority';
 export type Options = {
   count?: number;
   log?: boolean;
+  from?: ResourceSource;
 }
 
 export class GainResources extends DeferredAction {
@@ -24,7 +25,7 @@ export class GainResources extends DeferredAction {
     if (this.options.count === 0) {
       return undefined;
     }
-    this.player.stock.add(this.resource, this.options.count ?? 1, {log: this.options.log});
+    this.player.stock.add(this.resource, this.options.count ?? 1, {log: this.options.log, from: this.options.from});
     this.cb(undefined);
     return undefined;
   }

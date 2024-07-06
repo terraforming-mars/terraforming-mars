ef3c2400771e893463c5c7c74be22f36404cb8c5
Robert Konigsberg <konigsberg@gmail.com>
2024-07-06 00:11:36 -0400

    Add the capability of more logging during manual testing

---
+++ tests/utils/log-the-log.ts
@@ -0,0 +1,17 @@
+import {setGameLog} from '../../src/server/Game';
+import {LogMessage} from '../../src/common/logs/LogMessage';
+import {formatLogMessage} from '../TestingUtils';
+
+
+class LoggingArray extends Array<LogMessage> {
+  override push(...items: Array<LogMessage>): number {
+    for (const item of items) {
+      console.warn(formatLogMessage(item));
+    }
+    return super.push(...items);
+  }
+}
+
+setGameLog(() => {
+  return new LoggingArray();
+});

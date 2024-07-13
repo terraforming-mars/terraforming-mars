b7945fc7b6304bf57bf83a8a31143502fb6b55d2
maserion <maserion@gmail.com>
2024-07-13 14:52:19 +0000

    [Prelude 2]: Envoys from Venus [Prelude 2]: Colonial Representation [Prelude 2]: Jovian Envoys [Prelude 2]: Microgravity Nutrition [Prelude 2]: Soil Studies

--- src/server/cards/prelude2/Prelude2CardManifest.ts
+++ src/server/cards/prelude2/Prelude2CardManifest.ts
@@ -5,19 +5,24 @@ import {AtmosphericEnhancers} from './AtmosphericEnhancers';
 import {BoardOfDirectors} from './BoardOfDirectors';
 import {CeresTechMarket} from './CeresTechMarket';
 import {CloudTourism} from './CloudTourism';
+import {ColonialRepresentation} from './ColonialRepresentation';
 import {ColonyTradeHub} from './ColonyTradeHub';
 import {CorridorsOfPower} from './CorridorsOfPower';
 import {Ecotec} from './Ecotec';
+import {EnvoysFromVenus} from './EnvoysFromVenus';
 import {FocusedOrganization} from './FocusedOrganization';
 import {GhgShipment} from './GhgShipment';
 import {IshtarExpedition} from './IshtarExpedition';
+import {JovianEnvoys} from './JovianEnvoys';
 import {L1TradeTerminal} from './L1TradeTerminal';
+import {MicrogravityNutrition} from './MicrogravityNutrition';
 import {NirgalEnterprises} from './NirgalEnterprises';
 import {NobelPrize} from './NobelPrize';
 import {OldMiningColony} from './OldMiningColony';
 import {PalladinShipping} from './PalladinShipping';
 import {RedAppeasement} from './RedAppeasement';
 import {SagittaFrontierServices} from './SagittaFrontierServices';
+import {SoilStudies} from './SoilStudies';
 import {SpaceLanes} from './SpaceLanes';
 import {Spire} from './Spire';
 import {SponsoringNation} from './SponsoringNation';
@@ -34,15 +39,15 @@ export const PRELUDE2_CARD_MANIFEST = new ModuleManifest({
     [CardName.CLOUD_TOURISM]: {Factory: CloudTourism, compatibility: 'venus'},
     [CardName.GHG_SHIPMENT]: {Factory: GhgShipment, compatibility: 'turmoil'},
     [CardName.SPONSORING_NATION]: {Factory: SponsoringNation, compatibility: 'turmoil'},
-    // [CardName.COLONIAL_REPRESENTATION]: {Factory: ColonialRepresentation, compatibility: 'turmoil'},
-    // [CardName.ENVOYS_FROM_VENUS]: {Factory: EnvoysFromVenus, compatibility: 'turmoil'},
+    [CardName.COLONIAL_REPRESENTATION]: {Factory: ColonialRepresentation, compatibility: 'turmoil'},
+    [CardName.ENVOYS_FROM_VENUS]: {Factory: EnvoysFromVenus, compatibility: ['turmoil', 'venus']},
     // [CardName.COLONIAL_ENVOYS]: {Factory: ColonialEnvoys, compatibility: ['colonies', 'turmoil']},
     // [CardName.SPECIAL_PERMIT]: {Factory: SpecialPermit, compatibility: 'turmoil'},
     // [CardName.FLOATING_REFINERY]: {Factory: FloatingRefinery, compatibility: 'venus'},
     // [CardName.FRONTIER_TOWN]: {Factory: FrontierTown, compatibility: 'turmoil'},
-    // [CardName.JOVIAN_ENVOYS]: {Factory: JovianEnvoys, compatibility: 'turmoil'},
-    // [CardName.MICROGRAVITY_NUTRITION]: {Factory: MicrogravityNutrition, compatibility: 'colonies'},
-    // [CardName.SOIL_STUDIES]: {Factory: SoilStudies, compatibility: ['venus', 'colonies']},
+    [CardName.JOVIAN_ENVOYS]: {Factory: JovianEnvoys, compatibility: 'turmoil'},
+    [CardName.MICROGRAVITY_NUTRITION]: {Factory: MicrogravityNutrition, compatibility: 'colonies'},
+    [CardName.SOIL_STUDIES]: {Factory: SoilStudies, compatibility: ['venus', 'colonies']},
     // [CardName.STRATOSPHERIC_EXPEDITION]: {Factory: StratosphericExpedition, compatibility: 'venus'},
     // [CardName.UNEXPECTED_APPLICATION]: {Factory: UnexpectedApplication, compatibility: 'venus'},
     // [CardName.VENUS_ALLIES]: {Factory: VenusAllies, compatibility: ['venus', 'colonies']},
---
+++ tests/cards/prelude2/MicrogravityNutrition.spec.ts
@@ -0,0 +1,22 @@
+import {MicrogravityNutrition} from '../../../src/server/cards/prelude2/MicrogravityNutrition';
+import {Luna} from '../../../src/server/colonies/Luna';
+import {IGame} from '../../../src/server/IGame';
+import {TestPlayer} from '../../TestPlayer';
+import {testGame} from '../../TestingUtils';
+
+describe('MicrogravityNutrition', function() {
+  let card: MicrogravityNutrition;
+  let player: TestPlayer;
+  let game: IGame;
+  let colony1: Luna;
+
+  beforeEach(function() {
+    card = new MicrogravityNutrition();
+    [game, player/* , player2 */] = testGame(2, {coloniesExtension: true});
+
+    colony1 = new Luna();
+    colony1.colonies.push(player.id);
+    player.game.colonies.push(colony1);
+  });
+
+});

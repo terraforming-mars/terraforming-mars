176abda8751ab1f44633e67fe65a488eea10daec
Robert Konigsberg <konigsberg@gmail.com>
2024-07-14 11:43:57 -0400

    Add exporting the log to game extract

--- src/server/tools/dump_log.ts
+++ src/server/tools/dump_log.ts
@@ -2,10 +2,10 @@
 // e.g. node build/src/server/tools/dump_log.js g4940001dbc
 require('dotenv').config();
 
-import {Log} from '../../common/logs/Log';
 import {isGameId} from '../../common/Types';
 import {Database} from '../database/Database';
 import {IDatabase} from '../database/IDatabase';
+import {exportLogs} from './exportLogs';
 
 const args = process.argv.slice(2);
 const [gameId] = args;
@@ -22,16 +22,9 @@ async function main() {
   if (!isGameId(gameId)) {
     throw new Error('game ids start with \'g\'');
   }
-  const saveIds = await db.getSaveIds(gameId);
-  let lastIdx = 0;
-  for (const saveId of saveIds) {
-    const {gameLog} = await db.getGameVersion(gameId, saveId);
-    for (let idx = lastIdx; idx < gameLog.length; idx++) {
-      const logEntry = gameLog[idx];
-      const text = Log.applyData(logEntry, (datum) => datum.value.toString());
-      console.log(`[${saveId}/${idx}]: ${text}`);
-    }
-    lastIdx = gameLog.length;
+  const logs = await exportLogs(db, gameId);
+  for (const entry of logs) {
+    console.log(entry);
   }
 }
 

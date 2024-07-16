19623fd9e690dba1e0735c607e06f7eb77e42686
Robert Konigsberg <konigsberg@gmail.com>
2024-07-15 23:21:23 -0400

    Update SoilStudies

--- tests/cards/prelude2/SoilStudies.spec.ts
+++ tests/cards/prelude2/SoilStudies.spec.ts
@@ -5,6 +5,7 @@ import {IGame} from '../../../src/server/IGame';
 import {setTemperature} from '../../TestingUtils';
 import {Luna} from '../../../src/server/colonies/Luna';
 import {testGame} from '../../TestGame';
+import {range} from '../../../src/common/utils/utils';
 
 describe('SoilStudies', function() {
   let card: SoilStudies;
@@ -21,13 +22,28 @@ describe('SoilStudies', function() {
     expect(card.canPlay(player)).is.not.true;
   });
 
-  it('Play', function() {
-    player.tagsForTest = {venus: 4, plant: 2, microbe: 2, wild: 1};
-    const colony1 = new Luna();
-    colony1.colonies.push(player.id);
-    game.colonies.push(colony1);
+  const playRuns = [
+    {tags: undefined, colonies: 0, expected: 1},
+    {tags: {microbe: 1}, colonies: 0, expected: 1},
+    {tags: {venus: 1}, colonies: 0, expected: 2},
+    {tags: {wild: 1}, colonies: 0, expected: 2},
+    {tags: {plant: 1}, colonies: 0, expected: 2},
+    {tags: undefined, colonies: 1, expected: 2},
+    {tags: undefined, colonies: 2, expected: 3},
+    {tags: {venus: 4, plant: 2, microbe: 2, wild: 1}, colonies: 1, expected: 9},
+  ] as const;
+  for (const run of playRuns) {
+    it('Play ' + JSON.stringify(run), function() {
+      player.tagsForTest = run.tags;
 
-    card.play(player);
-    expect(player.plants).eq(9);
-  });
+      const colony = new Luna();
+      game.colonies.push(colony);
+      for (const _ of range(run.colonies)) {
+        colony.colonies.push(player.id);
+      }
+
+      card.play(player);
+      expect(player.plants).eq(run.expected);
+    });
+  }
 });

b1e048067fc3c10113434617bd9c860f8ef3b7b5
Robert Konigsberg <kberg@users.noreply.github.com>
2024-07-22 21:09:21 -0400

    Update EarlyColonization.spec.ts

--- tests/cards/prelude2/EarlyColonization.spec.ts
+++ tests/cards/prelude2/EarlyColonization.spec.ts
@@ -1,32 +1,27 @@
-import {TestPlayer} from '../../TestPlayer';
-import {EarlyColonization} from '../../../src/server/cards/prelude2/EarlyColonization';
 import {expect} from 'chai';
 import {testGame} from '../../TestGame';
+import {TestPlayer} from '../../TestPlayer';
+import {EarlyColonization} from '../../../src/server/cards/prelude2/EarlyColonization';
+import {assertBuildColony} from '../../colonies/coloniesAssertions';
 
 describe('EarlyColonization', () => {
   let player: TestPlayer;
   let card: EarlyColonization;
 
   beforeEach(() => {
-    [/* game */, player] = testGame(1, {
-      coloniesExtension: true,
-    });
+    [/* game */, player] = testGame(1, {coloniesExtension: true});
     card = new EarlyColonization();
   });
 
   it('play', () => {
     player.game.colonies[2].colonies.push(player.id);
 
-    expect(player.game.colonies[0].trackPosition).eq(1);
-    expect(player.game.colonies[1].trackPosition).eq(1);
-    expect(player.game.colonies[2].trackPosition).eq(1);
-    expect(player.game.colonies[3].trackPosition).eq(1);
+    expect(player.game.colonies.map((c) => c.trackPosition).deep.eq([1, 1, 1, 1]);
 
     card.play(player);
 
-    expect(player.game.colonies[0].trackPosition).eq(3);
-    expect(player.game.colonies[1].trackPosition).eq(3);
-    expect(player.game.colonies[2].trackPosition).eq(3);
-    expect(player.game.colonies[3].trackPosition).eq(3);
+    expect(player.game.colonies.map((c) => c.trackPosition).deep.eq([3, 3, 3, 3]);
+    expect(player.energy).eq(3);
+    assertBuildColony(player, game.deferredActions.pop()!.execute());
   });
 });

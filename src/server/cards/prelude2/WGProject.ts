120d762ea7324bce5c45a001ee666fa22aa091e8
maserion <maserion@gmail.com>
2024-08-01 16:21:57 +0000

    [Prelude 2] WG Project

---
+++ src/server/cards/prelude2/WGProject.ts
@@ -0,0 +1,34 @@
+import {IProjectCard} from '../IProjectCard';
+import {CardName} from '../../../common/cards/CardName';
+import {CardType} from '../../../common/cards/CardType';
+import {Card} from '../Card';
+import {Tag} from '../../../common/cards/Tag';
+import {CardRenderer} from '../render/CardRenderer';
+import {PreludesExpansion} from '../../preludes/PreludesExpansion';
+import {IPlayer} from '../../IPlayer';
+
+
+export class WGProject extends Card implements IProjectCard {
+  constructor() {
+    super({
+      cost: 9,
+      tags: [Tag.EARTH],
+      name: CardName.WG_PROJECT,
+      type: CardType.AUTOMATED,
+
+      requirements: {chairman: true},
+
+      metadata: {
+        description: 'Requires that you are Chairman. DRAW 3 PRELUDE CARD AND PLAY 1 OF THEM, Discard the other 2.',
+        cardNumber: 'P91',
+        renderData: CardRenderer.builder((b) => b.prelude().asterix()),
+      },
+    });
+  }
+
+  public override bespokePlay(player: IPlayer) {
+    const game = player.game;
+    const cards = game.preludeDeck.drawN(game, 3);
+    return PreludesExpansion.playPrelude(player, cards);
+  }
+}

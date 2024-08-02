2005fd31d2a47efa8f488f734639621588929cc5
Robert Konigsberg <konigsberg@gmail.com>
2024-08-02 11:42:17 -0400

    Make prelude draw actions discard their draws. And don't let them take actions if there aren't enough preludes.

--- src/server/preludes/PreludesExpansion.ts
+++ src/server/preludes/PreludesExpansion.ts
@@ -18,9 +18,14 @@ export class PreludesExpansion {
     });
   }
 
-  public static playPrelude(
+  /**
+   * Return a `SelectCard` that asks a `player` to choose one of the `cards` to play,
+   * and then plays it.
+   */
+  public static selectPreludeToPlay(
     player: IPlayer,
     cards: Array<IPreludeCard>,
+    remainders: undefined | 'discard' = undefined,
     cardAction: CardAction = 'add'): SelectCard<IPreludeCard> {
     // This preps the warning attribute in preludes.
     // All preludes can be presented. Unplayable ones just fizzle.
@@ -38,6 +43,13 @@ export class PreludesExpansion {
           PreludesExpansion.fizzle(player, card);
         } else {
           player.playCard(card, undefined, cardAction);
+        }
+        if (remainders === 'discard') {
+          for (const c of cards) {
+            if (c !== card) {
+              player.game.preludeDeck.discard(c);
+            }
+          }
         }
         return undefined;
       });

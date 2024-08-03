3ba77491e17f3dfb7ec70ad1104fadfde1b8510e
maserion <maserion@gmail.com>
2024-08-04 01:51:16 +0200

    Update VenusShuttles.ts

--- src/server/cards/prelude2/VenusShuttles.ts
+++ src/server/cards/prelude2/VenusShuttles.ts
@@ -31,7 +31,7 @@ export class VenusShuttles extends Card implements IActionCard {
         description: 'Add 2 floaters to ANY VENUS CARD.',
         renderData: CardRenderer.builder((b) => {
           b.action('Spend 12 MC to raide Venus 1 step. This cost is REDUCED BY 1 FOR EACH VENUS TAG you have.', (eb) =>
-            eb.megacredits(12).text('(').megacredits(-1).slash().tag(Tag.VENUS).text(')').startAction.venus(1)).BR;
+            eb.megacredits(12).text('(').megacredits(-1).slash().tag(Tag.VENUS).text(')').startAction.venus(1)).br;
           b.venus(1).resource(CardResource.FLOATER, {amount: 2, secondaryTag: Tag.VENUS});
         }),
       },

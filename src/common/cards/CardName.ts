2f5676a6ffd82c93be2bb4326740d18d8af1df98
maserion <maserion@gmail.com>
2024-09-01 16:19:12 +0000

    Promo Card - Hospitals

--- src/common/cards/CardName.ts
+++ src/common/cards/CardName.ts
@@ -564,6 +564,7 @@ export enum CardName {
   SOIL_ENRICHMENT = 'Soil Enrichment',
   CITY_PARK = 'City Park',
   SUPERMARKETS = 'Supermarkets',
+  HOSPITALS = 'Hospitals',
 
   // Promo from contest
   FLOYD_CONTINUUM = 'Floyd Continuum',

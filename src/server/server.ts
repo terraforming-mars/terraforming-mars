b225f64a39aea22eebf6c0521979a42703b2e992
Robert Konigsberg <konigsberg@gmail.com>
2024-09-24 21:48:07 -0400

    Use 3pl for ansi.

--- src/server/server.ts
+++ src/server/server.ts
@@ -10,6 +10,7 @@ import * as fs from 'fs';
 import * as raw_settings from '../genfiles/settings.json';
 import * as prometheus from 'prom-client';
 import * as responses from './server/responses';
+import * as ansi from 'ansi-escape-sequences';
 
 import {Database} from './database/Database';
 import {runId, serverId} from './utils/server-ids';
@@ -100,7 +101,7 @@ async function start() {
   server.listen(port);
 
   if (!process.env.SERVER_ID) {
-    console.log(`The secret serverId for this server is \x1b[1m${serverId}\x1b[0m.`);
+    console.log(`The secret serverId for this server is ${ansi.style.bold}${serverId}${ansi.style.reset}.`);
     console.log(`Administrative routes can be found at admin?serverId=${serverId}`);
   }
   console.log(`The public run ID is ${runId}`);

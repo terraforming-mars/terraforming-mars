98529c9deb87c0802ff5d6a3a41eb8873679e0d4
Robert Konigsberg <kberg@users.noreply.github.com>
2024-10-05 10:22:39 -0400

    Suitable Infrastructure should not reward on other players' turns. (#7009)

--- tests/cards/prelude2/SuitableInfrastructure.spec.ts
+++ tests/cards/prelude2/SuitableInfrastructure.spec.ts
@@ -5,6 +5,8 @@ import {IPlayer} from '../../../src/server/IPlayer';
 import {Resource} from '../../../src/common/Resource';
 import {PowerPlantStandardProject} from '../../../src/server/cards/base/standardProjects/PowerPlantStandardProject';
 import {cast, runAllActions} from '../../TestingUtils';
+import {SaturnSystems} from '../../../src/server/cards/corporation/SaturnSystems';
+import {JovianLanterns} from '../../../src/server/cards/colonies/JovianLanterns';
 
 function simulateFinishingAction(player: IPlayer) {
   player.actionsTakenThisGame++;
@@ -47,6 +49,25 @@ describe('SuitableInfrastructure', () => {
 
     simulateFinishingAction(player);
 
+    expect(player.stock.megacredits).eq(2);
+  });
+
+  it('Does not reward when other player has an action that rewards you.', () => {
+    const card = new SuitableInfrastructure();
+    const [game, player, player2] = testGame(2);
+
+    const saturnSystems = new SaturnSystems();
+    player.playedCards.push(card);
+    // Gain 1 MC prouduction when anybody plays a card with a jovian tag.
+    player.corporations.push(saturnSystems);
+    const jovianLanterns = new JovianLanterns();
+
+    game.activePlayer = player2.id;
+    saturnSystems.onCardPlayed(player2, jovianLanterns);
+    expect(player.stock.megacredits).eq(0);
+
+    game.activePlayer = player.id;
+    saturnSystems.onCardPlayed(player2, jovianLanterns);
     expect(player.stock.megacredits).eq(2);
   });
 });

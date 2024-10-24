f31c1a0943fad42954dbdcc760691114153ba064
Robert Konigsberg <kberg@users.noreply.github.com>
2024-10-24 19:21:06 -0400

    Make Double Down compatible with preludes that hold resources. (#7048)

--- src/common/cards/Warning.ts
+++ src/common/cards/Warning.ts
@@ -13,4 +13,5 @@ export type Warning =
  'underworldMustExcavateEnergy' |
  'deckTooSmall' |
  'cannotAffordBoardOfDirectors' |
- 'marsIsTerraformed';
+ 'marsIsTerraformed' |
+ 'ineffectiveDoubleDown';

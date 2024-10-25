66cff53d1d991aa4c6af5b1f4a8edb8cb6a3fd41
Robert Konigsberg <konigsberg@gmail.com>
2024-10-24 23:01:32 -0400

    Some ordering in Productive Outpost.

--- src/server/cards/colonies/ProductiveOutpost.ts
+++ src/server/cards/colonies/ProductiveOutpost.ts
@@ -5,6 +5,8 @@ import {CardName} from '../../../common/cards/CardName';
 import {CardRenderer} from '../render/CardRenderer';
 import {Card} from '../Card';
 import {Size} from '../../../common/cards/render/Size';
+import {IColony} from '../../colonies/IColony';
+import {ColonyName} from '../../../common/colonies/ColonyName';
 
 export class ProductiveOutpost extends Card implements IProjectCard {
   constructor() {
@@ -22,8 +24,26 @@ export class ProductiveOutpost extends Card implements IProjectCard {
     });
   }
 
+  // Order:
+  // Titania
+  // All colonies
+  // Leavitt
+  //
+  // TODO(kberg): Make it possible for Leavitt to resolve before Titania.
+
   public override bespokePlay(player: IPlayer) {
-    player.game.colonies.forEach((colony) => {
+    const value = (c: IColony): number => {
+      if (c.name === ColonyName.TITANIA) {
+        return 1;
+      }
+      if (c.name === ColonyName.LEAVITT) {
+        return -1;
+      }
+      return 0;
+    };
+    const sorted = [...player.game.colonies].sort((a, b) => value(b) - value(a));
+
+    sorted.forEach((colony) => {
       colony.colonies.filter((owner) => owner === player.id).forEach((owner) => {
         // Not using GiveColonyBonus deferred action because it's only for the active player
         player.defer(() => colony.giveColonyBonus(player.game.getPlayerById(owner)));

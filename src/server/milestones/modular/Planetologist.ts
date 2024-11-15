36f1c13ef6d193c1781ffe4518815d9460b464c0
Robert Konigsberg <konigsberg@gmail.com>
2024-11-15 10:34:13 -0500

    Update and simplify Planetologist

--- src/server/milestones/modular/Planetologist.ts
+++ src/server/milestones/modular/Planetologist.ts
@@ -12,61 +12,10 @@ export class Planetologist extends BaseMilestone {
   }
 
   public getScore(player: IPlayer): number {
-    let score = 0;
-    let wildTags = player.tags.count(Tag.WILD);
-
-    const earthTags = player.tags.count(Tag.EARTH) - wildTags;
-    const venusTags = player.tags.count(Tag.VENUS) - wildTags;
-    const jovianTags = player.tags.count(Tag.JOVIAN) - wildTags;
-    // I feel I should be ashamed for code below...
-    // Earth tags
-    if (earthTags >= 2) {
-      score += 2;
-    } else if (earthTags === 1 && wildTags > 0) {
-      score += 2;
-      wildTags--;
-    } else if (earthTags === 1) {
-      score += 1;
-    } else if (earthTags === 0 && wildTags >= 2) {
-      score += 2;
-      wildTags--;
-    } else if (earthTags === 0 && wildTags === 1) {
-      score += 1;
-      wildTags--;
-    }
-
-    // Venus tags
-    if (venusTags >= 2) {
-      score += 2;
-    } else if (venusTags === 1 && wildTags > 0) {
-      score += 2;
-      wildTags--;
-    } else if (venusTags === 1) {
-      score += 1;
-    } else if (venusTags === 0 && wildTags >= 2) {
-      score += 2;
-      wildTags--;
-    } else if (venusTags === 0 && wildTags === 1) {
-      score += 1;
-      wildTags--;
-    }
-
-    // Jovian tags
-    if (jovianTags >= 2) {
-      score += 2;
-    } else if (jovianTags === 1 && wildTags > 0) {
-      score += 2;
-      wildTags--;
-    } else if (jovianTags === 1) {
-      score += 1;
-    } else if (jovianTags === 0 && wildTags >= 2) {
-      score += 2;
-      wildTags--;
-    } else if (jovianTags === 0 && wildTags === 1) {
-      score += 1;
-      wildTags--;
-    }
-
-    return score;
+    const earthTags = Math.min(player.tags.count(Tag.EARTH, 'raw'), 2);
+    const venusTags = Math.min(player.tags.count(Tag.VENUS, 'raw'), 2);
+    const jovianTags = Math.min(player.tags.count(Tag.JOVIAN, 'raw'), 2);
+    const wildTags = player.tags.count(Tag.WILD);
+    return Math.min(earthTags + venusTags + jovianTags + wildTags, 6);
   }
 }
--- tests/milestones/modular/Planetologist.spec.ts
+++ tests/milestones/modular/Planetologist.spec.ts
@@ -3,43 +3,23 @@ import {Planetologist} from '../../../src/server/milestones/modular/Planetologis
 import {TestPlayer} from '../../TestPlayer';
 
 describe('Planetologist', () => {
-  it('Standard test, cannot play', () => {
-    const milestone = new Planetologist();
-    const player = TestPlayer.BLUE.newPlayer();
-    expect(milestone.canClaim(player)).is.not.true;
-
-    player.tagsForTest = {earth: 1, jovian: 2, venus: 2};
-    expect(milestone.canClaim(player)).is.not.true;
-
-    player.tagsForTest = {jovian: 2, venus: 2};
-    expect(milestone.canClaim(player)).is.not.true;
-  });
-
-  it('Standard test, can play', () => {
-    const milestone = new Planetologist();
-    const player = TestPlayer.BLUE.newPlayer();
-
-    player.tagsForTest = {earth: 2, jovian: 2, venus: 2};
-    expect(milestone.canClaim(player)).is.true;
-
-    player.tagsForTest = {earth: 7, jovian: 3, venus: 7};
-    expect(milestone.canClaim(player)).is.true;
-  });
-
-  it('wild tag test - can be used but only once', () => {
-    const milestone = new Planetologist();
-    const player = TestPlayer.BLUE.newPlayer();
-
-    player.tagsForTest = {earth: 1, jovian: 2, venus: 2, wild: 1};
-    expect(milestone.canClaim(player)).is.true;
-  });
-
-  it('wild tag test - cannot be used more than once', () => {
-    const milestone = new Planetologist();
-    const player = TestPlayer.BLUE.newPlayer();
-
-    // Player needs one more of each tag, but only has 1 wild tag
-    player.tagsForTest = {earth: 1, jovian: 1, venus: 1, wild: 1};
-    expect(milestone.canClaim(player)).is.not.true;
-  });
+  const canClaimRuns = [
+    {earth: 0, jovian: 0, venus: 0, wild: 0, expected: {score: 0, canClaim: false}},
+    {earth: 0, jovian: 2, venus: 2, wild: 0, expected: {score: 4, canClaim: false}},
+    {earth: 2, jovian: 2, venus: 2, wild: 0, expected: {score: 6, canClaim: true}},
+    {earth: 7, jovian: 3, venus: 7, wild: 0, expected: {score: 6, canClaim: true}},
+    {earth: 1, jovian: 2, venus: 2, wild: 0, expected: {score: 5, canClaim: false}},
+    {earth: 1, jovian: 2, venus: 2, wild: 1, expected: {score: 6, canClaim: true}},
+    {earth: 1, jovian: 1, venus: 1, wild: 1, expected: {score: 4, canClaim: false}},
+    {earth: 0, jovian: 0, venus: 0, wild: 6, expected: {score: 6, canClaim: true}},
+  ] as const;
+  for (const run of canClaimRuns) {
+    it('canClaim ' + JSON.stringify(run), () => {
+      const milestone = new Planetologist();
+      const player = TestPlayer.BLUE.newPlayer();
+      player.tagsForTest = {earth: run.earth, venus: run.venus, jovian: run.jovian, wild: run.wild};
+      expect(milestone.getScore(player)).eq(run.expected.score);
+      expect(milestone.canClaim(player)).eq(run.expected.canClaim);
+    });
+  }
 });

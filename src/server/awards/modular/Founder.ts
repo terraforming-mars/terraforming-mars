69921ad1e389bfac25bde60ff1660567c457d1ef
Robert Konigsberg <konigsberg@gmail.com>
2024-11-15 10:24:57 -0500

    Update and simplify Founder

--- src/server/awards/modular/Founder.ts
+++ src/server/awards/modular/Founder.ts
@@ -1,7 +1,6 @@
 import {IAward} from '../IAward';
 import {IPlayer} from '../../IPlayer';
-import {isSpecialTileSpace} from '../../boards/Board';
-import {Space} from '../../boards/Space';
+import {Board, isSpecialTileSpace} from '../../boards/Board';
 
 export class Founder implements IAward {
   public readonly name = 'Founder';
@@ -9,22 +8,13 @@ export class Founder implements IAward {
 
   public getScore(player: IPlayer): number {
     const board = player.game.board;
-    const playerOwnedSpaces = board.spaces.filter((space) =>
-      space.player === player && space.tile !== undefined);
-    const specialTiles = board.spaces.filter((space) =>
-      isSpecialTileSpace(space));
-    const adjacentToSpecialSpaces = new Set<Space>();
-    for (const specialSpace of specialTiles) {
-      const adjacentSpaces = board.getAdjacentSpaces(specialSpace);
-      for (const adjSpace of adjacentSpaces) {
-        if (adjSpace) {
-          adjacentToSpecialSpaces.add(adjSpace);
-        }
-      }
-    }
-    const countAdjacentToSpecial = playerOwnedSpaces.filter((space) =>
-      adjacentToSpecialSpaces.has(space),
-    ).length;
-    return countAdjacentToSpecial;
+    const specialTiles = board.spaces.filter(isSpecialTileSpace);
+    const adjacentToSpecialSpaces = new Set(specialTiles.map((s) => board.getAdjacentSpaces(s)).flat());
+
+    const count = board.spaces.filter(Board.ownedBy(player))
+      .filter((space) => space.tile !== undefined)
+      .filter((space) => adjacentToSpecialSpaces.has(space))
+      .length;
+    return count;
   }
 }

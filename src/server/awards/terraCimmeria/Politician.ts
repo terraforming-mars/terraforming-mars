ea1795cd054875e304980f43e678eb301938915d
Robert Konigsberg <konigsberg@gmail.com>
2024-11-30 12:32:33 -0500

    Add TODO

--- src/server/awards/terraCimmeria/Politician.ts
+++ src/server/awards/terraCimmeria/Politician.ts
@@ -1,6 +1,7 @@
 import {IPlayer} from '../../IPlayer';
 import {IAward} from '../IAward';
 
+// TODO(kberg): Rename this file
 export class TPolitician implements IAward {
   public readonly name = 'T. Politician';
   public readonly description = 'Place the most delegates';

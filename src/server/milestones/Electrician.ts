3331902b2586f466581172429cd7d231f2e4faed
Robert Konigsberg <konigsberg@gmail.com>
2024-11-30 14:07:01 -0500

    Add TODO

--- src/server/milestones/Electrician.ts
+++ src/server/milestones/Electrician.ts
@@ -2,6 +2,7 @@ import {BaseMilestone} from './IMilestone';
 import {IPlayer} from '../IPlayer';
 import {Tag} from '../../common/cards/Tag';
 
+// TODO(kberg): Rename file to v.electrician or something.
 export class Electrician extends BaseMilestone {
   constructor() {
     super(

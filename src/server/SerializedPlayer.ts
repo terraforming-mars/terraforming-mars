ea3e7087c3084f76632d189739721f6d1c114d10
Robert Konigsberg <konigsberg@gmail.com>
2024-12-20 16:02:35 -0800

    Remove TODO

--- src/server/SerializedPlayer.ts
+++ src/server/SerializedPlayer.ts
@@ -76,6 +76,5 @@ export interface SerializedPlayer extends DeprecatedFields{
   turmoilPolicyActionUsed: boolean;
   underworldData: UnderworldPlayerData;
   victoryPointsByGeneration: Array<number>;
-  // TODO(kberg): remove '?' by 2024-12-15
-  globalParameterSteps?: Record<GlobalParameter, number>;
+  globalParameterSteps: Record<GlobalParameter, number>;
 }

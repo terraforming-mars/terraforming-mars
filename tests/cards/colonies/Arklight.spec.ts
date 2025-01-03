2883d56be215f307a3def0a7879a81110869ee0d
Robert Konigsberg <konigsberg@gmail.com>
2025-01-03 14:31:36 -0500

    Make Arklight compatible with Merger

--- tests/cards/colonies/Arklight.spec.ts
+++ tests/cards/colonies/Arklight.spec.ts
@@ -1,19 +1,35 @@
 import {expect} from 'chai';
 import {Predators} from '../../../src/server/cards/base/Predators';
 import {Arklight} from '../../../src/server/cards/colonies/Arklight';
-import {runAllActions, testGame} from '../../TestingUtils';
+import {cast, runAllActions, testGame} from '../../TestingUtils';
+import {EcoLine} from '../../../src/server/cards/corporation/EcoLine';
 
-describe('Arklight', function() {
-  it('Should play', function() {
+describe('Arklight', () => {
+  it('Should play', () => {
     const card = new Arklight();
     const [game, player/* , player2 */] = testGame(2);
-    const play = card.play(player);
-    expect(play).is.undefined;
-    runAllActions(game);
-    expect(card.resourceCount).to.eq(1);
+    cast(card.play(player), undefined);
     player.corporations.push(card);
+    runAllActions(game);
+
+    expect(card.resourceCount).to.eq(1);
+
     card.onCardPlayed(player, new Predators());
+
     expect(card.resourceCount).to.eq(2);
     expect(card.getVictoryPoints(player)).to.eq(1);
+  });
+
+  it('Compatible with Merger', () => {
+    const card = new Arklight();
+    const [game, player/* , player2 */] = testGame(2);
+    player.corporations.push(card);
+    runAllActions(game);
+
+    card.resourceCount = 0;
+
+    player.playAdditionalCorporationCard(new EcoLine());
+
+    expect(card.resourceCount).to.eq(1);
   });
 });

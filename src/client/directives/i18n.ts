922cc37235fcdbcce92a5a2b6afbcfde2f9e0909
alrusdi <alrusdi@gmail.com>
2025-01-06 23:01:59 +0500

    I18n fixes (#7158)
    * Various i18n fixes
    
    * Remove debug code
    
    * Fix linter errors
    
    ---------
    
    Co-authored-by: alrusdi <aleksandr.sheverdyaev@osinit.com>

--- src/client/directives/i18n.ts
+++ src/client/directives/i18n.ts
@@ -111,23 +111,28 @@ function normalizeText(text: string): string {
   return text.replace(/[\n\r]/g, '').replace(/[ ]+/g, ' ');
 }
 
-function translateChildren(node: Node) {
+function translateChildren(node: Node, params: string[] | undefined) {
   for (let i = 0, length = node.childNodes.length; i < length; i++) {
     const child = node.childNodes[i];
     if (child.nodeType === Node.TEXT_NODE) {
       const text = child as Text;
-      const translatedText = translateText(text.data);
+      const translatedText = params ? translateTextWithParams(text.data, params) : translateText(text.data);
       if (translatedText !== text.data) {
         text.data = translatedText;
       }
     } else {
-      translateChildren(child);
+      if (child.nodeType === Node.ELEMENT_NODE && (child as HTMLElement).getAttribute('tm-has-i18n') === 'true') continue;
+      translateChildren(child, params);
     }
   }
 }
 
-export function translateTextNode(el: HTMLElement) {
-  translateChildren(el);
+export function translateTextNode(el: HTMLElement, binding: any) {
+  let params: string[] | undefined = undefined;
+  if (binding?.value) {
+    params = binding.value instanceof Array ? binding.value : [binding.value];
+  }
+  translateChildren(el, params);
 }
 
 export const $t = function(msg: string | Message | number | undefined) {

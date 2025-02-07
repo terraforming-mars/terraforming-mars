bf949dda5bf36e1093703bcb7f0545a349fe0a56
Robert Konigsberg <konigsberg@gmail.com>
2025-02-07 14:42:55 -0500

    Clean CardParty one more time

--- src/client/components/card/CardParty.vue
+++ src/client/components/card/CardParty.vue
@@ -24,14 +24,8 @@ export default Vue.extend({
   computed: {
     classes(): string {
       const p = this.party.toLowerCase().replace(' ', '-');
-      switch (this.size) {
-      case 'card':
-        // TODO(kberg): remove card-description if it turns out to not be necessary.
-        return 'card-description card-party--' + p;
-      case 'req':
-        return 'card-party--' + p + '-req';
-      }
-      return '';
+      const suffix = this.size === 'req' ? '-req' : '';
+      return `card-party--${p}${suffix}`;
     },
   },
 });

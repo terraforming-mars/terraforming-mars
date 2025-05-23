3e67e9585b5f70cae6fdeb6e5451e682a280bcfe
Robert Konigsberg <konigsberg@gmail.com>
2025-05-23 15:19:22 -0400

    Add Xavier tests

--- tests/cards/ceo/Xavier.spec.ts
+++ tests/cards/ceo/Xavier.spec.ts
@@ -11,6 +11,8 @@ import {SulphurExports} from '../../../src/server/cards/venusNext/SulphurExports
 import {Ecologist} from '../../../src/server/milestones/Ecologist';
 import {forceGenerationEnd, runAllActions} from '../../TestingUtils';
 import {Diversifier} from '../../../src/server/milestones/Diversifier';
+import {Tag} from '../../../src/common/cards/Tag';
+import {CommunityServices} from '../../../src/server/cards/colonies/CommunityServices';
 
 
 describe('Xavier', () => {
@@ -21,26 +23,46 @@ describe('Xavier', () => {
   beforeEach(() => {
     card = new Xavier();
     [game, player] = testGame(2, {ceoExtension: true});
-    player.playedCards.push(card, new SearchForLife());
+    player.playedCards.push(card);
+  });
+
+  it('Counts tags', () => {
+    expect(card.tags).deep.eq([]);
+
+    card.action();
+
+    expect(card.tags).deep.eq([Tag.WILD, Tag.WILD]);
+
+    player.runProductionPhase();
+
+    expect(card.tags).deep.eq([]);
   });
 
   it('Takes action once per game: Can play cards with tag requirements', () => {
+    player.playedCards.push(new SearchForLife()); // Contains a science tag
     const lightningHarvest = new LightningHarvest();
     const geneRepair = new GeneRepair();
     player.cardsInHand.push(lightningHarvest);
+
     expect(lightningHarvest.canPlay(player)).is.false;
 
     // Once per game, can gain 2 wild tags for the generation
     card.action();
     player.actionsThisGeneration.add(card.name);
+
     expect(lightningHarvest.canPlay(player)).is.true;
+
     lightningHarvest.play(player);
+
     expect(geneRepair.canPlay(player)).is.true;
 
     // Bonus wild tags are lost next generation
     runAllActions(game);
+
     expect(card.isDisabled).is.true;
+
     player.runProductionPhase();
+
     expect(geneRepair.canPlay(player)).is.false;
   });
 
@@ -104,11 +126,38 @@ describe('Xavier', () => {
   it('Works with Diversifier', () => {
     const diversifier = new Diversifier();
 
-    // There's a science tag in Search for Life
-    expect(diversifier.getScore(player)).eq(1);
+    expect(diversifier.getScore(player)).eq(0);
 
     card.action();
 
-    expect(diversifier.getScore(player)).eq(3);
+    expect(diversifier.getScore(player)).eq(2);
+  });
+
+  it('Stays out of the way of counting card with no tags', () => {
+    expect(player.tags.numberOfCardsWithNoTags()).eq(1);
+
+    card.action();
+
+    expect(player.tags.numberOfCardsWithNoTags()).eq(1);
+    expect(card.tags).deep.eq([Tag.WILD, Tag.WILD]);
+
+    const communityServices = new CommunityServices();
+    communityServices.play(player);
+
+    player.runProductionPhase();
+
+    expect(player.tags.numberOfCardsWithNoTags()).eq(1);
+  });
+
+  it('Works with Community Services', () => {
+    const communityServices = new CommunityServices();
+    communityServices.play(player);
+
+    expect(player.production.megacredits).eq(2);
+
+    card.action();
+    communityServices.play(player);
+
+    expect(player.production.megacredits).eq(4);
   });
 });

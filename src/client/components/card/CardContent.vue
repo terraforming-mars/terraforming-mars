5b47498eaaba49e1903dece18071b33f59e1be6f
Katie Yang <13hy22@queensu.ca>
2025-06-05 13:40:39 -0700

    #6613 Some corporation cards get their description text hidden. (#7366)
    Co-authored-by: Robert Konigsberg <kberg@users.noreply.github.com>

--- src/client/components/card/CardContent.vue
+++ src/client/components/card/CardContent.vue
@@ -1,8 +1,10 @@
 <template>
-  <div :class="getClasses()">
+  <div :class="classes">
     <CardRequirementsComponent v-if="requirements.length > 0" :requirements="requirements"/>
-    <CardRenderData v-if="metadata.renderData" :renderData="metadata.renderData" />
-    <CardDescription v-if="hasDescription" :item="metadata.description" />
+    <CardRenderData v-if="firstRow !== undefined" :renderData="firstRow" />
+    <CardDescription v-if="isCorporation && hasDescription" :item="metadata.description"/>
+    <CardRenderData v-if="remainingRows !== undefined" :renderData="remainingRows" />
+    <CardDescription v-if="!isCorporation && hasDescription" :item="metadata.description"/>
     <CardVictoryPoints v-if="metadata.victoryPoints" :victoryPoints="metadata.victoryPoints" />
     <div class="padBottom" v-if="padBottom" style="padding-bottom: 22px;"></div>
   </div>
@@ -17,6 +19,7 @@ import CardVictoryPoints from './CardVictoryPoints.vue';
 import CardDescription from './CardDescription.vue';
 import CardRenderData from './CardRenderData.vue';
 import {CardRequirementDescriptor} from '@/common/cards/CardRequirementDescriptor';
+import {ICardRenderRoot, isICardRenderRoot} from '@/common/cards/render/Types';
 
 export default Vue.extend({
   name: 'CardContent',
@@ -43,18 +46,36 @@ export default Vue.extend({
     CardRenderData,
   },
   methods: {
-    getClasses(): string {
+  },
+  computed: {
+    classes(): string {
       const classes: Array<string> = ['card-content'];
       if (this.isCorporation) {
         classes.push('card-content-corporation');
       }
       return classes.join(' ');
     },
-  },
-  computed: {
     hasDescription(): boolean {
       const description = this.metadata.description;
       return description !== undefined && (typeof(description) !== 'string' || description.length > 0);
+    },
+    firstRow(): ICardRenderRoot | undefined {
+      if (isICardRenderRoot(this.metadata.renderData) && this.metadata.renderData.rows.length > 0) {
+        return {
+          is: 'root',
+          rows: [this.metadata.renderData.rows[0]],
+        };
+      }
+      return undefined;
+    },
+    remainingRows(): ICardRenderRoot | undefined {
+      if (isICardRenderRoot(this.metadata.renderData) && this.metadata.renderData.rows.length > 1) {
+        return {
+          is: 'root',
+          rows: this.metadata.renderData.rows.slice(1),
+        };
+      }
+      return undefined;
     },
   },
 });

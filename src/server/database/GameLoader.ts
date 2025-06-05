3b954f8bb76fb4074bb8ac128743ddc958c08f5a
kselaenne <kselaenne@users.noreply.github.com>
2025-06-05 12:23:31 +0200

    Fix condition to take GAME_CACHE's sweep_freq into account when it is set

--- src/server/database/GameLoader.ts
+++ src/server/database/GameLoader.ts
@@ -188,6 +188,6 @@ function parseConfigString(stringValue: string): CacheConfig {
   const evictMillis = durationToMilliseconds(parsed.eviction_age);
   if (!isNaN(evictMillis)) options.evictMillis = evictMillis;
   const sleepMillis = durationToMilliseconds(parsed.sweep_freq);
-  if (isNaN(sleepMillis)) options.sleepMillis = sleepMillis;
+  if (!isNaN(sleepMillis)) options.sleepMillis = sleepMillis;
   return options;
 }

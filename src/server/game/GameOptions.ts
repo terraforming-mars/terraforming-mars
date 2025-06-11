17b81a1d373348c7977012655d935cfee6b59426
Tarun/Theo Weisz <theo.weisz@gmail.com>
2025-06-11 17:55:43 +0100

    Add automatic selection for ceosDraftVariant when initial draft is selected

--- src/server/game/GameOptions.ts
+++ src/server/game/GameOptions.ts
@@ -87,6 +87,7 @@ export const DEFAULT_GAME_OPTIONS: GameOptions = {
   bannedCards: [],
   includedCards: [],
   ceoExtension: false,
+  ceosDraftVariant: false,
   clonedGamedId: undefined,
   coloniesExtension: false,
   communityCardsOption: false,

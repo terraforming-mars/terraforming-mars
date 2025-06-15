6ab495d02e2baff31093d7494615a0420a4f32ce
Robert Konigsberg <konigsberg@gmail.com>
2025-06-15 12:24:00 +0200

    Celebrity Leaders should include Pharmacy Union

--- src/server/turmoil/globalEvents/CelebrityLeaders.ts
+++ src/server/turmoil/globalEvents/CelebrityLeaders.ts
@@ -5,7 +5,6 @@ import {PartyName} from '../../../common/turmoil/PartyName';
 import {IGame} from '../../IGame';
 import {Resource} from '../../../common/Resource';
 import {Turmoil} from '../Turmoil';
-import {CardType} from '../../../common/cards/CardType';
 import {CardRenderer} from '../../cards/render/CardRenderer';
 import {Size} from '../../../common/cards/render/Size';
 import {Tag} from '../../../common/cards/Tag';
@@ -27,7 +26,7 @@ export class CelebrityLeaders extends GlobalEvent implements IGlobalEvent {
 
   public resolve(game: IGame, turmoil: Turmoil) {
     game.getPlayersInGenerationOrder().forEach((player) => {
-      const eventsCards = player.playedCards.filter((card) => card.type === CardType.EVENT).length;
+      const eventsCards = player.getPlayedEventsCount();
       player.stock.add(Resource.MEGACREDITS, 2 * (Math.min(5, eventsCards) + turmoil.getPlayerInfluence(player)), {log: true, from: this.name});
     });
   }

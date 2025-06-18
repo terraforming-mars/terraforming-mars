1128e623e3d424572e40405e19e14613bc6956d6
Ethan Dobbs <52511593+EthanDobbs@users.noreply.github.com>
2025-06-18 15:35:29 -0500

    Warning for playing microbe tags as PharmU
    Simple check added in the player's canPlay method

--- src/client/components/WarningsComponent.vue
+++ src/client/components/WarningsComponent.vue
@@ -31,6 +31,7 @@ const descriptions: Record<Warning, string> = {
   'noMatchingCards': 'No cards gain the reward of this action.',
   'noEffect': 'This action will have no effect.',
   'selfTarget': 'Note: This action will target you.',
+  'pharmacyUnion': 'Note: playing a card with a microbe tag will cause you to lose 4 M€ (or as much as possible).',
 };
 
 export default Vue.extend({

e252d760b844a31744ddb2b2344b35b4d0cd85e7
Robert Konigsberg <konigsberg@gmail.com>
2025-06-25 10:07:59 -0400

    Clean up drafting test. Remove duplication, etc etc

--- tests/TestGame.ts
+++ tests/TestGame.ts
@@ -3,10 +3,13 @@ import {IGame} from '../src/server/IGame';
 import {GameOptions} from '../src/server/game/GameOptions';
 import {TestPlayer} from './TestPlayer';
 import {SelectInitialCards} from '../src/server/inputs/SelectInitialCards';
+import {Deck} from '../src/server/cards/Deck';
 
 export type TestGameOptions = GameOptions & {
   /* skip initial card selection */
   skipInitialCardSelection: boolean;
+  /* skip shuffling when creating the game */
+  skipInitialShuffling: boolean;
 };
 
 function createPlayers(count: number, idSuffix: string): Array<TestPlayer> {
@@ -42,16 +45,24 @@ export function testGame(count: number, customOptions?: Partial<TestGameOptions>
     copy.aresHazards = false;
   }
 
-  const game = Game.newInstance(`game-id${idSuffix}`, players, players[0], customOptions, /* seed= */ undefined, `spectator-id${idSuffix}`);
-  if (customOptions?.skipInitialCardSelection !== false) {
-    for (const player of players) {
+  const shuffle = Deck.shuffle;
+  try {
+    if (customOptions?.skipInitialShuffling) {
+      Deck.shuffle = () => {};
+    }
+    const game = Game.newInstance(`game-id${idSuffix}`, players, players[0], customOptions, /* seed= */ undefined, `spectator-id${idSuffix}`);
+    if (customOptions?.skipInitialCardSelection !== false) {
+      for (const player of players) {
       /* Removes waitingFor if it is SelectInitialCards. Used when wanting it cleared out for further testing. */
-      if (player.getWaitingFor() instanceof SelectInitialCards) {
-        player.popWaitingFor();
+        if (player.getWaitingFor() instanceof SelectInitialCards) {
+          player.popWaitingFor();
+        }
       }
     }
+    return [game, ...players];
+  } finally {
+    Deck.shuffle = shuffle;
   }
-  return [game, ...players];
 }
 
 export function getTestPlayer(game: IGame, idx: number): TestPlayer {

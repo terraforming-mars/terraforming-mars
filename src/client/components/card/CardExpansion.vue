d06ab3ce4a5417e08d914c3fa951e78e41133104
Robert Konigsberg <konigsberg@gmail.com>
2025-07-01 11:05:17 -0400

    Standardize CSS name.

--- src/client/components/card/CardExpansion.vue
+++ src/client/components/card/CardExpansion.vue
@@ -1,32 +1,13 @@
 <template>
   <div :class="templateClasses">
-    <div :class="iconClasses(expansion)"></div>
-    <div v-for="module in modules" :class="iconClasses(module)" :key="module"></div>
+    <div class="project-icon" :class="iconClass(expansion)"></div>
+    <div v-for="module in modules" class="project-icon" :class="module + '-icon'" :key="module"></div>
   </div>
 </template>
 <script lang="ts">
 
 import Vue from 'vue';
-import {Expansion, GameModule} from '@/common/cards/GameModule';
-
-// TODO(kberg): replace corporate-icon with corpera-icon, and
-// this can be replaced with a template string.
-const MODULE_TO_CSS: Record<Expansion, string> = {
-  corpera: 'corporate-icon',
-  promo: 'promo-icon',
-  venus: 'venus-icon',
-  colonies: 'colonies-icon',
-  prelude: 'prelude-icon',
-  prelude2: 'prelude2-icon',
-  turmoil: 'turmoil-icon',
-  community: 'community-icon',
-  ares: 'ares-icon',
-  moon: 'moon-icon',
-  pathfinders: 'pathfinders-icon',
-  ceo: 'ceo-icon',
-  starwars: 'starwars-icon',
-  underworld: 'underworld-icon',
-};
+import {GameModule} from '@/common/cards/GameModule';
 
 export default Vue.extend({
   name: 'CardExpansion',
@@ -49,12 +30,8 @@ export default Vue.extend({
     },
   },
   methods: {
-    iconClasses(module: GameModule): string {
-      const classes = ['project-icon'];
-      if (module !== 'base') {
-        classes.push(MODULE_TO_CSS[module]);
-      }
-      return classes.join(' ');
+    iconClass(module: GameModule): string {
+      return module === 'base' ? '' : module + '-icon';
     },
   },
   computed: {

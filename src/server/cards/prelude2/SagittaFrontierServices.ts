af59788a7cfa4140c4499e111f511234c0b34630
Katie Yang <13hy22@queensu.ca>
2025-07-02 19:14:24 -0700

    Adding Corporation Boxes to Some Corporations

--- src/server/cards/prelude2/SagittaFrontierServices.ts
+++ src/server/cards/prelude2/SagittaFrontierServices.ts
@@ -26,8 +26,10 @@ export class SagittaFrontierServices extends CorporationCard implements ICorpora
         renderData: CardRenderer.builder((b) => {
           // TODO(kberg): provide reasonable secondary tag. It's not rendered on CardRenderItemComponent.
           b.megacredits(31).production((pb) => pb.energy(1).megacredits(2)).cards(1, {secondaryTag: AltSecondaryTag.NO_TAGS}).br;
-          b.effect('When you play a card with no tags, including this, gain 4 M€.', (eb) => eb.noTags().startEffect.megacredits(4)).br;
-          b.effect('When you play a card with EXACTLY 1 TAG, you gain 1 M€.', (eb) => eb.emptyTag().asterix().startEffect.megacredits(1)).br;
+          b.corpBox('effect', (ce) => {
+            ce.effect('When you play a card with no tags, including this, gain 4 M€.', (eb) => eb.noTags().startEffect.megacredits(4)).br;
+            ce.effect('When you play a card with EXACTLY 1 TAG, you gain 1 M€.', (eb) => eb.emptyTag().asterix().startEffect.megacredits(1)).br;
+          });
         }),
         description: 'You start with 31 M€. Increase energy production 1 step and M€ production 2 steps. Draw a card that has no tag.',
       },

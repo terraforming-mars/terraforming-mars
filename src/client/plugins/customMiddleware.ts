73d5562b23dbbf099fc26de9ac4f19fae15374c6
Daniel Cepeda <dcep93@gmail.com>
2025-10-04 19:47:07 -0400

    x

--- src/client/plugins/customMiddleware.ts
+++ src/client/plugins/customMiddleware.ts
@@ -30,11 +30,12 @@ window.customMiddlewareF = (ctx) => {
     const labels = Array.from(
       playerHomeBlock.querySelectorAll(".wf-options label")
     ).filter((label) => label.querySelector('input[type="radio"]:checked'));
+    if (labels.length === 1 && playerHomeBlock.querySelector(".card-title")) {
+      return;
+    }
     disregarded.push({
-      action: labels[0]?.textContent,
-      selectedCard: labels[1]
-        ?.querySelector(".card-title.card-title")!
-        .textContent.trim(),
+      action: labels[0]?.textContent.trim(),
+      selectedCard: labels[1]?.querySelector(".card-title")!.textContent.trim(),
     });
     ctx.app.updatePlayer();
   };
@@ -43,9 +44,35 @@ window.customMiddlewareF = (ctx) => {
   ) as HTMLElement[];
   disregarded.forEach(({ action, selectedCard }) => {
     const optionDiv = options.find(
-      (div) => div.querySelector(".form-radio")?.textContent === action
+      (div) => div.querySelector(".form-radio")?.textContent.trim() === action
     )!;
-    if (!selectedCard) {
+    if (selectedCard) {
+      optionDiv
+        .querySelector('input[type="radio"]')!
+        .addEventListener("change", () => {
+          const allCards = Array.from(
+            optionDiv.querySelectorAll("label")
+          ).filter((label) => !label.matches(".form-radio"));
+          const cardDiv = allCards.find(
+            (cardLabel) =>
+              cardLabel.querySelector(".card-title")!.textContent.trim() ===
+              selectedCard
+          ) as HTMLElement;
+          if (cardDiv) {
+            cardDiv.style.display = "none";
+            const nextCard = allCards.find((o) => o.style.display !== "none");
+            if (nextCard) {
+              nextCard.click();
+            } else {
+              optionDiv.style.display = "none";
+              options
+                .find((o) => o.style.display !== "none")!
+                .querySelector("label")!
+                .click();
+            }
+          }
+        });
+    } else {
       optionDiv.style.display = "none";
     }
   });

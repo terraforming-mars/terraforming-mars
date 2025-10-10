f7a72595c5189741c5a258041d7dce0f023236db
Robert Konigsberg <konigsberg@gmail.com>
2025-10-10 19:58:34 -0400

    Fix regression with outdated JSON import fields.

--- src/client/components/create/json.ts
+++ src/client/components/create/json.ts
@@ -1,7 +1,10 @@
 export const CUSTOM_CORPORATIONS = 'customCorporations';
+export const OLD_CUSTOM_CORPORATIONS = 'customCorporationsList';
 export const CUSTOM_COLONIES = 'customColonies';
+export const OLD_CUSTOM_COLONIES = 'customColoniesList';
 export const CUSTOM_PRELUDES = 'customPreludes';
 export const BANNED_CARDS = 'bannedCards';
+export const OLD_BANNED_CARDS = 'cardsBlackList';
 export const INCLUDED_CARDS = 'includedCards';
 export const CORPORATEERA = 'corporateEra';
 export const PRELUDE = 'prelude';

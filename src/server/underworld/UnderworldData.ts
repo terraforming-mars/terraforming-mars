987a9ac7c21e362e6c7cde96e5959e2454ee5233
Robert Konigsberg <konigsberg@gmail.com>
2025-12-17 22:52:37 -0800

    Remove transitionary code

--- src/server/underworld/UnderworldData.ts
+++ src/server/underworld/UnderworldData.ts
@@ -1,18 +1,9 @@
-import {TemporaryBonusToken, UndergroundResourceToken} from '../../common/underworld/UndergroundResourceToken';
-import {ClaimedToken} from '../../common/underworld/UnderworldPlayerData';
+import {UndergroundResourceToken} from '../../common/underworld/UndergroundResourceToken';
 import {Space} from '../boards/Space';
 
 export type UnderworldData = {
   tokens: Array<UndergroundResourceToken>;
 };
-
-export type SerializedUnderworldPlayerData = {
-  corruption: number;
-  temperatureBonus?: TemporaryBonusToken,
-  activeBonus?: TemporaryBonusToken | undefined;
-  // TODO(kberg): Remove ? 2025-10-01
-  tokens?: Array<UndergroundResourceToken> | Array<ClaimedToken>,
-}
 
 export type TokenSources = {
   spaces: Array<Space>,

39c7fb62acc95c0dcc244bd38c8121e26302c7e9
Robert Konigsberg <kberg@users.noreply.github.com>
2026-02-25 13:50:41 -0500

    Remove unused type (#7878)

--- src/server/underworld/UnderworldData.ts
+++ src/server/underworld/UnderworldData.ts
@@ -1,11 +1,6 @@
 import {UndergroundResourceToken} from '../../common/underworld/UndergroundResourceToken';
-import {Space} from '../boards/Space';
 
 export type UnderworldData = {
   tokens: Array<UndergroundResourceToken>;
 };
 
-export type TokenSources = {
-  spaces: Array<Space>,
-  tokens: Array<UndergroundResourceToken>,
-}

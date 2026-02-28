d44252655288698ed397e9527114044487848b72
Jelle Bosscher <jellebosscher@gmail.com>
2026-02-28 08:14:32 +0100

    make underground det playeable

--- src/server/cards/base/UndergroundDetonations.ts
+++ src/server/cards/base/UndergroundDetonations.ts
@@ -1,31 +1,43 @@
-import {IProjectCard} from '../IProjectCard';
+import { IProjectCard} from '../IProjectCard';
+import {IActionCard} from '../ICard';
 import {Tag} from '../../../common/cards/Tag';
-import {ActionCard} from '../ActionCard';
+import {Card} from '../Card';
 import {CardType} from '../../../common/cards/CardType';
 import {CardName} from '../../../common/cards/CardName';
 import {CardRenderer} from '../render/CardRenderer';
+import {IPlayer} from '../../IPlayer';
+import {SelectPaymentDeferred} from '../../deferredActions/SelectPaymentDeferred';
+import {Resource} from '../../../common/Resource';
+import {TITLES} from '../../inputs/titles';
 
-export class UndergroundDetonations extends ActionCard implements IProjectCard {
+const ACTION_COST = 8;
+
+export class UndergroundDetonations extends Card implements IActionCard, IProjectCard {
   constructor() {
     super({
       type: CardType.ACTIVE,
       name: CardName.UNDERGROUND_DETONATIONS,
       tags: [Tag.BUILDING],
-      cost: 6,
-
-      action: {
-        spend: {megacredits: 10},
-        production: {heat: 2},
-      },
+      cost: 2,
 
       metadata: {
         cardNumber: '202',
         renderData: CardRenderer.builder((b) => {
-          b.action('Spend 10M€ to increase your heat production 2 steps.', (eb) => {
-            eb.megacredits(10).startAction.production((pb)=>pb.heat(2));
+          b.action('Spend 8M€ to increase your heat production 2 steps. STEEL MAY BE USED as if playing a building card.', (eb) => {
+            eb.megacredits(8).super((b) => b.steel(2)).startAction.production((pb)=>pb.heat(2));
           });
         }),
       },
     });
+  }
+
+  public canAct(player: IPlayer): boolean {
+    return player.canAfford({cost: ACTION_COST, steel: true});
+  }
+
+  public action(player: IPlayer) {
+    player.game.defer(new SelectPaymentDeferred(player, ACTION_COST, {canUseSteel: true, title: TITLES.payForCardAction(this.name)}))
+      .andThen(() => player.production.add(Resource.HEAT, 2, {log: true}));
+    return undefined;
   }
 }
--- tests/cards/base/UndergroundDetonations.spec.ts
+++ tests/cards/base/UndergroundDetonations.spec.ts
@@ -2,7 +2,10 @@ import {expect} from 'chai';
 import {UndergroundDetonations} from '../../../src/server/cards/base/UndergroundDetonations';
 import {IGame} from '../../../src/server/IGame';
 import {TestPlayer} from '../../TestPlayer';
+import {cast} from '../../TestingUtils';
 import {testGame} from '../../TestGame';
+import {SelectPayment} from '../../../src/server/inputs/SelectPayment';
+import {Payment} from '../../../src/common/inputs/Payment';
 
 describe('UndergroundDetonations', () => {
   let card: UndergroundDetonations;
@@ -14,18 +17,36 @@ describe('UndergroundDetonations', () => {
     [game, player] = testGame(2);
   });
 
-  it('Can not act', () => {
-    player.megaCredits = 9;
-    expect(card.canAct(player)).is.not.true;
+  it('can not act without enough M€ or steel', () => {
+    player.megaCredits = 7;
+    player.steel = 0;
+    expect(card.canAct(player)).is.false;
   });
 
-  it('Should act', () => {
-    player.megaCredits = 10;
+  it('can act with steel substituting M€', () => {
+    player.megaCredits = 6; // 2 short
+    player.steel = 0;
+    expect(card.canAct(player)).is.false;
+    player.steel = 1; // 1 steel = 2 M€, covers the gap
     expect(card.canAct(player)).is.true;
+  });
 
-    card.action(player);
+  it('should act: spends 8 M€ and increases heat production 2 steps', () => {
+    player.megaCredits = 8;
+    cast(card.action(player), undefined);
     game.deferredActions.runNext();
     expect(player.megaCredits).to.eq(0);
+    expect(player.production.heat).to.eq(2);
+  });
+
+  it('should act: can pay with steel', () => {
+    player.megaCredits = 8;
+    player.steel = 1; // 8 M€ + 1 steel (worth 2) = 10
+    cast(card.action(player), undefined);
+    const selectPayment = cast(game.deferredActions.peek()!.execute(), SelectPayment);
+    selectPayment.cb({...Payment.EMPTY, megaCredits: 8, steel: 1});
+    expect(player.megaCredits).to.eq(0);
+    expect(player.steel).to.eq(0);
     expect(player.production.heat).to.eq(2);
   });
 });

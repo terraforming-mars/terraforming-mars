bcd7c61dc7e15f49594ec96d28eb618d94c908a9
Robert Konigsberg <kberg@users.noreply.github.com>
2026-03-11 12:33:58 -0400

    Make Hostile Takeover compatible with Darkside of the Moon Syndicate (#7922)

--- tests/cards/moon/TheDarksideofTheMoonSyndicate.spec.ts
+++ tests/cards/moon/TheDarksideofTheMoonSyndicate.spec.ts
@@ -114,7 +114,7 @@ describe('TheDarksideofTheMoonSyndicate', () => {
     const centerSpace = moonData.moon.getSpaceOrThrow('m07');
     const adjacentSpaces = moonData.moon.getAdjacentSpaces(centerSpace);
 
-    // Space 0 intentionallyleft blank
+    // Space 0 intentionally left blank
     MoonExpansion.addMineTile(player2, adjacentSpaces[1].id);
     MoonExpansion.addHabitatTile(player2, adjacentSpaces[2].id);
     MoonExpansion.addRoadTile(player2, adjacentSpaces[3].id);
@@ -148,7 +148,7 @@ describe('TheDarksideofTheMoonSyndicate', () => {
     const centerSpace = moonData.moon.getSpaceOrThrow('m07');
     const adjacentSpaces = moonData.moon.getAdjacentSpaces(centerSpace);
 
-    // Space 0 intentionallyleft blank
+    // Space 0 intentionally left blank
     MoonExpansion.addMineTile(player2, adjacentSpaces[1].id);
     MoonExpansion.addHabitatTile(player2, adjacentSpaces[2].id);
     MoonExpansion.addRoadTile(player2, adjacentSpaces[3].id);
@@ -169,6 +169,28 @@ describe('TheDarksideofTheMoonSyndicate', () => {
     MoonExpansion.addMineTile(player, centerSpace.id);
     expect(player2.megaCredits).eq(10);
     expect(player.megaCredits).eq(0);
+  });
+
+  it('Compatible with Hostile Takeover', () => {
+    const centerSpace = moonData.moon.getSpaceOrThrow('m07');
+    const adjacentSpaces = moonData.moon.getAdjacentSpaces(centerSpace);
+
+    const space = adjacentSpaces[1];
+
+    MoonExpansion.addMineTile(player2, space.id);
+    space.coOwner = player3;
+
+    player.megaCredits = 0;
+    player2.megaCredits = 10;
+    player3.megaCredits = 10;
+    player.playedCards.push(card);
+
+    // Trigger the effect.
+    MoonExpansion.addMineTile(player, centerSpace.id);
+
+    expect(player.megaCredits).eq(4);
+    expect(player2.megaCredits).eq(8);
+    expect(player3.megaCredits).eq(8);
   });
 });
 

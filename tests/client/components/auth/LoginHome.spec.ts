3334ba56217490b9f67d8ea8e6b00ff3818d741a
Robert Konigsberg <kberg@users.noreply.github.com>
2026-03-25 17:56:58 -0400

    Environment variable for discord client id (#7246)

--- tests/client/components/auth/LoginHome.spec.ts
+++ tests/client/components/auth/LoginHome.spec.ts
@@ -15,10 +15,29 @@ describe('LoginHome', () => {
     (global as any).fetch = originalFetch;
   });
 
-  it('mounts without errors', () => {
-    const wrapper = shallowMount(LoginHome, {
+  function mount(discordClientId: string) {
+    return shallowMount(LoginHome, {
       ...globalConfig,
+      data() {
+        return {discordClientId};
+      },
     });
+  }
+
+  it('mounts without errors', () => {
+    const wrapper = mount('');
     expect(wrapper.exists()).to.be.true;
+  });
+
+  it('loginUrl is empty when discordClientId is not set', () => {
+    const wrapper = mount('');
+    expect((wrapper.vm as any).loginUrl).to.equal('');
+  });
+
+  it('loginUrl is a well-formed Discord OAuth URL when discordClientId is set', () => {
+    const wrapper = mount('test-client-id');
+    expect((wrapper.vm as any).loginUrl).to.equal(
+      'https://discord.com/oauth2/authorize?client_id=test-client-id&response_type=code&scope=identify&redirect_uri=/auth/discord/callback',
+    );
   });
 });

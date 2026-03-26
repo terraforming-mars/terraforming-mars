f1f05931aa8fcf9fb55fb017deb569acc8d72cab
Robert Konigsberg <konigsberg@gmail.com>
2026-03-26 08:52:09 -0400

    Replace enums with consts.

--- src/client/utils/SoundManager.ts
+++ src/client/utils/SoundManager.ts
@@ -1,24 +1,24 @@
 export namespace SoundManager {
-  enum Notes {
-    G3 = 196.00,
-    A3 = 220.00,
-    B3 = 246.94,
-    C4 = 261.63,
-    D4 = 293.66,
-    E4 = 329.63,
-    F4 = 349.23,
-    G4 = 392.00,
-    A4 = 440.00,
-    B4 = 493.88,
-    C5 = 523.25,
-    D5 = 587.33,
-    E5 = 659.25,
-    F5 = 698.46,
-    G5 = 783.99,
-    A5 = 880.00,
-    B5 = 987.77,
-    C6 = 1046.50,
-  }
+  const Notes = {
+    G3: 196.00,
+    A3: 220.00,
+    B3: 246.94,
+    C4: 261.63,
+    D4: 293.66,
+    E4: 329.63,
+    F4: 349.23,
+    G4: 392.00,
+    A4: 440.00,
+    B4: 493.88,
+    C5: 523.25,
+    D5: 587.33,
+    E5: 659.25,
+    F5: 698.46,
+    G5: 783.99,
+    A5: 880.00,
+    B5: 987.77,
+    C6: 1046.50,
+  } as const;
 
   function setupGainNode(audioCtx: AudioContext, time: number, value: number) {
     time += audioCtx.currentTime;

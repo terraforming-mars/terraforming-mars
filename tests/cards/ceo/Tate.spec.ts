20a261b6c59cf477e29e9a395016c3fd6685c660
Robert Konigsberg <konigsberg@gmail.com>
2026-04-16 19:31:14 +0100

    Test that verifies #5491 is fixed.
    Fixes #5491

--- tests/cards/ceo/Tate.spec.ts
+++ tests/cards/ceo/Tate.spec.ts
@@ -10,6 +10,8 @@ import {OrOptions} from '../../../src/server/inputs/OrOptions';
 import {SelectOption} from '../../../src/server/inputs/SelectOption';
 import {Tag} from '../../../src/common/cards/Tag';
 import {Tate} from '../../../src/server/cards/ceos/Tate';
+import {LogMessageDataType} from '../../../src/common/logs/LogMessageDataType';
+import {toName} from '../../../src/common/utils/utils';
 
 describe('Tate', () => {
   let card: Tate;
@@ -64,6 +66,27 @@ describe('Tate', () => {
     expect(player.megaCredits).eq(3);
     expect(player.cardsInHand).has.length(1);
     expect(player.cardsInHand[0].tags).contains(Tag.BUILDING);
+  });
+
+  it('Reveals all drawn cards when buying (Confirms #5491)', () => {
+    game.gameLog = [];
+
+    const orOptions = cast(card.action(player), OrOptions);
+    const selectOption = cast(orOptions.options[0], SelectOption);
+    cast(selectOption.cb(undefined), undefined);
+    runAllActions(game);
+    const selectCard = cast(player.popWaitingFor(), SelectCard<ICard>);
+
+    selectCard.cb([selectCard.cards[0], selectCard.cards[1]]);
+    runAllActions(game);
+
+    const publicMessages = game.gameLog.filter((entry) => entry.playerId === undefined);
+    const message = publicMessages.find((msg) => msg.message === '${0} drew ${1}');
+
+    expect(message).to.not.be.undefined;
+    const data = message!.data[1];
+    expect(data.type).eq(LogMessageDataType.CARDS);
+    expect(data.value).to.have.members(selectCard.cards.map(toName));
   });
 
   it('Can only act once per game', () => {

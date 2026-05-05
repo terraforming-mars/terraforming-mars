b6cf339af253944ce7a77b345a7b814aa75d1a15
Robert Konigsberg <konigsberg@gmail.com>
2026-05-05 18:00:00 +0100

    Political Uprising now searches the discard pile too.

--- src/server/cards/community/PoliticalUprising.ts
+++ src/server/cards/community/PoliticalUprising.ts
@@ -32,22 +32,28 @@ export class PoliticalUprising extends PreludeCard implements IProjectCard {
     return undefined;
   }
 
-  // TODO(kberg): it is possible, though unlikely, that the draw deck won't have another Turmoil card, but this
-  // app ought to check the discard pile, or something.
   private drawTurmoilCard(player: IPlayer) {
     // Rather than draw and discard potentially dozens of cards, find one card in the deck that's a Turmoil card.
 
     // First get all the card names for Turmoil Project cards by indexing the manifest.
     const turmoilCardNames = CardManifest.keys(TURMOIL_CARD_MANIFEST.projectCards);
 
+    const projectDeck = player.game.projectDeck;
     // Then find the first card in the deck that matches one of those names.
-    const drawnCard = player.game.projectDeck.drawPile.find((card) => turmoilCardNames.includes(card.name));
+    let drawnCard = projectDeck.drawPile.find((card) => turmoilCardNames.includes(card.name));
+
+    // If there's none in the draw pile, reshuffle the deck and look through the discard pile.
+    if (drawnCard === undefined) {
+      player.game.log(`The project deck has no Turmoil cards, so the discard pile is being reshuffled to form a new deck.`);
+      projectDeck.shuffle();
+      drawnCard = projectDeck.drawPile.find((card) => turmoilCardNames.includes(card.name));
+    }
 
     if (drawnCard === undefined) {
       player.game.log('${0} played ${1} to find a Turmoil card but none were in the draw deck.', (b) => b.player(player).card(this));
     } else {
-      const cardIndex = player.game.projectDeck.drawPile.findIndex((c) => c.name === drawnCard.name);
-      player.game.projectDeck.drawPile.splice(cardIndex, 1);
+      const cardIndex = projectDeck.drawPile.findIndex((c) => c.name === drawnCard.name);
+      projectDeck.drawPile.splice(cardIndex, 1);
 
       player.cardsInHand.push(drawnCard);
       player.game.log('${0} drew ${1}', (b) => b.player(player).card(drawnCard));

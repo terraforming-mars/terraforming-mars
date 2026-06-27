e7a49920debb6858ed2b3a1d713b12bdaa1460be
Robert Konigsberg <konigsberg@gmail.com>
2026-06-26 23:15:41 -0400

    Restructure Jet Stream Microscrappers to use less vertical space.

--- src/server/cards/venusNext/JetStreamMicroscrappers.ts
+++ src/server/cards/venusNext/JetStreamMicroscrappers.ts
@@ -24,13 +24,10 @@ export class JetStreamMicroscrappers extends Card implements IActionCard {
       metadata: {
         cardNumber: '234',
         renderData: CardRenderer.builder((b) => {
-          b.action('Spend 1 titanium to add 2 floaters here', (eb) => {
-            eb.titanium(1).startAction.resource(CardResource.FLOATER, 2);
-          }).br;
-          b.or().br;
-          b.action('Spend 2 floaters here to raise Venus 1 step', (eb) => {
-            eb.resource(CardResource.FLOATER, 2).startAction.venus(1);
-          });
+          b.titanium(1).arrow().resource(CardResource.FLOATER, 2).nbsp.or().br;
+          b.resource(CardResource.FLOATER, 2).arrow().venus(1).br;
+
+          b.plainText('Action: Spend 1 titanium to add 2 floaters here, or spend 2 floaters here to raise Venus 1 step.', /* parens */ true);
         }),
       },
     });

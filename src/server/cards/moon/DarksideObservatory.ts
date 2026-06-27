41da482b9ad48091c9db5c46170e3d4b87946e3f
Robert Konigsberg <konigsberg@gmail.com>
2026-06-26 22:59:47 -0400

    Restructure Darkside Observatory to use less vertical space.

--- src/server/cards/moon/DarksideObservatory.ts
+++ src/server/cards/moon/DarksideObservatory.ts
@@ -21,13 +21,10 @@ export class DarksideObservatory extends Card implements IProjectCard, IActionCa
       metadata: {
         cardNumber: 'M75',
         renderData: CardRenderer.builder((b) => {
-          b.action('Add 1 science to ANY card [EXCEPT those giving 2 VP or more per science resource.]', (ab) => {
-            ab.empty().startAction.resource(CardResource.SCIENCE).asterix();
-          }).br;
-          b.or().br;
-          b.action('Add 2 data to ANY card.', (ab) => {
-            ab.empty().startAction.resource(CardResource.DATA, 2).asterix();
-          });
+          b.arrow().resource(CardResource.SCIENCE).asterix().nbsp.or().br;
+          b.arrow().resource(CardResource.DATA, 2).asterix().br;
+
+          b.plainText('Action: Add 1 science to ANY card [EXCEPT those giving 2 VP or more per science resource.], or add 2 data to ANY card.', /* parens */ true);
         }),
       },
     });

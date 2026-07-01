412dc63ac4c722d8cc90004d8d69d514ce7c50b6
Robert Konigsberg <konigsberg@gmail.com>
2026-07-01 00:00:50 -0400

    Update some card text

--- src/server/cards/ares/MagneticFieldGeneratorsAres.ts
+++ src/server/cards/ares/MagneticFieldGeneratorsAres.ts
@@ -14,13 +14,13 @@ export class MagneticFieldGeneratorsAres extends MagneticFieldGeneratorsPromo {
         cardNumber: 'Axx',
         renderData: CardRenderer.builder((b) => {
           b.production((pb) => {
-            pb.minus().energy(4, {digit}).br;
+            pb.minus().energy(4, {digit}).nbsp;
             pb.plus().plants(2);
           }).br;
           b.tr(3, {digit});
           b.tile(TileType.MAGNETIC_FIELD_GENERATORS, false, true).asterix().br;
         }),
-        description: 'Decrease your energy production 4 steps and increase your plant production 2 steps. Raise your TR 3 steps. Place this tile. It provides adjacency bonus of 1 plant and 1 microbe.',
+        description: 'Decrease your energy production 4 steps and increase your plant production 2 steps. Raise your TR 3 steps. Place this tile, which provides an ADJACENCY BONUS of 1 plant and 1 microbe.',
       });
   }
 }

054b435f5da7fa475581a1827a285a5d727f18db
Robert Konigsberg <konigsberg@gmail.com>
2026-07-29 11:46:45 -0400

    Clarify SortableCards test

--- tests/client/components/SortableCards.spec.ts
+++ tests/client/components/SortableCards.spec.ts
@@ -4,19 +4,52 @@ import {expect} from 'chai';
 import {CardName} from '@/common/cards/CardName';
 import SortableCards from '@/client/components/SortableCards.vue';
 import {FakeLocalStorage} from './FakeLocalStorage';
+import {PlayerId} from '@/common/Types';
 
-type DropPosition = 'before' | 'after';
+type DropSide = 'left' | 'right';
 
-async function dragCard(sortable: VueWrapper<InstanceType<typeof SortableCards>>, sourceIndex: number, targetIndex: number, position: DropPosition) {
+/**
+ * Drag card at `sourceIndex` to `targetIndex` on its left or right side.
+ */
+async function dragCard(sortable: VueWrapper<InstanceType<typeof SortableCards>>, sourceIndex: number, targetIndex: number, position: DropSide) {
   const draggers = sortable.findAll('[draggable=true]');
   const target = draggers[targetIndex];
-  // jsdom lacks layout; use unit bounds.
-  const targetBounds = {left: 0, width: 1};
-  target.element.getBoundingClientRect = () => targetBounds as DOMRect;
+
+  // This test doesn't use a real layout, so cards aren't 200px wide. Here,
+  // they're simulated at 10px. Positions 0-4 are the left side and positions
+  // 5-9 are the right side.
+  target.element.getBoundingClientRect = () => {
+    return {left: 0, width: 10} as DOMRect;
+  };
 
   await draggers[sourceIndex].trigger('dragstart');
-  await target.trigger('dragover', {clientX: position === 'before' ? targetBounds.left : targetBounds.left + targetBounds.width});
+  // 3 is the left side, 8 is the right side.
+  await target.trigger('dragover', {clientX: position === 'left' ? 3 : 8});
   await draggers[sourceIndex].trigger('dragend');
+}
+
+/**
+ * Returns the names of cards in this widget in their current order.
+ */
+function cardsInOrder(sortable: VueWrapper<InstanceType<typeof SortableCards>>): Array<CardName> {
+  return sortable.findAllComponents({
+    name: 'Card',
+  }).map((card) => card.props().card.name);
+}
+
+/**
+ * Returns the entries in local storage for the given player.
+ */
+function getStorageEntries(playerId: PlayerId): {[key in CardName]?: number} {
+  const entries: {[key in CardName]?: number} = {};
+  const item = localStorage.getItem(`cardOrder${playerId}`);
+  if (item) {
+    const parsed = JSON.parse(item);
+    for (const [cardName, order] of Object.entries(parsed)) {
+      entries[cardName as CardName] = order as number;
+    }
+  }
+  return entries;
 }
 
 describe('SortableCards', () => {
@@ -34,35 +67,23 @@ describe('SortableCards', () => {
     const sortable = mount(SortableCards, {
       ...globalConfig,
       props: {
-        cards: [{
-          name: CardName.ANTS,
-        }, {
-          name: CardName.CARTEL,
-        }],
-        playerId: 'foo',
+        cards: [{name: CardName.ANTS}, {name: CardName.CARTEL}],
+        playerId: 'player1',
       },
     });
-    let cards = sortable.findAllComponents({
-      name: 'Card',
-    });
-    expect(cards).has.length(2);
-    expect(cards[0].props().card.name).to.eq(CardName.ANTS);
-    expect(cards[1].props().card.name).to.eq(CardName.CARTEL);
-    await dragCard(sortable, 0, 1, 'after');
-    cards = sortable.findAllComponents({
-      name: 'Card',
-    });
-    expect(cards[0].props().card.name).to.eq(CardName.CARTEL);
-    expect(cards[1].props().card.name).to.eq(CardName.ANTS);
-    const order = localStorage.getItem('cardOrderfoo');
-    expect(order).not.to.be.undefined;
-    expect(JSON.parse(order!)).to.deep.eq({
+    expect(cardsInOrder(sortable)).to.deep.eq([CardName.ANTS, CardName.CARTEL]);
+
+    await dragCard(sortable, 0, 1, 'right');
+
+    expect(cardsInOrder(sortable)).to.deep.eq([CardName.CARTEL, CardName.ANTS]);
+    expect(getStorageEntries('player1')).to.deep.eq({
       [CardName.ANTS]: 2,
       [CardName.CARTEL]: 1,
     });
   });
+
   it('puts new cards at end of order and removes old', async () => {
-    localStorage.setItem('cardOrderfoo', JSON.stringify({
+    localStorage.setItem('cardOrderplayer1', JSON.stringify({
       [CardName.ANTS]: 2,
       [CardName.CARTEL]: 1,
       [CardName.DECOMPOSERS]: 3,
@@ -70,33 +91,17 @@ describe('SortableCards', () => {
     const sortable = mount(SortableCards, {
       ...globalConfig,
       props: {
-        cards: [{
-          name: CardName.ANTS,
-        }, {
-          name: CardName.CARTEL,
-        }, {
-          name: CardName.BIRDS,
-        }],
-        playerId: 'foo',
+        cards: [{name: CardName.ANTS}, {name: CardName.CARTEL}, {name: CardName.BIRDS}],
+        playerId: 'player1',
       },
     });
-    let cards = sortable.findAllComponents({
-      name: 'Card',
-    });
-    expect(cards).has.length(3);
-    expect(cards[0].props().card.name).to.eq(CardName.CARTEL);
-    expect(cards[1].props().card.name).to.eq(CardName.ANTS);
-    expect(cards[2].props().card.name).to.eq(CardName.BIRDS);
-    await dragCard(sortable, 0, 2, 'before');
-    cards = sortable.findAllComponents({
-      name: 'Card',
-    });
-    expect(cards[0].props().card.name).to.eq(CardName.ANTS);
-    expect(cards[1].props().card.name).to.eq(CardName.CARTEL);
-    expect(cards[2].props().card.name).to.eq(CardName.BIRDS);
-    const order = localStorage.getItem('cardOrderfoo');
-    expect(order).not.to.be.undefined;
-    expect(JSON.parse(order!)).to.deep.eq({
+
+    expect(cardsInOrder(sortable)).to.deep.eq([CardName.CARTEL, CardName.ANTS, CardName.BIRDS]);
+
+    await dragCard(sortable, 0, 2, 'left');
+
+    expect(cardsInOrder(sortable)).to.deep.eq([CardName.ANTS, CardName.CARTEL, CardName.BIRDS]);
+    expect(getStorageEntries('player1')).to.deep.eq({
       [CardName.ANTS]: 1,
       [CardName.CARTEL]: 2,
       [CardName.BIRDS]: 3,

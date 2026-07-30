0da55f50c1b6676ec4dacf59bfa75bb428cf6ad8
Robert Konigsberg <konigsberg@gmail.com>
2026-07-30 09:20:28 -0400

    Convert 6 cards' actions to behaviors.

--- src/server/cards/colonies/TitanAirScrapping.ts
+++ src/server/cards/colonies/TitanAirScrapping.ts
@@ -1,16 +1,12 @@
 import {IProjectCard} from '../IProjectCard';
 import {Tag} from '../../../common/cards/Tag';
 import {CardType} from '../../../common/cards/CardType';
-import {IPlayer} from '../../IPlayer';
 import {CardName} from '../../../common/cards/CardName';
 import {CardResource} from '../../../common/CardResource';
-import {SelectOption} from '../../inputs/SelectOption';
-import {OrOptions} from '../../inputs/OrOptions';
 import {CardRenderer} from '../render/CardRenderer';
-import {Card} from '../Card';
-import {Payment} from '../../../common/inputs/Payment';
+import {ActionCard} from '../ActionCard';
 
-export class TitanAirScrapping extends Card implements IProjectCard {
+export class TitanAirScrapping extends ActionCard implements IProjectCard {
   constructor() {
     super({
       cost: 21,
@@ -19,6 +15,24 @@ export class TitanAirScrapping extends Card implements IProjectCard {
       type: CardType.ACTIVE,
       resourceType: CardResource.FLOATER,
       victoryPoints: 2,
+
+      action: {
+        or: {
+          autoSelect: true,
+          behaviors: [
+            {
+              spend: {resourcesHere: 2},
+              tr: 1,
+              title: 'Remove 2 floaters here to increase your TR 1 step',
+            },
+            {
+              spend: {titanium: 1},
+              addResources: 2,
+              title: 'Spend 1 titanium to add 2 floaters here',
+            },
+          ],
+        },
+      },
 
       metadata: {
         cardNumber: 'C43',
@@ -30,50 +44,5 @@ export class TitanAirScrapping extends Card implements IProjectCard {
         }),
       },
     });
-  }
-
-
-  public canAct(player: IPlayer): boolean {
-    if (player.titanium > 0) {
-      return true;
-    }
-    if (this.resourceCount >= 2) {
-      return player.canAfford({cost: 0, tr: {tr: 1}});
-    }
-    return false;
-  }
-
-  public action(player: IPlayer) {
-    const opts = [];
-
-    const addResource = new SelectOption('Spend 1 titanium to add 2 floaters on this card', 'Spend titanium').andThen(() => this.addResource(player));
-    const spendResource = new SelectOption('Remove 2 floaters on this card to increase your TR 1 step', 'Remove floaters').andThen(() => this.spendResource(player));
-
-    if (this.resourceCount >= 2 && player.canAfford({cost: 0, tr: {tr: 1}})) {
-      opts.push(spendResource);
-    }
-
-
-    if (player.titanium > 0) {
-      opts.push(addResource);
-    }
-
-    if (opts.length === 1) {
-      return opts[0].cb(undefined);
-    }
-
-    return new OrOptions(...opts);
-  }
-
-  private addResource(player: IPlayer) {
-    player.pay(Payment.of({titanium: 1}));
-    player.addResourceTo(this, {qty: 2, log: true});
-    return undefined;
-  }
-
-  private spendResource(player: IPlayer) {
-    player.removeResourceFrom(this, 2);
-    player.increaseTerraformRating();
-    return undefined;
   }
 }

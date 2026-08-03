e8c4ed134c3bead000288127da5ef16a2f09e328
Robert Konigsberg <konigsberg@gmail.com>
2026-08-03 17:49:47 -0400

    Convert more global events to DSL

--- src/server/turmoil/globalEvents/GlobalDustStorm.ts
+++ src/server/turmoil/globalEvents/GlobalDustStorm.ts
@@ -2,12 +2,11 @@ import {IGlobalEvent} from './IGlobalEvent';
 import {GlobalEvent} from './GlobalEvent';
 import {GlobalEventName} from '../../../common/turmoil/globalEvents/GlobalEventName';
 import {PartyName} from '../../../common/turmoil/PartyName';
-import {IGame} from '../../IGame';
 import {Resource} from '../../../common/Resource';
 import {Tag} from '../../../common/cards/Tag';
-import {Turmoil} from '../Turmoil';
 import {CardRenderer} from '../../cards/render/CardRenderer';
 import {Size} from '../../../common/cards/render/Size';
+import {IPlayer} from '@/server/IPlayer';
 
 export class GlobalDustStorm extends GlobalEvent implements IGlobalEvent {
   constructor() {
@@ -16,24 +15,25 @@ export class GlobalDustStorm extends GlobalEvent implements IGlobalEvent {
       description: 'Lose all heat. Lose 2 M€ for each building tag (max 5, then reduced by influence).',
       revealedDelegate: PartyName.KELVINISTS,
       currentDelegate: PartyName.GREENS,
+      behavior: {
+        lose: {
+          stock: {
+            megacredits: {
+              tag: Tag.BUILDING,
+              turmoil: {max: 5, influence: {subtract: true}},
+              each: 2,
+            },
+          },
+        },
+      },
       renderData: CardRenderer.builder((b) => {
         b.text('Lose all').heat(1).nbsp.megacredits(-2).slash().tag(Tag.BUILDING).influence({size: Size.SMALL});
       }),
     });
   }
-  public override bespokeResolve(game: IGame): void {
-    const turmoil = Turmoil.getTurmoil(game);
-
-    game.playersInGenerationOrder.forEach((player) => {
-      if (player.heat > 0) {
-        player.stock.deduct(Resource.HEAT, player.heat, {log: true, from: {globalEvent: this}});
-      }
-      const maxedSteelTags = Math.min(5, player.tags.count(Tag.BUILDING, 'raw'));
-      player.stock.deduct(
-        Resource.MEGACREDITS,
-        2 * Math.max(0, maxedSteelTags - turmoil.getInfluence(player)),
-        {log: true, from: {globalEvent: this}},
-      );
-    });
+  public override bespokeResolvePlayer(player: IPlayer) {
+    if (player.heat > 0) {
+      player.stock.deduct(Resource.HEAT, player.heat, {log: true, from: {globalEvent: this}});
+    }
   }
 }
--- src/server/turmoil/globalEvents/Sabotage.ts
+++ src/server/turmoil/globalEvents/Sabotage.ts
@@ -2,10 +2,8 @@ import {IGlobalEvent} from './IGlobalEvent';
 import {GlobalEvent} from './GlobalEvent';
 import {GlobalEventName} from '../../../common/turmoil/globalEvents/GlobalEventName';
 import {PartyName} from '../../../common/turmoil/PartyName';
-import {Resource} from '../../../common/Resource';
 import {CardRenderer} from '../../cards/render/CardRenderer';
 import {Size} from '../../../common/cards/render/Size';
-import {IPlayer} from '@/server/IPlayer';
 
 export class Sabotage extends GlobalEvent implements IGlobalEvent {
   constructor() {
@@ -15,21 +13,17 @@ export class Sabotage extends GlobalEvent implements IGlobalEvent {
       revealedDelegate: PartyName.UNITY,
       currentDelegate: PartyName.REDS,
       behavior: {
-        stock: {steel: {turmoil: {influence: {}}}},
+        lose: {
+          production: {energy: 1, steel: 1},
+        },
+        stock: {
+          steel: {turmoil: {influence: {}}},
+        },
       },
       renderData: CardRenderer.builder((b) => {
         b.production((pb) => pb.minus().energy(1).steel(1)).nbsp.nbsp;
         b.steel(1).slash().influence({size: Size.MEDIUM});
       }),
     });
-  }
-  public override bespokeResolvePlayer(player: IPlayer) {
-    // This conditional isn't to prevent negative production, but to prevent misleading logging when the production diff is zero.
-    if (player.production.energy >= 1) {
-      player.production.add(Resource.ENERGY, -1, {log: true, from: {globalEvent: this}});
-    }
-    if (player.production.steel >= 1) {
-      player.production.add(Resource.STEEL, -1, {log: true, from: {globalEvent: this}});
-    }
   }
 }

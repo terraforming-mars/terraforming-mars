20c50bceb8c632ac2b1cf3822904aa9841a51d2e
Robert Konigsberg <konigsberg@gmail.com>
2026-08-07 17:45:55 -0400

    Add sanity test for Tempest Consultancy initial action. Remove dead code.

--- src/server/cards/moon/TempestConsultancy.ts
+++ src/server/cards/moon/TempestConsultancy.ts
@@ -37,13 +37,6 @@ export class TempestConsultancy extends CorporationCard implements ICorporationC
     });
   }
 
-  public override initialAction(player: IPlayer) {
-    const title = 'Tempest Consultancy first action - Select where to send two delegates';
-    player.game.defer(new SendDelegateToArea(player, title, {count: 2}));
-
-    return undefined;
-  }
-
   public canAct(player: IPlayer) {
     return player.tags.count(Tag.MOON) >= 5 && Turmoil.getTurmoil(player.game).getAvailableDelegateCount(player) > 0;
   }

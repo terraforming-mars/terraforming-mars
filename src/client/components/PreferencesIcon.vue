8026ca50441827797c0625d92bf5aa0f5b8d80f5
Christian Quintavalle <quintavalle.christian@gmail.com>
2026-08-08 18:29:19 +0200

    Migrate Preferences to <script setup>

--- src/client/components/PreferencesIcon.vue
+++ src/client/components/PreferencesIcon.vue
@@ -1,44 +1,25 @@
-<template>
-  <div class="sidebar_item sidebar_item--settings">
-    <i class="sidebar_icon sidebar_icon--settings" :title="$t('Player Settings')" :class="{'sidebar_item--is-active': preferencesPanelOpen}" @click="preferencesPanelOpen = !preferencesPanelOpen"></i>
-    <PreferencesDialog v-show="preferencesPanelOpen" @okButtonClicked="preferencesPanelOpen = false" :preferencesManager="preferencesManager"/>
-  </div>
-</template>
+<script setup lang="ts">
 
-<script lang="ts">
-
-import {defineComponent} from 'vue';
+import {onBeforeUnmount, onMounted, ref} from 'vue';
 import {PreferencesManager} from '@/client/utils/PreferencesManager';
 import PreferencesDialog from '@/client/components/PreferencesDialog.vue';
 
-export default defineComponent({
-  name: 'PreferencesIcon',
-  components: {
-    PreferencesDialog,
-  },
-  data() {
-    return {
-      preferencesPanelOpen: false,
-    };
-  },
-  mounted() {
-    document.addEventListener('click', this.closeOnOutsideClick);
-  },
-  beforeUnmount() {
-    document.removeEventListener('click', this.closeOnOutsideClick);
-  },
-  methods: {
-    closeOnOutsideClick(event: MouseEvent) {
-      if (this.preferencesPanelOpen && event.target instanceof Node && !this.$el.contains(event.target)) {
-        this.preferencesPanelOpen = false;
-      }
-    },
-  },
-  computed: {
-    preferencesManager(): PreferencesManager {
-      return PreferencesManager.INSTANCE;
-    },
-  },
-});
+const root = ref<HTMLElement>();
+const preferencesPanelOpen = ref(false);
+
+function closeOnOutsideClick(event: MouseEvent) {
+  if (preferencesPanelOpen.value && event.target instanceof Node && root.value?.contains(event.target) === false) {
+    preferencesPanelOpen.value = false;
+  }
+}
+
+onMounted(() => document.addEventListener('click', closeOnOutsideClick));
+onBeforeUnmount(() => document.removeEventListener('click', closeOnOutsideClick));
 
 </script>
+<template>
+  <div ref="root" class="sidebar_item sidebar_item--settings">
+    <i class="sidebar_icon sidebar_icon--settings" :title="$t('Player Settings')" :class="{'sidebar_item--is-active': preferencesPanelOpen}" @click="preferencesPanelOpen = !preferencesPanelOpen"></i>
+    <PreferencesDialog v-show="preferencesPanelOpen" @okButtonClicked="preferencesPanelOpen = false" :preferencesManager="PreferencesManager.INSTANCE"/>
+  </div>
+</template>

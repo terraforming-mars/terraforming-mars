8b8c40a02fc91b5c14c4c0c077ccee3ad6bfc23e
Robert Konigsberg <kberg@users.noreply.github.com>
2026-08-12 10:44:49 -0400

    Fix type assertion for currentTarget in selectedChange

--- src/client/components/SelectCard.vue
+++ src/client/components/SelectCard.vue
@@ -195,7 +195,7 @@ export default defineComponent({
       return this.playerView.thisPlayer.selfReplicatingRobotsCards?.find((r) => r.name === card.name);
     },
     selectedChange(e: Event) {
-      e.currentTarget?.querySelector('input')?.click();
+      (e.currentTarget as HTMLElement)?.querySelector('input')?.click();
     },
     toggleSelectAll() {
       if (this.allSelected) {

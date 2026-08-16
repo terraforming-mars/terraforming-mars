29733134cc7cb68536fd9623d1c7633de29efc3e
Robert Konigsberg <konigsberg@gmail.com>
2026-08-16 10:00:50 -0400

    Rolling back #7691
    > this broke mobile UX in a bad way...
    > Was doing "missclicks" for days until it finally hit me today:
    > - use mobile
    > - touch over a selectable card
    > - swipe up or down (to trigger scroll event)
    > - lift finger
    > Observed: the card is selected
    > Expected: click event not to be registered and the card not to be selected.

--- tests/client/components/SelectCard.spec.ts
+++ tests/client/components/SelectCard.spec.ts
@@ -1,10 +1,8 @@
-import {mount, shallowMount} from '@vue/test-utils';
+import {shallowMount} from '@vue/test-utils';
 import {expect} from 'chai';
 import {globalConfig} from './getLocalVue';
 import SelectCard from '@/client/components/SelectCard.vue';
 import {fakePlayerViewModel} from './testHelpers';
-import {CardName} from '@/common/cards/CardName';
-import {CardModel} from '@/common/models/CardModel';
 
 describe('SelectCard', () => {
   it('mounts without errors', () => {
@@ -30,42 +28,5 @@ describe('SelectCard', () => {
       },
     });
     expect(wrapper.exists()).to.be.true;
-  });
-
-  it('mouseup and touchend on a card toggle its selection, mirroring a label click', async () => {
-    const wrapper = mount(SelectCard, {
-      ...globalConfig,
-      props: {
-        playerView: fakePlayerViewModel(),
-        playerinput: {
-          title: 'Select a card',
-          buttonLabel: 'Save',
-          type: 'card',
-          cards: [{name: CardName.ANTS}, {name: CardName.BIRDS}] as Array<CardModel>,
-          max: 2,
-          min: 0,
-          showOnlyInLearnerMode: false,
-          selectBlueCardAction: false,
-          showOwner: false,
-          showSelectAll: false,
-        },
-        onsave: () => {},
-        showsave: true,
-        showtitle: true,
-      },
-    });
-
-    const cardBoxes = wrapper.findAll('.cardbox');
-    expect(cardBoxes).to.have.length(2);
-    const checkboxes = cardBoxes.map((box) => box.find('input[type=checkbox]').element as HTMLInputElement);
-
-    await cardBoxes[0].trigger('mouseup');
-    expect(checkboxes[0].checked).to.be.true;
-
-    await cardBoxes[0].trigger('mouseup');
-    expect(checkboxes[0].checked).to.be.false;
-
-    await cardBoxes[1].trigger('touchend');
-    expect(checkboxes[1].checked).to.be.true;
   });
 });

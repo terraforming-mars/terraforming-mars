41ee29ef99d08866133ceaf15b97db4dbd71f33b
Christian Quintavalle <quintavalle.christian@gmail.com>
2026-08-23 10:04:06 +0200

    Refine startScreen comment

--- src/client/components/StartScreen.vue
+++ src/client/components/StartScreen.vue
@@ -44,10 +44,10 @@ import * as constants from '@/common/constants';
 
 const previousViewport = ref('');
 
+// Set the viewport width to width=device-width on the start screen so mobile browsers use their actual CSS viewport width.
 // The current global viewport is width=1260, which prevents the home page from using the device width on phones.
-// On the start screen, this code changes it to width=device-width, so mobile browsers use their actual CSS viewport width.
 // This is a temporary solution in order to make this edit scoped to the start screen.
-// Once responsiveness covers the whole project, this code should be removed and the <meta name="viewport" content='width=1260, user-scalable=1' /> tag in index.html should be updated directly.
+// TODO: Once responsiveness covers the whole project, this code should be removed and the tag in index.html should be updated directly.
 onMounted(() => {
   const viewport = document.querySelector('meta[name="viewport"]');
   if (viewport !== null) {

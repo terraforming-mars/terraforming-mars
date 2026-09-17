162ad682918b703825cb9c92a2fad36c82c511f9
Robert Konigsberg <konigsberg@gmail.com>
2026-09-17 10:07:06 -0400

    Make Polaris compatible with Neptunian Power Consultants
    This allows a player to gain money from Polaris before Neptunian Power Consultants asks to spend the money.

--- tests/cards/pathfinders/Polaris.spec.ts
+++ tests/cards/pathfinders/Polaris.spec.ts
@@ -7,6 +7,9 @@ import {testGame} from '../../TestGame';
 import {SelectSpace} from '../../../src/server/inputs/SelectSpace';
 import {TileType} from '../../../src/common/TileType';
 import {cast} from '../../../src/common/utils/utils';
+import {NeptunianPowerConsultants} from '../../../src/server/cards/promo/NeptunianPowerConsultants';
+import {OrOptions} from '../../../src/server/inputs/OrOptions';
+import {Payment} from '../../../src/common/inputs/Payment';
 
 describe('Polaris', () => {
   let card: Polaris;
@@ -57,6 +60,25 @@ describe('Polaris', () => {
     expect(player.megaCredits).to.eq(4);
     expect(player2.production.megacredits).to.eq(0);
     expect(player2.megaCredits).to.eq(0);
+  });
+
+  it('Gains M€ before ocean-triggered effects ask you to spend', () => {
+    const neptunianPowerConsultants = new NeptunianPowerConsultants();
+    player.playedCards.push(neptunianPowerConsultants);
+    player.megaCredits = 1;
+
+    addOcean(player, '06');
+    runAllActions(game);
+
+    // The 4M€ from Polaris brings the player to 5M€, enough to use the other effect.
+    expect(player.megaCredits).to.eq(5);
+
+    const orOptions = cast(player.popWaitingFor(), OrOptions);
+    orOptions.options[0].cb(Payment.of({megacredits: 5}));
+
+    expect(neptunianPowerConsultants.resourceCount).to.eq(1);
+    expect(player.production.energy).to.eq(1);
+    expect(player.megaCredits).to.eq(0);
   });
 });
 

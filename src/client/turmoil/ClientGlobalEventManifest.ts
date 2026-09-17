e6331eca4a7f63d31e08617d3aafd2692495c345
Robert Konigsberg <konigsberg@gmail.com>
2026-09-17 18:14:18 -0400

    Rename IClientGlobalEvent to ClientGlobalEvent

--- src/client/turmoil/ClientGlobalEventManifest.ts
+++ src/client/turmoil/ClientGlobalEventManifest.ts
@@ -1,10 +1,10 @@
 import {GlobalEventName} from '@/common/turmoil/globalEvents/GlobalEventName';
-import {IClientGlobalEvent} from '@/common/turmoil/IClientGlobalEvent';
+import {ClientGlobalEvent} from '@/common/turmoil/ClientGlobalEvent';
 // @ts-ignore events.json doesn't exist during npm run build
 import eventJson from '@/genfiles/events.json';
 
-const events: Map<GlobalEventName, IClientGlobalEvent> = new Map();
-(eventJson as any as Array<IClientGlobalEvent>).forEach((card) => {
+const events: Map<GlobalEventName, ClientGlobalEvent> = new Map();
+(eventJson as any as Array<ClientGlobalEvent>).forEach((card) => {
   events.set(card.name, card);
 });
 
@@ -12,11 +12,11 @@ export function allGlobalEventNames() {
   return events.keys();
 }
 
-export function getGlobalEvent(globalEventName: GlobalEventName): IClientGlobalEvent | undefined {
+export function getGlobalEvent(globalEventName: GlobalEventName): ClientGlobalEvent | undefined {
   return events.get(globalEventName);
 }
 
-export function getGlobalEventOrThrow(globalEventName: GlobalEventName): IClientGlobalEvent {
+export function getGlobalEventOrThrow(globalEventName: GlobalEventName): ClientGlobalEvent {
   const globalEvent = getGlobalEvent(globalEventName);
   if (globalEvent === undefined) {
     throw new Error(`global event ${globalEventName} not found`);

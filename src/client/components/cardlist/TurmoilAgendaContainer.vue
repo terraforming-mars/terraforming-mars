1b2631972944ac487ac65a9ca4f160d9a113b23a
Robert Konigsberg <konigsberg@gmail.com>
2026-09-19 14:35:31 -0400

    Clean up the Turmoil Agenda Container used for card help.

--- src/client/components/cardlist/TurmoilAgendaContainer.vue
+++ src/client/components/cardlist/TurmoilAgendaContainer.vue
@@ -1,20 +1,18 @@
 <template>
-  <div class="container" @click="showDescription = !showDescription">
-  <TurmoilAgenda :id="agendaId" />
-  <div class="line1">{{ agendaId }}</div>
-  <div class="line2">
-    {{ $t(agenda.name) }} {{ $t(agenda.type) }} {{ agenda.num }}
-  </div>
-  <div class="description" v-if="showDescription" v-i18n>{{ description }}</div>
+  <div class="container">
+  <div class="agenda-title"><CardParty :party="party" /><span>{{ agendaType }} {{ agenda.num }} <span class="small">{{ $t(agenda.name) }} {{ $t(agenda.type) }}</span></span></div>
+  <TurmoilAgenda class="agenda" :id="agendaId" />
+  <div class="description" v-i18n>{{ description }}</div>
   </div>
 </template>
 
 <script setup lang="ts">
-import {computed, ref} from 'vue';
+import {computed} from 'vue';
 import {agendaInfoById, AgendaInfo, BonusId, PolicyId} from '@/common/turmoil/Types';
 import {getAgendaOrThrow} from '@/client/turmoil/ClientAgendaManifest';
 import TurmoilAgenda from '@/client/components/turmoil/TurmoilAgenda.vue';
-
+import CardParty from '@/client/components/card/CardParty.vue';
+import {PartyName} from '@/common/turmoil/PartyName';
 
 const props = defineProps({
   agendaId: {
@@ -24,19 +22,23 @@ const props = defineProps({
 });
 
 const agenda = computed<AgendaInfo>(() => agendaInfoById(props.agendaId));
-const showDescription = ref(false);
 const description = computed<string>(() => getAgendaOrThrow(props.agendaId).description);
+const party = computed<PartyName>(() => agenda.value.name);
+const agendaType = computed<string>(() => agenda.value.type === 'Bonus' ? '⚡' : '⚖️');
 </script>
 
 <style scoped lang="less">
 .container {
   padding: 12px;
-  background-image: linear-gradient(#9c602d, #25170a);
+  background-image: linear-gradient(#5c5c5c, #2a2a2a);
   border-radius: 8px;
   min-height: 120px;
   width: 250px;
   margin: 0 5px;
   cursor: pointer;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
 
   :deep(.policy-top-margin) {
     margin-top: 0 !important;
@@ -47,21 +49,30 @@ const description = computed<string>(() => getAgendaOrThrow(props.agendaId).desc
     margin-left: 80px !important;
   }
 
-  > div:first-child {
+  .agenda {
+    margin-bottom: auto;
     text-align: center;
     height: 50px;
   }
 
-  .line1, .line2 {
-    text-align: center;
+  .agenda-title {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 6px;
     font-size: 18px;
+    color: white;
+  }
+
+  .small {
+    font-size: 14px;
   }
 
   .description {
-    margin-top: 6px;
     text-align: center;
     font-size: 12px;
     line-height: 1.3;
+    color: white;
   }
 }
 </style>
